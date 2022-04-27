bfb021f018dda3a9884f30cad7d0a786384486cd
Oleg Isonen <oleg008@gmail.com>
2022-04-28 00:37:23 +0100

    render icons

--- app/designer/features/breakpoints/trigger-button.tsx
+++ app/designer/features/breakpoints/trigger-button.tsx
@@ -1,12 +1,37 @@
 import { forwardRef, type ComponentProps, type ElementRef } from "react";
+import { type Breakpoint } from "@webstudio-is/sdk";
 import {
   useCanvasWidth,
   useScale,
   useSelectedBreakpoint,
 } from "~/designer/shared/nano-values";
 import { Button, Text } from "~/shared/design-system";
+import {
+  DesktopIcon,
+  LaptopIcon,
+  MobileIcon,
+  TabletIcon,
+  DevicesIcon,
+} from "~/shared/icons";
 
 type TriggerButtonProps = ComponentProps<typeof Button>;
+
+const renderIcon = (breakpoint: Breakpoint, variant: "contrast" | "gray") => {
+  const color = variant === "contrast" ? "white" : "gray";
+  if (breakpoint.minWidth >= 1280) {
+    return <DesktopIcon color={color} />;
+  }
+  if (breakpoint.minWidth >= 1024) {
+    return <LaptopIcon color={color} />;
+  }
+  if (breakpoint.minWidth >= 768) {
+    return <TabletIcon color={color} />;
+  }
+  if (breakpoint.minWidth >= 360) {
+    return <MobileIcon color={color} />;
+  }
+  return <DevicesIcon color={color} />;
+};
 
 export const TriggerButton = forwardRef<
   ElementRef<typeof Button>,
@@ -16,6 +41,7 @@ export const TriggerButton = forwardRef<
   const [breakpoint] = useSelectedBreakpoint();
   const [canvasWidth = 0] = useCanvasWidth();
   if (breakpoint === undefined) return null;
+  const variant = canvasWidth >= breakpoint.minWidth ? "contrast" : "gray";
   return (
     <Button
       {...props}
@@ -24,10 +50,8 @@ export const TriggerButton = forwardRef<
       ghost
       aria-label="Show breakpoints"
     >
-      <Text
-        size="1"
-        variant={canvasWidth > breakpoint.minWidth ? "contrast" : "gray"}
-      >
+      {renderIcon(breakpoint, variant)}
+      <Text size="1" variant={variant}>
         {`${breakpoint.label} ${canvasWidth}px / ${scale}%`}
       </Text>
     </Button>
---
+++ app/shared/icons/tablet.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import { IconProps } from "./types";
+
+export const TabletIcon = React.forwardRef<SVGSVGElement, IconProps>(
+  ({ color = "currentColor", ...props }, forwardedRef) => {
+    return (
+      <svg
+        width="15"
+        height="15"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        {...props}
+        ref={forwardedRef}
+      >
+        <path
+          fill={color}
+          d="M18,0H6C4.34,0,3,1.34,3,3v18c0,1.66,1.34,3,3,3h12c1.66,0,3-1.34,3-3V3C21,1.34,19.66,0,18,0z M14,22h-4v-1h4V22z M19.25,19H4.75V3h14.5V19z"
+        />
+      </svg>
+    );
+  }
+);
+
+TabletIcon.displayName = "TabletIcon";
