d1d373edee95f74e240f947e0a6835c4ae3239a9
Sara Vieira <hey@iamsaravieira.com>
2022-05-26 01:04:02 +0300

    revert change to root

--- remix.config.js
+++ remix.config.js
@@ -3,8 +3,5 @@
  */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
+  cacheDirectory: "./node_modules/.cache/remix",
 };
