f313c70d877cce65a5e9b0a0ee07ef489aa07ac7
Oleg Isonen <oleg008@gmail.com>
2022-06-06 01:05:32 +0200

    rationale for  useWindowResize

--- app/shared/dom-hooks/use-window-resize.ts
+++ app/shared/dom-hooks/use-window-resize.ts
@@ -13,6 +13,11 @@ if (typeof window === "object") {
   );
 }
 
+/**
+ * Subscribe to DOM resize event only once and then notify all listeners over js only.
+ * This allows us to subscribe to resize by many listeners without perf issues, since its going to be the same resize event.
+ * TODO: We can add throttling and RAF if needed.
+ */
 export const useWindowResize = (onResize: () => void) => {
   useEffect(() => {
     emitter.on("resize", onResize);
