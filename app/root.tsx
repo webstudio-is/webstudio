4052b2cea6281272dd9746ff2e9955892744a6d4
Oleg Isonen <oleg008@gmail.com>
2022-06-20 03:31:58 +0200

    explain why root route exports default outlet

--- app/root.tsx
+++ app/root.tsx
@@ -1,1 +1,2 @@
+// Our root outlet doesn't contain a layout because we have 2 types of documents: canvas and designer and we need to decide down the line which one to render, thre is no single root document.
 export { Outlet as default } from "@remix-run/react";
