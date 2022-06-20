e3b2d1cec004a5d4046618577998e53af1bc610d
Oleg Isonen <oleg008@gmail.com>
2022-06-20 03:28:22 +0200

    cleanup and reuse layout
    we have 2 types of documents: canvas and designer
    
    canvas is used only for the /canvas and the /preview routes, the rest is designer until we have some special case where we need a different document for a page

--- app/routes/canvas.tsx
+++ app/routes/canvas.tsx
@@ -1,45 +1,8 @@
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet as DefaultOutlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
 import { type MetaFunction } from "@remix-run/node";
-import { CriticalCss } from "@webstudio-is/sdk";
+import { Canvas } from "~/shared/documents/canvas";
 
 export const meta: MetaFunction = () => {
   return { title: "Webstudio canvas" };
 };
 
-/**
- * We are using Outlet prop from index layout when user renders site from a subdomain.
- */
-const Document = ({
-  Outlet = DefaultOutlet,
-}: {
-  Outlet: typeof DefaultOutlet;
-}) => {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <Meta />
-        <Links />
-        <CriticalCss />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        {process.env.NODE_ENV === "development" && <LiveReload />}
-      </body>
-    </html>
-  );
-};
-
-export default Document;
+export default Canvas;
--- app/routes/login/index.tsx
+++ app/routes/login/index.tsx
@@ -1,7 +1,4 @@
-import { Links, LiveReload, Meta, Scripts } from "@remix-run/react";
-import { LoaderFunction, MetaFunction, redirect } from "@remix-run/node";
-import { CriticalCss } from "@webstudio-is/sdk";
-import { darkTheme } from "~/shared/design-system";
+import { LoaderFunction, redirect } from "@remix-run/node";
 
 import { authenticator } from "~/services/auth.server";
 import config from "~/config";
@@ -10,38 +7,17 @@ import { Login, links } from "~/auth/";
 
 export { links };
 
-export const meta: MetaFunction = () => {
-  return { title: "Webstudio Login" };
-};
-
-const LoginRoute = () => {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
-        <CriticalCss />
-      </head>
-      <body className={darkTheme}>
-        <Login />
-        <Scripts />
-        {process.env.NODE_ENV === "development" && <LiveReload />}
-      </body>
-    </html>
-  );
-};
-
 export const loader: LoaderFunction = async ({ request }) => {
   const user = await authenticator.isAuthenticated(request);
   if (user) {
     return redirect(config.dashboardPath);
   }
 
-  console.log(authenticator.sessionErrorKey);
-
   return {};
+};
+
+const LoginRoute = () => {
+  return <Login />;
 };
 
 export default LoginRoute;
