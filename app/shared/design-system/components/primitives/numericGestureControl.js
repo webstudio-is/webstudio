694a27197ba923fc460079d7356cb8e6ae4fc287
Sultan <105234327+natlusyht@users.noreply.github.com>
2022-06-25 17:59:53 +0300

    prettier

--- app/shared/design-system/components/primitives/numericGestureControl.js
+++ app/shared/design-system/components/primitives/numericGestureControl.js
@@ -8,36 +8,55 @@
 			}
 		})
 */
-export const numericGestureControl = (targetNode, {
-	initialValue = 0,
-	direction = 'horizontal',
-	onValueChange = ({}) => null,
-}) => {
-	const eventNames = [ 'pointerup', 'pointerdown', 'pointermove' ];
-	const state = { type: '', value: initialValue, cursor: direction === 'horizontal' ? 'ew-resize' : 'ns-resize' };
-	const handleEvent = ({ type, target, movementY, movementX, pointerId, pressure }) => {
-		const movement = direction === 'horizontal' ? movementX : movementY;
-		switch (type) {
-			case 'pointerup': {
-				targetNode.releasePointerCapture(pointerId);
-				break;
-			}
-			case 'pointerdown': {
-				targetNode.setPointerCapture(pointerId);
-				break;
-			}
-			case 'pointermove': {
-				if (pressure) onValueChange({target, value: state.value += movement, preventDefault: () => event.preventDefault()});
-				break;
-			}
-		}
-	};
-	targetNode.style.setProperty('cursor', state.cursor);
-	eventNames.forEach((eventName) => targetNode.addEventListener(eventName, handleEvent));
-	return {
-		disconnectedCallback: () => {
-			targetNode.style.removeProperty('cursor');
-			eventNames.forEach((eventName) => targetNode.removeEventListener(eventName, handleEvent))
-		},
-	};
+export const numericGestureControl = (
+  targetNode,
+  { initialValue = 0, direction = "horizontal", onValueChange = ({}) => null }
+) => {
+  const eventNames = ["pointerup", "pointerdown", "pointermove"];
+  const state = {
+    type: "",
+    value: initialValue,
+    cursor: direction === "horizontal" ? "ew-resize" : "ns-resize",
+  };
+  const handleEvent = ({
+    type,
+    target,
+    movementY,
+    movementX,
+    pointerId,
+    pressure,
+  }) => {
+    const movement = direction === "horizontal" ? movementX : movementY;
+    switch (type) {
+      case "pointerup": {
+        targetNode.releasePointerCapture(pointerId);
+        break;
+      }
+      case "pointerdown": {
+        targetNode.setPointerCapture(pointerId);
+        break;
+      }
+      case "pointermove": {
+        if (pressure)
+          onValueChange({
+            target,
+            value: (state.value += movement),
+            preventDefault: () => event.preventDefault(),
+          });
+        break;
+      }
+    }
+  };
+  targetNode.style.setProperty("cursor", state.cursor);
+  eventNames.forEach((eventName) =>
+    targetNode.addEventListener(eventName, handleEvent)
+  );
+  return {
+    disconnectedCallback: () => {
+      targetNode.style.removeProperty("cursor");
+      eventNames.forEach((eventName) =>
+        targetNode.removeEventListener(eventName, handleEvent)
+      );
+    },
+  };
 };
