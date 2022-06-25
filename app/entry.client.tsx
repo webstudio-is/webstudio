4c1209bf1fdb1d0385c47aa313e46f08af2d3571
Sara Vieira <hey@iamsaravieira.com>
2022-06-25 23:43:03 +0200

    add sentry

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,4 +1,12 @@
 import { hydrate } from "react-dom";
 import { RemixBrowser } from "@remix-run/react";
+import * as Sentry from "@sentry/browser";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://e4b439185ee94fca9599b768a75d211e@o1298958.ingest.sentry.io/6530661",
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 hydrate(<RemixBrowser />, document);
