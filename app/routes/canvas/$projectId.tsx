37b4349f97ad9b2f62e132a5d5c46917a60311d9
Sara Vieira <hey@iamsaravieira.com>
2022-06-28 18:41:35 +0200

    add env everywhere

--- app/routes/canvas/$projectId.tsx
+++ app/routes/canvas/$projectId.tsx
@@ -2,12 +2,13 @@ import { useLoaderData } from "@remix-run/react";
 import type { LoaderFunction } from "@remix-run/node";
 import { Canvas } from "~/canvas";
 import { loadCanvasData, type ErrorData, type CanvasData } from "~/shared/db";
+import env from "~/env.server";
 
 export const loader: LoaderFunction = async ({
   params,
 }): Promise<CanvasData | ErrorData> => {
   if (params.projectId === undefined) {
-    return { errors: "Missing projectId" };
+    return { errors: "Missing projectId", env };
   }
   try {
     return await loadCanvasData({ projectId: params.projectId });
@@ -15,10 +16,11 @@ export const loader: LoaderFunction = async ({
     if (error instanceof Error) {
       return {
         errors: error.message,
+        env,
       };
     }
   }
-  return { errors: "Unexpected error" };
+  return { errors: "Unexpected error", env };
 };
 
 const CanvasRoute = () => {
--- app/routes/designer.tsx
+++ app/routes/designer.tsx
@@ -1,5 +1,10 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LoaderFunction, MetaFunction } from "@remix-run/node";
 import { Designer } from "~/shared/documents/designer";
+import env from "~/env.server";
+
+export const loader: LoaderFunction = () => ({
+  env,
+});
 
 export const meta: MetaFunction = () => {
   return { title: "Webstudio" };
