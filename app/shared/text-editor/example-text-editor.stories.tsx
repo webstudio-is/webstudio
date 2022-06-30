a6ee6db21e09507022c5b9da2b7e31b8abd5a096
Oleg Isonen <oleg008@gmail.com>
2022-06-30 22:49:06 +0200

    fix more ts errors and linter

--- app/shared/text-editor/example-text-editor.stories.tsx
+++ app/shared/text-editor/example-text-editor.stories.tsx
@@ -1,14 +1,6 @@
-import React from "react";
 import type { ComponentStory, ComponentMeta } from "@storybook/react";
 import { ExampleTextEditor } from "./example-text-editor";
 import "./styles.css";
-import { $getRoot, $getSelection, type EditorState } from "./lexical";
-
-// When the editor changes, you can get notified via the
-// LexicalOnChangePlugin!
-const onChange = (editorState: EditorState) => {
-  editorState.read(() => {});
-};
 
 export default {
   title: "Text Editor",
@@ -20,6 +12,4 @@ const Template: ComponentStory<typeof ExampleTextEditor> = (args) => {
 };
 
 export const TextEditor = Template.bind({});
-TextEditor.args = {
-  onChange,
-};
+TextEditor.args = {};
