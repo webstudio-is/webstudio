77ef3c787ba72b28e2bd1031cb9a2481c0bd7b51
Sara Vieira <hey@iamsaravieira.com>
2022-07-01 23:08:50 +0200

    chore(env) - Add env check before starting app

--- remix.config.js
+++ remix.config.js
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const { env } = require("./app/shared/env/env-check");
+
 /**
  * @type {import('@remix-run/dev').AppConfig}
  */
