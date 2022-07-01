9af7dba962e12b3f222cc1ed38c465f82788c9b3
Sara Vieira <hey@iamsaravieira.com>
2022-07-01 20:19:26 +0200

    chore(sentry) - Use new @remix/sentry

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -2,6 +2,20 @@ import { hydrate } from "react-dom";
 import { RemixBrowser } from "@remix-run/react";
 import { initSentry } from "./shared/sentry";
 
-initSentry();
+import { useLocation, useMatches } from "@remix-run/react";
+import * as Sentry from "@sentry/remix";
+import { useEffect } from "react";
+
+initSentry({
+  integrations: [
+    new Sentry.BrowserTracing({
+      routingInstrumentation: Sentry.remixRouterInstrumentation(
+        useEffect,
+        useLocation,
+        useMatches
+      ),
+    }),
+  ],
+});
 
 hydrate(<RemixBrowser />, document);
