104716441ac30ccbaf0cfab11fd1f717bcdbe56b
Oleg Isonen <oleg008@gmail.com>
2022-07-08 11:33:58 +0200

    decouple feature flags from debug

--- apps/designer/app/env.server.ts
+++ apps/designer/app/env.server.ts
@@ -4,6 +4,7 @@ const env = {
   SENTRY_DSN: process.env.SENTRY_DSN,
   VERCEL_ENV: process.env.VERCEL_ENV,
   DEBUG: process.env.DEBUG,
+  FEATURE: process.env.FEATURE,
 } as const;
 
 export default env;
