3838fe90c5ca2a16f7d09552f6dd29dc53559626
Leon Salsiccia <leon.salsiccia@gmail.com>
2022-07-08 23:29:05 +0000

    fix vercel deployments

--- apps/designer/remix.config.js
+++ apps/designer/remix.config.js
@@ -4,9 +4,17 @@ require("./env-check");
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  appDirectory: "app",
-  assetsBuildDirectory: "public/build",
-  publicPath: "/build/",
-  serverBuildDirectory: "api/_build",
-  ignoredRouteFiles: [".*"],
+  serverBuildTarget: "vercel",
+  // When running locally in development mode, we use the built in remix
+  // server. This does not understand the vercel lambda module format,
+  // so we default back to the standard build output.
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
+  // This makes sure remix bundles monorepo packages which are always
+  // sym-linked and can't be hoisted. You could also manually name packages
+  // e.g. ["@webstudio-is/ui", ...]
+  serverDependenciesToBundle: [/@webstudio-is\/.*/],
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // serverBuildPath: "api/index.js",
+  // publicPath: "/build/",
 };
