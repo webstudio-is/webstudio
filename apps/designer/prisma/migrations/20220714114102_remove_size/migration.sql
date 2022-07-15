ec583174a387c13f6018e9d2329e1550cefca7f0
Sara Vieira <hey@iamsaravieira.com>
2022-07-15 20:20:00 +0200

    add schema

---
+++ apps/designer/prisma/migrations/20220714114102_remove_size/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `size` on the `Asset` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Asset" DROP COLUMN "size";
