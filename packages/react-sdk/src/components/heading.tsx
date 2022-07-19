c3c797a7c04b6e2083e61dbe2d6f1e6df08ac7ec
Oleg Isonen <oleg008@gmail.com>
2022-07-19 21:47:32 +0200

    enable choosing the tag for Heading

--- packages/react-sdk/src/components/heading.tsx
+++ packages/react-sdk/src/components/heading.tsx
@@ -2,9 +2,19 @@ import React, { forwardRef, type ElementRef, type ComponentProps } from "react";
 
 const defaultTag = "h1";
 
-export const Heading = forwardRef<
-  ElementRef<typeof defaultTag>,
-  ComponentProps<typeof defaultTag>
->((props, ref) => <h1 {...props} ref={ref} />);
+type Props = ComponentProps<typeof defaultTag> & {
+  tag: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+};
+
+export const Heading = forwardRef<ElementRef<typeof defaultTag>, Props>(
+  (props, ref) => {
+    const H = props.tag;
+    return <H {...props} ref={ref} />;
+  }
+);
+
+Heading.defaultProps = {
+  tag: "h1",
+};
 
 Heading.displayName = "Heading";
