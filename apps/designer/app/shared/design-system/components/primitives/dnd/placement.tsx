d328d6d15df421b12685d47c3b4e1c4bc49a5b42
Roman Pominov <rpominov+github@gmail.com>
2022-07-20 10:17:06 +0300

    remove any

--- apps/designer/app/shared/design-system/components/primitives/dnd/placement.tsx
+++ apps/designer/app/shared/design-system/components/primitives/dnd/placement.tsx
@@ -2,13 +2,18 @@ import { Box } from "../../box";
 
 export type Rect = Pick<DOMRect, "top" | "left" | "width" | "height">;
 
+export type Parameters = {
+  target: HTMLElement;
+  index?: number;
+};
+
 const probe = document.createElement("div");
 
 /**
  * We place a div at the position where future drop will happen.
  * We learn if the indicator will render vertically or horizontally as well as its width/heigh.
  */
-export const getPlacement = ({ target, index = 0 }: any = {}) => {
+export const getPlacement = ({ target, index = 0 }: Parameters) => {
   const { children } = target;
   if (index > children.length - 1) {
     target.appendChild(probe);
