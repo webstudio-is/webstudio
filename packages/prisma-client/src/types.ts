6a3748b92c5ece82a61074e38448b5a02646b371
Sara Vieira <hey@iamsaravieira.com>
2022-07-22 18:35:23 +0200

    fix ts

--- packages/prisma-client/src/types.ts
+++ packages/prisma-client/src/types.ts
@@ -1,6 +1,6 @@
-import { type Asset as DbAsset } from "@prisma/client";
+import type { Asset as DbAsset } from "@prisma/client";
 export { Location } from "@prisma/client";
 export type { InstanceProps, Project, User, Breakpoints } from "@prisma/client";
 
 export type Asset = DbAsset & { path: string };
-export { DbAsset };
+export type { DbAsset };
