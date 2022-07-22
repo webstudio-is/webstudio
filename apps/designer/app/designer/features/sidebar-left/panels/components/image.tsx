816ed74b105a5e2d8502ec9a434941e7b9cd95c1
Sara Vieira <hey@iamsaravieira.com>
2022-07-23 01:08:19 +0200

    remoive width and height css

--- apps/designer/app/designer/features/sidebar-left/panels/components/image.tsx
+++ apps/designer/app/designer/features/sidebar-left/panels/components/image.tsx
@@ -1,10 +1,10 @@
 import { Asset } from "@webstudio-is/prisma-client";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { Box } from "~/shared/design-system";
 import placholderImage from "~/shared/images/image-placeholder.svg";
 import brokenImage from "~/shared/images/broken-image-placeholder.svg";
 
-const useImageWithFallbaack = ({ path }: { path: string }) => {
+const useImageWithFallback = ({ path }: { path: string }) => {
   const [src, setSrc] = useState(placholderImage);
 
   useEffect(() => {
@@ -23,14 +23,12 @@ export const AssetManagerImage = ({
 }: {
   asset: Asset;
 }) => {
-  const src = useImageWithFallbaack({ path });
+  const src = useImageWithFallback({ path });
   return (
     <Box
       title={alt || ""}
       css={{
         aspectRatio: "1/1",
-        width: "100%",
-        height: "100%",
         backgroundImage: `url(${src})`,
         backgroundSize: "contain",
       }}
