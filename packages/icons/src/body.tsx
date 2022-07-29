bfeadbe27caee46bc71864942091769062c10a1b
Oleg Isonen <oleg008@gmail.com>
2022-07-29 02:29:49 +0200

    lint

--- packages/icons/src/body.tsx
+++ packages/icons/src/body.tsx
@@ -16,7 +16,7 @@ export const BodyIcon = React.forwardRef<SVGSVGElement, IconProps>(
         <path
           d="M1 3.96833H14M2.51667 14H12.4833C13.321 14 14 13.321 14 12.4833V2.51667C14 1.67903 13.321 1 12.4833 1H2.51667C1.67903 1 1 1.67903 1 2.51667V12.4833C1 13.321 1.67903 14 2.51667 14Z"
           stroke={color}
-          stroke-width="1.15"
+          strokeWidth="1.15"
         />
       </svg>
     );
