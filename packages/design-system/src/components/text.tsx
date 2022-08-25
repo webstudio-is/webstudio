9271035998b880b5bc9da5e6fa22e6df6e553199
Roman Pominov <rpominov+github@gmail.com>
2022-08-25 10:23:53 +0300

    more browser comp. solution for <Text truncate>

--- packages/design-system/src/components/text.tsx
+++ packages/design-system/src/components/text.tsx
@@ -57,15 +57,16 @@ export const Text = styled("div", {
       true: {
         whiteSpace: "nowrap",
         textOverflow: "ellipsis",
+        overflow: "hidden",
+
+        // To make sure text is not clipped vertically
+        pt: 10,
+        pb: 10,
+        mt: -10,
+        mb: -10,
+
         flexBasis: 0,
         flexGrow: 1,
-
-        // We want to use overflow-x:clip to make sure the text is not clipped vertically.
-        // overflow:hidden is a fallback for when `clip` is not supported,
-        // the text may be clipped vertically in these browsers.
-        overflow: "hidden",
-        overflowX: "clip",
-        overflowY: "visible",
 
         // For some reason flexBasis:0 is not enough
         // to stop it from growing past the container
