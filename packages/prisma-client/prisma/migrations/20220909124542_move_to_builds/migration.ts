254181eb9c65a5f202bc9bc29224d80a6a97d9e0
Roman Pominov <rpominov+github@gmail.com>
2022-09-09 13:18:40 +0300

    new migration

---
+++ packages/prisma-client/prisma/migrations/20220909124542_move_to_builds/migration.ts
@@ -0,0 +1,44 @@
+import { PrismaClient, Prisma } from "./client";
+import { v4 as uuidv4 } from "uuid";
+
+export default () => {
+  const client = new PrismaClient();
+  return client.$transaction(async (prisma) => {
+    const trees = await prisma.tree.findMany();
+    const projects = await prisma.project.findMany();
+
+    const builds: Prisma.BuildCreateManyInput[] = [];
+
+    for (const tree of trees) {
+      const project = projects.find(
+        (project) =>
+          project.devTreeId === tree.id ||
+          project.prodTreeId === tree.id ||
+          JSON.parse(project.prodTreeIdHistory).includes(tree.id)
+      );
+
+      if (project === undefined) {
+        continue;
+      }
+
+      builds.push({
+        pages: JSON.stringify({
+          homePage: {
+            id: uuidv4(),
+            name: "Home",
+            path: "",
+            title: "Home",
+            meta: {},
+            treeId: tree.id,
+          },
+          pages: [],
+        }),
+        isDev: project.devTreeId === tree.id,
+        isProd: project.prodTreeId === tree.id,
+        projectId: project.id,
+      });
+    }
+
+    await prisma.build.createMany({ data: builds });
+  });
+};
