f2907eb73cb2c8a145b8e52e1e0323775374fe4f
Roman Pominov <rpominov+github@gmail.com>
2022-09-09 15:18:23 +0300

    add zod validation to the migration

--- packages/prisma-client/prisma/migrations/20220909124542_move_to_builds/migration.ts
+++ packages/prisma-client/prisma/migrations/20220909124542_move_to_builds/migration.ts
@@ -1,5 +1,22 @@
 import { PrismaClient, Prisma } from "./client";
 import { v4 as uuid } from "uuid";
+import { z } from "zod";
+
+const TreeHistorySchema = z.array(z.string());
+
+const PageSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  path: z.string(),
+  title: z.string(),
+  meta: z.record(z.string(), z.string()),
+  treeId: z.string(),
+});
+
+const PagesSchema = z.object({
+  homePage: PageSchema,
+  pages: z.array(PageSchema),
+});
 
 export default () => {
   const client = new PrismaClient();
@@ -14,25 +31,27 @@ export default () => {
         (project) =>
           project.devTreeId === tree.id ||
           project.prodTreeId === tree.id ||
-          JSON.parse(project.prodTreeIdHistory).includes(tree.id)
+          TreeHistorySchema.parse(project.prodTreeIdHistory).includes(tree.id)
       );
 
       if (project === undefined) {
         continue;
       }
 
+      const pages = PagesSchema.parse({
+        homePage: {
+          id: uuid(),
+          name: "Home",
+          path: "",
+          title: "Home",
+          meta: {},
+          treeId: tree.id,
+        },
+        pages: [],
+      });
+
       builds.push({
-        pages: JSON.stringify({
-          homePage: {
-            id: uuid(),
-            name: "Home",
-            path: "",
-            title: "Home",
-            meta: {},
-            treeId: tree.id,
-          },
-          pages: [],
-        }),
+        pages: JSON.stringify(pages),
         isDev: project.devTreeId === tree.id,
         isProd: project.prodTreeId === tree.id,
         projectId: project.id,
