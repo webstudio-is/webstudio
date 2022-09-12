21fdd770fa42ea76978c6b7086c2c4917793056c
Oleg Isonen <oleg008@gmail.com>
2022-09-12 02:09:57 +0200

    try encode file name

--- packages/asset-uploader/src/targets/s3/upload.ts
+++ packages/asset-uploader/src/targets/s3/upload.ts
@@ -68,7 +68,7 @@ const uploadHandler = async ({
     throw new Error(`Asset cannot be bigger than ${maxSize}MB`);
   }
 
-  const uniqueFilename = getUniqueFilename(file.filename);
+  const uniqueFilename = encodeURIComponent(getUniqueFilename(file.filename));
 
   const s3Envs = S3EnvVariables.parse(process.env);
 
