99df39cbb18944c9e72be0f45db99d3c6877a2f4
Roman Pominov <rpominov+github@gmail.com>
2022-09-15 17:33:26 +0300

    change timeout

--- packages/prisma-client/prisma/migrations/20220912142938_assets-meta-data/migration.ts
+++ packages/prisma-client/prisma/migrations/20220912142938_assets-meta-data/migration.ts
@@ -12,22 +12,25 @@ import { PrismaClient } from "./client";
 export default () => {
   const client = new PrismaClient();
 
-  return client.$transaction(async (prisma) => {
-    const previousAssets = await prisma.asset.findMany();
-    const update = previousAssets.map((asset) => {
-      return {
-        id: asset.id,
-        description: asset.alt,
-        meta: JSON.stringify({
-          width: Number(asset.width),
-          height: Number(asset.height),
-        }),
-      };
-    });
-    await Promise.all(
-      update.map(({ id, ...data }) =>
-        prisma.asset.update({ where: { id }, data })
-      )
-    );
-  });
+  return client.$transaction(
+    async (prisma) => {
+      const previousAssets = await prisma.asset.findMany();
+      const update = previousAssets.map((asset) => {
+        return {
+          id: asset.id,
+          description: asset.alt,
+          meta: JSON.stringify({
+            width: Number(asset.width),
+            height: Number(asset.height),
+          }),
+        };
+      });
+      await Promise.all(
+        update.map(({ id, ...data }) =>
+          prisma.asset.update({ where: { id }, data })
+        )
+      );
+    },
+    { timeout: 1000 * 60 * 5 }
+  );
 };
