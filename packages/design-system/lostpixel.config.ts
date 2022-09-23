ef542e158b0b8c6c780546cc600cdadf50a9d630
Andrey Okonetchnikov <andrey@okonet.dev>
2022-09-23 10:47:15 +0200

    Integrate lost-pixel

---
+++ packages/design-system/lostpixel.config.ts
@@ -0,0 +1,8 @@
+import type { CustomProjectConfig } from "lost-pixel";
+
+export const config: CustomProjectConfig = {
+  storybookShots: {
+    storybookUrl: "storybook-static",
+  },
+  generateOnly: true,
+};
