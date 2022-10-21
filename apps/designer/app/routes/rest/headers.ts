d6d6a3acf36311f90a9c7631232475b439dcfb33
Roman Pominov <rpominov+github@gmail.com>
2022-10-21 12:58:30 +0400

    1

--- apps/designer/app/routes/rest/headers.ts
+++ apps/designer/app/routes/rest/headers.ts
@@ -1,5 +1,7 @@
 import { LoaderFunction } from "@remix-run/node";
 
 export const loader: LoaderFunction = async ({ request }) => {
+  // eslint-disable-next-line no-console
+  console.log("TEST LOG");
   return { headers: [...request.headers.entries()] };
 };
