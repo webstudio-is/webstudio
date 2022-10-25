903855045a2d26a6389e33b252be22612f89344a
Roman Pominov <rpominov+github@gmail.com>
2022-10-25 12:33:18 +0400

    add alert & don't put code into a string

--- packages/react-sdk/src/tree/session-storage-polyfill.tsx
+++ packages/react-sdk/src/tree/session-storage-polyfill.tsx
@@ -1,25 +1,31 @@
 // This is a temporary work around for https://github.com/remix-run/remix/issues/3659
 
 // The code is based on this discussion https://bugs.chromium.org/p/chromium/issues/detail?id=357625
-const polyfill = `(function () {
+const polyfill = function () {
   try {
     const key = "__session_storage_availability_test__";
-    sessionStorage.setItem(key, "test");
-    sessionStorage.removeItem(key);
-  } catch (e) {
+    sessionStorage.setItem(key, "test"); // test
+    sessionStorage.removeItem(key); // cleanup
+  } catch (error) {
+    alert(
+      'It looks like you have disabled cookies in your browser. Webstudio designer may not work properly.\n\nTo enable cookies, go to "Setting" > "Privacy and security" > "Cookies and other site data", and make sure neither "Block all cookies" nor "Block third-party cookies" are selected.\n\nRead more at https://support.google.com/chrome/answer/95647'
+    );
     const data = new Map();
     Object.defineProperty(window, "sessionStorage", {
       value: {
-        setItem: (key, val) => {
+        setItem: (key: string, val: unknown) => {
+          // eslint-disable-next-line no-console
           console.warn(
-            \`Session storage is unavailable due to Error "\${e.message}". A polyfill is used to set value of "\${key}". The value will be lost when the page is reloaded.\`
+            `Session storage is unavailable due to Error "${
+              (error as Error).message
+            }". A polyfill is used to set value of "${key}". The value will be lost when the page is reloaded.`
           );
           data.set(key, String(val));
         },
-        getItem: (key) => {
+        getItem: (key: string) => {
           return data.get(key);
         },
-        removeItem: (key) => {
+        removeItem: (key: string) => {
           data.delete(key);
         },
         clear: () => {
@@ -28,8 +34,8 @@ const polyfill = `(function () {
       },
     });
   }
-})();`;
+}.toString();
 
 export const SessionStoragePolyfill = () => (
-  <script dangerouslySetInnerHTML={{ __html: polyfill }} />
+  <script dangerouslySetInnerHTML={{ __html: `(${polyfill})()` }} />
 );
