3333a87f7c705a7584984fd8831a66a89e46f51e
Roman Pominov <rpominov+github@gmail.com>
2022-10-26 09:59:34 +0400

    test

--- apps/designer/server.js
+++ apps/designer/server.js
@@ -1,4 +1,13 @@
-import { createRequestHandler } from "@remix-run/vercel";
+import { createRequestHandler, RequestHandler } from "@remix-run/vercel";
 import * as build from "@remix-run/dev/server-build";
 
-export default createRequestHandler({ build, mode: process.env.NODE_ENV });
+const baseHandler = createRequestHandler({ build, mode: process.env.NODE_ENV });
+
+const handler: RequestHandler = async (request, response) => {
+  const start = Date.now();
+  await baseHandler(request, response);
+  // eslint-disable-next-line no-console
+  console.log(`${request.method} ${request.url} ${Date.now() - start}ms`);
+};
+
+export default handler;
