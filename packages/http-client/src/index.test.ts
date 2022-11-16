bb669d3abeb25a994e0ccd318fbb1c8acf1c90a3
Andras Bacsai <andras.bacsai@gmail.com>
2022-11-16 10:48:21 +0100

    feat: able to query pages with loadProject

--- packages/http-client/src/index.test.ts
+++ packages/http-client/src/index.test.ts
@@ -1,10 +1,26 @@
 import { loadProject } from "./index";
 
-const existingProjectId = "ab3619d3-831f-46f8-abb6-609558cffd99";
+const existingProjectId = "398d3918-9b00-4a43-8ab6-de4e151ae98a";
 const notPublishedProjectId = "7ec397c6-b3d0-4967-9073-9d83623fcf8e";
 const apiUrl = "http://localhost:3000";
 
 describe("getProjectDetails", () => {
+  test("include pages", async () => {
+    const response = await loadProject({
+      apiUrl,
+      projectId: existingProjectId,
+      include: { tree: true, props: true, breakpoints: true, pages: true },
+    });
+    expect(response.pages).toBeTruthy();
+  });
+  test("does not include pages", async () => {
+    const response = await loadProject({
+      apiUrl,
+      projectId: notPublishedProjectId,
+      include: { tree: true, props: true, breakpoints: true, pages: true },
+    });
+    expect(response.pages).toBeNull();
+  });
   test("loads existing project", async () => {
     const response = await loadProject({
       apiUrl,
@@ -18,7 +34,7 @@ describe("getProjectDetails", () => {
       projectId: notPublishedProjectId,
     });
     expect(response.tree.errors).toBe(
-      "Project 7ec397c6-b3d0-4967-9073-9d83623fcf8e needs to be published first"
+      `Project ${notPublishedProjectId} needs to be published first`
     );
   });
 });
