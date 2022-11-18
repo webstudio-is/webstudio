1caa9432bb134a5361333870bd8d1caf2ffcf8dd
Bogdan Chadkin <opensource@trysound.io>
2022-11-19 00:39:48 +0300

    Generate icons index.ts with after hook (#466)
    Ref
    https://github.com/svg/svgo-components/tree/main/packages/svgo-jsx#how-to-generate-indexjs-if-you-really-need-it
    
    Use svgo/jsx "after" hook instead of custom script for index.ts
    generation. This makes setup slightly simpler and more portable.
    
    Use tsx as loader to run svgo-jsx instead of using .bin as entry point.
    This is safer when use in various package managers.

--- packages/icons/svgo-jsx.config.ts
+++ packages/icons/svgo-jsx.config.ts
@@ -1,4 +1,6 @@
+import * as fs from "fs/promises";
 import * as path from "path";
+import type { Config } from "@svgo/jsx";
 
 const template = ({
   sourceFile,
@@ -25,7 +27,7 @@ const transformFilename = (filename: string) => {
   return path.basename(filename, path.extname(filename)) + ".tsx";
 };
 
-export const config = {
+export const config: Config = {
   inputDir: "./icons",
   outputDir: "./src/gen",
   template,
@@ -50,4 +52,12 @@ export const config = {
     // convert width/height to viewBox if missing
     { name: "removeDimensions" },
   ],
+  after: async ({ targets }) => {
+    let result = "";
+    for (const { file } of targets) {
+      const tsFile = path.basename(file, path.extname(file));
+      result += `export * from "./${tsFile}";\n`;
+    }
+    await fs.writeFile(path.join(process.cwd(), "./src/gen/index.ts"), result);
+  },
 };
