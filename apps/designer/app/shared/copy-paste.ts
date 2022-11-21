652a323db22669b6fe4cd07123729a36b41b74ad
Roman Pominov <rpominov+github@gmail.com>
2022-11-21 12:08:03 +0400

    avoid empty props objects

--- apps/designer/app/shared/copy-paste.ts
+++ apps/designer/app/shared/copy-paste.ts
@@ -5,7 +5,9 @@ import { useSubscribe, type Publish } from "~/shared/pubsub";
 import { useSelectedInstanceData } from "~/designer/shared/nano-states";
 import { useRootInstance } from "./nano-states";
 
-let currentData: { instance: Instance; props: InstanceProps } | undefined;
+let currentData:
+  | { instance: Instance; props: InstanceProps | undefined }
+  | undefined;
 
 export const useSubscribeCopyPaste = (publish: Publish) => {
   const [selectedInstanceData] = useSelectedInstanceData();
@@ -22,7 +24,13 @@ export const useSubscribeCopyPaste = (publish: Publish) => {
     if (instance === undefined) {
       return;
     }
-    currentData = { instance, props: selectedInstanceData.props };
+    currentData = {
+      instance,
+      props:
+        selectedInstanceData.props.props.length > 0
+          ? selectedInstanceData.props
+          : undefined,
+    };
   });
 
   useSubscribe("paste", () => {
@@ -32,7 +40,6 @@ export const useSubscribeCopyPaste = (publish: Publish) => {
 
     const instance = utils.tree.cloneInstance(currentData.instance);
 
-    // @todo: look into why in the old code they were undefined sometimes
     const props = currentData.props
       ? cloneProps(currentData.props, { instanceId: instance.id })
       : undefined;
