84035a2e3265d83b4a5bb6ce2f719b8df70b99e3
Roman Pominov <rpominov+github@gmail.com>
2022-11-24 12:50:17 +0400

    use native <a> for absolute url's

--- packages/react-sdk/src/components/link.tsx
+++ packages/react-sdk/src/components/link.tsx
@@ -1,12 +1,16 @@
 import { Link as RemixLink, useLocation } from "@remix-run/react";
 import { forwardRef, type ElementRef, type ComponentProps } from "react";
 
+const isAbsoluteUrl = (href: string) => {
+  // http:, mailto:, tel:, etc.
+  return /^[a-z]+:/i.test(href);
+};
+
 // @todo: Ideally this code should live in designer app.
 // Sdk should not know how routing is implemented in designer.
 // We can move it there later when we have a way of customizing components per environment.
 const preserveMode = (href: string, currentSearch: string) => {
-  // http:, mailto:, tel:, etc.
-  if (/^[a-z]+:/i.test(href)) {
+  if (isAbsoluteUrl(href)) {
     return href;
   }
 
@@ -35,6 +39,11 @@ type LinkProps = Omit<ComponentProps<typeof defaultTag>, "href"> & {
 export const Link = forwardRef<ElementRef<typeof defaultTag>, LinkProps>(
   ({ href = "", ...props }, ref) => {
     const hrefWithMode = usePreserveMode(href);
+
+    if (isAbsoluteUrl(href)) {
+      return <a {...props} href={href} ref={ref} />;
+    }
+
     return <RemixLink {...props} to={hrefWithMode} ref={ref} />;
   }
 );
