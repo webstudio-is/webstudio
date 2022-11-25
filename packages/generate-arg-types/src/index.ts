37f1c82251dbb733de63a05bbbcf9d8b3a5959ff
istarkov <istarkov@gmail.com>
2022-11-25 08:17:52 +0000

    Remove build step

--- packages/generate-arg-types/src/index.ts
+++ packages/generate-arg-types/src/index.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S node --experimental-specifier-resolution=node
+#!/usr/bin/env -S node --experimental-specifier-resolution=node --loader=tsx
 /* eslint-disable no-console */
 
 import path from "path";
