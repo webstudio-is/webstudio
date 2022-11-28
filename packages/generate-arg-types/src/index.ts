13790464fe303f8940cc050de75ab3f77598bfc3
Bogdan Chadkin <trysound@yandex.ru>
2022-11-28 22:47:05 +0300

    remove -S from shebangs

--- packages/generate-arg-types/src/index.ts
+++ packages/generate-arg-types/src/index.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S node --experimental-specifier-resolution=node --loader=tsx
+#!/usr/bin/env node --experimental-specifier-resolution=node --loader=tsx
 /* eslint-disable no-console */
 
 import path from "path";
