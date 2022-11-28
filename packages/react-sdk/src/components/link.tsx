749b33e1f870d83ab2937be172828abbd29e7b47
Roman Pominov <rpominov+github@gmail.com>
2022-11-28 15:51:35 +0400

    remove preserveBuildParams for now

--- packages/react-sdk/src/components/link.tsx
+++ packages/react-sdk/src/components/link.tsx
@@ -1,38 +1,9 @@
-import { Link as RemixLink, useLocation } from "@remix-run/react";
+import { Link as RemixLink } from "@remix-run/react";
 import { forwardRef, type ElementRef, type ComponentProps } from "react";
 
 const isAbsoluteUrl = (href: string) => {
   // http:, mailto:, tel:, etc.
   return /^[a-z]+:/i.test(href);
-};
-
-// @todo: Ideally this code should live in designer app.
-// Sdk should not know how routing is implemented in designer.
-// We can move it there later when we have a way of customizing components per environment.
-const preserveBuildParams = (href: string, currentSearch: string) => {
-  if (isAbsoluteUrl(href)) {
-    return href;
-  }
-
-  const url = new URL(href, "http://localhost");
-
-  const currentSearchParams = new URLSearchParams(currentSearch);
-
-  const mode = currentSearchParams.get("mode");
-  if (mode === "preview" || mode === "edit" || mode === "published") {
-    url.searchParams.set("mode", mode);
-  }
-
-  const projectId = currentSearchParams.get("projectId");
-  if (projectId) {
-    url.searchParams.set("projectId", projectId);
-  }
-
-  return `${url.pathname}?${url.searchParams.toString()}`;
-};
-const usePreserveBuildParams = (href: string) => {
-  const { search } = useLocation();
-  return preserveBuildParams(href, search);
 };
 
 const defaultTag = "a";
@@ -43,13 +14,10 @@ type LinkProps = Omit<ComponentProps<typeof defaultTag>, "href"> & {
 
 export const Link = forwardRef<ElementRef<typeof defaultTag>, LinkProps>(
   ({ href = "", ...props }, ref) => {
-    const hrefWithMode = usePreserveBuildParams(href);
-
     if (isAbsoluteUrl(href)) {
       return <a {...props} href={href} ref={ref} />;
     }
-
-    return <RemixLink {...props} to={hrefWithMode} ref={ref} />;
+    return <RemixLink {...props} to={href} ref={ref} />;
   }
 );
 
