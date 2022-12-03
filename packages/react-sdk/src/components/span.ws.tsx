77c12474851eb4a48b467f20aec130a78d3a22d4
Bogdan Chadkin <trysound@yandex.ru>
2022-12-03 16:00:24 +0300

    Fix span component type
    Mistyped when added type to components.

--- packages/react-sdk/src/components/span.ws.tsx
+++ packages/react-sdk/src/components/span.ws.tsx
@@ -3,7 +3,7 @@ import type { WsComponentMeta, MetaProps } from "./component-type";
 import props from "./__generated__/span.props.json";
 
 const meta: WsComponentMeta = {
-  type: "rich-text",
+  type: "rich-text-child",
   label: "Styled Text",
   Icon: BrushIcon,
   props: props as MetaProps,
