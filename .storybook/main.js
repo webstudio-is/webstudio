865f6389914c9c7e5c2e0187e6f037db3cd27946
Mateusz Burzyński <mateuszburzynski@gmail.com>
2022-12-03 09:24:23 +0100

    Try to proceed with the real URLs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,8 @@
-// const urls = {
-//   "css-engine": "https://638245275c0366cdfc707b3e-vgrgustujb.chromatic.com/",
-//   "designer-app": "https://6382151c8b47d4399fb9fc69-zxwosanrxy.chromatic.com/",
-//   "design-system": "https://638245578b47d4399fba191a-rbsvnglxvz.chromatic.com/",
-//   icons: "https://63824572e49212e991cb3f8b-ydvylgkybx.chromatic.com/",
-//   image: "https://6382458c8d61b8a05151a300-jgzbrmsouh.chromatic.com/",
-//   "react-sdk": "https://638245a7e49212e991cb3f8c-nfknjxkilc.chromatic.com/",
-// };
+if (!process.env.DEPLOYED_URLS) {
+  throw new Error("process.env.DEPLOYED_URLS is not set");
+}
 
-const urls = process.env.DEPLOYED_URLS;
-
-console.log("urls", urls);
-console.log("stringified", JSON.stringify(urls));
-throw new Error("stop");
+const urls = JSON.parse(process.env.DEPLOYED_URLS);
 
 module.exports = {
   // at least one story is required: https://github.com/storybookjs/storybook/issues/12705
