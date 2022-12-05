c33368a113dfdf6f52b8150a2d1c6fe6ad07b721
Oleg Isonen <oleg008@gmail.com>
2022-12-05 10:25:44 +0000

    forgotten files after they were moved

--- packages/design-tokens/src/schema.ts
+++ packages/design-tokens/src/schema.ts
@@ -1,39 +1,21 @@
 import { z } from "zod";
 import { designTokensGroups } from "./groups";
 
-// @todo this doesn't belong here https://github.com/webstudio-is/webstudio-designer/issues/544
-type UnionToIntersection<U> = (
-  U extends never ? never : (arg: U) => never
-) extends (arg: infer I) => void
-  ? I
-  : never;
-
-type UnionToTuple<T> = UnionToIntersection<
-  T extends never ? never : (t: T) => T
-> extends (_: never) => infer W
-  ? [...UnionToTuple<Exclude<T, W>>, W]
-  : [];
-
 type Type = typeof designTokensGroups[number]["type"];
 type Group = typeof designTokensGroups[number]["group"];
 
-// zod is super restrictive on enum type https://github.com/colinhacks/zod/issues/1118
-// so we convert union to tuple here its seriously safer than any as it will break on things like
-// ['a', 'b'] as UnionToTuple<Type> etc
 const types = designTokensGroups.map(
   (group) => group.type
-) as UnionToTuple<Type>;
+) as unknown as readonly [Type];
 const groupNames = designTokensGroups.map(
   (group) => group.group
-) as UnionToTuple<Group>;
+) as unknown as readonly [Group];
 
 export const DesignToken = z.object({
   name: z.string(),
-  // @todo if https://github.com/colinhacks/zod/issues/1118
   type: z.enum(types),
   value: z.string(),
   description: z.optional(z.string()),
-  // @todo if https://github.com/colinhacks/zod/issues/1118
   group: z.enum(groupNames),
 });
 
