078600ac733c84c4dd0c24fa52b360efc79be974
Mateusz Burzyński <mateuszburzynski@gmail.com>
2022-12-06 20:13:51 +0100

    Fixed publish by overriding the implicitly enforced yarn registry with the default one

--- bin/publish-to-npm.sh
+++ bin/publish-to-npm.sh
@@ -9,5 +9,8 @@ echo Current $npm_package_name $NPM_VERSION
 
 if [[ $PKG_VERSION != $NPM_VERSION ]]; then
   echo Publishing $npm_package_name $PKG_VERSION
-  npm publish --access public
+  # even though `npm` is used here this bash script gets invoked through `yarn`
+  # yarn 1 provides an env variable overriding the default npm registry with their own proxy
+  # to keep the authentication working we need to override the registry url since we only configure the auth token for the default registry
+  npm publish --access public --registry 'https://registry.npmjs.org/'
 fi
