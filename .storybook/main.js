3794f9a4978b527c8f39f5a6e038eb953d0c4110
Mateusz Burzyński <mateuszburzynski@gmail.com>
2022-12-09 14:46:23 +0100

    Add a composed Storybook (#529)
    This PR builds on top of the work done in
    https://github.com/webstudio-is/webstudio-designer/pull/519 and
    introduces a root-level storybook that just aggregates all of the
    per-package storybooks into one.
    
    This allows you to have a single, convenient, URL to visit and browse
    all of those existing storybooks at once. To access this storybook you
    need to click on the `Details` link attached to the "Storybook Publish:
    !webstudio-is/webstudio-designer" status check.
    
    The current URL from this branch is available
    [here](https://638affb61acca1e593c6e558-dksaophpxa.chromatic.com/) so
    you can take a look at how this looks in practice.

---
+++ .storybook/main.js
@@ -0,0 +1,19 @@
+if (!process.env.DEPLOYED_URLS) {
+  throw new Error("process.env.DEPLOYED_URLS is not set");
+}
+
+const urls = JSON.parse(process.env.DEPLOYED_URLS);
+
+module.exports = {
+  // at least one story is required: https://github.com/storybookjs/storybook/issues/12705
+  stories: ["./whole-repo.stories.js"],
+  refs: Object.fromEntries(
+    Object.entries(urls).map(([key, url]) => [
+      key,
+      { title: key, url, expanded: false },
+    ])
+  ),
+  features: {
+    storyStoreV7: true,
+  },
+};
