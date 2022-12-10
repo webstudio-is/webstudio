bfdf673546fdb589191c421722a339ce1237978f
Mateusz Burzyński <mateuszburzynski@gmail.com>
2022-12-10 09:00:25 +0100

    Unify Storybook config

---
+++ packages/storybook-config/index.js
@@ -0,0 +1,29 @@
+module.exports = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  framework: "@storybook/react",
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  features: {
+    storyStoreV7: true,
+  },
+  core: {
+    builder: {
+      name: "webpack5",
+      options: {
+        lazyCompilation: true,
+        fsCache: true,
+      },
+    },
+  },
+  webpackFinal: async (config) => {
+    // fix packages modules withoot extensions
+    for (const rule of config.module.rules) {
+      rule.resolve = rule.resolve || {};
+      rule.resolve.fullySpecified = false;
+    }
+    return config;
+  },
+};
