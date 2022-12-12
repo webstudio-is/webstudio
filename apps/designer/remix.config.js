95b5f357fe79e9ad8defe8f5d95e7326240687a5
Ivan Starkov <istarkov@gmail.com>
2022-12-12 22:51:37 +0300

    Hash css.

--- apps/designer/remix.config.js
+++ apps/designer/remix.config.js
@@ -12,7 +12,7 @@ module.exports = {
   // This makes sure remix bundles monorepo packages which are always
   // sym-linked and can't be hoisted. You could also manually name packages
   // e.g. ["@webstudio-is/ui", ...]
-  serverDependenciesToBundle: [/@webstudio-is\/.*/, "pretty-bytes"],
+  serverDependenciesToBundle: [/@webstudio-is\/.*/, "pretty-bytes", "djb2a"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   serverBuildPath: "api/index.js",
