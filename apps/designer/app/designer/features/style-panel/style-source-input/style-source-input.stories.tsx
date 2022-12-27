a74768738179dd470e38ca3ff650ba6d76299cb3
Oleg Isonen <oleg008@gmail.com>
2022-12-27 14:24:21 +0000

    add stories to style source input

--- apps/designer/app/designer/features/style-panel/style-source-input/style-source-input.stories.tsx
+++ apps/designer/app/designer/features/style-panel/style-source-input/style-source-input.stories.tsx
@@ -7,14 +7,75 @@ export default {
   component: StyleSourceInput,
 };
 
-export const Simple: ComponentStory<typeof StyleSourceInput> = () => {
-  const items: Array<StyleSource> = [
-    { id: "1", label: "Apple", type: "token" },
-    { id: "2", label: "Banana", type: "token" },
-    { id: "3", label: "Orange", type: "token" },
-  ];
+const items: Array<StyleSource> = [
+  { id: "1", label: "Apple", type: "token" },
+  { id: "2", label: "Banana", type: "token" },
+  { id: "3", label: "Orange", type: "token" },
+];
+
+export const Initial: ComponentStory<typeof StyleSourceInput> = () => {
   const [value, setValue] = useState<Array<StyleSource>>([
     { id: "0", label: "Local", type: "local" },
+  ]);
+  return (
+    <StyleSourceInput
+      css={{ width: 300 }}
+      items={items}
+      value={value}
+      onItemCreate={(label) => {
+        const item: StyleSource = { id: uuid(), label, type: "token" };
+        setValue([...value, item]);
+      }}
+      onItemSelect={(item) => {
+        setValue([...value, item]);
+      }}
+      onItemRemove={(itemToRemove) => {
+        if (itemToRemove.type === "local") {
+          return;
+        }
+        setValue(value.filter((item) => item.id !== itemToRemove.id));
+      }}
+    />
+  );
+};
+
+export const WithItems: ComponentStory<typeof StyleSourceInput> = () => {
+  const [value, setValue] = useState<Array<StyleSource>>([
+    { id: "0", label: "Local", type: "local" },
+    ...items,
+  ]);
+  return (
+    <StyleSourceInput
+      css={{ width: 300 }}
+      items={items}
+      value={value}
+      onItemCreate={(label) => {
+        const item: StyleSource = { id: uuid(), label, type: "token" };
+        setValue([...value, item]);
+      }}
+      onItemSelect={(item) => {
+        setValue([...value, item]);
+      }}
+      onItemRemove={(itemToRemove) => {
+        if (itemToRemove.type === "local") {
+          return;
+        }
+        setValue(value.filter((item) => item.id !== itemToRemove.id));
+      }}
+    />
+  );
+};
+
+export const WithTruncatedItem: ComponentStory<
+  typeof StyleSourceInput
+> = () => {
+  const [value, setValue] = useState<Array<StyleSource>>([
+    {
+      id: "0",
+      label:
+        "Local Something Something Something Something Something Something Something Something Something Something Something",
+      type: "local",
+    },
   ]);
   return (
     <StyleSourceInput
