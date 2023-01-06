31c236716163fc50b297ae814572854436cace87
Oleg Isonen <oleg008@gmail.com>
2023-01-06 09:40:14 +0000

    move  edittable text to a hook

--- apps/designer/app/designer/features/style-panel/style-source-input/style-source.tsx
+++ apps/designer/app/designer/features/style-panel/style-source-input/style-source.tsx
@@ -53,30 +53,13 @@ const Menu = (props: MenuProps) => {
   );
 };
 
-const EditableText = styled(Text, {
-  outline: "none",
-});
-
-const EditableButton = styled(Button, {
-  maxWidth: "100%",
-});
-
-type StyleSourceProps = {
+type UseEditableTextProps = {
   label: string;
-  hasMenu: boolean;
-  onDuplicate: () => void;
-  onRemove: () => void;
   onEdit: () => void;
   onChange: (value: string) => void;
 };
 
-export const StyleSource = ({
-  label,
-  hasMenu,
-  onEdit,
-  onChange,
-  ...menuProps
-}: StyleSourceProps) => {
+const useEditableText = ({ onEdit, onChange, label }: UseEditableTextProps) => {
   const [isEditing, setIsEditing] = useState(false);
   const textRef = useRef<HTMLDivElement>(null);
 
@@ -99,16 +82,53 @@ export const StyleSource = ({
     }
   };
 
+  const text = (
+    <Text
+      truncate
+      contentEditable={isEditing ? "plaintext-only" : false}
+      ref={textRef}
+      onKeyDown={handleKeyDown}
+      css={{ outline: "none" }}
+    >
+      {label}
+    </Text>
+  );
+
+  return {
+    isEditing,
+    handleEdit,
+    text,
+  };
+};
+
+const EditableButton = styled(Button, {
+  maxWidth: "100%",
+});
+
+type StyleSourceProps = {
+  label: string;
+  hasMenu: boolean;
+  onDuplicate: () => void;
+  onRemove: () => void;
+  onEdit: () => void;
+  onChange: (value: string) => void;
+};
+
+export const StyleSource = ({
+  label,
+  hasMenu,
+  onEdit,
+  onChange,
+  ...menuProps
+}: StyleSourceProps) => {
+  const { text, isEditing, handleEdit } = useEditableText({
+    onEdit,
+    onChange,
+    label,
+  });
   return (
     <EditableButton variant="gray">
-      <EditableText
-        truncate
-        contentEditable={isEditing ? "plaintext-only" : false}
-        ref={textRef}
-        onKeyDown={handleKeyDown}
-      >
-        {label}
-      </EditableText>
+      {text}
       {hasMenu === true && isEditing === false && (
         <Menu {...menuProps} onEdit={handleEdit} />
       )}
