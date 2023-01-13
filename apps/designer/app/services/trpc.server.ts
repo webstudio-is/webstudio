4c0b54b651b9537e8e62722ab495bef0743bee53
istarkov <istarkov@gmail.com>
2023-01-13 18:49:37 +0300

    Add import

---
+++ apps/designer/app/services/trpc.server.ts
@@ -0,0 +1,13 @@
+import { createTRPCProxyServiceClient } from "@webstudio-is/trpc-interface/server";
+
+const TRPC_SERVER_URL = process.env.TRPC_SERVER_URL;
+const TRPC_SERVER_API_TOKEN = process.env.TRPC_SERVER_API_TOKEN;
+
+export const trpcClient = createTRPCProxyServiceClient(
+  TRPC_SERVER_URL !== undefined
+    ? {
+        url: TRPC_SERVER_URL,
+        apiToken: TRPC_SERVER_API_TOKEN,
+      }
+    : undefined
+);
