afddad936dba953ecae59957388a21ba92c85c28
Bogdan Chadkin <trysound@yandex.ru>
2023-01-16 22:29:10 +0300

    Update token store directly
    We no longer need to use pubsub to notify canvas to change something
    and duplicate logic in designer.
    
    Here updated syncrhonized state directly and replaced tokens utils
    with generic array utils.

--- apps/designer/app/designer/features/sidebar-left/panels/design-tokens/design-tokens.tsx
+++ apps/designer/app/designer/features/sidebar-left/panels/design-tokens/design-tokens.tsx
@@ -1,16 +1,13 @@
 import { TokensIcon } from "@webstudio-is/icons";
 import { DesignTokensManager } from "~/designer/shared/design-tokens-manager";
-import type { Publish } from "~/shared/pubsub";
 import { TabName } from "../../types";
 import { Header, CloseButton } from "../../lib/header";
 import { Flex } from "@webstudio-is/design-system";
 
 export const TabContent = ({
   onSetActiveTab,
-  publish,
 }: {
   onSetActiveTab: (tabName: TabName) => void;
-  publish: Publish;
 }) => {
   return (
     <Flex direction="column" css={{ height: "100%" }}>
@@ -18,7 +15,7 @@ export const TabContent = ({
         title="Design Tokens"
         suffix={<CloseButton onClick={() => onSetActiveTab("none")} />}
       />
-      <DesignTokensManager publish={publish} />
+      <DesignTokensManager />
     </Flex>
   );
 };
