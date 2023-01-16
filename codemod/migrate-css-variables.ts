310cbada25f89ed9309fb40bfa55b48e7d0ba603
Roman Pominov <rpominov+github@gmail.com>
2023-01-16 18:40:25 +0400

    fix addImport

--- codemod/migrate-css-variables.ts
+++ codemod/migrate-css-variables.ts
@@ -155,6 +155,10 @@ const addImport = (code: string, filePath: string) => {
     nextLinesReverse.push(line);
   }
 
+  if (inserted === false) {
+    nextLinesReverse.push(importCode);
+  }
+
   return nextLinesReverse.reverse().join("\n");
 };
 
