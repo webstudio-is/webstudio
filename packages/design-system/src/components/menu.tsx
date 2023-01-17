ba28d78768ba6bf15b07e21edfced4187a7def04
Roman Pominov <rpominov+github@gmail.com>
2023-01-17 14:45:23 +0400

    remove @radix-ui/react-menu that's private and not supposed to be used

--- packages/design-system/src/components/menu.tsx
+++ packages/design-system/src/components/menu.tsx
@@ -1,10 +1,4 @@
-import React from "react";
-import * as MenuPrimitive from "@radix-ui/react-menu";
-import { CheckIcon } from "@webstudio-is/icons";
-import { styled, css, CSS } from "../stitches.config";
-import { Box } from "./box";
-import { panelStyles } from "./panel";
-import { theme } from "../stitches.config";
+import { css, theme } from "../stitches.config";
 
 export const baseItemCss = css({
   display: "flex",
@@ -49,59 +43,3 @@ export const separatorCss = css({
   my: theme.spacing[3],
   backgroundColor: theme.colors.slate6,
 });
-
-export const Menu = styled(MenuPrimitive.Root, menuCss);
-export const MenuContent = styled(MenuPrimitive.Content, panelStyles);
-
-export const MenuSeparator = styled(MenuPrimitive.Separator, separatorCss);
-
-export const MenuItem = styled(MenuPrimitive.Item, itemCss);
-
-const StyledMenuRadioItem = styled(MenuPrimitive.RadioItem, itemCss);
-
-type MenuRadioItemPrimitiveProps = React.ComponentProps<
-  typeof MenuPrimitive.RadioItem
->;
-type MenuRadioItemProps = MenuRadioItemPrimitiveProps & { css?: CSS };
-
-export const MenuRadioItem = React.forwardRef<
-  React.ElementRef<typeof StyledMenuRadioItem>,
-  MenuRadioItemProps
->(({ children, ...props }, forwardedRef) => (
-  <StyledMenuRadioItem {...props} ref={forwardedRef}>
-    <Box as="span" css={{ position: "absolute", left: theme.spacing[3] }}>
-      <MenuPrimitive.ItemIndicator>
-        <CheckIcon />
-      </MenuPrimitive.ItemIndicator>
-    </Box>
-    {children}
-  </StyledMenuRadioItem>
-));
-MenuRadioItem.displayName = "MenuRadioItem";
-
-const StyledMenuCheckboxItem = styled(MenuPrimitive.CheckboxItem, itemCss);
-
-type MenuCheckboxItemPrimitiveProps = React.ComponentProps<
-  typeof MenuPrimitive.CheckboxItem
->;
-type MenuCheckboxItemProps = MenuCheckboxItemPrimitiveProps & { css?: CSS };
-
-export const MenuCheckboxItem = React.forwardRef<
-  React.ElementRef<typeof StyledMenuCheckboxItem>,
-  MenuCheckboxItemProps
->(({ children, ...props }, forwardedRef) => (
-  <StyledMenuCheckboxItem {...props} ref={forwardedRef}>
-    <Box as="span" css={{ position: "absolute", left: theme.spacing[3] }}>
-      <MenuPrimitive.ItemIndicator>
-        <CheckIcon />
-      </MenuPrimitive.ItemIndicator>
-    </Box>
-    {children}
-  </StyledMenuCheckboxItem>
-));
-MenuCheckboxItem.displayName = "MenuCheckboxItem";
-
-export const MenuLabel = styled(MenuPrimitive.Label, labelCss);
-export const MenuRadioGroup = styled(MenuPrimitive.RadioGroup, {});
-export const MenuGroup = styled(MenuPrimitive.Group, {});
-export const MenuAnchor = MenuPrimitive.MenuAnchor;
