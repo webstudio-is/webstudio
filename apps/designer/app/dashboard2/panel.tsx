568f139c1aee393902b30e708b95a7c7e89fe65a
Oleg Isonen <oleg008@gmail.com>
2023-01-19 10:25:40 +0000

    remove styled

--- apps/designer/app/dashboard2/panel.tsx
+++ apps/designer/app/dashboard2/panel.tsx
@@ -1,12 +1,18 @@
-import { styled, Box, theme } from "@webstudio-is/design-system";
+import { Box, theme } from "@webstudio-is/design-system";
+import { type ComponentProps } from "react";
 
-export const Panel = styled(Box, {
-  background: theme.colors.backgroundPanel,
-  padding: theme.spacing[13],
-  margin: theme.spacing[13],
-  borderRadius: theme.borderRadius[6],
-  minWidth: 600,
-  color: theme.colors.foregroundMain,
-  // @todo - use theme
-  //boxShadow: theme.boxShadow.brandElevationBig
-});
+export const Panel = (props: ComponentProps<typeof Box>) => (
+  <Box
+    css={{
+      background: theme.colors.backgroundPanel,
+      padding: theme.spacing[13],
+      margin: theme.spacing[13],
+      borderRadius: theme.borderRadius[6],
+      minWidth: 600,
+      color: theme.colors.foregroundMain,
+      // @todo - use theme
+      //boxShadow: theme.boxShadow.brandElevationBig
+    }}
+    {...props}
+  />
+);
