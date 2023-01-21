9ff8c746922e5e4da62dc902ff92d32c7dbe1d01
Roman Pominov <rpominov+github@gmail.com>
2023-01-21 09:53:52 +0400

    Make shadow and typography tokens available to be used in code (#821)
    1. Added `@font-face`s to `designer` and `design-system` storybooks
    2. Shadow tokens are now available via `theme.shadows.somthing`
    3. Typography tokens are now available like this:
    
    ```ts
    import { typography, styled } from '@webstudio-is/design-system'; 
    
    const Something = styled("div", typography.something)
    
    <div className={typography.something()} />
    ```

---
+++ packages/storybook-config/setup-fonts.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-internal-modules */
+
+// This file is meant to be imported in .storybook/previews.js like so:
+// import "@webstudio-is/storybook-config/setup-fonts";
+
+// this adds <style> tags to the <head> of the document
+require("@fontsource/inter/variable.css");
+require("@fontsource/manrope/variable.css");
+require("@fontsource/roboto");
