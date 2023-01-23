76e50c4eee186e2e9e6e670fe86da0e1abe3fcaf
Oleg Isonen <oleg008@gmail.com>
2023-01-23 23:29:24 +0000

    set some min width for larger screen

--- apps/designer/app/dashboard/projects.tsx
+++ apps/designer/app/dashboard/projects.tsx
@@ -6,13 +6,17 @@ import type { DashboardProject } from "@webstudio-is/prisma-client";
 import { ProjectCard } from "./project-card";
 import { NewProject } from "./new-project";
 
+const projectsContainerStyle = {
+  "@bp2": { minWidth: 900 },
+};
+
 type ProjectsProps = {
   projects: Array<DashboardProject>;
 };
 
 export const Projects = ({ projects }: ProjectsProps) => {
   return (
-    <Panel>
+    <Panel css={projectsContainerStyle}>
       <Flex direction="column" gap="3">
         <Flex justify="between">
           <Heading variant="small">Projects</Heading>
