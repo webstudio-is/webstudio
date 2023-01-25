dd40f745a5e8e8315951b6adc4d15a1b8994c67b
Oleg Isonen <oleg008@gmail.com>
2023-01-25 21:33:23 +0000

    Dashboard styling (#849)
    ## Description
    
    Styling tweaks
    
    ## Steps for reproduction
    
    1. open /dashboard
    
    ## Code Review
    
    - [ ] hi @kof, I need you to do
      - conceptual review (architecture, feature-correctness)
      - detailed review (read every line)
      - test it on preview
    
    ## Before requesting a review
    
    - [ ] made a self-review
    - [ ] added inline comments where things may be not obvious (the "why",
    not "what")
    
    ## Before merging
    
    - [ ] tested locally and on preview environment (preview dev login:
    5de6)
    - [ ] updated [test
    cases](https://github.com/webstudio-is/webstudio-designer/blob/main/apps/designer/docs/test-cases.md)
    document
    - [ ] added tests
    - [ ] if any new env variables are added, added them to `.env.example`
    and the `designer/env-check.js` if mandatory

--- apps/designer/app/dashboard/projects.tsx
+++ apps/designer/app/dashboard/projects.tsx
@@ -6,17 +6,13 @@ import type { DashboardProject } from "@webstudio-is/prisma-client";
 import { ProjectCard } from "./project-card";
 import { NewProject } from "./new-project";
 
-const projectsContainerStyle = {
-  "@laptop": { minWidth: 900 },
-};
-
 type ProjectsProps = {
   projects: Array<DashboardProject>;
 };
 
 export const Projects = ({ projects }: ProjectsProps) => {
   return (
-    <Panel css={projectsContainerStyle}>
+    <Panel>
       <Flex direction="column" gap="3">
         <Flex justify="between">
           <Heading variant="small">Projects</Heading>
