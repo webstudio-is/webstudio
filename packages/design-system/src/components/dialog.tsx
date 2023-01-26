fcaa9971d850e6e28e17fc9c9f5078579e12e44d
Roman Pominov <rpominov+github@gmail.com>
2023-01-26 16:34:48 +0400

    dialog wip

---
+++ packages/design-system/src/components/dialog.tsx
@@ -0,0 +1,10 @@
+import * as Primitive from "@radix-ui/react-dialog";
+
+export const Dialog = Primitive.Root;
+export const DialogTrigger = Primitive.Trigger;
+export const DialogPortal = Primitive.Portal;
+export const DialogOverlay = Primitive.Overlay;
+export const DialogContent = Primitive.Content;
+export const DialogTitle = Primitive.Title;
+export const DialogDescription = Primitive.Description;
+export const DialogClose = Primitive.Close;
---
+++ packages/design-system/src/components/floating-panel.stories.tsx
@@ -0,0 +1,58 @@
+import {
+  Dialog,
+  DialogTrigger,
+  DialogPortal,
+  DialogOverlay,
+  DialogContent,
+  DialogTitle,
+  DialogDescription,
+  DialogClose,
+} from "./dialog";
+import { CrossIcon } from "@webstudio-is/icons";
+
+export default {
+  title: "Library/Floating Panel",
+};
+
+export const FloatingPanel = () => (
+  <Dialog>
+    <DialogTrigger asChild>
+      <button className="Button violet" size="large">
+        Edit profile
+      </button>
+    </DialogTrigger>
+    <DialogPortal>
+      <DialogOverlay className="DialogOverlay" />
+      <DialogContent className="DialogContent">
+        <DialogTitle className="DialogTitle">Edit profile</DialogTitle>
+        <DialogDescription className="DialogDescription">
+          Make changes to your profile here. Click save when you're done.
+        </DialogDescription>
+        <fieldset className="Fieldset">
+          <label className="Label" htmlFor="name">
+            Name
+          </label>
+          <input className="Input" id="name" defaultValue="Pedro Duarte" />
+        </fieldset>
+        <fieldset className="Fieldset">
+          <label className="Label" htmlFor="username">
+            Username
+          </label>
+          <input className="Input" id="username" defaultValue="@peduarte" />
+        </fieldset>
+        <div
+          style={{ display: "flex", marginTop: 25, justifyContent: "flex-end" }}
+        >
+          <DialogClose asChild>
+            <button className="Button green">Save changes</button>
+          </DialogClose>
+        </div>
+        <DialogClose asChild>
+          <button className="IconButton" aria-label="Close">
+            <CrossIcon />
+          </button>
+        </DialogClose>
+      </DialogContent>
+    </DialogPortal>
+  </Dialog>
+);
