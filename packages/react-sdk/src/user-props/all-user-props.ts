6f6de579e9af62ce19a6ba33b9f9bdbfdeba0245
Bogdan Chadkin <trysound@yandex.ru>
2023-01-27 22:22:20 +0300

    Inject props store to sdk
    Ref https://github.com/webstudio-is/webstudio-designer/issues/696
    
    We use store to render props in sdk components. Though it's easier to
    maintain actual store in designer and provide necessary data to sdk.
    
    Here I added utilities to inject computed store on module level.
    Now we can get rid of legacy store and rewrite rest api to use
    normalized props.

--- packages/react-sdk/src/user-props/all-user-props.ts
+++ packages/react-sdk/src/user-props/all-user-props.ts
@@ -1,26 +1,8 @@
-import { useRef } from "react";
 import { atom } from "nanostores";
-import { useStore } from "@nanostores/react";
 import type { Instance, PropsItem } from "@webstudio-is/project-build";
-import type { InstanceProps } from "../db";
 
 export type AllUserProps = {
   [id: Instance["id"]]: PropsItem[];
 };
 
 export const allUserPropsContainer = atom<AllUserProps>({});
-
-export const useAllUserProps = (initialUserProps?: Array<InstanceProps>) => {
-  // @todo ssr workaround for https://github.com/webstudio-is/webstudio-designer/issues/213
-  const ref = useRef(false);
-  if (ref.current === false && initialUserProps !== undefined) {
-    const propsMap: AllUserProps = {};
-    for (const item of initialUserProps) {
-      propsMap[item.instanceId] = item.props;
-    }
-    //We don't need to trigger rerender when setting the initial value
-    allUserPropsContainer.set(propsMap);
-    ref.current = true;
-  }
-  return useStore(allUserPropsContainer);
-};
