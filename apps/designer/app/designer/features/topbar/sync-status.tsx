a56af7a84f14c69bbdfa09b1b8392d7def1d62bd
Oleg Isonen <oleg008@gmail.com>
2023-01-30 01:12:31 +0000

    show the icon when error state only

--- apps/designer/app/designer/features/topbar/sync-status.tsx
+++ apps/designer/app/designer/features/topbar/sync-status.tsx
@@ -1,50 +1,32 @@
 import { useStore } from "@nanostores/react";
 import {
-  theme,
   Flex,
-  keyframes,
   AccessibleIcon,
-  styled,
-  Box,
+  rawTheme,
+  Tooltip,
 } from "@webstudio-is/design-system";
-import { CheckIcon, DotsHorizontalIcon } from "@webstudio-is/icons";
+import { CloudIcon } from "@webstudio-is/icons";
 import { syncStatus } from "~/designer/shared/sync";
-
-const iconSize = 15;
-
-const StyledCheckIcon = styled(CheckIcon, {
-  width: iconSize,
-  height: iconSize,
-  background: theme.colors.green9,
-  borderRadius: theme.borderRadius.round,
-});
-
-const ellipsisKeyframes = keyframes({
-  to: { width: iconSize },
-});
-
-const AnimatedDotsIcon = () => {
-  return (
-    <Flex direction="column" css={{ width: iconSize }}>
-      <Box
-        css={{
-          animation: `${ellipsisKeyframes} steps(4,end) 900ms infinite`,
-          width: 0,
-          overflow: "hidden",
-        }}
-      >
-        <DotsHorizontalIcon width="12" height="12" />
-      </Box>
-    </Flex>
-  );
-};
 
 export const SyncStatus = () => {
   const status = useStore(syncStatus);
+  if (status !== "error") {
+    return null;
+  }
   return (
     <Flex align="center" justify="center">
       <AccessibleIcon label={`Sync status: ${status}`}>
-        {status === "syncing" ? <AnimatedDotsIcon /> : <StyledCheckIcon />}
+        <Tooltip
+          variant="wrapped"
+          content="Offline changes will be synced with Webstudio once you go online."
+        >
+          {/* @todo replace the icon, waiting for figma */}
+          <CloudIcon
+            width={20}
+            height={20}
+            color={rawTheme.colors.foregroundDestructive}
+          />
+        </Tooltip>
       </AccessibleIcon>
     </Flex>
   );
