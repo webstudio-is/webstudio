1b8456adcb31b6b56d6daf622ebfa75b2d960219
Oleg Isonen <oleg008@gmail.com>
2023-01-30 01:23:59 +0000

    add internet connection hint

--- apps/designer/app/designer/features/topbar/sync-status.tsx
+++ apps/designer/app/designer/features/topbar/sync-status.tsx
@@ -6,19 +6,51 @@ import {
   Tooltip,
 } from "@webstudio-is/design-system";
 import { CloudIcon } from "@webstudio-is/icons";
+import { useEffect, useState } from "react";
 import { syncStatus } from "~/designer/shared/sync";
+
+const useIsOffline = () => {
+  const [isOffline, setIsOffline] = useState(false);
+  useEffect(() => {
+    const handleOffline = () => setIsOffline(true);
+    addEventListener("offline", handleOffline);
+    return () => removeEventListener("offline", handleOffline);
+  }, []);
+
+  useEffect(() => {
+    const handleOnline = () => setIsOffline(false);
+    addEventListener("online", handleOnline);
+    return () => removeEventListener("online", handleOnline);
+  }, []);
+  return isOffline;
+};
 
 export const SyncStatus = () => {
   const status = useStore(syncStatus);
+  const isOffline = useIsOffline();
+
   if (status !== "error") {
     return null;
   }
+
   return (
     <Flex align="center" justify="center">
       <AccessibleIcon label={`Sync status: ${status}`}>
         <Tooltip
           variant="wrapped"
-          content="Offline changes will be synced with Webstudio once you go online."
+          content={
+            <>
+              Offline changes will be synced with Webstudio once you go online.
+              {isOffline ? (
+                <>
+                  <br />
+                  Please check your internet connection.
+                </>
+              ) : (
+                ""
+              )}
+            </>
+          }
         >
           {/* @todo replace the icon, waiting for figma */}
           <CloudIcon
