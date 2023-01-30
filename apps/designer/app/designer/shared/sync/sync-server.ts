90ce5a06b101432d258a0cf307470edf5b090d68
Oleg Isonen <oleg008@gmail.com>
2023-01-30 16:56:45 +0100

    Connection/queue status (#893)
    Fixes https://github.com/webstudio-is/webstudio-designer/issues/863
    
    ## Description
    
    Icon used is temporary, until we have a better one. We can merge this
    because we only show it when offline, so most people won't see it
    anyways.
    
    - [x] check if the sync was successful, if not - don't remove it from
    the queue (or put it back) . Right now we are optimistically removing
    from the queue when syncing
    - [x] after 3 unsuccessful attempts to sync - show the disconnected icon
    - [x] add a tooltip (like in figma) that explains that we will sync as
    soon as we are online again
    - [x] use `addEventListener('offline'` event listener to explain in text
    of the tooltip that internet is not available, otherwise assume by
    default server is not available.
    - [x] When there are unsaved changes and user tries to close the window
    - use onbeforeunload to tell them about it and that they will loose the
    unsynced changes otherwise (until we add offline capability to save
    those and sync them once online)
    
    
    
    https://user-images.githubusercontent.com/52824/215371856-2d76a364-3918-42b6-a4eb-9693c54d5690.mp4
    
    
    
    ## Steps for reproduction
    
    1. go to chrome network and set connection to offline or go to the wifi
    and turn it off (if you are not locally running the server)
    
    
    ## Code Review
    
    - [ ] hi @TrySound , I need you to do
      - conceptual review (architecture, feature-correctness)
      - detailed review (read every line)
      - test it on preview
    
    ## Before requesting a review
    
    - [ ] made a self-review
    - [ ] added inline comments where things may be not obvious (the "why",
    not "what")
    
    ## Before merging
    
    - [ ] tested locally and on preview environment (preview dev login:
    5de6)
    - [ ] updated [test
    cases](https://github.com/webstudio-is/webstudio-designer/blob/main/apps/designer/docs/test-cases.md)
    document
    - [ ] added tests
    - [ ] if any new env variables are added, added them to `.env.example`
    and the `designer/env-check.js` if mandatory

---
+++ apps/designer/app/designer/shared/sync/sync-server.ts
@@ -0,0 +1,90 @@
+import { sync } from "immerhin";
+import type { Build, Project } from "@webstudio-is/project";
+import type { Tree } from "@webstudio-is/project-build";
+import { restPatchPath } from "~/shared/router-utils";
+import { useEffect } from "react";
+import { enqueue, dequeue, queueStatus } from "./queue";
+import { useBeforeUnload } from "react-use";
+
+// Periodic check for new entries to group them into one job/call in sync queue.
+const NEW_ENTRIES_INTERVAL = 1000;
+
+// First attempts we will simply retry without changing the state or notifying anyone.
+const INTERVAL_RECOVERY = 2000;
+
+// When we reached max failed attempts we will slow down the attempts interval.
+const INTERVAL_ERROR = 5000;
+
+const useRecoveryCheck = () => {
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (queueStatus.get() === "recovering") {
+        dequeue();
+      }
+    }, INTERVAL_RECOVERY);
+
+    return () => clearInterval(intervalId);
+  }, []);
+};
+
+const useErrorCheck = () => {
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (queueStatus.get() === "failed") {
+        dequeue();
+      }
+    }, INTERVAL_ERROR);
+
+    return () => clearInterval(intervalId);
+  }, []);
+};
+
+const useNewEntriesCheck = ({
+  treeId,
+  buildId,
+  projectId,
+}: UserSyncServerProps) => {
+  useEffect(() => {
+    // @todo setInterval can be completely avoided.
+    // Right now prisma can't do atomic updates yet with sandbox documents
+    // and backend fetches and updates big objects, so if we send quickly,
+    // we end up overwriting things
+    const intervalId = setInterval(() => {
+      const transactions = sync();
+
+      if (transactions.length === 0) {
+        return;
+      }
+
+      enqueue(() =>
+        fetch(restPatchPath(), {
+          method: "post",
+          body: JSON.stringify({
+            transactions,
+            treeId,
+            buildId,
+            projectId,
+          }),
+        })
+      );
+    }, NEW_ENTRIES_INTERVAL);
+
+    return () => clearInterval(intervalId);
+  }, [treeId, buildId, projectId]);
+};
+
+type UserSyncServerProps = {
+  buildId: Build["id"];
+  treeId: Tree["id"];
+  projectId: Project["id"];
+};
+
+export const useSyncServer = (props: UserSyncServerProps) => {
+  useNewEntriesCheck(props);
+  useRecoveryCheck();
+  useErrorCheck();
+  useBeforeUnload(
+    () => queueStatus.get() !== "idle",
+    "You have unsaved changes. Are you sure you want to leave?"
+  );
+};
