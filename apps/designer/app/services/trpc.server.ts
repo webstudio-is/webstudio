55856a444e18eff95648d2673ba83a788c768c45
istarkov <istarkov@gmail.com>
2023-02-08 14:10:54 +0400

    Add support of saas edge-api trpc preview router

--- apps/designer/app/services/trpc.server.ts
+++ apps/designer/app/services/trpc.server.ts
@@ -3,12 +3,14 @@ import env from "~/env/env.server";
 
 const TRPC_SERVER_URL = env.TRPC_SERVER_URL;
 const TRPC_SERVER_API_TOKEN = env.TRPC_SERVER_API_TOKEN;
+const BRANCH_NAME = env.BRANCH_NAME;
 
 export const trpcClient = createTrpcProxyServiceClient(
   TRPC_SERVER_URL !== undefined && TRPC_SERVER_API_TOKEN !== undefined
     ? {
         url: TRPC_SERVER_URL,
         token: TRPC_SERVER_API_TOKEN,
+        branchName: BRANCH_NAME,
       }
     : undefined
 );
