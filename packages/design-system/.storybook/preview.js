3313f57ebae55d17ffea1264d97e316eb2424a5f
Roman Pominov <rpominov+github@gmail.com>
2023-02-10 13:04:33 +0400

    add backgrounds

--- packages/design-system/.storybook/preview.js
+++ packages/design-system/.storybook/preview.js
@@ -1,4 +1,5 @@
 import "@webstudio-is/storybook-config/setup-fonts";
+import { color } from "../src/__generated__/figma-design-tokens";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -7,5 +8,16 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  backgrounds: {
+    default: "White",
+    values: [
+      { name: "White", value: "#ffffff" },
+      { name: "Black", value: "#000000" },
+      { name: "Panel", value: color.backgroundPanel },
+      { name: "Maintenance Dark", value: color.maintenanceDark },
+      { name: "Maintenance Medium", value: color.maintenanceMedium },
+      { name: "Maintenance Light", value: color.maintenanceLight },
+    ],
   },
 };
