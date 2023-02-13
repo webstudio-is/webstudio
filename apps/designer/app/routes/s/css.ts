f1b653086528ab2b2437718c531658091955a295
Bogdan Chadkin <trysound@yandex.ru>
2023-02-13 16:44:46 +0300

    Add utility to generate css text from data
    Ref https://github.com/webstudio-is/webstudio-designer/issues/1019
    
    We do a lot of changes with styles and ofte forget to keep saas up to
    date. So instead whole logic to generate css will be hidden in shared
    utility.

--- apps/designer/app/routes/s/css.ts
+++ apps/designer/app/routes/s/css.ts
@@ -1,9 +1,11 @@
 import { type ActionArgs, json } from "@remix-run/node";
+import { generateCssText } from "@webstudio-is/project";
+import { db } from "@webstudio-is/project/server";
 import env from "~/env/env.public.server";
 import { getBuildParams } from "~/shared/router-utils";
 import { sentryException } from "~/shared/sentry";
-import { generateCssText } from "~/shared/css-utils";
 import { createContext } from "~/shared/context.server";
+import { loadCanvasData } from "~/shared/db";
 
 export const loader = async ({ request }: ActionArgs) => {
   try {
@@ -14,7 +16,29 @@ export const loader = async ({ request }: ActionArgs) => {
       throw json("Required project info", { status: 400 });
     }
 
-    const cssText = await generateCssText(buildParams, context);
+    const project = await db.project.loadByParams(buildParams, context);
+    if (project === null) {
+      throw json("Project not found", { status: 404 });
+    }
+
+    const canvasData = await loadCanvasData(
+      {
+        project,
+        env: buildParams.mode === "published" ? "prod" : "dev",
+        pageIdOrPath:
+          "pageId" in buildParams ? buildParams.pageId : buildParams.pagePath,
+      },
+      context
+    );
+    if (canvasData === undefined) {
+      throw json("Page not found", { status: 404 });
+    }
+
+    const cssText = generateCssText({
+      assets: canvasData.assets,
+      build: canvasData.build ?? undefined,
+      tree: canvasData.tree ?? undefined,
+    });
 
     return new Response(cssText, {
       headers: {
--- packages/project/src/shared/styles/index.ts
+++ packages/project/src/shared/styles/index.ts
@@ -1,2 +1,3 @@
 export * from "./global-rules";
 export * from "./style-rules";
+export * from "./css";
