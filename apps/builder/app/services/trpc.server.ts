691bc4ada0573ce7cbb6ccbad3e3cf8804851a9d
Bogdan Chadkin <trysound@yandex.ru>
2023-02-15 11:45:35 +0300

    Fix trpc client setup with default .env
    We have trpc envs specified like this
    
    TRPC_SERVER_URL=
    TRPC_SERVER_API_TOKEN=
    
    Which gives empty string in runtime though we check for undefined
    expecting envs are not defined.
    
    In this PR I covered both undefined and empty string to not break
    new setups.

--- apps/builder/app/services/trpc.server.ts
+++ apps/builder/app/services/trpc.server.ts
@@ -1,12 +1,12 @@
 import { createTrpcProxyServiceClient } from "@webstudio-is/trpc-interface/server";
 import env from "~/env/env.server";
 
-const TRPC_SERVER_URL = env.TRPC_SERVER_URL;
-const TRPC_SERVER_API_TOKEN = env.TRPC_SERVER_API_TOKEN;
+const TRPC_SERVER_URL = env.TRPC_SERVER_URL ?? "";
+const TRPC_SERVER_API_TOKEN = env.TRPC_SERVER_API_TOKEN ?? "";
 const BRANCH_NAME = env.BRANCH_NAME;
 
 export const trpcClient = createTrpcProxyServiceClient(
-  TRPC_SERVER_URL !== undefined && TRPC_SERVER_API_TOKEN !== undefined
+  TRPC_SERVER_URL !== "" && TRPC_SERVER_API_TOKEN !== ""
     ? {
         url: TRPC_SERVER_URL,
         token: TRPC_SERVER_API_TOKEN,
