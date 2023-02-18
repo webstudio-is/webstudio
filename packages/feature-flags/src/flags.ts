67f2c467ca75016fcae16f67b0f4c86ac3a7f28b
Bogdan Chadkin <opensource@trysound.io>
2023-02-18 12:34:14 +0300

    Enable style source input for all users (#1074)
    Style source input is getting usable so we can enable it by default.

--- packages/feature-flags/src/flags.ts
+++ packages/feature-flags/src/flags.ts
@@ -3,4 +3,4 @@ export const example = false;
 export const dark = false;
 export const propertyReset = false;
 export const share2 = false;
-export const styleSourceInput = false;
+export const styleSourceInput = true;
