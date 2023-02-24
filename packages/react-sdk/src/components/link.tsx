afa83c8d55fcad6e57bf9333b147dc7d0df75ee1
Oleg Isonen <oleg008@gmail.com>
2023-02-24 10:17:40 +0100

    Add target and prefetch to a link (#1117)
    ## Description
    
    Adding target and prefetch to initial properties of a Link component
    along with the defaults
    
    ## Steps for reproduction
    
    1. add a link
    2. try target prop
    3. try prefetch options
    
    ## Code Review
    
    - [ ] hi @rpominov, I need you to do
      - conceptual review (architecture, feature-correctness)
      - detailed review (read every line)
      - test it on preview
    
    ## Before requesting a review
    
    - [ ] made a self-review
    - [ ] added inline comments where things may be not obvious (the "why",
    not "what")
    
    ## Before merging
    
    - [ ] tested locally and on preview environment (preview dev login:
    5de6)
    - [ ] updated [test
    cases](https://github.com/webstudio-is/webstudio-builder/blob/main/apps/builder/docs/test-cases.md)
    document
    - [ ] added tests
    - [ ] if any new env variables are added, added them to `.env.example`
    and the `builder/env-check.js` if mandatory

--- packages/react-sdk/src/components/link.tsx
+++ packages/react-sdk/src/components/link.tsx
@@ -1,9 +1,18 @@
 import { forwardRef, type ElementRef, type ComponentProps } from "react";
 
-type Props = Omit<ComponentProps<"a">, "href"> & { href?: string };
+// @todo props that come from remix link, shouldn't be here at all
+// - prefetch should be only on remix component and it already is
+// - props meta should be generated from Remix link
+type Props = Omit<ComponentProps<"a">, "href" | "target"> & {
+  href?: string;
+  target?: "self" | "blank" | "parent" | "top";
+  prefetch?: "none" | "intent" | "render";
+};
 
 export const Link = forwardRef<ElementRef<"a">, Props>(
-  ({ href = "", ...props }, ref) => <a {...props} href={href} ref={ref} />
+  ({ href = "", target = "self", ...props }, ref) => {
+    return <a {...props} target={`_${target}`} href={href} ref={ref} />;
+  }
 );
 
 Link.displayName = "Link";
