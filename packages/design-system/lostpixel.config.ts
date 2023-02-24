ccca55cca306281b217870ce2c0c446be9862e4a
Andras Bacsai <andras.bacsai@gmail.com>
2023-02-24 13:25:13 +0100

    update projectId

--- packages/design-system/lostpixel.config.ts
+++ packages/design-system/lostpixel.config.ts
@@ -4,6 +4,6 @@ export const config = {
   storybookShots: {
     storybookUrl: "packages/design-system/storybook-static",
   },
-  lostPixelProjectId: "clee4j8le039xgm0e6ba7sbfd",
+  lostPixelProjectId: "cleii5n4g0efjim0e69w6rme0",
   apiKey: process.env.LOST_PIXEL_API_KEY,
 };
