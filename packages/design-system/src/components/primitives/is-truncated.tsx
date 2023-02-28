40a974395e80cc1b95d096f639407559fb5c88cb
Roman Pominov <rpominov+github@gmail.com>
2023-02-28 16:26:21 +0400

    add useIsTruncated

---
+++ packages/design-system/src/components/primitives/is-truncated.tsx
@@ -0,0 +1,18 @@
+import { type RefObject, useState, useEffect } from "react";
+
+/**
+ * Checks whether text was truncated in an element with `text-overflow: ellipsis`
+ */
+export const useIsTruncated = (ref: RefObject<HTMLElement>, text: string) => {
+  const [isTruncated, setIsTruncated] = useState(false);
+
+  useEffect(() => {
+    if (ref.current) {
+      const { offsetWidth, scrollWidth } = ref.current;
+      // https://stackoverflow.com/a/10017343/478603
+      setIsTruncated(offsetWidth < scrollWidth);
+    }
+  }, [ref, text]);
+
+  return isTruncated;
+};
