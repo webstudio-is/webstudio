f2c8466b8443aee95da70a6713c7ee300fe7f1fc
Bogdan Chadkin <trysound@yandex.ru>
2023-02-28 18:10:06 +0300

    Add root instance id to page schema
    Ref https://github.com/webstudio-is/webstudio-builder/issues/1116
    
    Before we assumed the first instance in the list of instances is the
    root. Though we are gonna introduce slots and for this have to combine
    instances of all pages into single array. So there can be many roots in
    one list.
    
    So now we gonna specify root instance id on page. treeId will be removed
    once instances are migrated to Build table.

--- packages/project/src/shared/schema.ts
+++ packages/project/src/shared/schema.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 import type { Data } from "@webstudio-is/react-sdk";
-import type { Build, Page } from "@webstudio-is/project-build";
+import type { Build } from "@webstudio-is/project-build";
 
 const MIN_TITLE_LENGTH = 2;
 
@@ -25,7 +25,5 @@ export const Projects = z.array(Project);
 export type Projects = z.infer<typeof Projects>;
 
 export type CanvasData = Data & {
-  build: null | Build;
   buildId: Build["id"];
-  page: Page;
 };
