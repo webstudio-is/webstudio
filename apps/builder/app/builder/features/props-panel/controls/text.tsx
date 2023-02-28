527bde408f5eeb71d788fef69411bf01ebec5485
Roman Pominov <rpominov+github@gmail.com>
2023-02-28 16:26:56 +0400

    TextControl implementation

--- apps/builder/app/builder/features/props-panel/controls/text.tsx
+++ apps/builder/app/builder/features/props-panel/controls/text.tsx
@@ -1,6 +1,96 @@
-import { TrashIcon } from "@webstudio-is/icons";
-import { TextField, Label, Button, Flex } from "@webstudio-is/design-system";
-import { type ControlProps, getLabel } from "../shared";
+import {
+  TextField,
+  Flex,
+  theme,
+  useId,
+  TextArea,
+  Box,
+} from "@webstudio-is/design-system";
+import {
+  type ControlProps,
+  getLabel,
+  RemovePropButton,
+  Label,
+  useLocalValue,
+  DefaultControlLayout,
+} from "../shared";
+
+type ImplementationProps = {
+  label: string;
+  id: string;
+  value: string;
+  onChange: (value: string) => void;
+  onDelete?: () => void;
+};
+
+const AsInput = ({
+  label,
+  id,
+  value,
+  onChange,
+  onDelete,
+}: ImplementationProps) => {
+  const [localValue, setLocalValue] = useLocalValue(value);
+
+  return (
+    <Flex
+      css={{ height: theme.spacing[13] }}
+      justify="between"
+      align="center"
+      gap="2"
+    >
+      <Label htmlFor={id}>{label}</Label>
+      <Flex align="center" gap="2">
+        <TextField
+          id={id}
+          value={localValue}
+          onChange={(event) => setLocalValue(event.target.value)}
+          // @todo: shold we worry about unmout without blur and loosing the changes?
+          onBlur={() => {
+            if (localValue !== value) {
+              onChange(localValue);
+            }
+          }}
+          css={{ width: 120 }}
+        />
+        {onDelete && <RemovePropButton onClick={onDelete} />}
+      </Flex>
+    </Flex>
+  );
+};
+
+const TEXTAREA_LINE_HEIGHT = 16;
+
+const AsTextarea = ({
+  label,
+  id,
+  value,
+  onChange,
+  onDelete,
+  rows,
+}: ImplementationProps & { rows: number }) => {
+  const [localValue, setLocalValue] = useLocalValue(value);
+  const height = rows * TEXTAREA_LINE_HEIGHT;
+
+  return (
+    <DefaultControlLayout label={label} id={id} onDelete={onDelete}>
+      <Flex css={{ px: theme.spacing[2] }}>
+        <TextArea
+          id={id}
+          value={localValue}
+          onChange={(event) => setLocalValue(event.target.value)}
+          // @todo: shold we worry about unmout without blur and loosing the changes?
+          onBlur={() => {
+            if (localValue !== value) {
+              onChange(localValue);
+            }
+          }}
+          css={{ flexGrow: 1, height, minHeight: height }}
+        />
+      </Flex>
+    </DefaultControlLayout>
+  );
+};
 
 export const TextControl = ({
   meta,
@@ -8,26 +98,20 @@ export const TextControl = ({
   propName,
   onChange,
   onDelete,
-}: ControlProps<"text", "string">) => (
-  <div>
-    <Label>{getLabel(meta, propName)}</Label>
-    <Flex gap="1">
-      <TextField
-        name={propName}
-        value={prop?.value ?? ""}
-        onChange={(event) => {
-          onChange({ type: "string", value: event.target.value });
-        }}
-        css={{ flexGrow: 1 }}
-      />
-      {onDelete && (
-        <Button
-          color="ghost"
-          prefix={<TrashIcon />}
-          onClick={onDelete}
-          css={{ flexShrink: 1 }}
-        />
-      )}
-    </Flex>
-  </div>
-);
+}: ControlProps<"text", "string">) => {
+  const props: ImplementationProps = {
+    id: useId(),
+    label: getLabel(meta, propName),
+    value: prop?.value ?? "",
+    onChange(value) {
+      onChange({ type: "string", value });
+    },
+    onDelete,
+  };
+
+  return meta.rows === 0 ? (
+    <AsInput {...props} />
+  ) : (
+    <AsTextarea {...props} rows={meta.rows ?? 1} />
+  );
+};
--- apps/builder/app/builder/features/props-panel/shared.tsx
+++ apps/builder/app/builder/features/props-panel/shared.tsx
@@ -1,6 +1,21 @@
+import {
+  type ComponentPropsWithoutRef,
+  type ReactNode,
+  useRef,
+  useState,
+} from "react";
 import type { WsComponentPropsMeta } from "@webstudio-is/react-sdk";
 import type { Prop } from "@webstudio-is/project-build";
 import type { Asset } from "@webstudio-is/asset-uploader";
+import { SubtractIcon } from "@webstudio-is/icons";
+import {
+  SmallIconButton,
+  Label as BaseLabel,
+  useIsTruncated,
+  Tooltip,
+  Box,
+  Flex,
+} from "@webstudio-is/design-system";
 import { humanizeString } from "~/shared/string-utils";
 
 export type PropMeta = WsComponentPropsMeta["props"][string];
@@ -33,3 +48,63 @@ export type ControlProps<Control, PropType> = {
 
 export const getLabel = (meta: { label?: string }, fallback: string) =>
   meta.label || humanizeString(fallback);
+
+export const RemovePropButton = (props: { onClick: () => void }) => (
+  <SmallIconButton icon={<SubtractIcon />} {...props} />
+);
+
+export const Label = ({
+  children,
+  ...rest
+}: ComponentPropsWithoutRef<typeof BaseLabel> & { children: string }) => {
+  const ref = useRef<HTMLLabelElement>(null);
+  const truncated = useIsTruncated(ref, children);
+
+  const label = (
+    <BaseLabel truncate {...rest} ref={ref}>
+      {children}
+    </BaseLabel>
+  );
+
+  return truncated ? <Tooltip content={children}>{label}</Tooltip> : label;
+};
+
+/**
+ * Return `[localValue, setLocalValue]` where `localValue` contains:
+ *  - either the latest `savedValue`
+ *  - or the latest value set via `setLocalValue`
+ * (whichever changed most recently)
+ */
+export const useLocalValue = <Type,>(savedValue: Type) => {
+  const [localValue, setLocalValue] = useState(savedValue);
+
+  // Not using an effect to avoid re-rendering
+  // https://beta.reactjs.org/reference/react/useState#storing-information-from-previous-renders
+  const [previousSavedValue, setPreviousSavedValue] = useState(savedValue);
+  if (previousSavedValue !== savedValue) {
+    setLocalValue(savedValue);
+    setPreviousSavedValue(savedValue);
+  }
+
+  return [localValue, setLocalValue] as const;
+};
+
+export const DefaultControlLayout = ({
+  label,
+  id,
+  onDelete,
+  children,
+}: {
+  label: string;
+  id: string;
+  onDelete?: () => void;
+  children: ReactNode;
+}) => (
+  <Box>
+    <Flex align="center" gap="1" justify="between">
+      <Label htmlFor={id}>{label}</Label>
+      {onDelete && <RemovePropButton onClick={onDelete} />}
+    </Flex>
+    {children}
+  </Box>
+);
