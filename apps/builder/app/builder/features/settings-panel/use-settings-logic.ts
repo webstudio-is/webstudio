dc689f6f0e6be37961781b5ead8d018ba444d4d5
Oleg Isonen <oleg008@gmail.com>
2023-03-08 21:00:55 +0100

    make the settings update logic more robust

--- apps/builder/app/builder/features/settings-panel/use-settings-logic.ts
+++ apps/builder/app/builder/features/settings-panel/use-settings-logic.ts
@@ -7,28 +7,47 @@ import {
 } from "react";
 import { instancesStore, selectedInstanceStore } from "~/shared/nano-states";
 
-type SettingUpdate = { label?: string };
+type Setting = "label";
+type Value = string;
+
+type Operation =
+  | {
+      operation: "delete";
+      property: Setting;
+    }
+  | {
+      operation: "set";
+      property: Setting;
+      value: Value;
+    };
 
 export const useSettingsLogic = () => {
-  const changes = useRef<SettingUpdate>({});
+  const updates = useRef<Array<Operation>>([]);
 
-  const setLabel = (value: SettingUpdate[keyof SettingUpdate]) => {
-    // Empty string should be replaced with `undefined` so that we can render default label
-    changes.current.label = value || undefined;
+  const setLabel = (value?: Value) => {
+    if (value === undefined) {
+      updates.current.push({ operation: "delete", property: "label" });
+      return;
+    }
+    updates.current.push({ operation: "set", property: "label", value });
   };
 
   const updateLabel = useCallback(() => {
     const selectedInstance = selectedInstanceStore.get();
-    if (
-      selectedInstance === undefined ||
-      changes.current.label === selectedInstance.label
-    ) {
+    if (selectedInstance === undefined) {
       return;
     }
     store.createTransaction([instancesStore], (instances) => {
       const instance = instances.get(selectedInstance.id);
-      if (instance !== undefined) {
-        instance.label = changes.current.label;
+      if (instance === undefined) {
+        return;
+      }
+      for (const update of updates.current) {
+        if (update.operation === "delete") {
+          delete instance.label;
+          continue;
+        }
+        instance.label = update.value;
       }
     });
   }, []);
