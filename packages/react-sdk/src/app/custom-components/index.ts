b58440a4c784b15f3a62e2039501e244c3ae3425
Oleg Isonen <oleg008@gmail.com>
2023-03-09 22:02:15 +0100

    use remix link for link block

--- packages/react-sdk/src/app/custom-components/index.ts
+++ packages/react-sdk/src/app/custom-components/index.ts
@@ -1,5 +1,6 @@
 import { Image } from "./image";
 import { Link } from "./link";
+import { LinkBlock } from "./link-block";
 import { RichTextLink } from "./rich-text-link";
 import { imageProps } from "@webstudio-is/image";
 import type { WsComponentPropsMeta } from "../../components/component-type";
@@ -8,6 +9,7 @@ export const customComponents = {
   Image,
   Link,
   RichTextLink,
+  LinkBlock,
 };
 
 export const customComponentPropsMetas: Record<string, WsComponentPropsMeta> = {
