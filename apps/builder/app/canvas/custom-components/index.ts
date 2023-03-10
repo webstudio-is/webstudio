73f5f99dc29e2c90dd4f943223b7d0c3ac761f4a
Oleg Isonen <oleg008@gmail.com>
2023-03-10 12:17:19 +0000

    Use remix link for link block (#1217)
    ## Description
    
    Link block wasn't using remix link
    
    
    ## Code Review
    
    - [ ] hi @rpominov , I need you to do
      - conceptual review (architecture, feature-correctness)
      - detailed review (read every line)
      - test it on preview
    
    ## Before requesting a review
    
    - [ ] made a self-review
    - [ ] added inline comments where things may be not obvious (the "why",
    not "what")
    
    ## Before merging
    
    - [ ] tested locally and on preview environment (preview dev login:
    5de6)
    - [ ] updated [test
    cases](https://github.com/webstudio-is/webstudio-builder/blob/main/apps/builder/docs/test-cases.md)
    document
    - [ ] added tests
    - [ ] if any new env variables are added, added them to `.env.example`
    and the `builder/env-check.js` if mandatory

--- apps/builder/app/canvas/custom-components/index.ts
+++ apps/builder/app/canvas/custom-components/index.ts
@@ -4,5 +4,6 @@ import { wrapLinkComponent } from "./link";
 export const customComponents = {
   ...baseCustomComponents,
   Link: wrapLinkComponent(baseCustomComponents.Link),
+  LinkBlock: wrapLinkComponent(baseCustomComponents.LinkBlock),
   RichTextLink: wrapLinkComponent(baseCustomComponents.RichTextLink),
 };
