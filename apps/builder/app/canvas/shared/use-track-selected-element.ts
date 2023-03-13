1a7aba521183e7a1685e532f008144077f380c33
Bogdan Chadkin <trysound@yandex.ru>
2023-03-13 21:47:59 +0300

    Imropve instance selection
    "Click" based implemention had aa few important bugs.
    Selecting other instance was not possible when some text
    was selected in editing mode. This is consequence of a fix
    when other instance is selected user select text and move
    cursor outside of editor while still selecting.
    
    Here's how all solved. We track both mousedown and mouseup.
    When their targets are different we do nothing because this
    is most likely text selection or drag'n'drop.
    
    Editing state a little simplified by just looing for closest
    contenteditable instead of editing id comparison.
    
    Instance address now computed by traversing dom.

--- apps/builder/app/canvas/shared/use-track-selected-element.ts
+++ apps/builder/app/canvas/shared/use-track-selected-element.ts
@@ -1,106 +1,23 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { useStore } from "@nanostores/react";
 import {
-  instancesIndexStore,
-  instancesStore,
   selectedInstanceAddressStore,
-  useRootInstance,
-  useTextEditingInstanceId,
+  textEditingInstanceIdStore,
 } from "~/shared/nano-states";
-import { publish } from "~/shared/pubsub";
-import {
-  findClosestRichTextInstance,
-  getInstanceAddress,
-} from "~/shared/tree-utils";
-import { componentAttribute } from "@webstudio-is/react-sdk";
-
-declare module "~/shared/pubsub" {
-  export interface PubsubMap {
-    clickCanvas: undefined;
-  }
-}
-
-const eventOptions = {
-  passive: true,
-};
-
-const isHighlighting = () => getSelection()?.type === "Range";
 
 export const useTrackSelectedElement = () => {
   const selectedInstanceAddress = useStore(selectedInstanceAddressStore);
   const selectedInstanceId = selectedInstanceAddress?.[0];
-  const [editingInstanceId, setEditingInstanceId] = useTextEditingInstanceId();
-  const editingInstanceIdRef = useRef(editingInstanceId);
-  editingInstanceIdRef.current = editingInstanceId;
-  const [rootInstance] = useRootInstance();
 
+  // @todo disable based on user input instead of tracking selected
   useEffect(() => {
+    const editingInstanceId = textEditingInstanceIdStore.get();
     if (
-      editingInstanceIdRef.current !== undefined &&
+      editingInstanceId !== undefined &&
       // @todo compare instance addresses
-      selectedInstanceId !== editingInstanceIdRef.current
+      selectedInstanceId !== editingInstanceId
     ) {
-      setEditingInstanceId(undefined);
+      textEditingInstanceIdStore.set(undefined);
     }
-  }, [selectedInstanceId, setEditingInstanceId]);
-
-  useEffect(() => {
-    const handleClick = (event: MouseEvent) => {
-      // When user is selecting text inside content editable and mouse goes up
-      // on a different instance - we don't want to select a different instance
-      // because that would cancel the text selection.
-      if (isHighlighting()) {
-        return;
-      }
-      // Notify in general that document was clicked
-      // e.g. to hide the side panel
-      publish({ type: "clickCanvas" });
-      let element = event.target as HTMLElement;
-
-      // If we click on an element that is not a component, we search for a parent component.
-      if (element.dataset.wsComponent === undefined) {
-        const instanceElement = element.closest<HTMLElement>(
-          `[${componentAttribute}]`
-        );
-        if (instanceElement === null) {
-          return;
-        }
-        element = instanceElement;
-      }
-
-      // @todo find rendered instance address
-      const { id } = element;
-
-      // Enable clicking inside of content editable without trying to select the element as an instance.
-      if (editingInstanceIdRef.current === id) {
-        return;
-      }
-
-      // the second click in a double click.
-      if (event.detail === 2) {
-        const instancesIndex = instancesIndexStore.get();
-        // enable text editor when double click on its instance or one of its descendants
-        const richTextInstance = findClosestRichTextInstance(
-          instancesIndex,
-          id
-        );
-        if (richTextInstance) {
-          selectedInstanceAddressStore.set(
-            getInstanceAddress(instancesStore.get(), richTextInstance.id)
-          );
-          setEditingInstanceId(richTextInstance.id);
-        }
-        return;
-      }
-      selectedInstanceAddressStore.set(
-        getInstanceAddress(instancesStore.get(), id)
-      );
-    };
-
-    window.addEventListener("click", handleClick, eventOptions);
-
-    return () => {
-      window.removeEventListener("click", handleClick);
-    };
-  }, [setEditingInstanceId, rootInstance]);
+  }, [selectedInstanceId]);
 };
