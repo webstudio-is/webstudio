e19d3498ff10086701afca13dafe2b46e1b64995
Andras Bacsai <andras.bacsai@gmail.com>
2023-03-13 09:18:38 +0100

    fix key

--- apps/builder/lostpixel.config.ts
+++ apps/builder/lostpixel.config.ts
@@ -4,6 +4,6 @@ export const config = {
   storybookShots: {
     storybookUrl: "apps/builder/storybook-static",
   },
-  lostPixelProjectId: "cleii5n4g0efjim0e69w6rme0",
+  lostPixelProjectId: "cleiive6c0gchi40em3uzx1xv",
   apiKey: process.env.LOST_PIXEL_API_KEY,
 };
