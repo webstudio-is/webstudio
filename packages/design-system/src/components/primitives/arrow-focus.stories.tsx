475c32943df8bd724ae701f679793d9e8b075969
Roman Pominov <rpominov+github@gmail.com>
2023-03-17 12:11:46 +0400

    add storybook for arrow-focus

---
+++ packages/design-system/src/components/primitives/arrow-focus.stories.tsx
@@ -0,0 +1,141 @@
+import { StorySection } from "../storybook";
+import {
+  handleArrowFocus,
+  setArrowFocusRow,
+  setArrowFocusColumn,
+} from "./arrow-focus";
+
+const main = {
+  width: 160,
+  height: 30,
+  display: "inline-block",
+  textAlign: "left",
+} as const;
+
+const secondary = {
+  display: "inline-flex",
+  gap: 4,
+  marginLeft: -70,
+} as const;
+
+export const Demo = () => {
+  return (
+    <>
+      <StorySection title="Button with nested buttons">
+        <div onKeyDown={handleArrowFocus}>
+          <button style={main}>Main</button>
+          <div style={secondary}>
+            <button tabIndex={-1}>A</button>
+            <button tabIndex={-1}>B</button>
+          </div>
+        </div>
+      </StorySection>
+
+      <StorySection title="Input with nested buttons">
+        <div onKeyDown={handleArrowFocus}>
+          <input placeholder="Main" style={main} />
+          <div style={secondary}>
+            <button tabIndex={-1}>A</button>
+            <button tabIndex={-1}>B</button>
+          </div>
+        </div>
+      </StorySection>
+
+      <StorySection title="List">
+        <ul onKeyDown={handleArrowFocus}>
+          <li>
+            <button>First</button>
+          </li>
+          <li>
+            <button tabIndex={-1}>Second</button>
+          </li>
+          <li>
+            <button tabIndex={-1}>Third</button>
+          </li>
+          <li>
+            <button tabIndex={-1}>Fourth</button>
+          </li>
+        </ul>
+      </StorySection>
+
+      <StorySection title="List with nested controls">
+        <ul onKeyDown={handleArrowFocus}>
+          <li>
+            <button {...setArrowFocusRow("1")} style={main}>
+              First
+            </button>
+            <div style={secondary}>
+              <button {...setArrowFocusRow("1")} tabIndex={-1}>
+                A
+              </button>
+              <button {...setArrowFocusRow("1")} tabIndex={-1}>
+                B
+              </button>
+            </div>
+          </li>
+          <li>
+            <button {...setArrowFocusRow("2")} tabIndex={-1} style={main}>
+              Second
+            </button>
+            <div style={secondary}>
+              <button {...setArrowFocusRow("2")} tabIndex={-1}>
+                A
+              </button>
+              <button {...setArrowFocusRow("2")} tabIndex={-1}>
+                B
+              </button>
+            </div>
+          </li>
+          <li>
+            <button {...setArrowFocusRow("3")} tabIndex={-1} style={main}>
+              Third
+            </button>
+            <div style={secondary}>
+              <button {...setArrowFocusRow("3")} tabIndex={-1}>
+                A
+              </button>
+              <button {...setArrowFocusRow("3")} tabIndex={-1}>
+                B
+              </button>
+            </div>
+          </li>
+          <li>
+            <button {...setArrowFocusRow("4")} tabIndex={-1} style={main}>
+              Fourth
+            </button>
+            <div style={secondary}>
+              <button {...setArrowFocusRow("4")} tabIndex={-1}>
+                A
+              </button>
+              <button {...setArrowFocusRow("4")} tabIndex={-1}>
+                B
+              </button>
+            </div>
+          </li>
+        </ul>
+      </StorySection>
+
+      <StorySection title="Grid">
+        <ul onKeyDown={handleArrowFocus}>
+          {["A", "B", "C"].map((row) => (
+            <li key={row}>
+              {["1", "2", "3"].map((column) => (
+                <button
+                  {...setArrowFocusRow(row)}
+                  {...setArrowFocusColumn(column)}
+                  key={column}
+                  tabIndex={row === "B" && column === "2" ? 0 : -1}
+                >
+                  {row}
+                  {column}
+                </button>
+              ))}
+            </li>
+          ))}
+        </ul>
+      </StorySection>
+    </>
+  );
+};
+
+export default { component: Demo };
