7f6310039e2f6e7bcaddd0e26fd640dc04724eb1
Bogdan Chadkin <trysound@yandex.ru>
2023-03-23 01:37:03 +0700

    Remove root check from navigator

--- apps/builder/app/builder/features/sidebar-left/navigator/navigator.tsx
+++ apps/builder/app/builder/features/sidebar-left/navigator/navigator.tsx
@@ -1,6 +1,4 @@
-import { useStore } from "@nanostores/react";
 import { Flex } from "@webstudio-is/design-system";
-import { rootInstanceStore } from "~/shared/nano-states";
 import { NavigatorTree } from "~/builder/shared/navigator-tree";
 import { Header, CloseButton } from "../header";
 
@@ -10,11 +8,6 @@ type NavigatorProps = {
 };
 
 export const Navigator = ({ isClosable, onClose }: NavigatorProps) => {
-  const rootInstance = useStore(rootInstanceStore);
-
-  if (rootInstance === undefined) {
-    return null;
-  }
   return (
     <Flex css={{ height: "100%", flexDirection: "column" }}>
       <Header
