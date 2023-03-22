51f3230b84223b46329fe60f96112873a2fa51ab
Oleg Isonen <oleg008@gmail.com>
2023-03-22 19:17:01 +0100

    Component card (#1275)
    ## Description
    
    
    https://www.figma.com/file/xCBegXEWxROLqA1Y31z2Xo/%F0%9F%93%96-Webstudio-Design-Docs?node-id=3217-86010&t=Muy6FruCcEJ5xYIj-4
    
    Card component for adding components to the canvas
    
    <img width="552" alt="Screenshot 2023-03-22 at 18 44 41"
    src="https://user-images.githubusercontent.com/52824/226992472-54c71cc5-ab19-4344-9589-04233c5c0ebd.png">
    
    
    ## Code Review
    
    - [ ] hi @rpominov , I need you to do
      - conceptual review (architecture, feature-correctness)
      - detailed review (read every line)
      - test it on preview
    
    ## Before requesting a review
    
    - [ ] made a self-review
    - [ ] added inline comments where things may be not obvious (the "why",
    not "what")
    
    ## Before merging
    
    - [ ] tested locally and on preview environment (preview dev login:
    5de6)
    - [ ] updated [test
    cases](https://github.com/webstudio-is/webstudio-builder/blob/main/apps/builder/docs/test-cases.md)
    document
    - [ ] added tests
    - [ ] if any new env variables are added, added them to `.env.example`
    and the `builder/env-check.js` if mandatory

---
+++ packages/design-system/src/components/component-card.tsx
@@ -0,0 +1,85 @@
+/**
+ * Implementation of the "Component Card" component from:
+ * https://www.figma.com/file/sfCE7iLS0k25qCxiifQNLE/%F0%9F%93%9A-Webstudio-Library?node-id=2608-8921
+ */
+
+import { forwardRef, type ElementRef, ComponentProps } from "react";
+import { textVariants } from "../";
+import { css, theme } from "../stitches.config";
+
+const cardStyle = css({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  textAlign: "center",
+  px: theme.spacing[3],
+  width: theme.spacing[19],
+  height: theme.spacing[19],
+  border: `1px solid`,
+  borderColor: theme.colors.borderMain,
+  borderRadius: theme.borderRadius[2],
+  userSelect: "none",
+  color: theme.colors.foregroundIconMain,
+  cursor: "grab",
+  background: theme.colors.backgroundPanel,
+  "&:hover, &[data-state=hover]": {
+    background: theme.colors.backgroundHover,
+  },
+  "&[data-state=disabled]": {
+    background: theme.colors.backgroundPanel,
+    color: theme.colors.foregroundDisabled,
+  },
+  "&:focus-visible, &[data-state=focus]": {
+    outline: `2px solid ${theme.colors.borderFocus}`,
+    outlineOffset: "-2px",
+  },
+  "& svg": {
+    flexGrow: 0,
+    marginTop: theme.spacing[7],
+    width: 22,
+    height: 22,
+  },
+});
+
+const textContainerStyle = css({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  flexGrow: 1,
+  width: "100%",
+});
+
+const textStyle = css(textVariants.small, {
+  display: "-webkit-box",
+  WebkitLineClamp: 2,
+  WebkitBoxOrient: "vertical",
+  overflow: "hidden",
+  textOverflow: "ellipsis",
+});
+
+type ComponentCardProps = {
+  label: string;
+  icon: JSX.Element;
+  state?: "hover" | "disabled" | "focus";
+} & ComponentProps<"div">;
+
+export const ComponentCard = forwardRef<ElementRef<"div">, ComponentCardProps>(
+  ({ icon, label, className, state, ...props }, ref) => {
+    return (
+      <div
+        className={cardStyle({ className })}
+        ref={ref}
+        data-state={state}
+        {...props}
+      >
+        {icon}
+
+        <div className={textContainerStyle()}>
+          <div className={textStyle()}>{label}</div>
+        </div>
+      </div>
+    );
+  }
+);
+
+ComponentCard.displayName = "ComponentCard";
