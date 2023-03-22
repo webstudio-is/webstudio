9d9ddb254c253368195a49f9998746b9ef100d5a
istarkov <istarkov@gmail.com>
2023-03-22 13:22:13 +0400

    Fix icons

--- packages/icons/src/__generated__/border-radius.tsx
+++ packages/icons/src/__generated__/border-radius.tsx
@@ -7,7 +7,7 @@ import type { IconProps } from "../types";
 export const BorderRadiusIcon = forwardRef<SVGSVGElement, IconProps>(
   ({ color = "currentColor", size = 16, ...props }, forwardedRef) => {
     return (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width={size} height={size} fill={color} {...props} ref={forwardedRef}><path d="M1.917 10.666a8.75 8.75 0 0 1 8.75-8.75h2.666a.75.75 0 0 1 0 1.5h-2.667a7.25 7.25 0 0 0-7.25 7.25v2.667a.75.75 0 0 1-1.5 0v-2.667Z" clipRule="evenodd" /></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width={size} height={size} fill={color} {...props} ref={forwardedRef}><path fillRule="evenodd" d="M1.917 10.666a8.75 8.75 0 0 1 8.75-8.75h2.666a.75.75 0 0 1 0 1.5h-2.667a7.25 7.25 0 0 0-7.25 7.25v2.667a.75.75 0 0 1-1.5 0v-2.667Z" clipRule="evenodd" /></svg>
     );
   }
 );
--- packages/icons/src/__generated__/border-width-individual.tsx
+++ packages/icons/src/__generated__/border-width-individual.tsx
@@ -7,7 +7,7 @@ import type { IconProps } from "../types";
 export const BorderWidthIndividualIcon = forwardRef<SVGSVGElement, IconProps>(
   ({ color = "currentColor", size = 16, ...props }, forwardedRef) => {
     return (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width={size} height={size} fill={color} {...props} ref={forwardedRef}><path d="M4.684 2a.65.65 0 0 1 .65-.65h5.332a.65.65 0 1 1 0 1.3H5.334a.65.65 0 0 1-.65-.65ZM14 4.683a.65.65 0 0 1 .65.65v5.334a.65.65 0 1 1-1.3 0V5.333a.65.65 0 0 1 .65-.65Zm-12 0a.65.65 0 0 1 .65.65v5.334a.65.65 0 0 1-1.3 0V5.333a.65.65 0 0 1 .65-.65ZM4.684 14a.65.65 0 0 1 .65-.65h5.332a.65.65 0 1 1 0 1.3H5.334a.65.65 0 0 1-.65-.65Z" clipRule="evenodd" /></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width={size} height={size} fill={color} {...props} ref={forwardedRef}><path fillRule="evenodd" d="M4.684 2a.65.65 0 0 1 .65-.65h5.332a.65.65 0 1 1 0 1.3H5.334a.65.65 0 0 1-.65-.65ZM14 4.683a.65.65 0 0 1 .65.65v5.334a.65.65 0 1 1-1.3 0V5.333a.65.65 0 0 1 .65-.65Zm-12 0a.65.65 0 0 1 .65.65v5.334a.65.65 0 0 1-1.3 0V5.333a.65.65 0 0 1 .65-.65ZM4.684 14a.65.65 0 0 1 .65-.65h5.332a.65.65 0 1 1 0 1.3H5.334a.65.65 0 0 1-.65-.65Z" clipRule="evenodd" /></svg>
     );
   }
 );
