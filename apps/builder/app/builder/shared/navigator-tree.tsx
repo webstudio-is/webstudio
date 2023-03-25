2769ccfc724ae30af8fe7651be5df5c4821da9f5
Bogdan Chadkin <trysound@yandex.ru>
2023-03-26 01:07:51 +0700

    Fix lint

--- apps/builder/app/builder/shared/navigator-tree.tsx
+++ apps/builder/app/builder/shared/navigator-tree.tsx
@@ -31,7 +31,6 @@ export const NavigatorTree = () => {
         position: payload.dropTarget.position,
       });
       setState({ isDragging: false });
-      console.log(payload);
     },
     []
   );
