169ead76353927ebc5c8ab261fb996836c57c3f9
Bogdan Chadkin <trysound@yandex.ru>
2023-03-27 01:11:22 +0700

    Compute bounding box by children when display: contents
    Slots have display: contents to not affect layout.
    So we need a way to show correct hover and selected state.
    Here's added utility to traverse all descendants and get their rects and
    then compute the sum of those rects.

--- apps/builder/app/shared/dom-utils.ts
+++ apps/builder/app/shared/dom-utils.ts
@@ -36,3 +36,53 @@ export const getElementByInstanceSelector = (
     .join(" ");
   return document.querySelector(domSelector) ?? undefined;
 };
+
+type Rect = {
+  top: number;
+  right: number;
+  bottom: number;
+  left: number;
+};
+
+const sumRects = (first: Rect, second: Rect) => {
+  return {
+    top: Math.min(first.top, second.top),
+    right: Math.max(first.right, second.right),
+    bottom: Math.max(first.bottom, second.bottom),
+    left: Math.min(first.left, second.left),
+  };
+};
+
+export const getAllElementsBoundingBox = (element: Element): DOMRect => {
+  const rect = element.getBoundingClientRect();
+  if (element.children.length === 0) {
+    return rect;
+  }
+  // possible display: contents
+  if (rect.width !== 0 || rect.height !== 0) {
+    return rect;
+  }
+
+  const rects: Rect[] = [];
+
+  for (const child of element.children) {
+    const childRect = getAllElementsBoundingBox(child);
+    // possible display: contents
+    if (childRect.width !== 0 || childRect.height !== 0) {
+      const { top, right, bottom, left } = childRect;
+      rects.push({ top, right, bottom, left });
+    }
+  }
+
+  if (rects.length === 0) {
+    return rect;
+  }
+
+  const { top, right, bottom, left } = rects.reduce(sumRects);
+  return DOMRect.fromRect({
+    x: left,
+    y: top,
+    height: bottom - top,
+    width: right - left,
+  });
+};
--- packages/feature-flags/src/flags.ts
+++ packages/feature-flags/src/flags.ts
@@ -2,3 +2,4 @@ export const example = false;
 export const dark = false;
 export const propertyReset = false;
 export const unsupportedBrowsers = false;
+export const displayContents = false;
