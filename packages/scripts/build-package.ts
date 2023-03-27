5ba50e441b4acb1f2a69b66e4924959d628d3c8e
Bogdan Chadkin <trysound@yandex.ru>
2023-03-27 12:05:50 +0700

    Upgrade esbuild
    - got rid of unused esbuild-node-externals package
    - removed esbuild override/resolution to old version
    - deduplicateed webpack 4 and 5 into single 4 instance
    - upgraded esbuild to the latest version with "context" api instead of
      incremental flag which potentially forces to fix the issue with remix
      once they upgrade
    
    Esbuild bump fixed the issue with remix going infinite watch loop after
    any changes in packages and looks like esbuild in remix no longer blocks
    paralllel turbo run!!!

--- packages/scripts/build-package.ts
+++ packages/scripts/build-package.ts
@@ -3,7 +3,7 @@
 import { rm, cp } from "node:fs/promises";
 import { join } from "node:path";
 import { totalist } from "totalist";
-import { build } from "esbuild";
+import { build, context } from "esbuild";
 
 const args = process.argv.slice(2);
 const watch = args.includes("--watch");
@@ -25,30 +25,30 @@ await totalist("./src", (rel) => {
 
 await rm("lib", { recursive: true, force: true });
 
-await build({
+const esmConfig = {
   entryPoints,
   outdir: "lib",
   format: "esm",
-  watch: watch && {
-    onRebuild(error) {
-      if (error) {
-        console.error("watch build failed:", error);
-      } else {
-        console.info("watch build succeeded");
-      }
-    },
-  },
-});
+} as const;
 
-await build({
+const cjsConfig = {
   entryPoints,
   outdir: "lib/cjs",
   format: "cjs",
   outExtension: {
     ".js": ".cjs",
   },
-  watch,
-});
+} as const;
+
+if (watch) {
+  const esmContext = await context(esmConfig);
+  await esmContext.watch();
+  const cjsContext = await context(cjsConfig);
+  await cjsContext.watch();
+} else {
+  await build(esmConfig);
+  await build(cjsConfig);
+}
 
 for (const rel of assets) {
   await cp(join("src", rel), join("lib", rel));
