b3503747041098a11d284711f7052e5db9e06aa9
istarkov <istarkov@gmail.com>
2023-03-28 19:09:12 +0400

    Add useEffect

--- apps/builder/app/shared/hook-utils/use-sync-initialize-once.ts
+++ apps/builder/app/shared/hook-utils/use-sync-initialize-once.ts
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 // This hook allows to
 // 1. run a function only once on the client
@@ -8,9 +8,13 @@ import { useRef } from "react";
 // Can't use useState initializer because it can be called twice in strict mode and also isn't designed for this.
 export const useSyncInitializeOnce = (fn: () => void) => {
   const ref = useRef(false);
-  if (ref.current) {
-    return;
-  }
-  ref.current = true;
-  fn();
+
+  useEffect(() => {
+    if (ref.current) {
+      return;
+    }
+    ref.current = true;
+
+    fn();
+  }, [fn]);
 };
