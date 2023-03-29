2618d47e2743fd7888f25c7b25e93d4dbb63f888
Roman Pominov <rpominov+github@gmail.com>
2023-03-29 13:21:53 +0400

    add StyleCollapsibleSection

---
+++ apps/builder/app/builder/features/style-panel/shared/style-collapsible-section.tsx
@@ -0,0 +1,36 @@
+import { SectionTitle, SectionTitleLabel } from "@webstudio-is/design-system";
+import type { ReactNode } from "react";
+import {
+  CollapsibleSectionBase,
+  useOpenState,
+} from "~/builder/shared/collapsible-section";
+import type { StyleSource } from "./style-info";
+
+export const StyleCollapsibleSection = (props: {
+  label: string;
+  children: ReactNode;
+  sources: StyleSource[];
+  isOpen?: boolean;
+}) => {
+  const { label, children, sources } = props;
+  const [isOpen, setIsOpen] = useOpenState(props);
+
+  return (
+    <CollapsibleSectionBase
+      label={label}
+      isOpen={isOpen}
+      onOpenChange={setIsOpen}
+      trigger={
+        <SectionTitle
+          dots={sources.flatMap((source) =>
+            source === "local" || source === "remote" ? [source] : []
+          )}
+        >
+          <SectionTitleLabel>{label}</SectionTitleLabel>
+        </SectionTitle>
+      }
+    >
+      {children}
+    </CollapsibleSectionBase>
+  );
+};
