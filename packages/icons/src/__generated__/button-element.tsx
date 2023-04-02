14e466dd67b31e396b34e4a61d739ca7fbe81e15
Bogdan Chadkin <trysound@yandex.ru>
2023-04-02 18:40:10 +0700

    Update components icons
    Added icons from figma for components panel
    
    See diff for new icons

---
+++ packages/icons/src/__generated__/button-element.tsx
@@ -0,0 +1,15 @@
+// Generated from icons/button-element.svg
+
+import { forwardRef } from "react";
+import type { IconProps } from "../types";
+
+// prettier-ignore
+export const ButtonElementIcon = forwardRef<SVGSVGElement, IconProps>(
+  ({ color = "currentColor", size = 16, ...props }, forwardedRef) => {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width={size} height={size} fill={color} {...props} ref={forwardedRef}><path fillRule="evenodd" d="M.683 7.474c0-1.85 1.5-3.35 3.35-3.35h7.934c1.85 0 3.35 1.5 3.35 3.35v1.3c0 1.85-1.5 3.35-3.35 3.35H4.033a3.35 3.35 0 0 1-3.35-3.35v-1.3Zm3.35-2.05a2.05 2.05 0 0 0-2.05 2.05v1.3a2.05 2.05 0 0 0 2.05 2.05h7.934a2.05 2.05 0 0 0 2.05-2.05v-1.3a2.05 2.05 0 0 0-2.05-2.05H4.033Zm-.183 2.7a.817.817 0 1 1 1.633 0 .817.817 0 0 1-1.633 0Zm3.333 0a.817.817 0 1 1 1.634 0 .817.817 0 0 1-1.634 0Zm3.334 0a.817.817 0 1 1 1.633 0 .817.817 0 0 1-1.633 0Z" clipRule="evenodd" /></svg>
+    );
+  }
+);
+
+ButtonElementIcon.displayName = "ButtonElementIcon";
---
+++ packages/icons/src/__generated__/form-text-field.tsx
@@ -0,0 +1,15 @@
+// Generated from icons/form-text-field.svg
+
+import { forwardRef } from "react";
+import type { IconProps } from "../types";
+
+// prettier-ignore
+export const FormTextFieldIcon = forwardRef<SVGSVGElement, IconProps>(
+  ({ color = "currentColor", size = 16, ...props }, forwardedRef) => {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width={size} height={size} fill={color} {...props} ref={forwardedRef}><path fillRule="evenodd" d="M1.35 4.66c0-.723.586-1.31 1.31-1.31h10.68c.723 0 1.31.587 1.31 1.31v6.68a1.31 1.31 0 0 1-1.31 1.31H2.66a1.31 1.31 0 0 1-1.31-1.31V4.66Zm1.3-.004V11.344a.012.012 0 0 0 .006.006H13.344l.003-.003a.011.011 0 0 0 .002-.003V4.656a.01.01 0 0 0-.002-.003.01.01 0 0 0-.003-.002H2.656s-.002 0-.003.002a.011.011 0 0 0-.003.003Zm2.017 1.36a.65.65 0 0 1 .65.65v2.67a.65.65 0 0 1-1.3 0v-2.67a.65.65 0 0 1 .65-.65Z" clipRule="evenodd" /></svg>
+    );
+  }
+);
+
+FormTextFieldIcon.displayName = "FormTextFieldIcon";
