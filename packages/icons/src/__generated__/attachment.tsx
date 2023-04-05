2a1fbfbb3758cd79ee33efd03ea0ad5f50594084
Roman Pominov <rpominov+github@gmail.com>
2023-04-05 12:28:14 +0400

    add icons

---
+++ packages/icons/src/__generated__/attachment.tsx
@@ -0,0 +1,15 @@
+// Generated from icons/attachment.svg
+
+import { forwardRef } from "react";
+import type { IconProps } from "../types";
+
+// prettier-ignore
+export const AttachmentIcon = forwardRef<SVGSVGElement, IconProps>(
+  ({ color = "currentColor", size = 16, ...props }, forwardedRef) => {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width={size} height={size} fill={color} {...props} ref={forwardedRef}><path fillRule="evenodd" d="M9.13 1.842a2.415 2.415 0 0 1 3.415 3.415l-5.889 5.888a.65.65 0 1 1-.919-.919l5.889-5.888a1.115 1.115 0 1 0-1.577-1.577L4.161 8.65a2.88 2.88 0 0 0 4.072 4.072l4.64-4.641a.65.65 0 1 1 .92.92l-4.641 4.64a4.18 4.18 0 1 1-5.91-5.91L9.13 1.84Z" clipRule="evenodd" /></svg>
+    );
+  }
+);
+
+AttachmentIcon.displayName = "AttachmentIcon";
