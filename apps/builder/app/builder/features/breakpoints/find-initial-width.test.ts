a4974ff179fe3b050bfa6d6a28bc34eae0e11918
Oleg Isonen <oleg008@gmail.com>
2023-04-08 21:27:39 +0200

    use 320px default width when the selected breakpoint has min-width 0

--- apps/builder/app/builder/features/breakpoints/find-initial-width.test.ts
+++ apps/builder/app/builder/features/breakpoints/find-initial-width.test.ts
@@ -1,7 +1,7 @@
 import { describe, test, expect } from "@jest/globals";
 import { findInitialWidth } from "./find-initial-width";
 
-const fallbackWidth = 1000;
+const workspaceWidth = 1000;
 
 const breakpoints = [
   { id: "0", label: "Base" },
@@ -15,32 +15,53 @@ const breakpoints = [
 describe("Find initial width", () => {
   test("base", () => {
     expect(
-      findInitialWidth(breakpoints, breakpoints[0], fallbackWidth)
-    ).toStrictEqual(fallbackWidth);
+      findInitialWidth(breakpoints, breakpoints[0], workspaceWidth)
+    ).toStrictEqual(workspaceWidth);
   });
   test("tablet", () => {
     expect(
-      findInitialWidth(breakpoints, breakpoints[1], fallbackWidth)
+      findInitialWidth(breakpoints, breakpoints[1], workspaceWidth)
     ).toStrictEqual(768);
   });
   test("mobile landscape", () => {
     expect(
-      findInitialWidth(breakpoints, breakpoints[2], fallbackWidth)
+      findInitialWidth(breakpoints, breakpoints[2], workspaceWidth)
     ).toStrictEqual(480);
   });
   test("mobile portrait", () => {
     expect(
-      findInitialWidth(breakpoints, breakpoints[3], fallbackWidth)
+      findInitialWidth(breakpoints, breakpoints[3], workspaceWidth)
     ).toStrictEqual(320);
   });
   test("large", () => {
     expect(
-      findInitialWidth(breakpoints, breakpoints[4], fallbackWidth)
+      findInitialWidth(breakpoints, breakpoints[4], workspaceWidth)
     ).toStrictEqual(1280);
   });
   test("extra large", () => {
     expect(
-      findInitialWidth(breakpoints, breakpoints[5], fallbackWidth)
+      findInitialWidth(breakpoints, breakpoints[5], workspaceWidth)
     ).toStrictEqual(1440);
+  });
+
+  test("0 min-width", () => {
+    const breakpoints = [{ id: "0", label: "x", minWidth: 0 }];
+    expect(
+      findInitialWidth(breakpoints, breakpoints[0], workspaceWidth)
+    ).toStrictEqual(320);
+  });
+
+  test("low min-width", () => {
+    const breakpoints = [{ id: "0", label: "x", minWidth: 123 }];
+    expect(
+      findInitialWidth(breakpoints, breakpoints[0], workspaceWidth)
+    ).toStrictEqual(123);
+  });
+
+  test("no previous max-width", () => {
+    const breakpoints = [{ id: "0", label: "x", maxWidth: 100 }];
+    expect(
+      findInitialWidth(breakpoints, breakpoints[0], workspaceWidth)
+    ).toStrictEqual(320);
   });
 });
--- apps/builder/app/builder/features/breakpoints/find-initial-width.ts
+++ apps/builder/app/builder/features/breakpoints/find-initial-width.ts
@@ -1,22 +1,28 @@
 import { compareMedia } from "@webstudio-is/css-engine";
 import type { Breakpoint } from "@webstudio-is/project-build";
 
+const defaultWidth = 320;
+
 // Find a canvas width that is within the selected breakpoint's range, but is at it's minimum.
 // Goal is to allow user to consistently know the direction they want to resize to after they switched to a breakpoint.
 // In this case they will want to always increase the width after switching.
 export const findInitialWidth = (
   breakpoints: Array<Breakpoint>,
   selectedBreakpoint: Breakpoint,
-  fallbackWidth: number
+  workspaceWidth: number
 ) => {
+  // When on a base breakpoint, we want to show maximum width canvas
   if (
     selectedBreakpoint.minWidth === undefined &&
     selectedBreakpoint.maxWidth === undefined
   ) {
-    return fallbackWidth;
+    return workspaceWidth;
   }
 
   if (selectedBreakpoint.minWidth !== undefined) {
+    if (selectedBreakpoint.minWidth === 0) {
+      return defaultWidth;
+    }
     return selectedBreakpoint.minWidth;
   }
 
@@ -28,6 +34,9 @@ export const findInitialWidth = (
   const index = sorted.findIndex(
     (breakpoint) => breakpoint.id === selectedBreakpoint.id
   );
-
-  return Math.max(320, (sorted[index - 1]?.maxWidth ?? 0) + 1);
+  const previousBreakpointMaxWidth = sorted[index - 1]?.maxWidth;
+  if (previousBreakpointMaxWidth === undefined) {
+    return defaultWidth;
+  }
+  return previousBreakpointMaxWidth + 1;
 };
