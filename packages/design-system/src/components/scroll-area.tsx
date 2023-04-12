4c22d4960f2a0b179b2ea41f2dbf85c8e5f28f70
Oleg Isonen <oleg008@gmail.com>
2023-04-12 18:45:58 +0200

    Align scrollbars with new design (#1427)
    ## Description
    
    This aligns scrollbars with new design
    
    ## Steps for reproduction
    
    1. scroll add components panel
    2. window needs to be small
    
    ## Code Review
    
    - [ ] hi @kof, I need you to do
      - conceptual review (architecture, feature-correctness)
      - detailed review (read every line)
      - test it on preview
    
    ## Before requesting a review
    
    - [ ] made a self-review
    - [ ] added inline comments where things may be not obvious (the "why",
    not "what")
    
    ## Before merging
    
    - [ ] tested locally and on preview environment (preview dev login:
    5de6)
    - [ ] updated [test
    cases](https://github.com/webstudio-is/webstudio-builder/blob/main/apps/builder/docs/test-cases.md)
    document
    - [ ] added tests
    - [ ] if any new env variables are added, added them to `.env.example`
    and the `builder/env-check.js` if mandatory

--- packages/design-system/src/components/scroll-area.tsx
+++ packages/design-system/src/components/scroll-area.tsx
@@ -1,7 +1,5 @@
-import { styled } from "../stitches.config";
+import { styled, theme } from "../stitches.config";
 import { Root, Viewport, Scrollbar, Thumb } from "@radix-ui/react-scroll-area";
-
-const SCROLLBAR_SIZE = 15;
 
 const ScrollAreaRoot = styled(Root, {
   boxSizing: "border-box",
@@ -17,9 +15,8 @@ const ScrollAreaViewport = styled(Viewport, {
 
 const ScrollAreaThumb = styled(Thumb, {
   boxSizing: "border-box",
-  flex: 1,
-  background: "rgba(0, 0, 0, 0.5)",
-  borderRadius: 10,
+  background: theme.colors.foregroundMoreSubtle,
+  borderRadius: theme.spacing[4],
   // increase target size for touch devices https://www.w3.org/WAI/WCAG21/Understanding/target-size.html
   position: "relative",
   "&::before": {
@@ -37,26 +34,18 @@ const ScrollAreaThumb = styled(Thumb, {
 
 const ScrollAreaScrollbar = styled(Scrollbar, {
   boxSizing: "border-box",
-  display: "flex",
   // ensures no selection
   userSelect: "none",
   // disable browser handling of all panning and scaleUp gestures on touch devices
   touchAction: "none",
-  paddingRight: 2,
-  paddingLeft: 6,
-  background: "rgba(248, 248, 248, 0.76)",
-  transition: "all 160ms ease-out",
-  "&:hover": {
-    paddingRight: 2,
-    paddingLeft: 2,
-    borderLeft: `1px solid #e8e8e8`,
-  },
+  paddingRight: theme.spacing[2],
+  paddingLeft: theme.spacing[2],
   '&[data-orientation="vertical"]': {
-    width: SCROLLBAR_SIZE,
+    width: theme.spacing[6],
   },
   '&[data-orientation="horizontal"]': {
     flexDirection: "column",
-    height: SCROLLBAR_SIZE,
+    height: theme.spacing[6],
   },
 });
 
@@ -64,7 +53,7 @@ type ScrollAreaProps = { children: JSX.Element | Array<JSX.Element> };
 
 export const ScrollArea = ({ children }: ScrollAreaProps) => {
   return (
-    <ScrollAreaRoot>
+    <ScrollAreaRoot scrollHideDelay={0}>
       <ScrollAreaViewport>{children}</ScrollAreaViewport>
       <ScrollAreaScrollbar orientation="vertical">
         <ScrollAreaThumb />
