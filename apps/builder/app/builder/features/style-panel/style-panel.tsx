bcd615022b23fc548c1995bbcdf081c099f4c399
Ivan Starkov <istarkov@gmail.com>
2023-04-13 01:22:46 +0400

    Remove useless warning (#1435)
    ## Description
    
    No longer needed, as impossible to set width less than allowed
    
    ## Steps for reproduction
    
    1. click button
    2. expect xyz
    
    ## Code Review
    
    - [ ] hi @kof, I need you to do
      - conceptual review (architecture, feature-correctness)
      - detailed review (read every line)
      - test it on preview
    
    ## Before requesting a review
    
    - [ ] made a self-review
    - [ ] added inline comments where things may be not obvious (the "why",
    not "what")
    
    ## Before merging
    
    - [ ] tested locally and on preview environment (preview dev login:
    5de6)
    - [ ] updated [test
    cases](https://github.com/webstudio-is/webstudio-builder/blob/main/apps/builder/docs/test-cases.md)
    document
    - [ ] added tests
    - [ ] if any new env variables are added, added them to `.env.example`
    and the `builder/env-check.js` if mandatory

--- apps/builder/app/builder/features/style-panel/style-panel.tsx
+++ apps/builder/app/builder/features/style-panel/style-panel.tsx
@@ -1,50 +1,15 @@
-import { useStore } from "@nanostores/react";
-import {
-  theme,
-  Box,
-  Card,
-  DeprecatedParagraph,
-  ScrollArea,
-} from "@webstudio-is/design-system";
+import { theme, Box, ScrollArea } from "@webstudio-is/design-system";
 import type { Instance } from "@webstudio-is/project-build";
 import type { Publish } from "~/shared/pubsub";
-import { selectedBreakpointStore } from "~/shared/nano-states";
+
 import { useStyleData } from "./shared/use-style-data";
 import { StyleSettings } from "./style-settings";
-import { canvasWidthContainer } from "~/builder/shared/nano-states";
+
 import { StyleSourcesSection } from "./style-source-section";
-import { matchMedia } from "@webstudio-is/css-engine";
-import { useEffect, useState } from "react";
 
 type StylePanelProps = {
   publish: Publish;
   selectedInstance: Instance;
-};
-
-const useMatchMedia = () => {
-  const [matched, setMatched] = useState(false);
-
-  useEffect(() => {
-    return canvasWidthContainer.subscribe((canvasWidth) => {
-      const breakpoint = selectedBreakpointStore.get();
-      if (breakpoint === undefined) {
-        return;
-      }
-
-      if (canvasWidth === undefined) {
-        return;
-      }
-
-      if (matchMedia(breakpoint, canvasWidth) === false) {
-        setMatched(true);
-        return;
-      }
-
-      setMatched(false);
-    });
-  }, []);
-
-  return matched;
 };
 
 export const StylePanel = ({ selectedInstance, publish }: StylePanelProps) => {
@@ -54,35 +19,8 @@ export const StylePanel = ({ selectedInstance, publish }: StylePanelProps) => {
       publish,
     });
 
-  const renderWarning = useMatchMedia();
-
-  const breakpoint = useStore(selectedBreakpointStore);
-
-  if (
-    currentStyle === undefined ||
-    selectedInstance === undefined ||
-    breakpoint === undefined
-  ) {
+  if (currentStyle === undefined || selectedInstance === undefined) {
     return null;
-  }
-
-  if (renderWarning) {
-    return (
-      <Box css={{ p: theme.spacing[5] }}>
-        <Card css={{ p: theme.spacing[9], mt: theme.spacing[9] }}>
-          <DeprecatedParagraph css={{ marginBottom: theme.spacing[5] }}>
-            {`Please increase the canvas width.`}
-          </DeprecatedParagraph>
-          <DeprecatedParagraph>
-            {"minWidth" in breakpoint
-              ? `"${breakpoint.label}" breakpoint minimum width is ${breakpoint.minWidth}px.`
-              : "maxWidth" in breakpoint
-              ? `"${breakpoint.label}" breakpoint maximum width is ${breakpoint.maxWidth}px.`
-              : ""}
-          </DeprecatedParagraph>
-        </Card>
-      </Box>
-    );
   }
 
   return (
