759827c80e377704bcaf0b53b4e1250498139d61
Ivan Starkov <istarkov@gmail.com>
2023-04-13 02:52:19 +0400

    Fix display-table (#1438)
    ## Description
    
    1. What is this PR about (link the issue and add a short description)
    
    ## Steps for reproduction
    
    1. click button
    2. expect xyz
    
    ## Code Review
    
    - [ ] hi @kof, I need you to do
      - conceptual review (architecture, feature-correctness)
      - detailed review (read every line)
      - test it on preview
    
    ## Before requesting a review
    
    - [ ] made a self-review
    - [ ] added inline comments where things may be not obvious (the "why",
    not "what")
    
    ## Before merging
    
    - [ ] tested locally and on preview environment (preview dev login:
    5de6)
    - [ ] updated [test
    cases](https://github.com/webstudio-is/webstudio-builder/blob/main/apps/builder/docs/test-cases.md)
    document
    - [ ] added tests
    - [ ] if any new env variables are added, added them to `.env.example`
    and the `builder/env-check.js` if mandatory
    
    ---------
    
    Co-authored-by: Oleg Isonen <oleg008@gmail.com>

--- apps/builder/app/builder/shared/collapsible-section.tsx
+++ apps/builder/app/builder/shared/collapsible-section.tsx
@@ -7,6 +7,7 @@ import {
   SectionTitle,
   SectionTitleLabel,
   SectionTitleButton,
+  css,
 } from "@webstudio-is/design-system";
 import { theme } from "@webstudio-is/design-system";
 import type { ComponentProps, ReactNode } from "react";
@@ -44,6 +45,8 @@ type CollapsibleSectionBaseProps = {
   onOpenChange: (value: boolean) => void;
 };
 
+const rootStyle = css({ display: "grid" });
+
 export const CollapsibleSectionBase = ({
   label,
   trigger,
@@ -52,7 +55,14 @@ export const CollapsibleSectionBase = ({
   isOpen,
   onOpenChange,
 }: CollapsibleSectionBaseProps) => (
-  <Collapsible.Root open={isOpen} onOpenChange={onOpenChange}>
+  <Collapsible.Root
+    // Grid elements inside `display:table` element doesn't use children size and perfectly fit the parent
+    // I don't know why, but this fixes our issues with ScrollArea
+    // @todo probably pull request here to allow change display https://github.com/radix-ui/primitives/blob/main/packages/react/scroll-area/src/ScrollArea.tsx#L183
+    className={rootStyle()}
+    open={isOpen}
+    onOpenChange={onOpenChange}
+  >
     <Box css={{ boxShadow: `0px 1px 0 ${theme.colors.panelOutline}` }}>
       <Collapsible.Trigger asChild>
         {trigger ?? (
