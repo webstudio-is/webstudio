80ba6bd48f0a7a1924022ee6e902c478403c316a
Roman Pominov <rpominov+github@gmail.com>
2023-04-21 12:18:19 +0300

    add icons

---
+++ packages/icons/src/__generated__/j-c-end.tsx
@@ -0,0 +1,15 @@
+// Generated from icons/j-c-end.svg
+
+import { forwardRef } from "react";
+import type { IconProps } from "../types";
+
+// prettier-ignore
+export const JCEndIcon = forwardRef<SVGSVGElement, IconProps>(
+  ({ color = "currentColor", size = 16, ...props }, forwardedRef) => {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width={size} height={size} fill={color} {...props} ref={forwardedRef}><path fillRule="evenodd" d="M13.8 14.5c-.442 0-.8-.344-.8-.768V2.268c0-.424.358-.768.8-.768.442 0 .8.344.8.768v11.464c0 .424-.358.768-.8.768Z" clipRule="evenodd" /><path d="M12 11.2a.8.8 0 0 1-.8.8H9.8a.8.8 0 0 1-.8-.8V4.8a.8.8 0 0 1 .8-.8h1.4a.8.8 0 0 1 .8.8v6.4ZM8 11.2a.8.8 0 0 1-.8.8H5.8a.8.8 0 0 1-.8-.8V4.8a.8.8 0 0 1 .8-.8h1.4a.8.8 0 0 1 .8.8v6.4Z" /></svg>
+    );
+  }
+);
+
+JCEndIcon.displayName = "JCEndIcon";
---
+++ packages/icons/src/__generated__/link-2-unlinked.tsx
@@ -0,0 +1,15 @@
+// Generated from icons/link-2-unlinked.svg
+
+import { forwardRef } from "react";
+import type { IconProps } from "../types";
+
+// prettier-ignore
+export const Link2UnlinkedIcon = forwardRef<SVGSVGElement, IconProps>(
+  ({ color = "currentColor", size = 16, ...props }, forwardedRef) => {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width={size} height={size} fill={color} {...props} ref={forwardedRef}><path fillRule="evenodd" d="M13.573 8c0-1.008-.966-1.97-2.35-1.97h-1.141a.65.65 0 1 1 0-1.3h1.141c1.93 0 3.65 1.384 3.65 3.27 0 1.886-1.72 3.27-3.65 3.27h-1.141a.65.65 0 1 1 0-1.3h1.141c1.384 0 2.35-.962 2.35-1.97ZM6.568 5.38a.65.65 0 0 1-.65.65H4.776c-1.384 0-2.35.962-2.35 1.97s.966 1.97 2.35 1.97h1.142a.65.65 0 1 1 0 1.3H4.776c-1.93 0-3.65-1.384-3.65-3.27 0-1.886 1.72-3.27 3.65-3.27h1.142a.65.65 0 0 1 .65.65Z" clipRule="evenodd" /></svg>
+    );
+  }
+);
+
+Link2UnlinkedIcon.displayName = "Link2UnlinkedIcon";
