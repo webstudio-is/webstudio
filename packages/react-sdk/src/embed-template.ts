0f615afd01b7aec1102523346abaa3edb57bee35
Bogdan Chadkin <opensource@trysound.io>
2023-04-22 20:47:44 +0300

    Add props support in embed template instances (#1519)
    Ref https://github.com/webstudio-is/webstudio-builder/pull/1516
    
    This will allow further customization of react components through meta

--- packages/react-sdk/src/embed-template.ts
+++ packages/react-sdk/src/embed-template.ts
@@ -1,6 +1,10 @@
 import { z } from "zod";
 import { nanoid } from "nanoid";
-import type { Instance, InstancesList } from "@webstudio-is/project-build";
+import {
+  type Instance,
+  type InstancesList,
+  PropsList,
+} from "@webstudio-is/project-build";
 
 const EmbedTemplateText = z.object({
   type: z.literal("text"),
@@ -9,32 +13,73 @@ const EmbedTemplateText = z.object({
 
 type EmbedTemplateText = z.infer<typeof EmbedTemplateText>;
 
+const EmbedTemplateProp = z.union([
+  z.object({
+    type: z.literal("number"),
+    name: z.string(),
+    value: z.number(),
+  }),
+  z.object({
+    type: z.literal("string"),
+    name: z.string(),
+    value: z.string(),
+  }),
+  z.object({
+    type: z.literal("boolean"),
+    name: z.string(),
+    value: z.boolean(),
+  }),
+  z.object({
+    type: z.literal("string[]"),
+    name: z.string(),
+    value: z.array(z.string()),
+  }),
+]);
+
+type EmbedTemplateProp = z.infer<typeof EmbedTemplateProp>;
+
 type EmbedTemplateInstance = {
   type: "instance";
   component: string;
+  props?: EmbedTemplateProp[];
   children: Array<EmbedTemplateInstance | EmbedTemplateText>;
 };
 
-const EmbedTemplateInstancen: z.ZodType<EmbedTemplateInstance> = z.object({
+const EmbedTemplateInstance: z.ZodType<EmbedTemplateInstance> = z.object({
   type: z.literal("instance"),
   component: z.string(),
+  props: z.optional(z.array(EmbedTemplateProp)),
   children: z.lazy(() => WsEmbedTemplate),
 });
 
 export const WsEmbedTemplate = z.array(
-  z.union([z.lazy(() => EmbedTemplateInstancen), EmbedTemplateText])
+  z.union([z.lazy(() => EmbedTemplateInstance), EmbedTemplateText])
 );
 
 export type WsEmbedTemplate = z.infer<typeof WsEmbedTemplate>;
 
 const createInstancesFromTemplate = (
   treeTemplate: WsEmbedTemplate,
-  instances: InstancesList
+  instances: InstancesList,
+  props: PropsList
 ) => {
   const parentChildren: Instance["children"] = [];
   for (const item of treeTemplate) {
     if (item.type === "instance") {
       const instanceId = nanoid();
+
+      // populate props
+      if (item.props) {
+        for (const prop of item.props) {
+          props.push({
+            id: nanoid(),
+            instanceId,
+            ...prop,
+          });
+        }
+      }
+
+      // populate instances
       const instance: Instance = {
         type: "instance",
         id: instanceId,
@@ -43,7 +88,11 @@ const createInstancesFromTemplate = (
       };
       instances.push(instance);
       // traverse children after to preserve top down order
-      instance.children = createInstancesFromTemplate(item.children, instances);
+      instance.children = createInstancesFromTemplate(
+        item.children,
+        instances,
+        props
+      );
       parentChildren.push({
         type: "id",
         value: instanceId,
@@ -63,6 +112,7 @@ export const generateDataFromEmbedTemplate = (
   treeTemplate: WsEmbedTemplate
 ) => {
   const instances: InstancesList = [];
-  const children = createInstancesFromTemplate(treeTemplate, instances);
-  return { children, instances };
+  const props: PropsList = [];
+  const children = createInstancesFromTemplate(treeTemplate, instances, props);
+  return { children, instances, props };
 };
