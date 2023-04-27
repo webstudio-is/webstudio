b8ceb1f942650b326861dd5612610c8946610a00
Roman Pominov <rpominov+github@gmail.com>
2023-04-27 15:39:47 +0300

    better code comment; isOnCanvas -> source

--- apps/builder/app/shared/shortcuts/shortcuts.ts
+++ apps/builder/app/shared/shortcuts/shortcuts.ts
@@ -17,7 +17,11 @@ export const options: Options = {
   enableOnFormTags: true,
 };
 
-export const useSharedShortcuts = ({ isOnCanvas }: { isOnCanvas: boolean }) => {
+export const useSharedShortcuts = ({
+  source,
+}: {
+  source: "canvas" | "builder";
+}) => {
   useHotkeys(
     // safari use cmd+z to reopen closed tabs so fallback to ctrl
     "meta+z, ctrl+z",
@@ -38,8 +42,10 @@ export const useSharedShortcuts = ({ isOnCanvas }: { isOnCanvas: boolean }) => {
     "backspace, delete",
     deleteSelectedInstance,
     {
-      // prevent instance deletion while deleting text in a control at style panel etc.
-      enableOnFormTags: isOnCanvas,
+      // When in builder, we don't want to delete instances,
+      // when user edits text in a control on style panel etc.
+      // But when a form control on canvas has focus, we want the shortcut to work.
+      enableOnFormTags: source === "canvas",
       enableOnContentEditable: false,
     },
     []
