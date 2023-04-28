2266fc07ddfdb109d925d16d90e3acd21c672aa5
Roman Pominov <rpominov+github@gmail.com>
2023-04-28 11:16:13 +0300

    update flex-child icons

---
+++ packages/icons/src/__generated__/grow.tsx
@@ -0,0 +1,15 @@
+// Generated from icons/grow.svg
+
+import { forwardRef } from "react";
+import type { IconProps } from "../types";
+
+// prettier-ignore
+export const GrowIcon = forwardRef<SVGSVGElement, IconProps>(
+  ({ color = "currentColor", size = 16, ...props }, forwardedRef) => {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width={size} height={size} fill={color} {...props} ref={forwardedRef}><path fillRule="evenodd" d="M1.5 1.85a.65.65 0 0 1 .65.65v11a.65.65 0 1 1-1.3 0v-11a.65.65 0 0 1 .65-.65Zm13 0a.65.65 0 0 1 .65.65v11a.65.65 0 1 1-1.3 0v-11a.65.65 0 0 1 .65-.65ZM6.156 5.54a.65.65 0 0 1 0 .92l-.89.89h5.468l-.89-.89a.65.65 0 1 1 .92-.92l2 2a.65.65 0 0 1 0 .92l-2 2a.65.65 0 1 1-.92-.92l.89-.89H5.266l.89.89a.65.65 0 1 1-.92.92l-2-2a.65.65 0 0 1 0-.92l2-2a.65.65 0 0 1 .92 0Z" clipRule="evenodd" /></svg>
+    );
+  }
+);
+
+GrowIcon.displayName = "GrowIcon";
