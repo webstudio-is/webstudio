c2ac8ed35494308084f03339c0fba327586ba95f
Bogdan Chadkin <opensource@trysound.io>
2023-04-29 09:22:18 +0300

    Allow to reset drop target (#1541)
    Here I allows to return undefined in swapDropTarget. This enables us to
    reset drop target when move cursor out of droppable elements.
    
    For example before Body was always selected as drop target. Though this
    behavior prevent us from restricting parents for cases like Tabs>Tab and
    List>ListItem.
    
    Also got rid from emulatePointerAlwaysInRootBounds which was used to
    workaround that fallback to Body. So now no longer necessary too.
    
    https://user-images.githubusercontent.com/5635476/235142653-48b95ebe-9714-44c4-90d9-b69b930f2379.mov

--- apps/builder/app/canvas/shared/use-drag-drop.ts
+++ apps/builder/app/canvas/shared/use-drag-drop.ts
@@ -10,7 +10,7 @@ import {
   computeIndicatorPlacement,
 } from "@webstudio-is/design-system";
 import { getComponentMeta } from "@webstudio-is/react-sdk";
-import { instancesStore, selectedPageStore } from "~/shared/nano-states";
+import { instancesStore } from "~/shared/nano-states";
 import { textEditingInstanceSelectorStore } from "~/shared/nano-states";
 import { publish, useSubscribe } from "~/shared/pubsub";
 import {
@@ -33,8 +33,8 @@ declare module "~/shared/pubsub" {
     dragEnd: DragEndPayload;
     dragMove: DragMovePayload;
     dragStart: DragStartPayload;
-    dropTargetChange: ItemDropTarget;
-    placementIndicatorChange: Placement;
+    dropTargetChange: undefined | ItemDropTarget;
+    placementIndicatorChange: undefined | Placement;
   }
 }
 
@@ -102,20 +102,6 @@ const sharedDropOptions = {
   },
 };
 
-const getDefaultDropTarget = () => {
-  const selectedPage = selectedPageStore.get();
-  if (selectedPage === undefined) {
-    throw new Error("Could not find selected page");
-  }
-  const rootInstanceSelector = [selectedPage.rootInstanceId];
-  const element = getElementByInstanceSelector(rootInstanceSelector);
-  // Should never happen
-  if (element === undefined) {
-    throw new Error("Could not find root instance element");
-  }
-  return { element, data: rootInstanceSelector };
-};
-
 export const useDragAndDrop = () => {
   const state = useRef({ ...initialState });
 
@@ -137,13 +123,10 @@ export const useDragAndDrop = () => {
       const { dragPayload } = state.current;
 
       if (dropTarget === undefined || dragPayload === undefined) {
-        return getDefaultDropTarget();
+        return;
       }
 
       const dropInstanceSelector = dropTarget.data;
-      if (dropInstanceSelector.length === 1) {
-        return dropTarget;
-      }
 
       const newDropInstanceSelector = dropInstanceSelector.slice();
       if (dropTarget.area !== "center") {
@@ -164,7 +147,7 @@ export const useDragAndDrop = () => {
         newDropInstanceSelector
       );
       if (droppableInstanceSelector === undefined) {
-        return getDefaultDropTarget();
+        return;
       }
 
       if (
@@ -178,7 +161,7 @@ export const useDragAndDrop = () => {
 
       const element = getElementByInstanceSelector(droppableInstanceSelector);
       if (element === undefined) {
-        return getDefaultDropTarget();
+        return;
       }
 
       return { data: droppableInstanceSelector, element };
@@ -187,11 +170,14 @@ export const useDragAndDrop = () => {
     onDropTargetChange(dropTarget) {
       publish({
         type: "dropTargetChange",
-        payload: {
-          placement: dropTarget.placement,
-          indexWithinChildren: dropTarget.indexWithinChildren,
-          itemSelector: dropTarget.data,
-        },
+        payload:
+          dropTarget === undefined
+            ? undefined
+            : {
+                placement: dropTarget.placement,
+                indexWithinChildren: dropTarget.indexWithinChildren,
+                itemSelector: dropTarget.data,
+              },
       });
     },
   });
@@ -282,6 +268,13 @@ export const useDragAndDrop = () => {
 
   useSubscribe("dropTargetChange", (dropTarget) => {
     state.current.dropTarget = dropTarget;
+    if (dropTarget === undefined) {
+      publish({
+        type: "placementIndicatorChange",
+        payload: undefined,
+      });
+      return;
+    }
     const element = getElementByInstanceSelector(dropTarget.itemSelector);
     if (element === undefined) {
       return;
