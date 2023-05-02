34c8c383a2a81c4a931c4f3ec77107433e7edc9e
Oleg Isonen <oleg008@gmail.com>
2023-05-02 23:30:41 +0200

    fix fill

--- packages/icons/src/__generated__/radio-unchecked.tsx
+++ packages/icons/src/__generated__/radio-unchecked.tsx
@@ -7,7 +7,7 @@ import type { IconProps } from "../types";
 export const RadioUncheckedIcon = forwardRef<SVGSVGElement, IconProps>(
   ({ color = "currentColor", size = 16, ...props }, forwardedRef) => {
     return (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width={size} height={size} fill={color} {...props} ref={forwardedRef}><path fill="#11181C" fillRule="evenodd" d="M8 1.35a6.65 6.65 0 1 0 0 13.3 6.65 6.65 0 0 0 0-13.3ZM2.65 8a5.35 5.35 0 1 1 10.7 0 5.35 5.35 0 0 1-10.7 0Z" clipRule="evenodd" /></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width={size} height={size} fill={color} {...props} ref={forwardedRef}><path d="M8 1.35a6.65 6.65 0 1 0 0 13.3 6.65 6.65 0 0 0 0-13.3ZM2.65 8a5.35 5.35 0 1 1 10.7 0 5.35 5.35 0 0 1-10.7 0Z" clipRule="evenodd" /></svg>
     );
   }
 );
