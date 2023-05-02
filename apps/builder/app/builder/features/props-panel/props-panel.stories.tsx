bdc876f22ecb17a4daf09198c28ab8d656d40b3e
Roman Pominov <rpominov+github@gmail.com>
2023-05-02 15:39:21 +0300

    fix import

--- apps/builder/app/builder/features/props-panel/props-panel.stories.tsx
+++ apps/builder/app/builder/features/props-panel/props-panel.stories.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { ButtonElementIcon } from "@webstudio-is/icons";
 import { PropsPanel } from "./props-panel";
 import { usePropsLogic } from "./use-props-logic";
