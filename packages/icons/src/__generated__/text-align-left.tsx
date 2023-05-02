1c7e2159a4a82c823ca3272d358db53625ba4fb5
Oleg Isonen <oleg008@gmail.com>
2023-05-02 23:45:03 +0200

    fix icons

--- packages/icons/src/__generated__/text-align-left.tsx
+++ packages/icons/src/__generated__/text-align-left.tsx
@@ -7,7 +7,7 @@ import type { IconProps } from "../types";
 export const TextAlignLeftIcon = forwardRef<SVGSVGElement, IconProps>(
   ({ color = "currentColor", size = 16, ...props }, forwardedRef) => {
     return (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width={size} height={size} fill={color} {...props} ref={forwardedRef}><path d="M1.85 4a.65.65 0 0 1 .65-.65h12a.65.65 0 1 1 0 1.3h-12A.65.65 0 0 1 1.85 4Zm0 4a.65.65 0 0 1 .65-.65h6a.65.65 0 0 1 0 1.3h-6A.65.65 0 0 1 1.85 8Zm0 4a.65.65 0 0 1 .65-.65h8a.65.65 0 1 1 0 1.3h-8a.65.65 0 0 1-.65-.65Z" clipRule="evenodd" /></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width={size} height={size} fill={color} {...props} ref={forwardedRef}><path fillRule="evenodd" d="M1.85 4a.65.65 0 0 1 .65-.65h12a.65.65 0 1 1 0 1.3h-12A.65.65 0 0 1 1.85 4Zm0 4a.65.65 0 0 1 .65-.65h6a.65.65 0 0 1 0 1.3h-6A.65.65 0 0 1 1.85 8Zm0 4a.65.65 0 0 1 .65-.65h8a.65.65 0 1 1 0 1.3h-8a.65.65 0 0 1-.65-.65Z" clipRule="evenodd" /></svg>
     );
   }
 );
