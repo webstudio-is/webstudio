5c2f0621d4080d1e0d53ec5bed8c1b5e899661d6
Roman Pominov <rpominov+github@gmail.com>
2023-05-05 13:55:24 +0300

    add n8n handler

--- packages/form-handlers/src/index.ts
+++ packages/form-handlers/src/index.ts
@@ -1,2 +1,3 @@
-export { mailchannelsHandler } from "./mailchanels";
 export { formDataToEmailContent } from "./shared";
+export { mailchannelsHandler } from "./mailchanels";
+export { n8nHandler } from "./n8n";
