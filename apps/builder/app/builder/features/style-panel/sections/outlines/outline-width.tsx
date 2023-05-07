11729c01b5e08e7226f70e669b313f4fe0725d5e
JayaKrishnaNamburu <namburu1995@gmail.com>
2023-05-07 23:25:26 +0200

    Ability to set and delete outline offset and outline width

--- apps/builder/app/builder/features/style-panel/sections/outlines/outline-width.tsx
+++ apps/builder/app/builder/features/style-panel/sections/outlines/outline-width.tsx
@@ -1,26 +1,17 @@
-import type { StyleProperty } from "@webstudio-is/css-data";
-import {
-  Grid,
-  theme,
-  Box,
-  ToggleGroup,
-  ToggleGroupItem,
-  Tooltip,
-} from "@webstudio-is/design-system";
-import {
-  DashBorderIcon,
-  DashedBorderIcon,
-  DottedBorderIcon,
-  SmallXIcon,
-} from "@webstudio-is/icons";
+import type { StyleProperty, UnitValue } from "@webstudio-is/css-data";
+import { Grid, theme } from "@webstudio-is/design-system";
 import type { RenderCategoryProps } from "../../style-sections";
-import { toPascalCase } from "../../shared/keyword-utils";
 import { PropertyName } from "../../shared/property-name";
 import { CssValueInputContainer } from "../../controls/position/css-value-input-container";
+import { styleConfigByName } from "../../shared/configs";
+import { getStyleSource } from "../../shared/style-info";
 
-const outlineWidthProperties = [
-  "outlineWidth",
-] as const satisfies readonly StyleProperty[];
+const property: StyleProperty = "outlineWidth";
+const defaultOutlineWidthValue: UnitValue = {
+  type: "unit",
+  value: 0,
+  unit: "number",
+};
 
 export const OutlineWidth = (
   props: Pick<
@@ -28,27 +19,35 @@ export const OutlineWidth = (
     "currentStyle" | "setProperty" | "deleteProperty" | "createBatchUpdate"
   >
 ) => {
+  const { deleteProperty, setProperty, currentStyle } = props;
+  const outlineWidthValue =
+    currentStyle[property]?.value ?? defaultOutlineWidthValue;
+  const outlineStyleConfig = styleConfigByName(property);
+
   return (
     <Grid
       css={{
-        gridTemplateColumns: `1fr ${theme.spacing[20]} ${theme.spacing[12]}`,
+        gridTemplateColumns: `1fr ${theme.spacing[20]}`,
       }}
       gap={2}
     >
       <PropertyName
-        property={outlineWidthProperties}
+        property={property}
         style={props.currentStyle}
         label={"Width"}
-        onReset={() => {
-          console.log("called reset");
-        }}
+        onReset={() => deleteProperty(property)}
       />
 
-      <Box>
-        {/* <CssValueInputContainer
-          label={"Outline"}
-        /> */}
-      </Box>
+      <CssValueInputContainer
+        key={property}
+        property={property}
+        label={outlineStyleConfig?.label || ""}
+        styleSource={getStyleSource(currentStyle[property])}
+        keywords={[]}
+        setValue={setProperty(property)}
+        deleteProperty={deleteProperty}
+        value={outlineWidthValue}
+      />
     </Grid>
   );
 };
