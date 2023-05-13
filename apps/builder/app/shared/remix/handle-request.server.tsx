2374b5f22b10969d79abf8b67d848db15338f4be
Ivan Starkov <istarkov@gmail.com>
2023-05-13 18:11:41 +0400

    Fix assets vercel uploading  (#1626)
    ## Description
    
    1. Add 404 instead of redirect
    2. Fix vercel/remix upload
    
    ## Steps for reproduction
    
    1. click button
    2. expect xyz
    
    ## Code Review
    
    - [ ] hi @kof, I need you to do
      - conceptual review (architecture, feature-correctness)
      - detailed review (read every line)
      - test it on preview
    
    ## Before requesting a review
    
    - [ ] made a self-review
    - [ ] added inline comments where things may be not obvious (the "why",
    not "what")
    
    ## Before merging
    
    - [ ] tested locally and on preview environment (preview dev login:
    5de6)
    - [ ] updated [test
    cases](https://github.com/webstudio-is/webstudio-builder/blob/main/apps/builder/docs/test-cases.md)
    document
    - [ ] added tests
    - [ ] if any new env variables are added, added them to `.env.example`
    and the `builder/env-check.js` if mandatory

--- apps/builder/app/shared/remix/handle-request.server.tsx
+++ apps/builder/app/shared/remix/handle-request.server.tsx
@@ -4,13 +4,7 @@ import type { EntryContext } from "@remix-run/node";
 import { flushCss } from "@webstudio-is/design-system";
 import { renderHeadToString } from "remix-island";
 import { Head } from "./root";
-// eslint-disable-next-line import/no-internal-modules
-import interFont from "@fontsource/inter/variable.css";
-// eslint-disable-next-line import/no-internal-modules
-import manropeVariableFont from "@fontsource/manrope/variable.css";
-// eslint-disable-next-line import/no-internal-modules
-import robotoMonoFont from "@fontsource/roboto-mono/index.css";
-import appCss from "../app.css";
+import { links } from "~/routes/vercel-deploy-workaround";
 
 export const handleRequest = (
   request: Request,
@@ -35,10 +29,13 @@ export const handleRequest = (
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <link rel="stylesheet" href="${interFont}" />
-        <link rel="stylesheet" href="${manropeVariableFont}" />
-        <link rel="stylesheet" href="${robotoMonoFont}" />
-        <link rel="stylesheet" href="${appCss}" />
+        ${links()
+          .map((link) =>
+            "href" in link
+              ? `<link rel="${link.rel}" href="${link.href}" />`
+              : ""
+          )
+          .join("\n")}
         ${head}
         <style id="stitches">${flushCss()}</style>
       </head>
