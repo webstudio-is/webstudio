813977d3cc01a7fe18cb41e46b382bbd5fc5bf4a
nettohadi <32300655+nettohadi@users.noreply.github.com>
2023-05-13 21:55:06 +0800

    convert to inline SVG

--- apps/builder/app/dashboard/loading-state/loading.tsx
+++ apps/builder/app/dashboard/loading-state/loading.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Flex, css } from "@webstudio-is/design-system";
-import spinner from "./spinner.svg";
+import { useDebounce } from "use-debounce";
+import { SvgLoading } from "~/dashboard/loading-state/svg-loading";
 
 const containerStyle = css({
   position: "absolute",
@@ -11,33 +12,24 @@ const containerStyle = css({
   background:
     "radial-gradient(34.37% 50% at 50% 50%, rgba(255, 255, 255, 0.5) 0%, rgba(248, 248, 248, 0.5) 100%);",
   backdropFilter: "blur(8px)",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
 });
 
 export const Loading = ({ delay = 0 }: { delay?: number }) => {
   const [isVisible, setIsVisible] = useState(false);
+  const [visible] = useDebounce(isVisible, delay ?? 0);
 
-  useEffect(() => {
-    // we're using a timeout to avoid showing the loading state for a very short time
-    // If the connection is very fast, the user will not see a loading state
-    // This is just an idea, we can remove it if we don't like it
-    const timer = setTimeout(() => {
-      setIsVisible(true);
-    }, delay);
+  if (!isVisible) {
+    setIsVisible(true);
+  }
 
-    return () => clearTimeout(timer);
-  }, [delay]);
-
-  return isVisible ? (
+  return visible ? (
     <Flex
       direction="row"
       justify="center"
       align="center"
       className={containerStyle()}
     >
-      <img src={spinner} alt="Loading" width={64} height={64} />
+      <SvgLoading />
     </Flex>
   ) : null;
 };
