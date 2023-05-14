01c1839c46bc264b773c2df1ddb0f4e2611a41df
Bogdan Chadkin <trysound@yandex.ru>
2023-05-14 14:40:29 +0700

    Add comment to params

--- packages/react-sdk/src/app/params.ts
+++ packages/react-sdk/src/app/params.ts
@@ -1,5 +1,27 @@
 export type Params = {
+  /**
+   * Base url ir base path for images with ending slash.
+   * Used for configuring image with different sizes.
+   * Concatinated with "name?width=&quality=&format=".
+   *
+   * For example
+   * /asset/image/ used by default in builder
+   * or
+   * https://webstudio.is/asset/image/ can be specified
+   * by user to use custom server
+   */
   imageBaseUrl: string;
+  /**
+   * Base url or base path for any asset with ending slash.
+   * Used to load assets like fonts or images in styles
+   * Concatinated with "name".
+   *
+   * For example
+   * /s/uploads/
+   * /asset/file/
+   * or
+   * https://webstudio.is/asset/file/
+   */
   assetBaseUrl: string;
 };
 
