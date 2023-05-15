282d17392043d86b95773cca96f35e73cfb2b382
Bogdan Chadkin <trysound@yandex.ru>
2023-05-15 16:43:39 +0700

    Remove artifact from not existing migration

--- packages/prisma-client/prisma/migrations/20230515112405_file_uploader_project/migration.sql
+++ packages/prisma-client/prisma/migrations/20230515112405_file_uploader_project/migration.sql
@@ -1,9 +1,5 @@
--- DropForeignKey
-ALTER TABLE "File" DROP CONSTRAINT "File_userId_fkey";
-
 -- AlterTable
-ALTER TABLE "File" DROP COLUMN "userId",
-ADD COLUMN     "uploaderProjectId" TEXT;
+ALTER TABLE "File" ADD COLUMN     "uploaderProjectId" TEXT;
 
 -- AddForeignKey
 ALTER TABLE "File" ADD CONSTRAINT "File_uploaderProjectId_fkey" FOREIGN KEY ("uploaderProjectId") REFERENCES "Project"("id") ON DELETE SET NULL ON UPDATE CASCADE;
