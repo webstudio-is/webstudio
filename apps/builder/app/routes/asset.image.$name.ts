9dea0997dc03e124143f8b0866b6a3cde6e58fce
istarkov <istarkov@gmail.com>
2023-05-23 17:20:28 +0400

    Show headers

--- apps/builder/app/routes/asset.image.$name.ts
+++ apps/builder/app/routes/asset.image.$name.ts
@@ -23,6 +23,21 @@ export const loader = async ({ params, request }: LoaderArgs) => {
 
   const options = `width=${width},quality=${quality},format=${format}`;
 
+  // eslint-disable-next-line no-console
+  console.log(
+    JSON.stringify(
+      {
+        origin: request.headers.get("origin"),
+        Origin: request.headers.get("Origin"),
+        referer: request.headers.get("referer"),
+        referrer: request.headers.get("referrer"),
+        keys: request.headers.keys(),
+      },
+      null,
+      2
+    )
+  );
+
   if (request.headers.get("origin") !== url.origin) {
     throw new Response("Forbidden", {
       status: 403,
