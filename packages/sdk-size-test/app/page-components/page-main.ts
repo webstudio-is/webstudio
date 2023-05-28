d6b4ea01538c44a3e0c4d5f8ed96220c282747ae
Bogdan Chadkin <trysound@yandex.ru>
2023-05-28 13:01:33 +0700

    Fix sdk size test

--- packages/sdk-size-test/app/page-components/page-main.ts
+++ packages/sdk-size-test/app/page-components/page-main.ts
@@ -1,3 +1,5 @@
-import { Box, Button, Body, createGetComponent } from "@webstudio-is/react-sdk";
+import { type Components, Box, Button, Body } from "@webstudio-is/react-sdk";
 
-export const getComponent = createGetComponent({ Box, Button, Body });
+export const components = new Map(
+  Object.entries({ Box, Button, Body })
+) as Components;
--- packages/sdk-size-test/app/routes/$.tsx
+++ packages/sdk-size-test/app/routes/$.tsx
@@ -1,10 +1,10 @@
 import { useLoaderData } from "@remix-run/react";
 import { InstanceRoot, Root } from "@webstudio-is/react-sdk";
-import { getComponent } from "../page-components/page-main";
+import { components } from "../page-components/page-main";
 
 export default function Index() {
   const data = useLoaderData<unknown>();
 
-  const Outlet = () => <InstanceRoot data={data} getComponent={getComponent} />;
+  const Outlet = () => <InstanceRoot data={data} components={components} />;
   return <Root Outlet={Outlet} />;
 }
