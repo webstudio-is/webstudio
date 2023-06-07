a32ed0bb3ce66d558672f1e5d26a341858d6c46d
Oleg Isonen <oleg008@gmail.com>
2023-06-08 00:17:49 +0200

    use hidden category

--- packages/sdk-components-react/src/rich-text-link.ws.tsx
+++ packages/sdk-components-react/src/rich-text-link.ws.tsx
@@ -4,10 +4,9 @@ import type {
 } from "@webstudio-is/react-sdk";
 import { meta as linkMeta, propsMeta as linkPropsMeta } from "./link.ws";
 
-const { category, ...linkMetaRest } = linkMeta;
-
 export const meta: WsComponentMeta = {
-  ...linkMetaRest,
+  ...linkMeta,
+  category: "hidden",
   type: "rich-text-child",
   template: [],
 };
