d64f0b44a2af37fb5952a6f3f3350a2bd5c4fe3c
Oleg Isonen <oleg008@gmail.com>
2023-06-07 23:01:17 +0200

    spinner is now box

--- packages/sdk-components-react/src/vimeo-spinner.ws.ts
+++ packages/sdk-components-react/src/vimeo-spinner.ws.ts
@@ -2,21 +2,17 @@ import {
   type WsComponentMeta,
   type WsComponentPropsMeta,
 } from "@webstudio-is/react-sdk";
-import {
-  propsOverrides as imagePropsOverrides,
-  propsMeta as imagePropsMeta,
-  meta as imageMeta,
-} from "./image.ws";
+import { propsMeta as boxPropsMeta, meta as boxMeta } from "./box.ws";
 import { props } from "./__generated__/vimeo-spinner.props";
 
 export const meta: WsComponentMeta = {
-  ...imageMeta,
+  ...boxMeta,
   category: "hidden",
   label: "Spinner",
   requiredAncestors: ["Vimeo"],
 };
 
 export const propsMeta: WsComponentPropsMeta = {
-  props: { ...props, ...imagePropsOverrides },
-  initialProps: imagePropsMeta.initialProps,
+  props,
+  initialProps: boxPropsMeta.initialProps,
 };
