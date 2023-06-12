558e2916d48ec615f6291fef580bb4e1afd770f8
Giuseppe Gurgone <ggurgone@users.noreply.github.com>
2023-06-12 20:12:31 +0200

    Remove colors and misc fixes

--- packages/css-data/bin/property-value-descriptions.ts
+++ packages/css-data/bin/property-value-descriptions.ts
@@ -12,13 +12,7 @@ import { keywordValues } from "../src/__generated__/keyword-values";
  * - properties
  * - declarations
  *
- * By default it generates descriptions only for new properties/values, however you can use the --forceRegenerate flag to regenerate everything.
- *
- * When it comes to generating descriptions for declarations whose value is a color eg. `background-color: orange`
- * we generate a description for a generic `property: {color}` declaration (e.g. `background-color: {color}`)
- * and GPT will generate a description template with a placeholder `{color}`.
- * The script repeats the template for every color value, replacing `{color}`.
- * This is to avoid generating a ton of similar description and wasting GPT tokens.
+ * By default it generates descriptions only for new properties/values, however you can use the --force flag to regenerate everything.
  *
  * The script needs two env variables:
  * - process.env.OPENAI_KEY - Your OpenAI API key https://platform.openai.com/account/api-keys
@@ -27,9 +21,9 @@ import { keywordValues } from "../src/__generated__/keyword-values";
 
 const args = process.argv.slice(2);
 let forceRegenerate = false;
-if (args.includes("--forceRegenerate")) {
+if (args.includes("--force")) {
   forceRegenerate = true;
-  args.splice(args.indexOf("--forceRegenerate"), 1);
+  args.splice(args.indexOf("--force"), 1);
 }
 
 const fileName = "property-value-descriptions.ts";
@@ -45,25 +39,26 @@ try {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore Fix this else it'll complain that we cannot use top-level await.
   } = await import(path.join(targetPath, fileName)));
-  if (!propertiesDescriptions) {
+  if (propertiesDescriptions === undefined) {
     propertiesDescriptions = {};
   }
   if (!declarationsDescriptions) {
     declarationsDescriptions = {};
   }
-} catch (error) {
-  console.log(error);
-}
+} catch (error) {}
 
-const batchSize = 20;
+const batchSize = 16;
 
 /**
  * Properties descriptions
  */
-const newPropertiesNames = Object.keys(keywordValues).filter(
-  (property) =>
-    forceRegenerate || typeof propertiesDescriptions[property] !== "string"
-);
+const newPropertiesNames = Object.keys(keywordValues)
+  // Slice to generate only X - useful for testing.
+  // .slice(0, 1)
+  .filter(
+    (property) =>
+      forceRegenerate || typeof propertiesDescriptions[property] !== "string"
+  );
 
 for (let i = 0; i < newPropertiesNames.length; ) {
   const properties = newPropertiesNames.slice(i, i + batchSize);
@@ -82,13 +77,29 @@ for (let i = 0; i < newPropertiesNames.length; ) {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore Fix this else it'll complain that we cannot use top-level await.
   const result = await generate(`
-Please generate a short description (max 140 characters) for **every** CSS property in the following list:
+Please generate a good, newbie friendly and self-explanatory description (max 140 characters) for all the CSS properties in the following list:
 
+\`\`\`
 ${properties.map((name) => `- ${name}`).join("\n")}
+\`\`\`
 
-Respond with a matching bulleted list which contains only the description as Markdown code block, omitting the original property and any other explanation.
+Respond with a matching bulleted list of the same length which contains all the descriptions as Markdown code block.
 
-Example output:
+Important:
+
+- The response list should include a description for every property in the list above.
+- The descriptions should be beginner friendly and understandable by someone who doesn't know CSS.
+- Don't add any other explanation.
+- Don't repeat the property names in the descriptions.
+
+Example properties list (input):
+
+\`\`\`
+- background-color
+- flex-direction
+\`\`\`
+
+Example response:
 
 \`\`\`markdown
 - Sets the background color
@@ -99,7 +110,7 @@ Example output:
   if (Array.isArray(result)) {
     // Retry
     if (result[0] === 429) {
-      console.log("❌  Error: 429. Retrying...");
+      console.log(`❌  Error: 429 ${result[1]}. Retrying...`);
       continue;
     }
 
@@ -113,7 +124,7 @@ Example output:
       "❌ Error: the number of generated descriptions does not match the amount of inputs. Retrying..."
     );
 
-    console.log({ properties, result });
+    console.log({ input: properties.join("\n"), output: result });
     continue;
   }
 
@@ -134,45 +145,59 @@ console.log("\n✅ Properties description generated!\n");
  * Declarations descriptions
  */
 const newDeclarationsDescriptions: Record<string, string> = {};
-const nonColorValues = [
-  "auto",
-  "initial",
-  "inherit",
-  "unset",
-  "currentColor",
-  "transparent",
-];
 const propertyColors: Record<string, string[]> = {};
 
-Object.entries(keywordValues).forEach(([keyword, values]) => {
-  return values.forEach((value, index) => {
-    let descriptionKey = `${keyword}:${value}`;
-    if (
-      !forceRegenerate &&
-      typeof declarationsDescriptions[descriptionKey] === "string"
-    ) {
-      return;
-    }
-
-    const property = toKebabCase(keyword);
-
-    if (
-      keyword.toLowerCase().includes("color") &&
-      !nonColorValues.includes(value)
-    ) {
-      // 1. In order to avoid generating descriptions for all the standard colors eg. `background-color: orange;`
-      // we generate a description for a generic `property: {color}` declaration and use that as a template for all the possible colors.
-      descriptionKey = `${keyword}:{color}`;
-      if (!propertyColors[descriptionKey]) {
-        propertyColors[descriptionKey] = [];
+Object.entries(keywordValues)
+  // Slice to generate only X - useful for testing.
+  // .slice(0, 10)
+  .forEach(([keyword, values]) => {
+    return values.forEach((value, index) => {
+      const descriptionKey = `${keyword}:${value}`;
+      if (
+        !forceRegenerate &&
+        typeof declarationsDescriptions[descriptionKey] === "string"
+      ) {
+        return;
       }
-      propertyColors[descriptionKey].push(value);
-      newDeclarationsDescriptions[descriptionKey] = `${property}: {color}`;
-    } else {
-      newDeclarationsDescriptions[descriptionKey] = `${property}: ${value}`;
-    }
+
+      const property = toKebabCase(keyword);
+
+      // We are currently skipping generation for color-related properies. Uncomment the code below to enable colors generation.
+      // Also add the following line to the GPT prompt:
+      // "When you encounter a declaration where the value is \`{color}\` make the description generic: this is a template that I will later use for all my colors."
+      //
+      // const nonColorValues = [
+      //   "auto",
+      //   "initial",
+      //   "inherit",
+      //   "unset",
+      //   "currentColor",
+      //   "transparent",
+      //   "none"
+      // ];
+      if (
+        keyword.toLowerCase().includes("color")
+        // && !nonColorValues.includes(value)
+      ) {
+        return;
+        /*
+          When it comes to generating descriptions for declarations whose value is a color eg. `background-color: orange`
+          we generate a description for a generic `property: {color_value}` declaration (e.g. `background-color: {color}`)
+          and GPT will generate a description template with a placeholder `{color_value}`.
+          The script repeats the template for every color value, replacing `{color_value}`.
+          This is to avoid generating a ton of similar description and wasting GPT tokens.
+         */
+        // descriptionKey = `${keyword}:{color}`;
+        // if (!propertyColors[descriptionKey]) {
+        //   propertyColors[descriptionKey] = [];
+        // }
+        // propertyColors[descriptionKey].push(value);
+        // newDeclarationsDescriptions[descriptionKey] = `${property}: {color}`;
+      } else {
+        newDeclarationsDescriptions[descriptionKey] = `${property}: ${value}`;
+      }
+    });
   });
-});
 
 const newDescriptionsEntries = Object.entries(newDeclarationsDescriptions);
 
@@ -190,18 +215,32 @@ for (let i = 0; i < newDescriptionsEntries.length; ) {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore Fix this else it'll complain that we cannot use top-level await.
   const result = await generate(`
-Please generate a short description (max 140 characters) for **every** CSS declaration in the following list:
+Please generate a good, newbie friendly and self-explanatory description (max 140 characters) for all the CSS declarations in the following list:
 
+\`\`\`
 ${list.join("\n")}
+\`\`\`
 
-Respond with a matching bulleted list which contains only the description as Markdown code block, omitting the original declaration and any other explanation.
+Respond with a matching bulleted list of the same length which contains all the descriptions as Markdown code block.
 
-When you encounter a declaration where the value is \`{color}\` make the description generic: this is a template that I will later use for all my colors.
+Important:
 
-For example:
+- The response should include a description for every declaration above.
+- The descriptions should be beginner friendly and understandable by someone who doesn't know CSS.
+- Don't add any other explanation.
+- Don't include the declarations (input) in the descriptions (output).
+
+Example declarations list (input):
+
+\`\`\`
+- align-items: center
+- flex-direction: row
+\`\`\`
+
+Example response:
 
 \`\`\`markdown
-- Sets the background color to {color}
+- Sets the alignment of flex items to the center of the container
 - Sets the flex container main axis direction to row
 \`\`\`
 `);
@@ -209,7 +248,7 @@ For example:
   if (Array.isArray(result)) {
     // Retry
     if (result[0] === 429) {
-      console.log("❌  Error: 429. Retrying...");
+      console.log(`❌  Error: 429 ${result[1]}. Retrying...`);
       continue;
     }
 
@@ -223,7 +262,7 @@ For example:
       "❌ Error: the number of generated descriptions does not match the amount of inputs. Retrying..."
     );
 
-    console.log({ list, result });
+    console.log({ input: list.join("\n"), output: result });
     continue;
   }
 
@@ -300,11 +339,11 @@ async function generate(message: string): Promise<string | [number, string]> {
     body: JSON.stringify({
       model: "gpt-3.5-turbo",
       messages: [{ role: "user", content: message }],
-      temperature: 0.7,
+      temperature: 0.5,
     }),
   });
 
-  if (!response.ok) {
+  if (response.ok === false) {
     return [response.status, response.statusText];
   }
 
@@ -312,7 +351,7 @@ async function generate(message: string): Promise<string | [number, string]> {
 
   const content = completion.choices[0].message?.content.trim() || "";
 
-  if (!content) {
+  if (content === "") {
     return [404, "No response"];
   }
 
