ba7c40b83c72cf7d983cc6e771751be96a2d25f1
Oleg Isonen <oleg008@gmail.com>
2023-06-13 02:43:46 +0200

    Add first component description for slot

--- packages/sdk-components-react/src/slot.ws.ts
+++ packages/sdk-components-react/src/slot.ws.ts
@@ -8,6 +8,8 @@ export const meta: WsComponentMeta = {
   category: "general",
   type: "container",
   label: "Slot",
+  description:
+    "Slot is a container for content that you want to reference across the project. Changes made to a Slot's children will be reflected in all other instances of that Slot.",
   icon: SlotComponentIcon,
   stylable: false,
   order: 6,
