0750cd03f5f4d62833cd8e71269de458af5cc1de
Ivan Starkov <istarkov@gmail.com>
2023-06-14 17:32:49 +0300

    Fix body layout (#1784)
    ## Description
    
    closes #1781
    Also makes body/html layout more modern fixing additional bugs
    More details
    https://twitter.com/ChallengesCss/status/1471128244720181258
    
    ## Steps for reproduction
    
    1. click button
    2. expect xyz
    
    ## Code Review
    
    - [ ] hi @kof, I need you to do
      - conceptual review (architecture, feature-correctness)
      - detailed review (read every line)
      - test it on preview
    
    ## Before requesting a review
    
    - [ ] made a self-review
    - [ ] added inline comments where things may be not obvious (the "why",
    not "what")
    
    ## Before merging
    
    - [ ] tested locally and on preview environment (preview dev login:
    5de6)
    - [ ] updated [test
    cases](https://github.com/webstudio-is/webstudio-builder/blob/main/apps/builder/docs/test-cases.md)
    document
    - [ ] added tests
    - [ ] if any new env variables are added, added them to `.env.example`
    and the `builder/env-check.js` if mandatory

--- packages/react-sdk/src/css/global-rules.ts
+++ packages/react-sdk/src/css/global-rules.ts
@@ -9,7 +9,8 @@ export const addGlobalRules = (
   // @todo we need to figure out all global resets while keeping
   // the engine aware of all of them.
   // Ideally, the user is somehow aware and in control of the reset
-  engine.addPlaintextRule("html {margin: 0; height: 100%}");
+  // Layout source https://twitter.com/ChallengesCss/status/1471128244720181258
+  engine.addPlaintextRule("html {margin: 0; display: grid; min-height: 100%}");
 
   const fontAssets: FontAsset[] = [];
   for (const asset of assets.values()) {
