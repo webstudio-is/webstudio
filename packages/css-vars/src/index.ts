c45c08807b8caf5a415736c03ad5db173dfc813a
istarkov <istarkov@gmail.com>
2023-06-19 19:43:30 +0300

    renamed postfix

--- packages/css-vars/src/index.ts
+++ packages/css-vars/src/index.ts
@@ -1,32 +1,19 @@
-const WEBSTUDIO_CSS_VARIABLE_POSTFIX = "-webstudio-variable";
+/**
+ * To reduce probability of intersection with any existing CSS variables
+ */
+const CSS_VARIABLE_POSTFIX = "-w7ovd";
+
 const cssVarsSet = new Set<string>();
+
 const define = <Name extends string>(name: Name) => {
   if (cssVarsSet.has(name)) {
     throw new Error(`Variable ${name} already defined`);
   }
   cssVarsSet.add(name);
 
-  return `--${name}${WEBSTUDIO_CSS_VARIABLE_POSTFIX}` as const;
+  return `--${name}${CSS_VARIABLE_POSTFIX}` as const;
 };
 
-type AppendString<
-  TResult extends string,
-  TDelimiter extends string,
-  TNextPart extends string
-> = TResult extends "" ? TNextPart : `${TResult}${TDelimiter}${TNextPart}`;
-
-type Join<
-  TInput extends readonly string[],
-  TDelimiter extends string,
-  TResult extends string = ""
-> = TInput extends []
-  ? TResult
-  : TInput extends [string, ...infer Rest extends string[]]
-  ? Join<Rest, TDelimiter, AppendString<TResult, TDelimiter, TInput[0]>>
-  : never;
-
-const use = <Args extends string[]>(...args: Args) => {
-  return `var(${args.join(", ") as Join<Args, ", ">})` as const;
-};
+const use = (...args: string[]) => `var(${args.join(", ")})`;
 
 export const cssVars = { define, use };
