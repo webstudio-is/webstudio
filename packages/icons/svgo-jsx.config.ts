8be343f115d8f16c0b6aa0aab978ab97e59c78da
Bogdan Chadkin <trysound@yandex.ru>
2023-06-25 19:13:56 +0300

    Run eslint from the root with cache
    While turborepo does not work for us we can try to solve long checks time
    with builtin caching. Eslint provides similar one to prettier we already
    use.
    
    ```
    pnpm -r lint
    51.53s
    
    pnpm eslint . --ext .ts,.tsx --max-warnings 0
    18.10s
    
    pnpm eslint . --ext .ts,.tsx --max-warnings 0 --cache (with cache)
    1.82s
    ```
    
    In this PR I removed all lint scripts in packages and added global one
    in the to github workflow. It saves like 30s on my laptop because pnpm.
    Already stored cache makes linting almost instant.
    
    Also bumped all eslint dependencies.

--- packages/icons/svgo-jsx.config.ts
+++ packages/icons/svgo-jsx.config.ts
@@ -1,5 +1,5 @@
-import * as fs from "fs/promises";
-import * as path from "path";
+import * as fs from "node:fs/promises";
+import * as path from "node:path";
 import type { Config } from "@svgo/jsx";
 
 const template: Config["template"] = ({
