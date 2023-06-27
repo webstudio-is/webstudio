ee49fbbc402f5f7ab50622a84451a4fdda1ddd71
Oleg Isonen <oleg008@gmail.com>
2023-06-27 11:20:35 +0200

    unnecessary return

--- apps/builder/app/builder/features/style-panel/controls/toggle/toggle-control.tsx
+++ apps/builder/app/builder/features/style-panel/controls/toggle/toggle-control.tsx
@@ -38,7 +38,6 @@ export const ToggleGroupControl = ({
               onClick={(event) => {
                 if (event.altKey) {
                   onReset?.();
-                  return;
                 }
               }}
             >
