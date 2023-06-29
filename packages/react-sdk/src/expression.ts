37aed7edfaf8512484a188eb066cec055e982108
Bogdan Chadkin <opensource@trysound.io>
2023-06-29 14:21:10 +0300

    Add expression utilities (#1874)
    Ref https://github.com/webstudio-is/webstudio-builder/issues/1529
    
    Here added two utilities:
    
    validateExpression to parse syntax and replace identifier. For parsing
    used jsep package which is very limited subset of js.
    
    executeExpression to execute the chain of expressions. The code is
    unoptimized and will not cache results between calls. But still should
    be fast enough because expressions are indented to be quite simple.

---
+++ packages/react-sdk/src/expression.ts
@@ -0,0 +1,160 @@
+import jsep from "jsep";
+
+type TransformIdentifier = (id: string) => string;
+
+type Node = jsep.CoreExpression;
+
+const generateCode = (
+  node: Node,
+  failOnForbidden: boolean,
+  transformIdentifier: TransformIdentifier
+): string => {
+  if (node.type === "Identifier") {
+    return transformIdentifier(node.name);
+  }
+  if (node.type === "MemberExpression") {
+    if (failOnForbidden) {
+      const object = generateCode(
+        node.object as Node,
+        false,
+        transformIdentifier
+      );
+      const property = generateCode(
+        node.property as Node,
+        false,
+        transformIdentifier
+      );
+      throw Error(`Cannot access "${property}" of "${object}"`);
+    }
+    const object = generateCode(
+      node.object as Node,
+      failOnForbidden,
+      transformIdentifier
+    );
+    const property = generateCode(
+      node.property as Node,
+      failOnForbidden,
+      transformIdentifier
+    );
+    return `${object}.${property}`;
+  }
+  if (node.type === "Literal") {
+    return node.raw;
+  }
+  if (node.type === "UnaryExpression") {
+    const arg = generateCode(
+      node.argument as Node,
+      failOnForbidden,
+      transformIdentifier
+    );
+    return `${node.operator}${arg}`;
+  }
+  if (node.type === "BinaryExpression") {
+    const left = generateCode(
+      node.left as Node,
+      failOnForbidden,
+      transformIdentifier
+    );
+    const right = generateCode(
+      node.right as Node,
+      failOnForbidden,
+      transformIdentifier
+    );
+    return `${left} ${node.operator} ${right}`;
+  }
+  if (node.type === "ArrayExpression") {
+    const elements = node.elements.map((element) =>
+      generateCode(element as Node, failOnForbidden, transformIdentifier)
+    );
+    return `[${elements.join(", ")}]`;
+  }
+  if (node.type === "CallExpression") {
+    if (failOnForbidden) {
+      const callee = generateCode(
+        node.callee as Node,
+        false,
+        transformIdentifier
+      );
+      throw Error(`Cannot call "${callee}"`);
+    }
+    const callee = generateCode(
+      node.callee as Node,
+      failOnForbidden,
+      transformIdentifier
+    );
+    const args = node.arguments.map((arg) =>
+      generateCode(arg as Node, failOnForbidden, transformIdentifier)
+    );
+    return `${callee}(${args.join(", ")})`;
+  }
+  if (node.type === "ThisExpression") {
+    if (failOnForbidden) {
+      throw Error(`"this" is not supported`);
+    }
+    return "this";
+  }
+  if (node.type === "Compound") {
+    throw Error("Cannot use multiple expressions");
+  }
+  // plugin is not added
+  node.type satisfies "ConditionalExpression";
+  return "";
+};
+
+export const validateExpression = (
+  code: string,
+  transformIdentifier: TransformIdentifier = (id) => id
+) => {
+  const expression = jsep(code) as Node;
+  return generateCode(expression, true, transformIdentifier);
+};
+
+export const executeExpression = (
+  expressionId: string,
+  variables: Map<string, unknown>,
+  expressions: Map<string, string>
+) => {
+  const depsById = new Map<string, Set<string>>();
+  for (const [id, code] of expressions) {
+    const deps = new Set<string>();
+    validateExpression(code, (identifier) => {
+      if (variables.has(identifier) || expressions.has(identifier)) {
+        deps.add(identifier);
+        return identifier;
+      }
+      throw Error(`Unknown dependency "${identifier}"`);
+    });
+    depsById.set(id, deps);
+  }
+
+  // sort topologically
+  const sortedExpressions = Array.from(expressions.keys()).sort(
+    (left, right) => {
+      if (depsById.get(left)?.has(right)) {
+        return 1;
+      }
+      if (depsById.get(right)?.has(left)) {
+        return -1;
+      }
+      return 0;
+    }
+  );
+
+  // execute chain of expressions
+  let header = "";
+  for (const [id, value] of variables) {
+    header += `const ${id} = ${JSON.stringify(value)};\n`;
+  }
+  for (const id of sortedExpressions) {
+    const code = expressions.get(id);
+    if (code === undefined) {
+      continue;
+    }
+    const executeFn = new Function(`${header}\nreturn (${code});`);
+    const value = executeFn();
+    header += `const ${id} = ${JSON.stringify(value)};\n`;
+    if (id === expressionId) {
+      return value;
+    }
+  }
+};
