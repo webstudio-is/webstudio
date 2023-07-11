330fe8167d0d358a46deca79f89179b3c590ccea
istarkov <istarkov@gmail.com>
2023-07-11 12:25:05 +0300

    Show position when needed

--- apps/builder/app/builder/features/style-panel/sections/position/position.tsx
+++ apps/builder/app/builder/features/style-panel/sections/position/position.tsx
@@ -6,62 +6,129 @@ import { SelectControl, TextControl } from "../../controls";
 import { PropertyName } from "../../shared/property-name";
 import { styleConfigByName } from "../../shared/configs";
 import { PositionControl } from "./position-control";
+import { useStyleInfoByInstanceId } from "../../shared/style-info";
+import { selectedInstanceSelectorStore } from "~/shared/nano-states";
+import { useStore } from "@nanostores/react";
 
 const properties: StyleProperty[] = ["position"];
+
+const useParentStyle = () => {
+  const selectedInstanceSelector = useStore(selectedInstanceSelectorStore);
+  const parentInstanceSelector =
+    // root does not have parent
+    selectedInstanceSelector?.length === 1
+      ? undefined
+      : selectedInstanceSelector?.slice(1);
+  const parentStyleInfo = useStyleInfoByInstanceId(parentInstanceSelector);
+
+  return parentStyleInfo;
+};
+
+const positionControlVisibleProperties = [
+  "relative",
+  "absolute",
+  "fixed",
+  "sticky",
+] as const;
+
+const zIndexParents = ["flex", "grid", "inline-flex", "inline-grid"] as const;
 
 export const PositionSection = ({
   setProperty,
   deleteProperty,
   currentStyle,
   createBatchUpdate,
-}: RenderCategoryProps) => (
-  <CollapsibleSection
-    label="Position"
-    currentStyle={currentStyle}
-    properties={properties}
-  >
-    <Grid gap={2}>
-      <Grid
-        gap={2}
-        css={{
-          gridTemplateColumns: `1fr ${theme.spacing[24]}`,
-        }}
-      >
-        <PropertyName
-          label={styleConfigByName("position").label}
-          properties={["position"]}
-          style={currentStyle}
-          onReset={() => deleteProperty("position")}
-        />
-        <SelectControl
-          property={"position"}
-          currentStyle={currentStyle}
-          setProperty={setProperty}
-          deleteProperty={deleteProperty}
-        />
-      </Grid>
-      <Grid gap={3} columns={2}>
-        <PositionControl
-          currentStyle={currentStyle}
-          setProperty={setProperty}
-          deleteProperty={deleteProperty}
-          createBatchUpdate={createBatchUpdate}
-        />
-        <Grid gap={1}>
+}: RenderCategoryProps) => {
+  const parentStyle = useParentStyle();
+
+  const positionValue = currentStyle.position?.value;
+
+  const showPositionControls =
+    positionValue?.type === "keyword" &&
+    positionControlVisibleProperties.includes(positionValue.value as never);
+
+  const showZindexControl =
+    showPositionControls ||
+    (parentStyle?.display?.value.type === "keyword" &&
+      zIndexParents.includes(parentStyle?.display?.value.value as never));
+
+  const { items: unfilteredPositionItems } = styleConfigByName("position");
+
+  // Filter out "inherit" as we have no a good way to handle it
+  // @todo remove after https://github.com/webstudio-is/webstudio-builder/issues/1536
+  const positionItems = unfilteredPositionItems.filter(
+    (item) => item.name !== "inherit"
+  );
+
+  return (
+    <CollapsibleSection
+      label="Position"
+      currentStyle={currentStyle}
+      properties={properties}
+    >
+      <Grid gap={2}>
+        <Grid
+          gap={2}
+          css={{
+            gridTemplateColumns: `1fr ${theme.spacing[23]}`,
+          }}
+        >
           <PropertyName
-            label={styleConfigByName("zIndex").label}
-            properties={["zIndex"]}
+            label={styleConfigByName("position").label}
+            properties={["position"]}
             style={currentStyle}
-            onReset={() => deleteProperty("zIndex")}
+            onReset={() => deleteProperty("position")}
           />
-          <TextControl
-            property={"zIndex"}
+          <SelectControl
+            property={"position"}
             currentStyle={currentStyle}
             setProperty={setProperty}
             deleteProperty={deleteProperty}
+            items={positionItems}
           />
+          {showZindexControl && showPositionControls === false && (
+            <>
+              <PropertyName
+                label={styleConfigByName("zIndex").label}
+                properties={["zIndex"]}
+                style={currentStyle}
+                onReset={() => deleteProperty("zIndex")}
+              />
+              <TextControl
+                property={"zIndex"}
+                currentStyle={currentStyle}
+                setProperty={setProperty}
+                deleteProperty={deleteProperty}
+              />
+            </>
+          )}
         </Grid>
+
+        {showPositionControls && (
+          <Grid gap={3} columns={2}>
+            <PositionControl
+              currentStyle={currentStyle}
+              setProperty={setProperty}
+              deleteProperty={deleteProperty}
+              createBatchUpdate={createBatchUpdate}
+            />
+            <Grid gap={1}>
+              <PropertyName
+                label={styleConfigByName("zIndex").label}
+                properties={["zIndex"]}
+                style={currentStyle}
+                onReset={() => deleteProperty("zIndex")}
+              />
+              <TextControl
+                property={"zIndex"}
+                currentStyle={currentStyle}
+                setProperty={setProperty}
+                deleteProperty={deleteProperty}
+              />
+            </Grid>
+          </Grid>
+        )}
       </Grid>
-    </Grid>
-  </CollapsibleSection>
-);
+    </CollapsibleSection>
+  );
+};
