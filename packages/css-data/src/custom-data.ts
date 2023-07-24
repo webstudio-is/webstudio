26918065c2822c1ba4ae593b84fcb9dac3946810
Oleg Isonen <oleg008@gmail.com>
2023-07-24 10:54:18 +0200

    add font smoothing by default to the body

---
+++ packages/css-data/src/custom-data.ts
@@ -0,0 +1,47 @@
+import type { StyleValue } from "./schema";
+import { popularityIndex } from "./popularity-index";
+
+// Data type used before we generate a the constants.
+type RawPropertyData = {
+  unitGroups: string[];
+  inherited: boolean;
+  initial: StyleValue;
+  popularity: number;
+  appliesTo: string;
+};
+
+export const propertiesData: { [property: string]: RawPropertyData } = {};
+export const keywordValues: { [property: string]: Array<string> } = {};
+
+const getPopularityIndex = (property: string) =>
+  popularityIndex.find((data) => data.property === property)?.dayPercentage ??
+  0;
+
+propertiesData.WebkitFontSmoothing = {
+  unitGroups: [],
+  inherited: true,
+  initial: {
+    type: "keyword",
+    value: "auto",
+  },
+  popularity: getPopularityIndex("webkit-font-smoothing"),
+  appliesTo: "allElements",
+};
+keywordValues.WebkitFontSmoothing = [
+  "auto",
+  "none",
+  "antialiased",
+  "subpixel-antialiased",
+];
+
+propertiesData.MozOsxFontSmoothing = {
+  unitGroups: [],
+  inherited: true,
+  initial: {
+    type: "keyword",
+    value: "auto",
+  },
+  popularity: getPopularityIndex("moz-osx-font-smoothing"),
+  appliesTo: "allElements",
+};
+keywordValues.MozOsxFontSmoothing = ["auto", "grayscale"];
