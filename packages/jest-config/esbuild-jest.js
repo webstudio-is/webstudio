36b5c5facbdd75eb1b8ccb2691a08d732a41ded1
Bogdan Chadkin <opensource@trysound.io>
2023-07-26 21:16:55 +0300

    Migrate to custom jest transform (#1993)
    Esbuild is not officially supported by jest
    and all available packages do not fit all our need.
    
    esbuild-jest we used for a long time have problems too.
    
    The most important one is frigile heuristik to instrument any code with
    `ock(` in text. And we have some `pointerLock(` in design-system.
    
    The other problem is instrumentation converts to commonjs and result
    goes to esbuild which tries to convert back to esm but with only default
    export. So jest fails anywhere we try to import design-system in tests.
    
    The other problem is lack of maintanance and very old packages inside.
    
    Here refactored original package and removed everything we don't need
    including mocks instrumentation.
    
    This should fix the build in
    https://github.com/webstudio-is/webstudio-builder/pull/1986
    
    ```diff
    Packages:
    - 1924
    + 1782
    ```

---
+++ packages/jest-config/esbuild-jest.js
@@ -0,0 +1,43 @@
+// inspired by https://github.com/aelbore/esbuild-jest
+
+const { extname } = require("path");
+const { transformSync } = require("esbuild");
+
+const loaders = ["js", "jsx", "ts", "tsx", "json"];
+
+const createTransformer = () => ({
+  process(content, filename) {
+    const ext = extname(filename).slice(1);
+    const loader = loaders.includes(ext) ? ext : "text";
+
+    const result = transformSync(content, {
+      loader,
+      format: "esm",
+      target: "es2022",
+      // Sourcemaps are necessary for inline snapshots to work
+      // See: https://github.com/aelbore/esbuild-jest#setting-up-jest-config-file-with-transformoptions
+      sourcemap: true,
+      sourcesContent: false,
+      sourcefile: filename,
+    });
+
+    const map = {
+      ...JSON.parse(result.map),
+      sourcesContent: null,
+    };
+
+    // Append the inline sourcemap manually to ensure the "sourcesContent"
+    // is null. Otherwise, breakpoints won't pause within the actual source.
+    const code =
+      result.code +
+      "\n//# sourceMappingURL=data:application/json;base64," +
+      Buffer.from(JSON.stringify(map)).toString("base64");
+
+    return { code, map };
+  },
+});
+
+module.exports = {
+  canInstrument: true,
+  createTransformer,
+};
