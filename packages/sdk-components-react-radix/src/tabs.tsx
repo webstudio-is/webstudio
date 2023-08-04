12511c1d2ec9b5107aaaa94722335475ad1907c7
Bogdan Chadkin <trysound@yandex.ru>
2023-08-05 01:36:00 +0400

    Simplify trigger

--- packages/sdk-components-react-radix/src/tabs.tsx
+++ packages/sdk-components-react-radix/src/tabs.tsx
@@ -5,53 +5,41 @@ import {
   type ComponentPropsWithoutRef,
   type ForwardRefExoticComponent,
   type ComponentPropsWithRef,
+  type ReactNode,
   forwardRef,
   Children,
 } from "react";
 import { Root, List, Trigger, Content } from "@radix-ui/react-tabs";
-import {
-  type WebstudioAttributes,
-  getIndexWithinAncestorFromComponentProps,
-  splitPropsWithWebstudioAttributes,
-} from "@webstudio-is/react-sdk";
-
-/**
- * TabsTrigger is HTML-less components.
- * To make them work in our system, we wrap their attributes with a div that has a display: contents property.
- *
- * These divs function like fragments, with all web studio-related attributes attached to them.
- */
-const displayContentsStyle = { display: "contents" };
+import { getIndexWithinAncestorFromComponentProps } from "@webstudio-is/react-sdk";
 
 export const Tabs: ForwardRefExoticComponent<
-  WebstudioAttributes &
-    Omit<ComponentPropsWithRef<typeof Root>, "asChild" | "defaultValue">
+  Omit<ComponentPropsWithRef<typeof Root>, "asChild" | "defaultValue">
 > = Root;
 
 export const TabsList = List;
 
+/**
+ * We're not exposing the 'asChild' property for the Trigger.
+ * Instead, we're enforcing 'asChild=true' for the Trigger and making it style-less.
+ * This avoids situations where the Trigger inadvertently passes all styles to its child,
+ * which would prevent us from displaying styles properly in the builder.
+ */
 export const TabsTrigger = forwardRef<
-  HTMLDivElement,
-  WebstudioAttributes &
-    Omit<ComponentPropsWithoutRef<typeof Trigger>, "asChild">
+  HTMLButtonElement,
+  { value: string; children: ReactNode }
 >(({ value, children, ...props }, ref) => {
   const firstChild = Children.toArray(children)[0];
-  const [webstudioAttributes, restProps] =
-    splitPropsWithWebstudioAttributes(props);
   const index = getIndexWithinAncestorFromComponentProps(props);
   return (
-    <div ref={ref} style={displayContentsStyle} {...webstudioAttributes}>
-      <Trigger value={value ?? index} asChild={true} {...restProps}>
-        {firstChild ?? <button>Add button</button>}
-      </Trigger>
-    </div>
+    <Trigger ref={ref} value={value ?? index} asChild={true} {...props}>
+      {firstChild ?? <button>Add button</button>}
+    </Trigger>
   );
 });
 
 export const TabsContent = forwardRef<
   HTMLDivElement,
-  WebstudioAttributes &
-    Omit<ComponentPropsWithoutRef<typeof Content>, "asChild">
+  Omit<ComponentPropsWithoutRef<typeof Content>, "asChild">
 >(({ value, ...props }, ref) => {
   const index = getIndexWithinAncestorFromComponentProps(props);
   return <Content ref={ref} value={value ?? index} {...props} />;
