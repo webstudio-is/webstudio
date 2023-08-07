fe50a537fec8d083bae620809ea8b1a7c1ec122e
Bogdan Chadkin <opensource@trysound.io>
2023-08-07 14:11:11 +0400

    Use stream consumers while uploading (#2064)
    Looks like node now provides nice utilities to convert stream into json,
    buffer, text etc. Super handy
    
    https://nodejs.org/docs/latest-v18.x/api/webstreams.html#utility-consumers
    
    Cloudflare node compat supports this as well
    https://developers.cloudflare.com/workers/runtime-apis/nodejs/streams/

--- packages/asset-uploader/src/utils/to-uint8-array.ts
+++
@@ -1,8 +0,0 @@
-export const toUint8Array = async (data: AsyncIterable<ArrayBuffer>) => {
-  const result = [];
-  for await (const chunk of data) {
-    result.push(Buffer.from(chunk));
-  }
-
-  return new Uint8Array(Buffer.concat(result));
-};
