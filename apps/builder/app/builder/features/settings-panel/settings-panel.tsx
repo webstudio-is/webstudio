6f3ceca6cc10cecd72852dc1a92daeda7bfa6d2b
istarkov <istarkov@gmail.com>
2023-08-09 22:24:37 +0300

    Remove debug

--- apps/builder/app/builder/features/settings-panel/settings-panel.tsx
+++ apps/builder/app/builder/features/settings-panel/settings-panel.tsx
@@ -2,7 +2,6 @@ import type { Publish } from "~/shared/pubsub";
 import type { Instance } from "@webstudio-is/project-build";
 import { SettingsSection } from "./settings-section/settings-section";
 import { PropsSectionContainer } from "./props-section/props-section";
-import { Tooltip } from "@webstudio-is/design-system";
 
 export const SettingsPanelContainer = ({
   selectedInstance,
@@ -14,10 +13,6 @@ export const SettingsPanelContainer = ({
   return (
     <>
       <SettingsSection />
-      <Tooltip defaultOpen={true} content="This is a tooltip">
-        <button>TEST</button>
-      </Tooltip>
-
       <PropsSectionContainer
         publish={publish}
         selectedInstance={selectedInstance}
