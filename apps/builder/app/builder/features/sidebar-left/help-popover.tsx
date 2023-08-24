8e31b5f790ae908a2aba946833a76bf7f390c27c
Oleg Isonen <oleg008@gmail.com>
2023-08-24 17:25:36 +0200

    use new icons, use portal instead of zIndex

--- apps/builder/app/builder/features/sidebar-left/help-popover.tsx
+++ apps/builder/app/builder/features/sidebar-left/help-popover.tsx
@@ -3,16 +3,16 @@ import {
   Flex,
   Popover,
   PopoverContent,
+  PopoverPortal,
   PopoverTrigger,
-  styled,
   theme,
 } from "@webstudio-is/design-system";
 import {
   DiscordIcon,
   GithubIcon,
   TwitterIcon,
-  WebstudioLogoFlatIcon,
-  YoutubeIcon,
+  Webstudio1cIcon,
+  Youtube1cIcon,
 } from "@webstudio-is/icons";
 import { type ComponentProps } from "react";
 
@@ -27,66 +27,65 @@ export const HelpPopover = ({
   return (
     <Popover {...popoverProps}>
       {children}
-      <PopoverContent
-        avoidCollisions
-        sideOffset={0}
-        collisionPadding={{ bottom: 30 }}
-        side={side}
-        css={{
-          zIndex: theme.zIndices["max"],
-        }}
-      >
-        <Flex
-          as="form"
-          target="_blank"
-          direction="column"
-          align="stretch"
-          css={{ padding: `${theme.spacing[3]} ${theme.spacing[7]}` }}
-          gap="2"
+      <PopoverPortal>
+        <PopoverContent
+          avoidCollisions
+          sideOffset={0}
+          collisionPadding={{ bottom: 30 }}
+          side={side}
         >
-          <Button
-            formAction="https://www.youtube.com/playlist"
-            name="list"
-            value="PL4vVqpngzeT4sDlanyPe99dYl8BgUYCac"
-            prefix={<YoutubeIcon />}
-            color="destructive"
+          <Flex
+            as="form"
+            target="_blank"
+            direction="column"
+            align="stretch"
+            css={{ px: theme.spacing[7], py: theme.spacing[3] }}
+            gap="2"
           >
-            Learn with videos
-          </Button>
+            <Button
+              formAction="https://www.youtube.com/playlist"
+              name="list"
+              value="PL4vVqpngzeT4sDlanyPe99dYl8BgUYCac"
+              prefix={<Youtube1cIcon />}
+              color="destructive"
+            >
+              Learn with videos
+            </Button>
 
-          <Button
-            formAction="https://webstudio.is/blog"
-            prefix={<WebstudioLogoFlatIcon />}
-            color="gradient"
-          >
-            Learn on our blog
-          </Button>
+            <Button
+              formAction="https://webstudio.is/blog"
+              prefix={<Webstudio1cIcon />}
+              color="gradient"
+            >
+              Learn on our blog
+            </Button>
 
-          <Button
-            formAction="https://discord.gg/UNdyrDkq5r"
-            prefix={<DiscordIcon />}
-            color="primary"
-          >
-            Chat with us on Discord
-          </Button>
+            <Button
+              formAction="https://discord.gg/UNdyrDkq5r"
+              prefix={<DiscordIcon />}
+              color="primary"
+            >
+              Chat with us on Discord
+            </Button>
 
-          <Button
-            formAction="https://github.com/webstudio-is/webstudio-community/discussions"
-            prefix={<GithubIcon />}
-            color="dark"
-          >
-            Join Github discussions
-          </Button>
+            <Button
+              formAction="https://github.com/webstudio-is/webstudio-community/discussions"
+              prefix={<GithubIcon />}
+              color="dark"
+            >
+              Join Github discussions
+            </Button>
 
-          <Button
-            formAction="https://twitter.com/getwebstudio"
-            prefix={<TwitterIcon />}
-            color="dark"
-          >
-            Follow us on Twitter
-          </Button>
-        </Flex>
-      </PopoverContent>
+            <Button
+              formAction="https://twitter.com/getwebstudio"
+              prefix={<TwitterIcon />}
+              color="dark"
+            >
+              Follow us on Twitter
+            </Button>
+          </Flex>
+        </PopoverContent>
+      </PopoverPortal>
     </Popover>
   );
 };
