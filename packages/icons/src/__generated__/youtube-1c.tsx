b8457fc51dc7a6248b4cb299e3e7f42c7bcf9988
Oleg Isonen <oleg008@gmail.com>
2023-08-24 17:25:19 +0200

    replace icons

---
+++ packages/icons/src/__generated__/youtube-1c.tsx
@@ -0,0 +1,15 @@
+// Generated from icons/youtube-1c.svg
+
+import { forwardRef } from "react";
+import type { IconProps } from "../types";
+
+// prettier-ignore
+export const Youtube1cIcon = forwardRef<SVGSVGElement, IconProps>(
+  ({ color = "currentColor", size = 16, ...props }, forwardedRef) => {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width={size} height={size} fill={color} {...props} ref={forwardedRef}><path fillRule="evenodd" d="M13.47 3.299a1.771 1.771 0 0 1 1.238 1.263C15 5.675 15 8 15 8s0 2.325-.293 3.438a1.771 1.771 0 0 1-1.238 1.263C12.38 13 8 13 8 13s-4.378 0-5.47-.299a1.771 1.771 0 0 1-1.237-1.263C1 10.325 1 8 1 8s0-2.325.293-3.438A1.771 1.771 0 0 1 2.53 3.299C3.622 3 8 3 8 3s4.378 0 5.47.299Zm-3.232 4.7L6.6 10.144V5.857L10.238 8Z" clipRule="evenodd" /></svg>
+    );
+  }
+);
+
+Youtube1cIcon.displayName = "Youtube1cIcon";
