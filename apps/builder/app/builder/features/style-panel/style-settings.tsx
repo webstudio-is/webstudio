9647e90f524a42499b93e6da55c043bb4c159bc9
istarkov <istarkov@gmail.com>
2023-08-24 19:50:45 +0300

    Fix parent style calculation for unstylable parent components

--- apps/builder/app/builder/features/style-panel/style-settings.tsx
+++ apps/builder/app/builder/features/style-panel/style-settings.tsx
@@ -13,26 +13,14 @@ import {
   selectedInstanceIntanceToTagStore,
   selectedInstanceSelectorStore,
 } from "~/shared/nano-states";
-import { useStyleInfoByInstanceId } from "./shared/style-info";
 import { computed } from "nanostores";
+import { useParentStyle } from "./parent-style";
 
 export type StyleSettingsProps = {
   currentStyle: StyleInfo;
   setProperty: SetProperty;
   deleteProperty: (property: StyleProperty) => void;
   createBatchUpdate: CreateBatchUpdate;
-};
-
-const useParentStyle = () => {
-  const selectedInstanceSelector = useStore(selectedInstanceSelectorStore);
-  const parentInstanceSelector =
-    // root does not have parent
-    selectedInstanceSelector?.length === 1
-      ? undefined
-      : selectedInstanceSelector?.slice(1);
-  const parentStyleInfo = useStyleInfoByInstanceId(parentInstanceSelector);
-
-  return parentStyleInfo;
 };
 
 const selectedInstanceTagStore = computed(
