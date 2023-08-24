9336f05f257d85711a77a1c115481993e353b1aa
Oleg Isonen <oleg008@gmail.com>
2023-08-24 22:44:38 +0200

    Add help button with popover in dashboard and builder (#2166)
    Originally started in
    https://github.com/webstudio-is/webstudio-builder/pull/2132
    
    Closes https://github.com/webstudio-is/webstudio-builder/issues/2039
    
    ## Description
    
    Help dialog in builder and dashboard
    
    ## Steps for reproduction
    
    1. open builder
    2. open help dialog in the left sidebar
    3. open dashboard
    4. click help button
    
    ## Code Review
    
    - [ ] hi @istarkov , I need you to do
      - conceptual review (architecture, feature-correctness)
      - detailed review (read every line)
      - test it on preview
    
    ## Before requesting a review
    
    - [ ] made a self-review
    - [ ] added inline comments where things may be not obvious (the "why",
    not "what")
    
    ## Before merging
    
    - [ ] tested locally and on preview environment (preview dev login:
    5de6)
    - [ ] updated [test
    cases](https://github.com/webstudio-is/webstudio-builder/blob/main/apps/builder/docs/test-cases.md)
    document
    - [ ] added tests
    - [ ] if any new env variables are added, added them to `.env.example`
    and the `builder/env-check.js` if mandatory
    
    ---------
    
    Co-authored-by: wasimTQ <wasimthoufiq21@gmail.com>

---
+++ packages/icons/src/__generated__/webstudio-1c.tsx
@@ -0,0 +1,15 @@
+// Generated from icons/webstudio-1c.svg
+
+import { forwardRef } from "react";
+import type { IconProps } from "../types";
+
+// prettier-ignore
+export const Webstudio1cIcon = forwardRef<SVGSVGElement, IconProps>(
+  ({ color = "currentColor", size = 16, ...props }, forwardedRef) => {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width={size} height={size} fill={color} {...props} ref={forwardedRef}><path fillRule="evenodd" d="m12.32 12.416 2.62-8.085a1.205 1.205 0 1 0-2.292-.746l-2.62 8.084a1.205 1.205 0 1 0 2.292.747Z" clipRule="evenodd" /><path fillRule="evenodd" d="M8 7.624c.297 0 .517.175.704.394.207.243.373.545.514.866.634 1.44.753 3.241.753 3.241a1.206 1.206 0 0 0 1.285 1.122 1.207 1.207 0 0 0 1.12-1.287s-.16-2.25-.952-4.05C10.744 6.364 9.594 5.208 8 5.208c-1.594 0-2.744 1.156-3.424 2.7-.792 1.8-.951 4.05-.951 4.05a1.207 1.207 0 0 0 1.12 1.288 1.206 1.206 0 0 0 1.284-1.122s.119-1.8.753-3.24a3.52 3.52 0 0 1 .514-.867c.187-.22.406-.394.704-.394Z" clipRule="evenodd" /><path fillRule="evenodd" d="M5.973 11.669 3.352 3.585a1.205 1.205 0 1 0-2.293.746l2.622 8.084a1.205 1.205 0 1 0 2.292-.746Z" clipRule="evenodd" /></svg>
+    );
+  }
+);
+
+Webstudio1cIcon.displayName = "Webstudio1cIcon";
