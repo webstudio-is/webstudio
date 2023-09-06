d624b79b28cebff95df5f465353eb138e03c22a3
Bogdan Chadkin <trysound@yandex.ru>
2023-09-06 22:14:53 +0400

    Fix radix tabs and accordion on published sites
    Missing piece to make accordion and tabs components work on published sites.

--- packages/cli/src/prebuild.ts
+++ packages/cli/src/prebuild.ts
@@ -6,6 +6,7 @@ import { cwd } from "node:process";
 import {
   generateCssText,
   generateUtilsExport,
+  namespaceMeta,
   type Params,
   type WsComponentMeta,
 } from "@webstudio-is/react-sdk";
@@ -205,7 +206,12 @@ export const prebuild = async () => {
     radixComponentMetas
   ).reduce(
     (r, [name, meta]) => {
-      r[`@webstudio-is/sdk-components-react-radix:${name}`] = meta;
+      const namespace = "@webstudio-is/sdk-components-react-radix";
+      r[`${namespace}:${name}`] = namespaceMeta(
+        meta,
+        namespace,
+        new Set(Object.keys(radixComponentMetas))
+      );
       return r;
     },
     {} as Record<string, WsComponentMeta>
