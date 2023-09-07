242e3244b981a220c06639a41228b3bbf6898799
Bogdan Chadkin <trysound@yandex.ru>
2023-09-07 23:37:09 +0400

    Simplify generated remix overrides
    Here replaced Object.assign logic with overriding components
    at build time. This will simplify migration to jsx generator
    and let bundler load less components.
    
    Changed fixture to include link from remix overrides.

--- packages/sdk-components-react-remix/src/metas.ts
+++ packages/sdk-components-react-remix/src/metas.ts
@@ -1,1 +1,3 @@
+export { meta as Link } from "./link.ws";
+export { meta as RichTextLink } from "./rich-text-link.ws";
 export { meta as Form } from "./form.ws";
--- packages/sdk-components-react-remix/src/props.ts
+++ packages/sdk-components-react-remix/src/props.ts
@@ -1,3 +1,3 @@
-export { propsMeta as Form } from "./form.ws";
 export { propsMeta as Link } from "./link.ws";
 export { propsMeta as RichTextLink } from "./rich-text-link.ws";
+export { propsMeta as Form } from "./form.ws";
