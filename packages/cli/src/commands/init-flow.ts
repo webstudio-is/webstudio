47d0e565382bf18ca4a144fd86d75bb571c30847
Jaya Krishna <namburu1995@gmail.com>
2023-09-07 14:48:47 +0530

    change the sequence of prompts and cancel the flow when the abort signal is received

--- packages/cli/src/commands/init-flow.ts
+++ packages/cli/src/commands/init-flow.ts
@@ -22,34 +22,47 @@ export const initFlow = async (
         name: "folder",
         message: "Do you want to create a folder",
         initial: true,
+        onState: (state) => {
+          if (state.aborted) {
+            process.nextTick(() => {
+              process.exit(0);
+            });
+          }
+        },
       },
       {
         type: (prev) => (prev === true ? "text" : null),
         name: "folderName",
         message: "Enter a project name",
+        onState: (state) => {
+          if (state.aborted) {
+            process.nextTick(() => {
+              process.exit(0);
+            });
+          }
+        },
       },
       {
         type: (prev) => (prev === undefined ? null : "text"),
         name: "projectLink",
         message: "Enter a project link",
+        onState: (state) => {
+          if (state.aborted) {
+            process.nextTick(() => {
+              process.exit(0);
+            });
+          }
+        },
       }
     );
-  }
-
-  const response = await prompts(prompsList);
-
-  /*
-    If users wanted to create a folder, we need to create it and link the project.
-  */
-  if (response.folderName) {
-    await ensureFolderExists(join(cwd(), response.folderName));
-    chdir(join(cwd(), response.folderName));
+    const response = await prompts(prompsList);
+    if (response.folderName) {
+      await ensureFolderExists(join(cwd(), response.folderName));
+      chdir(join(cwd(), response.folderName));
+    }
     await link({ link: response.projectLink });
   }
 
-  /*
-    If the project is already set up, we can sync and build it.
-  */
   await sync();
   await build(options);
   const spinner = ora().start();
