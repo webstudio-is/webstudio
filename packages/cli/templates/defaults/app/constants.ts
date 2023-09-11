bc279c0ae76393d7f6c001ad40a3c40f1c05a1dc
istarkov <istarkov@gmail.com>
2023-09-11 19:34:13 +0300

    Add vercel images support

--- packages/cli/templates/defaults/app/constants.ts
+++ packages/cli/templates/defaults/app/constants.ts
@@ -1,2 +1,24 @@
+import type { ImageLoader } from "@webstudio-is/image";
+
 export const assetBaseUrl = "/assets/";
 export const imageBaseUrl = "/assets/";
+
+export const imageLoader: ImageLoader = ({ quality, src, width }) => {
+  if (process.env.NODE_ENV !== "production") {
+    return imageBaseUrl + src;
+  }
+
+  const searchParams = new URLSearchParams();
+  searchParams.set("w", width.toString());
+  searchParams.set("q", quality.toString());
+
+  // https://vercel.com/blog/build-your-own-web-framework#automatic-image-optimization
+  return (
+    "/_vercel/image?url=" +
+    encodeURIComponent(src) +
+    "&w=" +
+    width +
+    "&q=" +
+    quality
+  );
+};
