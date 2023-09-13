f15ea30ae1a70373b2e9779abe33e40296a9c372
Bogdan Chadkin <opensource@trysound.io>
2023-09-13 23:02:58 +0300

    Add jsx component generator (#2291)
    Here added jsx component generator. It allows us to have less tree
    traversing and more clear output for debugging.
    
    For now data sources are managed the old way with stores. In the future
    we can switch to react `useState`.
    
    In the future will be integrated into stories generator and cli

--- packages/react-sdk/src/index.ts
+++ packages/react-sdk/src/index.ts
@@ -37,3 +37,4 @@ export { renderComponentTemplate } from "./component-renderer";
 export { getIndexesWithinAncestors } from "./instance-utils";
 export * from "./hook";
 export { generateUtilsExport } from "./generator";
+export { generatePageComponent } from "./component-generator";
