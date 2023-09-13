0784f0465d6ade48a81ed8718aed5d93464dee19
Bogdan Chadkin <opensource@trysound.io>
2023-09-13 18:09:11 +0300

    Replace sharp with image-size (#2303)
    Sharp is image processing library. Though we use it only to detect
    format and size of images. Proocessing is done on build step or on
    request by vercel or cloudflare.
    
    Here I replaced sharp with smaller and simpler alternative designed
    specifically for format and size detection. Potentially this library can
    be used for uploading in worker.
    
    https://github.com/image-size/image-size
    
    ```diff
    du -ckd1 node_modules
    - 680068
    + 659988
    Packages
    - 1983
    + 1962
    ```

--- packages/asset-uploader/src/utils/get-asset-data.ts
+++ packages/asset-uploader/src/utils/get-asset-data.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import sharp from "sharp";
+import { imageSize } from "image-size";
 import { FontMeta } from "@webstudio-is/fonts";
 import { ImageMeta } from "@webstudio-is/sdk";
 import { getFontData } from "./font-data";
@@ -27,15 +27,24 @@ export const getAssetData = async (
   options: AssetOptions
 ): Promise<AssetData> => {
   if (options.type === "image") {
-    const sharpImage = sharp(options.data);
-    const { width, height, format } = await sharpImage.metadata();
-    if (format === undefined) {
+    let image: undefined | { format: string; width: number; height: number };
+    try {
+      const parsed = imageSize(Buffer.from(options.data));
+      if (parsed.type && parsed.width && parsed.height) {
+        image = {
+          format: parsed.type,
+          width: parsed.width,
+          height: parsed.height,
+        };
+      }
+    } catch {
+      // empty block
+    }
+    if (image === undefined) {
       throw new Error("Unknown image format");
     }
-    if (width === undefined || height === undefined) {
-      throw new Error("Unknown image dimensions");
-    }
 
+    const { format, width, height } = image;
     return {
       size: options.size,
       format,
