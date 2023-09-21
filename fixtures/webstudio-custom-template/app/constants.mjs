00a735c22567287f8e78386b47ec942d16a40b56
Bogdan Chadkin <trysound@yandex.ru>
2023-09-21 15:11:40 +0400

    Fix custom template generation

--- fixtures/webstudio-custom-template/app/constants.mjs
+++ fixtures/webstudio-custom-template/app/constants.mjs
@@ -2,8 +2,8 @@
  * We use mjs extension as constants in this file is shared with the build script
  * and we use `node --eval` to extract the constants.
  */
-export const assetBaseUrl = "/assets/";
-export const imageBaseUrl = "/assets/";
+export const assetBaseUrl = "/custom-folder/";
+export const imageBaseUrl = "/custom-folder/";
 
 /**
  * @type {import("@webstudio-is/image").ImageLoader}
