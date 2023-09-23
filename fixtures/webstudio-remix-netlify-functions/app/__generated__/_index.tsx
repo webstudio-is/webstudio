ae36b78936de8be6955fbd923942ade64f987ead
Jaya Krishna <namburu1995@gmail.com>
2023-09-23 15:16:14 +0530

    Update example project fox fixture

--- fixtures/webstudio-remix-netlify-functions/app/__generated__/_index.tsx
+++ fixtures/webstudio-remix-netlify-functions/app/__generated__/_index.tsx
@@ -7,158 +7,46 @@ import type { Asset } from "@webstudio-is/sdk";
 import {
   Body as Body,
   Heading as Heading,
-  Box as Box,
-  Paragraph as Paragraph,
-  Image as Image,
+  Text as Text,
 } from "@webstudio-is/sdk-components-react";
-import { Link as Link } from "@webstudio-is/sdk-components-react-remix";
 
 export const fontAssets: Asset[] = [];
 export const pageData: PageData = {
-  build: {
-    props: [
-      [
-        "rTRZFZEd03RBH4gUWj9LW",
-        {
-          id: "rTRZFZEd03RBH4gUWj9LW",
-          instanceId: "pX1ovPI7NdC0HRjkw6Kpw",
-          name: "src",
-          type: "asset",
-          value: "cd939c56-bcdd-4e64-bd9c-567a9bccd3da",
-        },
-      ],
-      [
-        "SYK4hpLQ9tHnESKDtPvI9",
-        {
-          id: "SYK4hpLQ9tHnESKDtPvI9",
-          instanceId: "l9AI_pShC-BH4ibxK6kNT",
-          name: "href",
-          type: "string",
-          value: "https://github.com/",
-        },
-      ],
-      [
-        "tUn-hTQ0dKjsaRZ4q3m21",
-        {
-          id: "tUn-hTQ0dKjsaRZ4q3m21",
-          instanceId: "9I4GRU1sev48hREkQcKQ-",
-          name: "href",
-          type: "page",
-          value: "szYLvBduHPmbtqQKCDY0b",
-        },
-      ],
-    ],
-  },
+  build: { props: [] },
   pages: [
     {
-      id: "7Db64ZXgYiRqKSQNR-qTQ",
+      id: "9di_L14CzctvSruIoKVvE",
       name: "Home",
       title: "Home",
       meta: {},
-      rootInstanceId: "On9cvWCxr5rdZtY9O1Bv0",
+      rootInstanceId: "MMimeobf_zi4ZkRGXapju",
       path: "",
-    },
-    {
-      id: "xfvB4UThQXmQ_OubPYrkg",
-      name: "radix",
-      title: "radix",
-      meta: { description: "" },
-      rootInstanceId: "uKWGyE9JY3cPwY-xI9vk6",
-      path: "/radix",
-    },
-    {
-      id: "szYLvBduHPmbtqQKCDY0b",
-      name: "RouteWithSymbols",
-      title: "RouteWithSymbols",
-      meta: { description: "" },
-      rootInstanceId: "EDEfpMPRqDejthtwkH7ws",
-      path: "/_route_with_symbols_",
-    },
-    {
-      id: "U1tRJl2ERr8_OFe0g9cN_",
-      name: "form",
-      title: "form",
-      meta: { description: "" },
-      rootInstanceId: "a-4nDFkaWy4px1fn38XWJ",
-      path: "/form",
     },
   ],
   page: {
-    id: "7Db64ZXgYiRqKSQNR-qTQ",
+    id: "9di_L14CzctvSruIoKVvE",
     name: "Home",
     title: "Home",
     meta: {},
-    rootInstanceId: "On9cvWCxr5rdZtY9O1Bv0",
+    rootInstanceId: "MMimeobf_zi4ZkRGXapju",
     path: "",
   },
-  assets: [
-    {
-      id: "9a8bc926-7804-4d3f-af81-69196b1d2ed8",
-      name: "small-avif-kitty_FnabJsioMWpBtXZSGf4DR.webp",
-      description: null,
-      projectId: "cddc1d44-af37-4cb6-a430-d300cf6f932d",
-      size: 2906,
-      type: "image",
-      format: "webp",
-      createdAt: "2023-09-12T09:44:22.120Z",
-      meta: { width: 100, height: 100 },
-    },
-    {
-      id: "cd939c56-bcdd-4e64-bd9c-567a9bccd3da",
-      name: "_937084ed-a798-49fe-8664-df93a2af605e_uiBk3o6UWdqolyakMvQJ9.jpeg",
-      description: null,
-      projectId: "cddc1d44-af37-4cb6-a430-d300cf6f932d",
-      size: 210614,
-      type: "image",
-      format: "jpeg",
-      createdAt: "2023-09-06T11:28:43.031Z",
-      meta: { width: 1024, height: 1024 },
-    },
-  ],
+  assets: [],
 };
 export const user: { email: string | null } | undefined = {
   email: "hello@webstudio.is",
 };
-export const projectId = "cddc1d44-af37-4cb6-a430-d300cf6f932d";
+export const projectId = "d845c167-ea07-4875-b08d-83e97c09dcce";
 
 const Page = (props: { scripts?: ReactNode }) => {
   return (
-    <Body data-ws-id="On9cvWCxr5rdZtY9O1Bv0" data-ws-component="Body">
-      <Heading data-ws-id="nVMWvMsaLCcb0o1wuNQgg" data-ws-component="Heading">
-        {"DO NOT TOUCH THIS PROJECT, IT'S USED FOR FIXTURES"}
+    <Body data-ws-id="MMimeobf_zi4ZkRGXapju" data-ws-component="Body">
+      <Heading data-ws-id="MYDt0guk1-vzc7yzqyN6A" data-ws-component="Heading">
+        {"Simple Project to test CLI"}
       </Heading>
-      <Box data-ws-id="f0kF-WmL7DQg7MSyRvqY1" data-ws-component="Box">
-        <Box data-ws-id="5XDbqPrZDeCwq4YJ3CHsc" data-ws-component="Box">
-          <Heading
-            data-ws-id="oLXYe1UQiVMhVnZGvJSMr"
-            data-ws-component="Heading"
-          >
-            {"Heading"}
-          </Heading>
-          <Paragraph
-            data-ws-id="p34JHWcU6UNrd9FVnY80Q"
-            data-ws-component="Paragraph"
-          >
-            {
-              "a little kitten painted in black and white gouache with a thick brush"
-            }
-          </Paragraph>
-          <Link
-            data-ws-id="l9AI_pShC-BH4ibxK6kNT"
-            data-ws-component="Link"
-            href={"https://github.com/"}
-          >
-            {"Click here to adore more kittens"}
-          </Link>
-          <Box data-ws-id="82HYqzxZeahPxSDFNWem5" data-ws-component="Box" />
-          <Link data-ws-id="9I4GRU1sev48hREkQcKQ-" data-ws-component="Link">
-            {"Symbols in path"}
-          </Link>
-        </Box>
-        <Box data-ws-id="qPnkiFGDj8dITWb1kmpGl" data-ws-component="Box">
-          <Image data-ws-id="pX1ovPI7NdC0HRjkw6Kpw" data-ws-component="Image" />
-        </Box>
-      </Box>
+      <Text data-ws-id="BMJfjOzunWs8XkQgvvx1e" data-ws-component="Text">
+        {"Please don't change directly in the fixture"}
+      </Text>
       {props.scripts}
     </Body>
   );
