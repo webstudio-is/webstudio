be84c1046088d644976f1cd80b407a987fc71912
Jaya Krishna <namburu1995@gmail.com>
2023-09-25 19:46:35 +0530

    update ci test file and update fixtures

---
+++ fixtures/webstudio-custom-template/app/__generated__/[radix]._index.tsx
@@ -0,0 +1,316 @@
+/* eslint-disable */
+/* This is a auto generated file for building the project */
+
+import { type ReactNode, useState } from "react";
+import type { PageData } from "~/routes/_index";
+import type { Asset } from "@webstudio-is/sdk";
+import {
+  Body as Body,
+  Text as Text,
+  Box as Box,
+  HtmlEmbed as HtmlEmbed,
+} from "@webstudio-is/sdk-components-react";
+import {
+  Accordion as Accordion,
+  AccordionItem as AccordionItem,
+  AccordionHeader as AccordionHeader,
+  AccordionTrigger as AccordionTrigger,
+  AccordionContent as AccordionContent,
+} from "@webstudio-is/sdk-components-react-radix";
+
+export const fontAssets: Asset[] = [];
+export const pageData: PageData = {
+  build: {
+    props: [
+      [
+        "_x-dxwbTQ-XBLRuYQE9Pm",
+        {
+          id: "_x-dxwbTQ-XBLRuYQE9Pm",
+          instanceId: "AM9fD6dv2Ftc3Xjcsd7Uc",
+          name: "collapsible",
+          type: "boolean",
+          value: true,
+        },
+      ],
+      [
+        "XDeoQFsXw3NhVjns6I5HM",
+        {
+          id: "XDeoQFsXw3NhVjns6I5HM",
+          instanceId: "AM9fD6dv2Ftc3Xjcsd7Uc",
+          name: "value",
+          type: "dataSource",
+          value: "RR_FthRebEUcAKUJIXl0j",
+        },
+      ],
+      [
+        "UZg-1PcrNODgUo94IvnGC",
+        {
+          id: "UZg-1PcrNODgUo94IvnGC",
+          instanceId: "AM9fD6dv2Ftc3Xjcsd7Uc",
+          name: "onValueChange",
+          type: "action",
+          value: [
+            {
+              type: "execute",
+              args: ["value"],
+              code: "$ws$dataSource$RR_FthRebEUcAKUJIXl0j = value",
+            },
+          ],
+        },
+      ],
+      [
+        "XxpufvbtZ9iuhkPDDigde",
+        {
+          id: "XxpufvbtZ9iuhkPDDigde",
+          instanceId: "d0sd_G-kHirxgjq6s6Uq1",
+          name: "code",
+          type: "string",
+          value:
+            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" width="100%" height="100%" style="display: block;"><path d="M4.04 6.284a.65.65 0 0 1 .92.001L8 9.335l3.04-3.05a.65.65 0 1 1 .921.918l-3.5 3.512a.65.65 0 0 1-.921 0L4.039 7.203a.65.65 0 0 1 .001-.92Z"/></svg>',
+        },
+      ],
+      [
+        "d26zsuAR2XZt1RRN6oJXk",
+        {
+          id: "d26zsuAR2XZt1RRN6oJXk",
+          instanceId: "StPslEr81nfBISqBE2R-Y",
+          name: "code",
+          type: "string",
+          value:
+            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" width="100%" height="100%" style="display: block;"><path d="M4.04 6.284a.65.65 0 0 1 .92.001L8 9.335l3.04-3.05a.65.65 0 1 1 .921.918l-3.5 3.512a.65.65 0 0 1-.921 0L4.039 7.203a.65.65 0 0 1 .001-.92Z"/></svg>',
+        },
+      ],
+      [
+        "8Ar0H_oY5LOeyp2mMksps",
+        {
+          id: "8Ar0H_oY5LOeyp2mMksps",
+          instanceId: "sO80m5u4f87jVGG91t6u8",
+          name: "code",
+          type: "string",
+          value:
+            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" width="100%" height="100%" style="display: block;"><path d="M4.04 6.284a.65.65 0 0 1 .92.001L8 9.335l3.04-3.05a.65.65 0 1 1 .921.918l-3.5 3.512a.65.65 0 0 1-.921 0L4.039 7.203a.65.65 0 0 1 .001-.92Z"/></svg>',
+        },
+      ],
+    ],
+  },
+  pages: [
+    {
+      id: "7Db64ZXgYiRqKSQNR-qTQ",
+      name: "Home",
+      title: "Home",
+      meta: {},
+      rootInstanceId: "On9cvWCxr5rdZtY9O1Bv0",
+      path: "",
+    },
+    {
+      id: "xfvB4UThQXmQ_OubPYrkg",
+      name: "radix",
+      title: "radix",
+      meta: { description: "" },
+      rootInstanceId: "uKWGyE9JY3cPwY-xI9vk6",
+      path: "/radix",
+    },
+    {
+      id: "szYLvBduHPmbtqQKCDY0b",
+      name: "RouteWithSymbols",
+      title: "RouteWithSymbols",
+      meta: { description: "" },
+      rootInstanceId: "EDEfpMPRqDejthtwkH7ws",
+      path: "/_route_with_symbols_",
+    },
+    {
+      id: "U1tRJl2ERr8_OFe0g9cN_",
+      name: "form",
+      title: "form",
+      meta: { description: "" },
+      rootInstanceId: "a-4nDFkaWy4px1fn38XWJ",
+      path: "/form",
+    },
+    {
+      id: "-J9I4Oo6mONfQlf_3-OqG",
+      name: "heading-with-id",
+      title: "heading-with-id",
+      meta: { description: "" },
+      rootInstanceId: "O-ljaGZQ0iRNTlEshMkgE",
+      path: "/heading-with-id",
+    },
+  ],
+  page: {
+    id: "xfvB4UThQXmQ_OubPYrkg",
+    name: "radix",
+    title: "radix",
+    meta: { description: "" },
+    rootInstanceId: "uKWGyE9JY3cPwY-xI9vk6",
+    path: "/radix",
+  },
+  assets: [
+    {
+      id: "9a8bc926-7804-4d3f-af81-69196b1d2ed8",
+      name: "small-avif-kitty_FnabJsioMWpBtXZSGf4DR.webp",
+      description: null,
+      projectId: "cddc1d44-af37-4cb6-a430-d300cf6f932d",
+      size: 2906,
+      type: "image",
+      format: "webp",
+      createdAt: "2023-09-12T09:44:22.120Z",
+      meta: { width: 100, height: 100 },
+    },
+    {
+      id: "cd939c56-bcdd-4e64-bd9c-567a9bccd3da",
+      name: "_937084ed-a798-49fe-8664-df93a2af605e_uiBk3o6UWdqolyakMvQJ9.jpeg",
+      description: null,
+      projectId: "cddc1d44-af37-4cb6-a430-d300cf6f932d",
+      size: 210614,
+      type: "image",
+      format: "jpeg",
+      createdAt: "2023-09-06T11:28:43.031Z",
+      meta: { width: 1024, height: 1024 },
+    },
+  ],
+};
+export const user: { email: string | null } | undefined = {
+  email: "hello@webstudio.is",
+};
+export const projectId = "cddc1d44-af37-4cb6-a430-d300cf6f932d";
+
+const Page = (props: { scripts?: ReactNode }) => {
+  let [accordionValue, set$accordionValue] = useState<any>("0");
+  let onValueChange = (value: any) => {
+    accordionValue = value;
+    set$accordionValue(accordionValue);
+  };
+  return (
+    <Body data-ws-id="uKWGyE9JY3cPwY-xI9vk6" data-ws-component="Body">
+      <Accordion
+        data-ws-id="AM9fD6dv2Ftc3Xjcsd7Uc"
+        data-ws-component="@webstudio-is/sdk-components-react-radix:Accordion"
+        collapsible={true}
+        value={accordionValue}
+        onValueChange={onValueChange}
+      >
+        <AccordionItem
+          data-ws-id="zJ927zk9txwUbYycKB7QA"
+          data-ws-component="@webstudio-is/sdk-components-react-radix:AccordionItem"
+          data-ws-index="0"
+        >
+          <AccordionHeader
+            data-ws-id="sMxg7xT1hwYt05hbOvoPL"
+            data-ws-component="@webstudio-is/sdk-components-react-radix:AccordionHeader"
+          >
+            <AccordionTrigger
+              data-ws-id="qQSA4NoyKC88O68mBiQe2"
+              data-ws-component="@webstudio-is/sdk-components-react-radix:AccordionTrigger"
+            >
+              <Text data-ws-id="q-DVI4YTNrQ1LizmEyJHI" data-ws-component="Text">
+                {"Is it accessible?"}
+              </Text>
+              <Box data-ws-id="RSk81lLj2IGXgchTuXF7V" data-ws-component="Box">
+                <HtmlEmbed
+                  data-ws-id="d0sd_G-kHirxgjq6s6Uq1"
+                  data-ws-component="HtmlEmbed"
+                  code={
+                    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" width="100%" height="100%" style="display: block;"><path d="M4.04 6.284a.65.65 0 0 1 .92.001L8 9.335l3.04-3.05a.65.65 0 1 1 .921.918l-3.5 3.512a.65.65 0 0 1-.921 0L4.039 7.203a.65.65 0 0 1 .001-.92Z"/></svg>'
+                  }
+                />
+              </Box>
+            </AccordionTrigger>
+          </AccordionHeader>
+          <AccordionContent
+            data-ws-id="IUftdfjK-ilSzfOTdIx1u"
+            data-ws-component="@webstudio-is/sdk-components-react-radix:AccordionContent"
+          >
+            {"Yes. It adheres to the WAI-ARIA design pattern."}
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem
+          data-ws-id="C838wkvIcA1BQu30Xu2G8"
+          data-ws-component="@webstudio-is/sdk-components-react-radix:AccordionItem"
+          data-ws-index="1"
+        >
+          <AccordionHeader
+            data-ws-id="fYUOB_brm6s0Ky68lzMfU"
+            data-ws-component="@webstudio-is/sdk-components-react-radix:AccordionHeader"
+          >
+            <AccordionTrigger
+              data-ws-id="dfd4gonev_AX6BpuCsxjb"
+              data-ws-component="@webstudio-is/sdk-components-react-radix:AccordionTrigger"
+            >
+              <Text data-ws-id="lZ7sI6Kw_0VZkURriKscB" data-ws-component="Text">
+                {"Is it styled?"}
+              </Text>
+              <Box data-ws-id="wRw75kuvFzl5NWD8IGJoI" data-ws-component="Box">
+                <HtmlEmbed
+                  data-ws-id="StPslEr81nfBISqBE2R-Y"
+                  data-ws-component="HtmlEmbed"
+                  code={
+                    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" width="100%" height="100%" style="display: block;"><path d="M4.04 6.284a.65.65 0 0 1 .92.001L8 9.335l3.04-3.05a.65.65 0 1 1 .921.918l-3.5 3.512a.65.65 0 0 1-.921 0L4.039 7.203a.65.65 0 0 1 .001-.92Z"/></svg>'
+                  }
+                />
+              </Box>
+            </AccordionTrigger>
+          </AccordionHeader>
+          <AccordionContent
+            data-ws-id="wNRVuu0L5E8TVufKdswp1"
+            data-ws-component="@webstudio-is/sdk-components-react-radix:AccordionContent"
+          >
+            {
+              "Yes. It comes with default styles that matches the other components' aesthetic."
+            }
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem
+          data-ws-id="65djoTmSBGemZ2L5izQ5M"
+          data-ws-component="@webstudio-is/sdk-components-react-radix:AccordionItem"
+          data-ws-index="2"
+        >
+          <AccordionHeader
+            data-ws-id="UJYfe6kH7HqhH0YYeJwe7"
+            data-ws-component="@webstudio-is/sdk-components-react-radix:AccordionHeader"
+          >
+            <AccordionTrigger
+              data-ws-id="600nGddaNxGGdsuGgpxJR"
+              data-ws-component="@webstudio-is/sdk-components-react-radix:AccordionTrigger"
+            >
+              <Text data-ws-id="1iNKIMG91n83PzJnEdxq9" data-ws-component="Text">
+                {"Is it animated?"}
+              </Text>
+              <Box data-ws-id="Ta70VqUb_fGJXBT_zsnxQ" data-ws-component="Box">
+                <HtmlEmbed
+                  data-ws-id="sO80m5u4f87jVGG91t6u8"
+                  data-ws-component="HtmlEmbed"
+                  code={
+                    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" width="100%" height="100%" style="display: block;"><path d="M4.04 6.284a.65.65 0 0 1 .92.001L8 9.335l3.04-3.05a.65.65 0 1 1 .921.918l-3.5 3.512a.65.65 0 0 1-.921 0L4.039 7.203a.65.65 0 0 1 .001-.92Z"/></svg>'
+                  }
+                />
+              </Box>
+            </AccordionTrigger>
+          </AccordionHeader>
+          <AccordionContent
+            data-ws-id="mOVPnIrlt6IwVAzI_i2Fc"
+            data-ws-component="@webstudio-is/sdk-components-react-radix:AccordionContent"
+          >
+            {
+              "Yes. It's animated by default, but you can disable it if you prefer."
+            }
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+      {props.scripts}
+    </Body>
+  );
+};
+
+export { Page };
+
+export const pagesPaths = new Set([
+  "",
+  "/radix",
+  "/_route_with_symbols_",
+  "/form",
+  "/heading-with-id",
+]);
+
+export const formsProperties = new Map<
+  string,
+  { method?: string; action?: string }
+>([]);
