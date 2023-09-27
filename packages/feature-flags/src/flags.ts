35dab38dc0c21c1fad7626f6459db2d8aa98f31c
Jaya Krishna Namburu <namburu1995@gmail.com>
2023-09-27 23:14:25 +0530

    chore: remove radix components flag (#2384)
    ## Description
    
    Removes feature flag for radix elements in the editor.
    
    ## Code Review
    
    - [ ] hi @TrySound , I need you to do
      - detailed review (read every line)

--- packages/feature-flags/src/flags.ts
+++ packages/feature-flags/src/flags.ts
@@ -2,7 +2,6 @@ export const example = false;
 export const dark = false;
 export const unsupportedBrowsers = false;
 export const displayContents = false;
-export const radix = false;
 // @todo this should be pro users check
 export const adminRole = false;
 // A general flag to enable/disable all the AI features.
