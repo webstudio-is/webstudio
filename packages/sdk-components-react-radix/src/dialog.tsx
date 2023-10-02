ec89b4650fc16953dc565fbfa4c0bdc1277d62d4
Juan Di Toro <ditorojuanf@gmail.com>
2023-10-02 16:49:43 +0200

    refactor: better not use Heading as I dont know how forwardRef within a forwardRef behaves, also encourages isolation per lib

--- packages/sdk-components-react-radix/src/dialog.tsx
+++ packages/sdk-components-react-radix/src/dialog.tsx
@@ -6,9 +6,9 @@ import {
   type ReactNode,
   forwardRef,
   Children,
+  createElement,
 } from "react";
 import * as DialogPrimitive from "@radix-ui/react-dialog";
-import { Heading } from "@webstudio-is/sdk-components-react";
 import { getClosestInstance, type Hook } from "@webstudio-is/react-sdk";
 
 // wrap in forwardRef because Root is functional component without ref
@@ -51,14 +51,17 @@ export const DialogOverlay = forwardRef<
 
 export const DialogContent = DialogPrimitive.Content;
 export const DialogClose = DialogPrimitive.Close;
+
+type Tag = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 export const DialogTitle = forwardRef<
   HTMLHeadingElement,
-  React.ComponentProps<typeof DialogPrimitive.DialogTitle & typeof Heading>
+  React.ComponentProps<typeof DialogPrimitive.DialogTitle> & { tag?: Tag }
 >((props, ref) => (
   <DialogPrimitive.DialogTitle asChild>
-    <Heading {...props} ref={ref} />
+    {createElement(props.tag ?? "h1", { ...props, ref })}
   </DialogPrimitive.DialogTitle>
 ));
+
 export const DialogDescription = DialogPrimitive.Description;
 
 /* BUILDER HOOKS */
