92079f7635b9c0041dcb82c9914eaa90e719cfbe
istarkov <istarkov@gmail.com>
2023-10-07 10:27:54 +0400

    Add shadow

--- packages/design-system/src/components/ai-command-bar/command-bar.tsx
+++ packages/design-system/src/components/ai-command-bar/command-bar.tsx
@@ -16,6 +16,9 @@ export const CommandBar = (props: Props) => {
         padding: theme.spacing[5],
         backgroundColor: "#1D1D1D",
         borderRadius: 22,
+        border: "1px solid #323232",
+        boxShadow:
+          "0px 5px 17px 0px rgba(0, 0, 0, 0.30), 0px 2px 7px 0px rgba(0, 0, 0, 0.10)",
       }}
       gap={2}
     >
