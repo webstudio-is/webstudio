51b4d927b83c9d821460233b6c8d09917a6c04ba
istarkov <istarkov@gmail.com>
2023-10-07 12:36:10 +0400

    Add content

--- packages/design-system/src/components/ai-command-bar/command-bar.stories.tsx
+++ packages/design-system/src/components/ai-command-bar/command-bar.stories.tsx
@@ -1,23 +1,90 @@
-import { CommandBar } from "./command-bar";
+import {
+  CommandBar,
+  CommandBarContentPrompt,
+  CommandBarContentSection,
+  CommandBarContentSeparator,
+  CommandBarTrigger,
+} from "./command-bar";
 import { AutogrowTextArea } from "./autogrow-text-area";
-import { ScrollArea } from "../scroll-area";
-import { Box } from "../box";
-import { Grid } from "../grid";
 import {
   AiIcon,
   // StopIcon,
   MicIcon,
   // LargeXIcon,
   ChevronUpIcon,
+  ExternalLinkIcon,
 } from "@webstudio-is/icons";
 import { CommandButton } from "./command-button";
-import { theme } from "../..";
+import { Button, Flex, ScrollArea, Box, Grid, Text, theme } from "../..";
+import { useState } from "react";
 
 export default {
   title: "Library/AI Command Bar",
 };
 
+const CommandBarContent = () => {
+  return (
+    <>
+      <CommandBarContentSection gap={2}>
+        <Text variant={"labelsSentenceCase"} align={"center"}>
+          Welcome to Webstudio AI alpha!
+        </Text>
+        <div />
+        <Text variant={"labelsSentenceCase"}>
+          Ask me to generate or edit sections, text, or images.
+          <br /> For example you can say: ”Make a new contact section”
+        </Text>
+        <Flex align={"end"}>
+          <Button
+            color="dark"
+            css={{
+              width: theme.spacing[30],
+            }}
+            suffix={<ExternalLinkIcon />}
+          >
+            Learn more
+          </Button>
+          <Text
+            variant={"labelsSentenceCase"}
+            color={"subtle"}
+            css={{ flex: 1 }}
+            align={"center"}
+          >
+            shortcut ⌘⇧Q
+          </Text>
+        </Flex>
+      </CommandBarContentSection>
+
+      <CommandBarContentSeparator />
+
+      <CommandBarContentSection>
+        <Text variant={"labelsSentenceCase"} align={"center"}>
+          Previous propmts
+        </Text>
+        <div />
+        <CommandBarContentPrompt>
+          Make a new section with a contact form
+        </CommandBarContentPrompt>
+        <CommandBarContentPrompt>
+          Add an image of a cat smiling wearing eyeglasses
+        </CommandBarContentPrompt>
+        <CommandBarContentPrompt>
+          Write a breakup letter for my girlfriend and make it long enough to
+          break into multiple lines
+        </CommandBarContentPrompt>
+        <CommandBarContentPrompt>
+          Make all headings bold
+        </CommandBarContentPrompt>
+      </CommandBarContentSection>
+    </>
+  );
+};
+
 export const Demo = () => {
+  const [open, setOpen] = useState(false);
+
+  const commandBarContent = <CommandBarContent />;
+
   return (
     <Box
       css={{
@@ -31,15 +98,21 @@ export const Demo = () => {
         alignItems: "flex-end",
       }}
     >
-      <CommandBar>
-        <CommandButton color="dark-ghost">
-          <ChevronUpIcon />
-        </CommandButton>
+      <CommandBar
+        open={open}
+        onOpenChange={setOpen}
+        content={commandBarContent}
+      >
+        <CommandBarTrigger>
+          <CommandButton color="dark-ghost">
+            <ChevronUpIcon />
+          </CommandButton>
+        </CommandBarTrigger>
 
         <Grid
           css={{
             alignSelf: "center",
-            color: theme.colors.foregroundContrastMain,
+            // color: theme.colors.foregroundContrastMain,
             // Set maxWidth to autogrow in width
             width: theme.spacing[31],
           }}
--- packages/design-system/src/components/ai-command-bar/command-bar.tsx
+++ packages/design-system/src/components/ai-command-bar/command-bar.tsx
@@ -1,29 +1,102 @@
-// import { theme } from "../../stitches.config";
-import { theme } from "../../stitches.config";
+import * as Collapsible from "@radix-ui/react-collapsible";
+import * as Separator from "@radix-ui/react-separator";
+import { styled, theme } from "../../stitches.config";
 import { Grid } from "../grid";
+import { Button } from "../button";
+import { type ReactNode } from "react";
 
 type Props = {
   children: React.ReactNode;
+  content?: React.ReactNode;
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
 };
 
+const borderRadius = 22;
+
+const GridContainer = styled(Grid, {
+  gridTemplateColumns: `max-content 1fr`,
+  gridAutoColumns: "max-content",
+  gridAutoFlow: "column",
+  padding: theme.spacing[5],
+  backgroundColor: "#1D1D1D",
+  borderRadius,
+  variants: {
+    open: {
+      true: {
+        borderTopLeftRadius: 0,
+        borderTopRightRadius: 0,
+        borderTop: "1px solid #323232",
+      },
+    },
+  },
+});
+
+const CollapsibleRoot = styled(Collapsible.Root, {
+  color: theme.colors.foregroundContrastMain,
+  backgroundColor: "#1D1D1D",
+  borderRadius,
+  border: "1px solid #323232",
+  boxShadow:
+    "0px 5px 17px 0px rgba(0, 0, 0, 0.30), 0px 2px 7px 0px rgba(0, 0, 0, 0.10)",
+});
+
+const CollapsibleContent = styled(Collapsible.Content, {
+  width: "min-content",
+  minWidth: "100%",
+});
+
 export const CommandBar = (props: Props) => {
+  const open = props.content !== undefined && props.open;
+  const onOpenChange =
+    props.content !== undefined ? props.onOpenChange : undefined;
+
   return (
-    <Grid
-      css={{
-        gridTemplateColumns: `max-content 1fr`,
-        gridAutoColumns: "max-content",
-        gridAutoFlow: "column",
-        padding: theme.spacing[5],
-        backgroundColor: "#1D1D1D",
-        borderRadius: 22,
-        border: "1px solid #323232",
-        boxShadow:
-          "0px 5px 17px 0px rgba(0, 0, 0, 0.30), 0px 2px 7px 0px rgba(0, 0, 0, 0.10)",
-      }}
-      gap={2}
-    >
-      {props.children}
-    </Grid>
+    <CollapsibleRoot open={open} onOpenChange={onOpenChange}>
+      <CollapsibleContent>{props.content}</CollapsibleContent>
+      <GridContainer gap={2} open={props.open}>
+        {props.children}
+      </GridContainer>
+    </CollapsibleRoot>
+  );
+};
+
+export const CommandBarTrigger = Collapsible.Trigger;
+
+CommandBarTrigger.defaultProps = {
+  asChild: true,
+};
+
+export const CommandBarContentSection = styled(Grid, {
+  margin: theme.spacing[9],
+});
+
+CommandBarContentSection.defaultProps = {
+  gap: 2,
+};
+
+export const CommandBarContentSeparator = styled(Separator.Root, {
+  backgroundColor: "#323232",
+  height: 1,
+});
+
+const CommandBarContentPromptStyled = styled(Button, {
+  justifyContent: "start",
+  p: theme.spacing[4],
+  height: "auto",
+});
+const CommandBarContentPromptText = styled("span", {
+  overflow: "auto",
+  whiteSpace: "break-spaces",
+});
+
+export const CommandBarContentPrompt = (props: { children: ReactNode }) => {
+  return (
+    <CommandBarContentPromptStyled color="dark">
+      <CommandBarContentPromptText>
+        {props.children}
+      </CommandBarContentPromptText>
+    </CommandBarContentPromptStyled>
   );
 };
 
