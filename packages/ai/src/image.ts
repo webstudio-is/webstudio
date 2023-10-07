e0d13c019835a011548281c4f535df93eaa77416
Bogdan Chadkin <trysound@yandex.ru>
2023-10-07 15:35:17 +0400

    Fix can regenerate

--- packages/ai/src/image.ts
+++ packages/ai/src/image.ts
@@ -26,35 +26,36 @@ const queryImageAndMutateNode = async (
       }
     }
     const alt = node.props.find((prop) => prop.name === "alt");
-    if (alt?.type === "string") {
-      const photosSearchResult = await api.search.getPhotos({
-        query: alt.value,
-      });
-      if (photosSearchResult.type === "success") {
-        const [result] = photosSearchResult.response.results;
-        const url = new URL(result.urls.raw);
-        url.searchParams.set("wsai", "true");
-        const src: EmbedTemplateProp = {
-          name: "src",
-          type: "string",
-          value: url.href,
-        };
-        const srcIndex = node.props.findIndex((prop) => prop.name === "src");
-        if (srcIndex === -1) {
-          node.props.push(src);
-        } else {
-          node.props[srcIndex] = src;
-        }
-        const hasObjectFit = node.styles?.some(
-          (styleDecl) => styleDecl.property === "objectFit"
-        );
-        if (hasObjectFit === false) {
-          node.styles = node.styles ?? [];
-          node.styles.push({
-            property: "objectFit",
-            value: { type: "keyword", value: "cover" },
-          });
-        }
+    if (canRegenerate === false || alt?.type !== "string") {
+      return;
+    }
+    const photosSearchResult = await api.search.getPhotos({
+      query: alt.value,
+    });
+    if (photosSearchResult.type === "success") {
+      const [result] = photosSearchResult.response.results;
+      const url = new URL(result.urls.raw);
+      url.searchParams.set("wsai", "true");
+      const src: EmbedTemplateProp = {
+        name: "src",
+        type: "string",
+        value: url.href,
+      };
+      const srcIndex = node.props.findIndex((prop) => prop.name === "src");
+      if (srcIndex === -1) {
+        node.props.push(src);
+      } else {
+        node.props[srcIndex] = src;
+      }
+      const hasObjectFit = node.styles?.some(
+        (styleDecl) => styleDecl.property === "objectFit"
+      );
+      if (hasObjectFit === false) {
+        node.styles = node.styles ?? [];
+        node.styles.push({
+          property: "objectFit",
+          value: { type: "keyword", value: "cover" },
+        });
       }
     }
   }
