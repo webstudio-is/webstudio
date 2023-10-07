7b789c0dc0b5d33ec32d2159832b58cc08396dd8
istarkov <istarkov@gmail.com>
2023-10-07 07:11:47 +0400

    Add icons

---
+++ packages/icons/src/__generated__/mic.tsx
@@ -0,0 +1,15 @@
+// Generated from icons/mic.svg
+
+import { forwardRef } from "react";
+import type { IconProps } from "../types";
+
+// prettier-ignore
+export const MicIcon = forwardRef<SVGSVGElement, IconProps>(
+  ({ color = "currentColor", size = 16, ...props }, forwardedRef) => {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width={size} height={size} fill={color} {...props} ref={forwardedRef}><path fillRule="evenodd" d="M5.35 3.333a2.65 2.65 0 0 1 5.3 0V8a2.65 2.65 0 1 1-5.3 0V3.333ZM8 1.983a1.35 1.35 0 0 0-1.35 1.35V8a1.35 1.35 0 1 0 2.7 0V3.333A1.35 1.35 0 0 0 8 1.983ZM3.333 6.017a.65.65 0 0 1 .65.65V8a4.017 4.017 0 0 0 8.034 0V6.667a.65.65 0 1 1 1.3 0V8a5.318 5.318 0 0 1-4.667 5.277v.74h2.017a.65.65 0 1 1 0 1.3H5.333a.65.65 0 1 1 0-1.3H7.35v-.74A5.318 5.318 0 0 1 2.683 8V6.667a.65.65 0 0 1 .65-.65Z" clipRule="evenodd" /></svg>
+    );
+  }
+);
+
+MicIcon.displayName = "MicIcon";
