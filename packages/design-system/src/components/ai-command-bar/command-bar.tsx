33d640f7aa184f4bbb1df7c8468a6f31d4a7d23d
Ivan Starkov <istarkov@gmail.com>
2023-10-07 14:40:22 +0400

    feat: AI Command bar (#2426)
    ## Description
    
    
    https://638245578b47d4399fba191a-slxedcgyqd.chromatic.com/?path=/story/library-ai-command-bar--demo
    
    <img width="512" alt="image"
    src="https://github.com/webstudio-is/webstudio/assets/5077042/f43a0590-e632-4dc3-9105-75e425b053d4">
    
    <img width="459" alt="image"
    src="https://github.com/webstudio-is/webstudio/assets/5077042/c7593dcc-93c3-4ef2-ac33-e93015395fc8">
    
    scroll
    <img width="435" alt="image"
    src="https://github.com/webstudio-is/webstudio/assets/5077042/78af59d1-ec66-485b-aa7b-602ca4b6c407">
    
    
    ## Steps for reproduction
    
    Open design-system storybook, find CommandBar
    
    ## Code Review
    
    - [ ] hi @kof, I need you to do
      - conceptual review (architecture, feature-correctness)
      - detailed review (read every line)
      - test it on preview
    
    ## Before requesting a review
    
    - [ ] made a self-review
    - [ ] added inline comments where things may be not obvious (the "why",
    not "what")
    
    ## Before merging
    
    - [ ] tested locally and on preview environment (preview dev login:
    5de6)
    - [ ] updated [test
    cases](https://github.com/webstudio-is/webstudio/blob/main/apps/builder/docs/test-cases.md)
    document
    - [ ] added tests
    - [ ] if any new env variables are added, added them to `.env.example`
    and the `builder/env-check.js` if mandatory

---
+++ packages/design-system/src/components/ai-command-bar/command-bar.tsx
@@ -0,0 +1,109 @@
+/**
+ * Implementation of the "ai command bar" component from:
+ * https://www.figma.com/file/xCBegXEWxROLqA1Y31z2Xo/%F0%9F%93%96-Webstudio-Design-Docs?node-id=7586%3A48927&mode=dev
+ */
+import * as Collapsible from "@radix-ui/react-collapsible";
+import * as Separator from "@radix-ui/react-separator";
+import { styled, theme } from "../../stitches.config";
+import { Grid } from "../grid";
+import { Button } from "../button";
+import { type ReactNode } from "react";
+
+type Props = {
+  children: React.ReactNode;
+  content?: React.ReactNode;
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+};
+
+// @todo replace with theme variables, as of now they are no ready yet
+const borderRadius = 22;
+const backgroundColor = "#1D1D1D";
+const borderColor = "#323232";
+const boxShadow =
+  "0px 5px 17px 0px rgba(0, 0, 0, 0.30), 0px 2px 7px 0px rgba(0, 0, 0, 0.10)";
+
+const GridContainer = styled(Grid, {
+  gridTemplateColumns: `max-content 1fr`,
+  gridAutoColumns: "max-content",
+  gridAutoFlow: "column",
+  padding: theme.spacing[5],
+  borderRadius,
+  variants: {
+    open: {
+      true: {
+        borderTopLeftRadius: 0,
+        borderTopRightRadius: 0,
+        borderTop: `1px solid ${borderColor}`,
+      },
+    },
+  },
+});
+
+const CollapsibleRoot = styled(Collapsible.Root, {
+  color: theme.colors.foregroundContrastMain,
+  backgroundColor,
+  borderRadius,
+  border: `1px solid ${borderColor}`,
+  boxShadow,
+});
+
+const CollapsibleContent = styled(Collapsible.Content, {
+  width: "min-content",
+  minWidth: "100%",
+});
+
+export const CommandBar = (props: Props) => {
+  const open = props.content !== undefined && props.open;
+  const onOpenChange =
+    props.content !== undefined ? props.onOpenChange : undefined;
+
+  return (
+    <CollapsibleRoot open={open} onOpenChange={onOpenChange}>
+      <CollapsibleContent>{props.content}</CollapsibleContent>
+      <GridContainer gap={2} open={props.open}>
+        {props.children}
+      </GridContainer>
+    </CollapsibleRoot>
+  );
+};
+
+export const CommandBarTrigger = (props: { children: ReactNode }) => (
+  <Collapsible.Trigger asChild {...props} />
+);
+
+export const CommandBarContentSection = (props: { children: ReactNode }) => (
+  <Grid
+    css={{
+      margin: theme.spacing[9],
+    }}
+    gap={2}
+    {...props}
+  />
+);
+
+export const CommandBarContentSeparator = styled(Separator.Root, {
+  backgroundColor: borderColor,
+  height: 1,
+});
+
+const CommandBarContentPromptStyled = styled(Button, {
+  justifyContent: "start",
+  p: theme.spacing[4],
+  height: "auto",
+});
+
+const CommandBarContentPromptText = styled("span", {
+  overflow: "auto",
+  whiteSpace: "break-spaces",
+});
+
+export const CommandBarContentPrompt = (props: { children: ReactNode }) => {
+  return (
+    <CommandBarContentPromptStyled color="dark">
+      <CommandBarContentPromptText>
+        {props.children}
+      </CommandBarContentPromptText>
+    </CommandBarContentPromptStyled>
+  );
+};
