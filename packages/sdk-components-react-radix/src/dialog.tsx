cf7711eb7f92dc3853bb68ef3043511ebd1f8c36
Juan Di Toro <ditorojuanf@gmail.com>
2023-10-07 22:51:46 +0200

    fix: use proper heading props for dialog and sheet components (#2400)
    ## Description
    
    Fixes #2206 
    Copies what the `sdk-components-react` package does for the Headings so
    users can control whats being rendered
    
    ## Steps for reproduction
    
    1. Click the + button
    2. Add a Radix Sheet or Radix Dialog component
    3. Select the root on the layers
    4. On the right side panel Toggle the `Open` to true
    5. Select the Sheet/Dialog Title
    6. On the right side panel go to `Settings`
    7. Should be able to see a Tag input with a predefined value of `h1`

--- packages/sdk-components-react-radix/src/dialog.tsx
+++ packages/sdk-components-react-radix/src/dialog.tsx
@@ -6,6 +6,7 @@ import {
   type ReactNode,
   forwardRef,
   Children,
+  type ComponentProps,
 } from "react";
 import * as DialogPrimitive from "@radix-ui/react-dialog";
 import { getClosestInstance, type Hook } from "@webstudio-is/react-sdk";
@@ -50,7 +51,20 @@ export const DialogOverlay = forwardRef<
 
 export const DialogContent = DialogPrimitive.Content;
 export const DialogClose = DialogPrimitive.Close;
-export const DialogTitle = DialogPrimitive.Title;
+
+type Tag = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+const defaultTag = "h1";
+export const DialogTitle = forwardRef<
+  HTMLHeadingElement,
+  ComponentProps<typeof DialogPrimitive.DialogTitle> & { tag?: Tag }
+>(({ tag: Tag = defaultTag, children, ...props }, ref) => (
+  <DialogPrimitive.DialogTitle asChild>
+    <Tag ref={ref} {...props}>
+      {children ?? "Heading title you can edit"}
+    </Tag>
+  </DialogPrimitive.DialogTitle>
+));
+
 export const DialogDescription = DialogPrimitive.Description;
 
 /* BUILDER HOOKS */
