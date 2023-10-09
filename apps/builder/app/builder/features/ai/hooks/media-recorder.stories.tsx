2f4c0643d056de3b41dedbc97b8a1c4521c98509
Ivan Starkov <istarkov@gmail.com>
2023-10-09 11:29:12 +0400

    feat: Add Media Recorder (#2431)
    ## Description
    
    Media recorder hook and useLongPressToggle hook.
    
    
    https://6382151c8b47d4399fb9fc69-gumktzhrtx.chromatic.com/?path=/story/library-media-recorder--media-recorder
    
    ## Steps for reproduction
    
    See storybook for usage.
    
    ## Code Review
    
    - [ ] hi @kof, I need you to do
      - conceptual review (architecture, feature-correctness)
      - detailed review (read every line)
      - test it on preview
    
    ## Before requesting a review
    
    - [ ] made a self-review
    - [ ] added inline comments where things may be not obvious (the "why",
    not "what")
    
    ## Before merging
    
    - [ ] tested locally and on preview environment (preview dev login:
    5de6)
    - [ ] updated [test
    cases](https://github.com/webstudio-is/webstudio/blob/main/apps/builder/docs/test-cases.md)
    document
    - [ ] added tests
    - [ ] if any new env variables are added, added them to `.env.example`
    and the `builder/env-check.js` if mandatory

---
+++ apps/builder/app/builder/features/ai/hooks/media-recorder.stories.tsx
@@ -0,0 +1,79 @@
+import { useRef, useState } from "react";
+import { useLongPressToggle } from "./long-press-toggle";
+import { useMediaRecorder } from "./media-recorder";
+import { Button, Flex, Grid, css } from "@webstudio-is/design-system";
+
+export default {
+  title: "Library/Media Recorder",
+  argTypes: {
+    audioBitsPerSecond: {
+      options: [4000, 8000, 16000, 32000, 64000, 128000, 256000],
+      control: { type: "radio" },
+    },
+  },
+  args: {
+    audioBitsPerSecond: 16000,
+  },
+};
+
+const playAudio = (file: File) => {
+  const blobUrl = URL.createObjectURL(file);
+  const audioElement = new Audio(blobUrl);
+
+  audioElement.addEventListener("ended", () => {
+    URL.revokeObjectURL(blobUrl);
+  });
+
+  audioElement.play();
+};
+
+export const MediaRecorder = (options: { audioBitsPerSecond: number }) => {
+  const soundRef = useRef<HTMLDivElement>(null);
+
+  const [file, setFile] = useState<File>();
+
+  const { start, stop, cancel, state } = useMediaRecorder(
+    {
+      onComplete: (file) => {
+        setFile(file);
+      },
+      onReportSoundAmplitude: (amplitude) => {
+        soundRef.current?.style.setProperty("--volume", amplitude.toString());
+      },
+    },
+    options
+  );
+
+  const clickAndHoldProps = useLongPressToggle({
+    onStart: start,
+    onEnd: stop,
+    onCancel: cancel,
+  });
+
+  return (
+    <Grid css={{ m: 4 }} gap={2}>
+      <Flex ref={soundRef} justify="center" align="center">
+        <div className={soundCss()}></div>
+      </Flex>
+      <Button {...clickAndHoldProps}>
+        {state === "inactive" ? "Start recording" : "Recording"}
+      </Button>
+      {file && (
+        <Button color="positive" onClick={() => playAudio(file)}>
+          Play, recorded size {file.size}
+        </Button>
+      )}
+    </Grid>
+  );
+};
+MediaRecorder.storyName = "Media Recorder";
+
+const soundCss = css({
+  transition: "transform 0.15s ease-in-out",
+  borderRadius: "9999px",
+  backgroundColor: "#F56565",
+  width: "3rem",
+  height: "3rem",
+  transformOrigin: "center",
+  transform: "scale(calc(1 + var(--volume, 0)))",
+});
---
+++ apps/builder/app/builder/features/ai/hooks/media-recorder.ts
@@ -0,0 +1,165 @@
+import { useRef, useState } from "react";
+import { useEffectEvent } from "./effect-event";
+
+// https://github.com/openai/whisper/discussions/870
+const DEFAULT_OPTIONS: MediaRecorderOptions = {
+  audioBitsPerSecond: 16000,
+};
+
+export const useMediaRecorder = (
+  {
+    onComplete,
+    onReportSoundAmplitude,
+  }: {
+    onComplete: (file: File) => void;
+    onReportSoundAmplitude?: (amplitude: number) => void;
+  },
+  options = DEFAULT_OPTIONS
+) => {
+  const disposeRef = useRef<() => void>();
+
+  const cancelRef = useRef(false);
+  const isActiveRef = useRef<boolean>(false);
+  const idRef = useRef(0);
+  const [error, setError] = useState<Error>();
+  const [state, setState] = useState<"inactive" | "recording">("inactive");
+
+  const start = useEffectEvent(async () => {
+    isActiveRef.current = true;
+    const chunks: Blob[] = [];
+    idRef.current++;
+    const id = idRef.current;
+
+    cancelRef.current = false;
+
+    let stream: MediaStream;
+
+    try {
+      stream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: false,
+      });
+    } catch (error) {
+      // Not allowed, do not start new recording
+      isActiveRef.current = false;
+
+      if (error instanceof Error) {
+        setError(error);
+        return;
+      }
+      setError(new Error("Unknown error"));
+      return;
+    }
+
+    // Recording was stopped, do not start new recording
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore wrong ts error based on isActiveRef.current = true above it think that isActiveRef.current is always true
+    if (isActiveRef.current === false) {
+      stream.getAudioTracks().forEach((track) => track.stop());
+      return;
+    }
+
+    // New recording started, do cleanup and return
+    if (id !== idRef.current) {
+      stream.getAudioTracks().forEach((track) => track.stop());
+      return;
+    }
+
+    setError(undefined);
+    setState("recording");
+
+    const subtype = MediaRecorder.isTypeSupported("audio/webm; codecs=opus")
+      ? "webm"
+      : "mp4";
+    const mimeType =
+      subtype === "webm" ? `audio/${subtype}; codecs=opus` : `audio/${subtype}`;
+
+    const recorder = new MediaRecorder(stream, {
+      mimeType,
+      ...options,
+    });
+
+    const audioContext = new AudioContext();
+    const source = audioContext.createMediaStreamSource(stream);
+    const analyser = audioContext.createAnalyser();
+    source.connect(analyser);
+
+    analyser.fftSize = 2048;
+    const bufferLength = analyser.fftSize;
+    const dataArray = new Float32Array(bufferLength);
+
+    disposeRef.current = () => {
+      source.disconnect();
+      analyser.disconnect();
+      audioContext.close();
+      // Safari bug: Calling `stop` on tracks delays next `getUserMedia` by 3-5s.
+      // Chrome: `stop` needed to remove recording tab indicator.
+      // @todo: Probably don't stop tracks in Safari, as subsequent getUserMedia blocks the main thread, and cause long-press logic to fail
+      stream.getAudioTracks().forEach((track) => track.stop());
+      recorder.stop();
+    };
+
+    const latestSamples = Array.from({ length: 10 }, () => 1);
+    let latestSamplesIndex = 0;
+
+    recorder.ondataavailable = (event) => {
+      analyser.getFloatTimeDomainData(dataArray);
+      const sampleMaxAmplitude = Math.max(...dataArray);
+      latestSamples[latestSamplesIndex] = sampleMaxAmplitude;
+      latestSamplesIndex = (latestSamplesIndex + 1) % latestSamples.length;
+
+      // To not normalize amplitude around near zero values
+      const normalizeThreshold = 0.1;
+
+      // Normalize amplitude to be between 0 and 1, and against lastest samples.
+      // The idea to use latest samples for normalization
+      onReportSoundAmplitude?.(
+        sampleMaxAmplitude /
+          Math.max(normalizeThreshold, Math.max(...latestSamples))
+      );
+
+      // New recording started, do cleanup and return
+      if (id !== idRef.current) {
+        chunks.length = 0;
+        return;
+      }
+
+      // Cancelled, do cleanup and return
+      if (cancelRef.current) {
+        chunks.length = 0;
+        return;
+      }
+
+      chunks.push(event.data);
+
+      if (recorder.state === "inactive") {
+        const audioFile = new File(chunks, "recording." + subtype, {
+          // Add type to be able to play in audio element
+          type: "audio/" + subtype,
+        });
+
+        if (audioFile.size > 0) {
+          onComplete(audioFile);
+          onReportSoundAmplitude?.(0);
+        }
+        chunks.length = 0;
+      }
+    };
+
+    recorder.start(50);
+  });
+
+  const stop = useEffectEvent(() => {
+    isActiveRef.current = false;
+    disposeRef.current?.();
+    disposeRef.current = undefined;
+    setState("inactive");
+  });
+
+  const cancel = useEffectEvent(() => {
+    cancelRef.current = true;
+    stop();
+  });
+
+  return { start, stop, cancel, error, state };
+};
