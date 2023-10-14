bc83057e67ac065f492eb3f08debe955e257c1b6
istarkov <istarkov@gmail.com>
2023-10-14 15:36:48 +0300

    Remove comments

--- apps/builder/app/builder/features/ai/ai-fetch-result.ts
+++ apps/builder/app/builder/features/ai/ai-fetch-result.ts
@@ -312,26 +312,3 @@ const $jsx = computed(
     ];
   }
 );
-
-/*
-export const fetchResult = async (text: string) => {
-  const formData = new FormData();
-  formData.append("file", file);
-
-  const response = await fetch(`${restAi()}/blabla`, {
-    method: "POST",
-    body: formData,
-  });
-
-  if (response.ok === false) {
-    // @todo: show error
-    return;
-  }
-
-  // @todo add response parsing
-  const { text } = await response.json();
-
-  // return text;
-  await new Promise((resolve) => setTimeout(resolve, 10000));
-};
-*/
