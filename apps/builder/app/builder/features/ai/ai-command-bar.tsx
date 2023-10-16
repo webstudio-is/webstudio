a02b81faf62b3624d0a8f0d346d33d3d2daedcd2
Oleg Isonen <oleg008@gmail.com>
2023-10-16 18:50:06 +0200

    feat: Add some initial prompts to ai bar (#2494)
    ## Description
    
    1. What is this PR about (link the issue and add a short description)
    
    ## Steps for reproduction
    
    1. click on a prompt
    2. run ai
    
    ## Code Review
    
    - [ ] hi @istarkov , I need you to do
      - conceptual review (architecture, feature-correctness)
      - detailed review (read every line)
      - test it on preview
    
    ## Before requesting a review
    
    - [ ] made a self-review
    - [ ] added inline comments where things may be not obvious (the "why",
    not "what")
    
    ## Before merging
    
    - [ ] tested locally and on preview environment (preview dev login:
    5de6)
    - [ ] updated [test
    cases](https://github.com/webstudio-is/webstudio/blob/main/apps/builder/docs/test-cases.md)
    document
    - [ ] added tests
    - [ ] if any new env variables are added, added them to `.env.example`
    and the `builder/env-check.js` if mandatory

--- apps/builder/app/builder/features/ai/ai-command-bar.tsx
+++ apps/builder/app/builder/features/ai/ai-command-bar.tsx
@@ -54,9 +54,15 @@ type PartialButtonProps<T = ComponentPropsWithoutRef<typeof Button>> = {
   [key in keyof T]?: T[key];
 };
 
+const initialPrompts = [
+  "Create a hero section with a heading, subheading in white, CTA button and an image of a mountain in the background,  add light blur to the image",
+  "Create a two column feature section with a heading and subheading in the left column, and an image that covers the right column",
+  "Create a testimonials section on 2 rows. The first row has a heading and subheading, the second row has 3 testimonial cards with an image, headline, description and link.",
+];
+
 export const AiCommandBar = ({ isPreviewMode }: { isPreviewMode: boolean }) => {
   const [value, setValue] = useState("");
-  const [prompts, setPrompts] = useState<string[]>([]);
+  const [prompts, setPrompts] = useState<string[]>(initialPrompts);
   const [clientSettings, setClientSetting, isClientSettingsLoaded] =
     useClientSettings();
   const open = isClientSettingsLoaded && clientSettings.isAiMenuOpen;
@@ -467,9 +473,7 @@ const CommandBarContent = (props: {
           <Button
             onClick={(event) => {
               // @todo: Link must be changed
-              const url = new URL(
-                `https://github.com/webstudio-is/webstudio-community/discussions/new?category=q-a&labels=AI`
-              );
+              const url = new URL("https://wstd.us/learn-webstudio-ai");
               window.open(url.href, "_blank");
             }}
             color="dark"
