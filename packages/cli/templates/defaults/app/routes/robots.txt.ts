c6ba0874574cb09a13fccb82f088234b63d9d15c
istarkov <istarkov@gmail.com>
2023-11-01 18:39:32 +0200

    Add robots.txt

---
+++ packages/cli/templates/defaults/app/routes/robots.txt.ts
@@ -0,0 +1,26 @@
+import { ActionArgs } from "@remix-run/server-runtime";
+
+export const actions = (arg: ActionArgs) => {
+  const host =
+    arg.request.headers.get("x-forwarded-host") ||
+    arg.request.headers.get("host") ||
+    "";
+
+  return new Response(
+    `
+User-agent: *
+Disallow: /api/
+
+# Uncomment
+# Sitemap: https://${host}/sitemap.xml
+
+
+  `,
+    {
+      headers: {
+        "Content-Type": "text/plain",
+      },
+      status: 200,
+    }
+  );
+};
