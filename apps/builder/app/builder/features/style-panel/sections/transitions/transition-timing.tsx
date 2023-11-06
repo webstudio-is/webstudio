a2077c9941429d92c6bb1fcfc810a86bf5945ddb
Jaya Krishna <namburu1995@gmail.com>
2023-11-06 16:48:01 +0530

    use predefined values instead of custom values for displaying timing functions

--- apps/builder/app/builder/features/style-panel/sections/transitions/transition-timing.tsx
+++ apps/builder/app/builder/features/style-panel/sections/transitions/transition-timing.tsx
@@ -15,79 +15,25 @@ import {
   ComboboxListboxItem,
   ComboboxSeparator,
 } from "@webstudio-is/design-system";
-
-const defaultFunctions = {
-  linear: "linear",
-  ease: "ease",
-  "ease in": "ease-in",
-  "ease out": "ease-out",
-  "ease-in-out": "ease-in-out",
-} as const;
-
-const easeInFunctions = {
-  "ease-in-sine": "cubic-bezier(0.12, 0, 0.39, 0)",
-  "ease-in-quad": "cubic-bezier(0.11, 0, 0.5, 0)",
-  "ease-in-cubic": "cubic-bezier(0.32, 0, 0.67, 0)",
-  "ease-in-quart": "cubic-bezier(0.5, 0, 0.75, 0)",
-  "ease-in-quint": "cubic-bezier(0.64, 0, 0.78, 0)",
-  "ease-in-expo": "cubic-bezier(0.7, 0, 0.84, 0)",
-  "ease-in-circ": "cubic-bezier(0.55, 0, 1, 0.45)",
-  "ease-in-back": "cubic-bezier(0.36, 0, 0.66, -0.56)",
-} as const;
-
-const easeOutFunctions = {
-  "ease-out-sine": "cubic-bezier(0.61, 1, 0.88, 1)",
-  "ease-out-quad": "cubic-bezier(0.5, 1, 0.89, 1)",
-  "ease-out-cubic": "cubic-bezier(0.33, 1, 0.68, 1)",
-  "ease-out-quart": "cubic-bezier(0.25, 1, 0.5, 1)",
-  "ease-out-quint": "cubic-bezier(0.22, 1, 0.36, 1)",
-  "ease-out-expo": "cubic-bezier(0.16, 1, 0.3, 1)",
-  "ease-out-circ": "cubic-bezier(0, 0.55, 0.45, 1)",
-  "ease-out-back": "cubic-bezier(0.34, 1.56, 0.64, 1)",
-} as const;
-
-const easeInOutFunctions = {
-  "ease-in-out-sine": "cubic-bezier(0.37, 0, 0.63, 1)",
-  "ease-in-out-quad": "cubic-bezier(0.45, 0, 0.55, 1)",
-  "ease-in-out-cubic": "cubic-bezier(0.65, 0, 0.35, 1)",
-  "ease-in-out-quart": "cubic-bezier(0.76, 0, 0.24, 1)",
-  "ease-in-out-quint": "cubic-bezier(0.83, 0, 0.17, 1)",
-  "ease-in-out-expo": "cubic-bezier(0.87, 0, 0.13, 1)",
-  "ease-in-out-circ": "cubic-bezier(0.85, 0, 0.15, 1)",
-  "ease-in-out-back": "cubic-bezier(0.68, -0.6, 0.32, 1.6)",
-} as const;
-
-type DefaultFunction = keyof typeof defaultFunctions;
-type EaseInFunction = keyof typeof easeInFunctions;
-type EaseOutFunction = keyof typeof easeOutFunctions;
-type EaseInOutFunction = keyof typeof easeInOutFunctions;
-
-type TimingFunctions =
-  | DefaultFunction
-  | EaseInFunction
-  | EaseOutFunction
-  | EaseInOutFunction;
-
-const timingFunctions = {
-  ...defaultFunctions,
-  ...easeInFunctions,
-  ...easeOutFunctions,
-  ...easeInOutFunctions,
-} as const;
+import {
+  defaultFunctions,
+  easeInFunctions,
+  easeOutFunctions,
+  easeInOutFunctions,
+  timingFunctions,
+  findTimingFunctionFromValue,
+  type TimingFunctions,
+  type EaseInFunction,
+  type EaseOutFunction,
+  type DefaultFunction,
+  type EaseInOutFunction,
+} from "./transition-utils";
 
 type NameAndLabel = { name: TimingFunctions; value: string };
 
 type TransitionTimingProps = {
   timing: KeywordValue;
   onTimingSelection: (timing: TupleValueItem) => void;
-};
-
-const findTimingFunctionFromValue = (
-  timingFunction: string
-): TimingFunctions | undefined => {
-  return (Object.keys(timingFunctions) as TimingFunctions[]).find(
-    (key: TimingFunctions) => timingFunctions[key] === timingFunction
-  );
 };
 
 const comboBoxStyles = css({ zIndex: theme.zIndices[1] });
@@ -166,7 +112,7 @@ export const TransitionTiming = ({
       (
         prop: NameAndLabel
       ): prop is { name: EaseInOutFunction; value: string } => {
-        return Object.keys(easeOutFunctions).includes(prop.name);
+        return Object.keys(easeInOutFunctions).includes(prop.name);
       }
     );
   }, [items]);
@@ -177,6 +123,7 @@ export const TransitionTiming = ({
         key={item.name}
         selectable={false}
         {...getItemProps({ item, index })}
+        css={{ paddingLeft: theme.spacing[11] }}
       >
         {item.name}
       </ComboboxListboxItem>
