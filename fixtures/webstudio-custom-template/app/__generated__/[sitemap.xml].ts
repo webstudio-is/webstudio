9055de33f62ed80bb0267effb1f63cbf985d2823
Bogdan Chadkin <trysound@yandex.ru>
2023-11-09 12:59:37 +0300

    Resync and rebuild fixtures

--- fixtures/webstudio-custom-template/app/__generated__/[sitemap.xml].ts
+++ fixtures/webstudio-custom-template/app/__generated__/[sitemap.xml].ts
@@ -2,7 +2,6 @@ export const sitemap = {
   pages: [
     {
       path: "",
-      lastModified: "2023-11-05T17:42:09.984Z",
     },
   ],
 };
--- fixtures/webstudio-remix-netlify-functions/app/__generated__/[sitemap.xml].ts
+++ fixtures/webstudio-remix-netlify-functions/app/__generated__/[sitemap.xml].ts
@@ -2,7 +2,6 @@ export const sitemap = {
   pages: [
     {
       path: "",
-      lastModified: "2023-09-23T09:29:28.107Z",
     },
   ],
 };
