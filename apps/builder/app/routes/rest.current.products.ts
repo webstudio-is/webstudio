32e5f032b3e408855a10d14e4e124a6f70433404
Ivan Starkov <istarkov@gmail.com>
2023-11-16 21:43:42 +0200

    feat: Product and Transaction Log tables (#2618)
    ## Description
    
    With high probability tables will be changed.
    rest endpoint created for debugging purposes.
    
    Product intent - synchonized from 3rd parties product (stripe) 
    TransactionLog - product, user, status, date + debug and idempotency
    params
    
    Response example:
    
    ```json
    {
      "id": "0d970e5f-3344-4658-bf5d-b58c33629e4e",
      "username": "admin",
      "checkout": [
        {
          "eventId": "evt_1OD7CmBvryG8KJjl9t37DsdG",
          "sessionId": "cs_test_b19O6wCvKi0MIADIs9QFZ5OB9lsC2G8HqzxyqMJO2NvnH2Bn43lyMN30V9",
          "createdAt": "2023-11-16T15:47:06.706Z",
          "product": {
            "id": "prod_P0NaZnTWpJJ2Ay",
            "name": "Webstudio Agency LTD Test",
            "description": null,
            "features": ["Admin permission sharing", "1000000 vews per month"],
            "meta": { "hello": "world", "kototto": "kototo" },
            "images": [
              "https://files.stripe.com/links/MDB8YWNjdF8xS2ZtQVNCdnJ5RzhLSmpsfGZsX3Rlc3RfMGlneEV4MVVGM3hsZlY4NWxRT21ZZVY300zmmq4R99"
            ]
          }
        },
        {
          "eventId": "evt_1OD7pXBvryG8KJjlhlmaIWpm",
          "sessionId": "cs_test_b1V2QOY7Lt21hDoCc1p9iuJ1ePaPnT2lE454dlPKHRdK62zh59K4C1tt0E",
          "createdAt": "2023-11-16T15:50:13.155Z",
          "product": {
            "id": "prod_P0NaZnTWpJJ2Ay",
            "name": "Webstudio Agency LTD Test",
            "description": null,
            "features": ["Admin permission sharing", "1000000 vews per month"],
            "meta": { "hello": "world", "kototto": "kototo" },
            "images": [
              "https://files.stripe.com/links/MDB8YWNjdF8xS2ZtQVNCdnJ5RzhLSmpsfGZsX3Rlc3RfMGlneEV4MVVGM3hsZlY4NWxRT21ZZVY300zmmq4R99"
            ]
          }
        }
      ]
    }
    
    ```
    
    ## Steps for reproduction
    
    1. click button
    2. expect xyz
    
    ## Code Review
    
    - [ ] hi @kof, I need you to do
      - conceptual review (architecture, feature-correctness)
      - detailed review (read every line)
      - test it on preview
    
    ## Before requesting a review
    
    - [ ] made a self-review
    - [ ] added inline comments where things may be not obvious (the "why",
    not "what")
    
    ## Before merging
    
    - [ ] tested locally and on preview environment (preview dev login:
    5de6)
    - [ ] updated [test
    cases](https://github.com/webstudio-is/webstudio/blob/main/apps/builder/docs/test-cases.md)
    document
    - [ ] added tests
    - [ ] if any new env variables are added, added them to `.env.example`
    and the `builder/env-check.js` if mandatory

---
+++ apps/builder/app/routes/rest.current.products.ts
@@ -0,0 +1,48 @@
+import { json, redirect, type LoaderArgs } from "@remix-run/server-runtime";
+import { findAuthenticatedUser } from "~/services/auth.server";
+import { loginPath } from "~/shared/router-utils";
+import { prisma } from "@webstudio-is/prisma-client";
+
+/**
+ * Created for ebugging purposes, to check that payments and products are working
+ * Showing current user's checkouts with products.
+ */
+export const loader = async ({ request, params }: LoaderArgs) => {
+  const user = await findAuthenticatedUser(request);
+
+  if (user === null) {
+    const url = new URL(request.url);
+    throw redirect(
+      loginPath({
+        returnTo: `${url.pathname}?${url.searchParams.toString()}`,
+      })
+    );
+  }
+
+  const userCheckoutsWithProducts = await prisma.user.findUnique({
+    where: { id: user.id },
+    select: {
+      id: true,
+      username: true,
+      checkout: {
+        select: {
+          eventId: true,
+          sessionId: true,
+          createdAt: true,
+          product: {
+            select: {
+              id: true,
+              name: true,
+              description: true,
+              features: true,
+              meta: true,
+              images: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  return json(userCheckoutsWithProducts);
+};
