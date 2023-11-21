80eab19ba0b55c65a8a63da58a4d1dd18d81640e
istarkov <istarkov@gmail.com>
2023-11-21 18:37:19 +0200

    Update

--- packages/prisma-client/prisma/migrations/20231120172840_add-event-type/migration.sql
+++ packages/prisma-client/prisma/migrations/20231120172840_add-event-type/migration.sql
@@ -3,3 +3,14 @@ ALTER TABLE "TransactionLog" ADD COLUMN     "eventType" TEXT NOT NULL DEFAULT 'c
 
 ALTER TABLE "TransactionLog"
 ALTER COLUMN "eventType" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "TransactionLog" ADD COLUMN     "eventCreated" INTEGER NOT NULL DEFAULT 0;
+-- Fill Initally
+UPDATE "TransactionLog" SET "eventCreated" = EXTRACT(EPOCH FROM "createdAt");
+
+ALTER TABLE "TransactionLog"
+ALTER COLUMN "eventCreated" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "TransactionLog" ADD CONSTRAINT "TransactionLog_pkey" PRIMARY KEY ("eventId");
