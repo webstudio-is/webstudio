61817e512ab094e02c3d6e52c7df3adbbaf37064
istarkov <istarkov@gmail.com>
2023-11-21 20:03:41 +0200

    Schema changes

--- packages/prisma-client/prisma/migrations/20231120172840_add-event-type/migration.sql
+++ packages/prisma-client/prisma/migrations/20231120172840_add-event-type/migration.sql
@@ -14,3 +14,20 @@ ALTER COLUMN "eventCreated" DROP DEFAULT;
 
 -- AlterTable
 ALTER TABLE "TransactionLog" ADD CONSTRAINT "TransactionLog_pkey" PRIMARY KEY ("eventId");
+
+-- DropIndex
+DROP INDEX "TransactionLog_sessionId_productId_key";
+
+
+CREATE VIEW "UserProduct" AS
+SELECT "userId", "subscriptionId", "productId", "customerId", "customerEmail"
+FROM "TransactionLog" AS tl
+WHERE "status" = 'paid'
+  AND NOT EXISTS (
+    SELECT 1
+    FROM "TransactionLog" AS tlexsists
+    WHERE tlexsists."subscriptionId" = tl."subscriptionId"
+      AND tlexsists."status" = 'canceled'
+      AND tlexsists."eventCreated" > tl."eventCreated"
+  )
+ORDER BY "userId", "eventCreated" DESC;
