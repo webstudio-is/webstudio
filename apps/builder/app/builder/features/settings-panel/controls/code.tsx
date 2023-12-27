453d7df1351b7d9fda5b85692d1361769cadf041
Bogdan Chadkin <trysound@yandex.ru>
2023-12-27 15:31:10 +0700

    feat: add html editor
    Ref https://github.com/webstudio-is/webstudio/issues/2359 https://github.com/webstudio-is/webstudio/issues/2388
    
    Here added codemirror based html editor for html embed component.
    It supports basic autocomplete and highlights embedded css and js.
    
    Splitted basic codemirror setup into CodeEditor component
    everything else can be customized around it.

--- apps/builder/app/builder/features/settings-panel/controls/code.tsx
+++ apps/builder/app/builder/features/settings-panel/controls/code.tsx
@@ -1,8 +1,15 @@
-import type { ControlProps } from "../shared";
-import { TextControl } from "./text";
+import { Box, theme } from "@webstudio-is/design-system";
+import {
+  useLocalValue,
+  type ControlProps,
+  getLabel,
+  VerticalLayout,
+  Label,
+} from "../shared";
+import { VariablesButton } from "../variables";
+import { HtmlEditor } from "~/builder/shared/html-editor";
 
 export const CodeControl = ({
-  instanceId,
   meta,
   prop,
   propName,
@@ -11,31 +18,46 @@ export const CodeControl = ({
   onChange,
   onDelete,
 }: ControlProps<"code", "string">) => {
+  const metaOverride = {
+    ...meta,
+    control: "text" as const,
+  };
+  const localValue = useLocalValue(prop?.value ?? "", (value) => {
+    // sanitize html before saving
+    // this is basically what browser does when innerHTML is set
+    // but isolated within temporary element
+    // so the result is correct markup
+    const div = document.createElement("div");
+    div.innerHTML = value;
+    onChange({ type: "string", value: div.innerHTML });
+  });
+  const label = getLabel(metaOverride, propName);
+
   return (
-    <TextControl
-      instanceId={instanceId}
-      meta={{
-        ...meta,
-        control: "text",
-      }}
-      prop={prop}
-      propName={propName}
-      readOnly={readOnly}
+    <VerticalLayout
+      label={
+        <Box css={{ position: "relative" }}>
+          <Label description={metaOverride.description} readOnly={readOnly}>
+            {label}
+          </Label>
+          <VariablesButton
+            propId={prop?.id}
+            propName={propName}
+            propMeta={metaOverride}
+          />
+        </Box>
+      }
       deletable={deletable}
-      onChange={(value) => {
-        if (value.type === "string") {
-          // sanitize html before saving
-          // this is basically what browser does when innerHTML is set
-          // but isolated within temporary element
-          // so the result is correct markup
-          const div = document.createElement("div");
-          div.innerHTML = value.value;
-          onChange({ type: "string", value: div.innerHTML });
-        } else {
-          onChange(value);
-        }
-      }}
       onDelete={onDelete}
-    />
+    >
+      <Box css={{ py: theme.spacing[2] }}>
+        <HtmlEditor
+          readOnly={readOnly}
+          value={localValue.value}
+          onChange={localValue.set}
+          onBlur={localValue.save}
+        />
+      </Box>
+    </VerticalLayout>
   );
 };
