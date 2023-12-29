21bc71d5bf0405803b35e6a6acb4546f02d711f8
Oleg Isonen <oleg008@gmail.com>
2023-12-29 02:09:27 +0100

    remove unnecessary sorting

--- packages/react-sdk/src/css/css.ts
+++ packages/react-sdk/src/css/css.ts
@@ -89,10 +89,7 @@ export const generateCss = (
   }
 
   const styleRules = getStyleRules(styles, styleSourceSelections);
-  // Put style rules without state first.
-  styleRules.sort((rule1, rule2) => {
-    return rule1.state === undefined ? -1 : rule2.state === undefined ? 1 : 0;
-  });
+
   const imageValueTransformer = createImageValueTransformer(assets, {
     assetBaseUrl,
   });
