ce2a5ebc52aee551e4745b5bc609ef4cbf22fa0c
Oleg Isonen <oleg008@gmail.com>
2024-01-21 17:22:31 +0100

    feat: Outline Section UX improvement  (#2789)
    ## Description
    
    1. What is this PR about (link the issue and add a short description)
    The PR has 2 improvements mentioned in the issue #2602 :
     * Ordered the Outline section after the Effects section
     * Hides all properties except Style until a Style value is selected
     
    ## Steps for reproduction
    
    1. Login and create a project.
    2. On the style section on the right side of the screen. scroll down to
    find the updates.
    3.  Outline section has shifted below Effects section.
    4. And only on selecting the other toggles on style property other than
    none, renders the colour, width and offset properties.
    
    
    ## Code Review
    
    - [x] hi @kof, I need you to do
      - conceptual review (architecture, feature-correctness)
      - detailed review (read every line)
      - test it on preview
    
    ## Before requesting a review
    
    - [x] made a self-review
    - [x] added inline comments where things may be not obvious (the "why",
    not "what")
    
    ## Before merging
    
    - [x] tested locally and on preview environment (preview dev login:
    d252)
    - [ ] updated [test
    cases](https://github.com/webstudio-is/webstudio/blob/main/apps/builder/docs/test-cases.md)
    document
    - [ ] added tests
    - [ ] if any new env variables are added, added them to `.env.example`
    and the `builder/env-check.js` if mandatory
    
    ---------
    
    Co-authored-by: ShyanRoyChoudhury <shayan.roy31@gmail.com>

--- apps/builder/app/builder/features/style-panel/style-sections.tsx
+++ apps/builder/app/builder/features/style-panel/style-sections.tsx
@@ -40,8 +40,8 @@ export const categories = [
   "backgrounds",
   "borders",
   "boxShadows",
-  "outline",
   "effects",
+  "outline",
   "transitions",
 ] as const;
 
@@ -141,7 +141,7 @@ export const shouldRenderCategory = (
 
 export const sections: Record<
   Category,
-  (props: RenderCategoryProps) => JSX.Element | null
+  (props: RenderCategoryProps) => JSX.Element | undefined
 > = {
   layout: LayoutSection,
   flexChild: FlexChildSection,
