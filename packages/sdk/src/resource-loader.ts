0a0d5471a7f8bf2423afbfef73dba36cd6b99558
Bogdan Chadkin <trysound@yandex.ru>
2024-01-22 22:46:47 +0700

    fix: handle invalid request errors in resource loader
    Here fixed the case with broken published site when invalid url
    is passed to fetch. The error is passed to user as statusText with
    500 status code.
    
    As a result correct everything will continue to render just with empty
    data.

--- packages/sdk/src/resource-loader.ts
+++ packages/sdk/src/resource-loader.ts
@@ -12,21 +12,30 @@ export const loadResource = async (resourceData: Resource) => {
   if (method !== "get" && body !== undefined) {
     requestInit.body = body;
   }
-  const response = await fetch(url, requestInit);
-  let data;
-  if (
-    response.ok &&
-    // accept json by default and when specified explicitly
-    (requestHeaders.has("accept") === false ||
-      requestHeaders.get("accept") === "application/json")
-  ) {
-    data = await response.json();
-  } else {
-    data = await response.text();
+  try {
+    const response = await fetch(url, requestInit);
+    let data;
+    if (
+      response.ok &&
+      // accept json by default and when specified explicitly
+      (requestHeaders.has("accept") === false ||
+        requestHeaders.get("accept") === "application/json")
+    ) {
+      data = await response.json();
+    } else {
+      data = await response.text();
+    }
+    return {
+      data,
+      status: response.status,
+      statusText: response.statusText,
+    };
+  } catch (error) {
+    const message = (error as unknown as Error).message;
+    return {
+      data: undefined,
+      status: 500,
+      statusText: message,
+    };
   }
-  return {
-    data,
-    status: response.status,
-    statusText: response.statusText,
-  };
 };
