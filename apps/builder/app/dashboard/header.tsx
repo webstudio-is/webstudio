7a49bde1e76b1103886b9ed7cc77c4b90e030ede
Oleg Isonen <oleg008@gmail.com>
2024-01-31 12:15:30 +0100

    feat:  Update the menu according to figma (#2849)
    ## Description
    
    <img width="243" alt="image"
    src="https://github.com/webstudio-is/webstudio/assets/52824/5a6d87fd-c27b-444d-8966-65d1325b212b">
    
    ## Steps for reproduction
    
    1. click button
    2. expect xyz
    
    ## Code Review
    
    - [ ] hi @istarkov , I need you to do
      - conceptual review (architecture, feature-correctness)
      - detailed review (read every line)
      - test it on preview
    
    ## Before requesting a review
    
    - [ ] made a self-review
    - [ ] added inline comments where things may be not obvious (the "why",
    not "what")
    
    ## Before merging
    
    - [ ] tested locally and on preview environment (preview dev login:
    5de6)
    - [ ] updated [test
    cases](https://github.com/webstudio-is/webstudio/blob/main/apps/builder/docs/test-cases.md)
    document
    - [ ] added tests
    - [ ] if any new env variables are added, added them to `.env.example`
    and the `builder/env-check.js` if mandatory

--- apps/builder/app/dashboard/header.tsx
+++ apps/builder/app/dashboard/header.tsx
@@ -17,6 +17,7 @@ import {
   theme,
   Button,
   ProBadge,
+  DropdownMenuSeparator,
 } from "@webstudio-is/design-system";
 import { useNavigate } from "@remix-run/react";
 import { logoutPath, userPlanSubscriptionPath } from "~/shared/router-utils";
@@ -72,10 +73,6 @@ const Menu = ({
       <DropdownMenuPortal>
         <DropdownMenuContent align="end">
           <DropdownMenuLabel>{title}</DropdownMenuLabel>
-          <DropdownMenuItem onSelect={() => navigate(logoutPath())}>
-            Logout
-          </DropdownMenuItem>
-
           {userPlanFeatures.hasSubscription && (
             <DropdownMenuItem
               onSelect={() => navigate(userPlanSubscriptionPath())}
@@ -86,7 +83,7 @@ const Menu = ({
           {userPlanFeatures.hasProPlan === false && (
             <DropdownMenuItem
               onSelect={() => {
-                window.location.assign("https://webstudio.is/pricing");
+                window.open("https://webstudio.is/pricing");
               }}
               css={{
                 gap: theme.spacing[3],
@@ -96,6 +93,10 @@ const Menu = ({
               <div>Upgrade to Pro</div>
             </DropdownMenuItem>
           )}
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onSelect={() => navigate(logoutPath())}>
+            Sign Out
+          </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenuPortal>
     </DropdownMenu>
