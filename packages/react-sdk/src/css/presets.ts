4f868c5a5f3d4eaef057f7990629c6f2e5cb2414
Oleg Isonen <oleg008@gmail.com>
2024-02-05 18:34:04 +0100

    fix: Normalize border style (#2867)
    ## Description
    
    We don't support inset/outset in the style panel which is the initial
    for inputs/buttons
    
    New defaults are:
    
    - input, textarea, button - solid
    - radio, checkbox - none (already was none, no change,but we didn't
    reflect that in sp)
    
    ## Steps for reproduction
    
    1. add those components to canvas
    2. see that input, textarea, button - have solid border style in style
    panel
    3. see that radio, checkbox - show none
    
    ## Code Review
    
    - [ ] hi @istarkov , I need you to do
      - conceptual review (architecture, feature-correctness)
      - detailed review (read every line)
      - test it on preview
    
    ## Before requesting a review
    
    - [ ] made a self-review
    - [ ] added inline comments where things may be not obvious (the "why",
    not "what")
    
    ## Before merging
    
    - [ ] tested locally and on preview environment (preview dev login:
    5de6)
    - [ ] updated [test
    cases](https://github.com/webstudio-is/webstudio/blob/main/apps/builder/docs/test-cases.md)
    document
    - [ ] added tests
    - [ ] if any new env variables are added, added them to `.env.example`
    and the `builder/env-check.js` if mandatory

--- packages/react-sdk/src/css/presets.ts
+++ packages/react-sdk/src/css/presets.ts
@@ -1,6 +1,6 @@
 import type { Styles } from "./normalize";
 
-export const borders: Styles = [
+export const borderWidth: Styles = [
   {
     property: "borderTopWidth",
     value: { type: "unit", value: 1, unit: "px" },
@@ -23,5 +23,24 @@ export const outline: Styles = [
   {
     property: "outlineWidth",
     value: { type: "unit", value: 1, unit: "px" },
+  },
+];
+
+export const borderStyle = (value: string): Styles => [
+  {
+    property: "borderTopStyle",
+    value: { type: "keyword", value },
+  },
+  {
+    property: "borderRightStyle",
+    value: { type: "keyword", value },
+  },
+  {
+    property: "borderBottomStyle",
+    value: { type: "keyword", value },
+  },
+  {
+    property: "borderLeftStyle",
+    value: { type: "keyword", value },
   },
 ];
