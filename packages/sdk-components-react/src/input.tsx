f8d32cfb57de5bd8029fbebb42c9c14bb4a3eef2
Oleg Isonen <oleg008@gmail.com>
2024-02-12 16:57:26 +0100

    add all input types, set text default

--- packages/sdk-components-react/src/input.tsx
+++ packages/sdk-components-react/src/input.tsx
@@ -1,13 +1,38 @@
-import { forwardRef, type ElementRef, type ComponentProps } from "react";
+import {
+  forwardRef,
+  type ElementRef,
+  type ComponentProps,
+  type HTMLInputTypeAttribute,
+} from "react";
 
 export const defaultTag = "input";
 
 export const Input = forwardRef<
   ElementRef<typeof defaultTag>,
   ComponentProps<typeof defaultTag> & {
-    type?: "text" | "email" | "password" | "number" | "tel" | "url";
+    type?:
+      | "button"
+      | "color"
+      | "date"
+      | "datetime-local"
+      | "email"
+      | "hidden"
+      | "month"
+      | "number"
+      | "password"
+      | "range"
+      | "reset"
+      | "search"
+      | "submit"
+      | "tel"
+      | "text"
+      | "time"
+      | "url"
+      | "week";
   }
   // Make sure children are not passed down to an input, because this will result in error.
->(({ children: _children, ...props }, ref) => <input {...props} ref={ref} />);
+>(({ children: _children, type = "text", ...props }, ref) => (
+  <input {...props} type={type} ref={ref} />
+));
 
 Input.displayName = "Input";
