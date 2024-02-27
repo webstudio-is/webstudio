27235775ec7a241e0e5dedce9ce9ab15275a978f
Bogdan Chadkin <trysound@yandex.ru>
2024-02-27 20:00:13 +0700

    refactor: revert remix.redirect method
    This reverts commit fa376d4204bc182ef51be9eb0de31cc344bf6686.
    
    Seems like native redirect does not suppot relative paths
    which are essential for redirect ui.

--- fixtures/webstudio-custom-template/app/routes/[hello]._index.ts
+++ fixtures/webstudio-custom-template/app/routes/[hello]._index.ts
@@ -1,3 +1,5 @@
-export const loader = () => {
-  return Response.redirect("/world", 301);
+import { type LoaderArgs, redirect } from "@remix-run/server-runtime";
+
+export const loader = (arg: LoaderArgs) => {
+  return redirect("/world", 301);
 };
