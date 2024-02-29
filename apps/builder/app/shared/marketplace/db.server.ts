f86a99d412c111bbcf1929ffb0ff29b5678f43a9
istarkov <istarkov@gmail.com>
2024-02-29 13:45:29 +0500

    Add fixes

--- apps/builder/app/shared/marketplace/db.server.ts
+++ apps/builder/app/shared/marketplace/db.server.ts
@@ -14,49 +14,48 @@ export const getItems = async (): Promise<Array<MarketplaceOverviewItem>> => {
     select: {
       id: true,
       latestBuild: {
-        select: { buildId: true },
+        select: {
+          build: {
+            select: { id: true, marketplaceProduct: true },
+          },
+        },
       },
     },
-  });
-
-  const buildIdProjectIdMap = new Map();
-  for (const project of projects) {
-    if (project.latestBuild) {
-      buildIdProjectIdMap.set(project.latestBuild.buildId, project.id);
-    }
-  }
-
-  const builds = await prisma.build.findMany({
-    where: {
-      id: {
-        in: Array.from(buildIdProjectIdMap.keys()),
-      },
-    },
-    select: { id: true, marketplaceProduct: true },
   });
 
   // @todo move this?
   const products = [];
-  for (const build of builds) {
-    const marketplaceProduct = MarketplaceProduct.parse(
-      JSON.parse(build.marketplaceProduct)
-    );
-    const projectId = buildIdProjectIdMap.get(build.id);
-    if (projectId) {
-      products.push({
-        projectId: projectId,
-        ...marketplaceProduct,
-      });
+
+  for (const project of projects) {
+    const { latestBuild } = project;
+    if (!latestBuild) {
+      continue;
     }
+
+    const marketplaceProduct = MarketplaceProduct.parse(
+      JSON.parse(latestBuild.build.marketplaceProduct)
+    );
+
+    products.push({
+      projectId: project.id,
+      ...marketplaceProduct,
+    });
   }
 
-  const assets = await prisma.asset.findMany({
-    where: {
-      id: {
-        in: products.map((product) => product.thumbnailAssetId!),
-      },
-    },
-  });
+  const ids = products
+    .map((product) => product.thumbnailAssetId)
+    .filter(<T>(value: T): value is NonNullable<T> => value !== undefined);
+
+  const assets =
+    ids.length > 0
+      ? await prisma.asset.findMany({
+          where: {
+            id: {
+              in: ids,
+            },
+          },
+        })
+      : [];
 
   return products.map((product) => {
     return {
