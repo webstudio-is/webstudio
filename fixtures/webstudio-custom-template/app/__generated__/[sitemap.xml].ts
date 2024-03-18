572f4282c7392e5634c17b394db3d6812d7429a3
Bogdan Chadkin <trysound@yandex.ru>
2024-03-18 11:43:05 +0700

    Resync fixtures

--- fixtures/webstudio-custom-template/app/__generated__/[sitemap.xml].ts
+++ fixtures/webstudio-custom-template/app/__generated__/[sitemap.xml].ts
@@ -2,15 +2,15 @@ export const sitemap = {
   pages: [
     {
       path: "",
-      lastModified: "2024-03-05T15:38:45.184Z",
+      lastModified: "2024-03-17T18:28:52.083Z",
     },
     {
       path: "/script-test",
-      lastModified: "2024-03-05T15:38:45.184Z",
+      lastModified: "2024-03-17T18:28:52.083Z",
     },
     {
       path: "/world",
-      lastModified: "2024-03-05T15:38:45.184Z",
+      lastModified: "2024-03-17T18:28:52.083Z",
     },
   ],
 };
