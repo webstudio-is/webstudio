4392e1fd1a2c04652a66381664001a0c364c39f7
Bogdan Chadkin <opensource@trysound.io>
2024-03-20 17:41:48 +0700

    refactor: move generated data into server module (#3035)
    Here another case to potentionally optimize client bundle by moving data
    used only in loaders, actions, meta and links into .server.tsx module to
    guarantee splitting from client bundle.
    
    Also removed circular PageData type dependecy between route and
    generated modules.

--- fixtures/webstudio-remix-netlify-edge-functions/app/__generated__/_index.tsx
+++ fixtures/webstudio-remix-netlify-edge-functions/app/__generated__/_index.tsx
@@ -2,34 +2,12 @@
 /* This is a auto generated file for building the project */
 
 import { Fragment, useState } from "react";
-import type {
-  Asset,
-  FontAsset,
-  ImageAsset,
-  ProjectMeta,
-} from "@webstudio-is/sdk";
 import { useResource } from "@webstudio-is/react-sdk";
 import { Body as Body } from "@webstudio-is/sdk-components-react-remix";
 import {
   Heading as Heading,
   Text as Text,
 } from "@webstudio-is/sdk-components-react";
-
-import type { PageData } from "~/routes/_index";
-export const imageAssets: ImageAsset[] = [];
-
-// Font assets on current page (can be preloaded)
-export const pageFontAssets: FontAsset[] = [];
-
-export const pageBackgroundImageAssets: ImageAsset[] = [];
-
-export const pageData: PageData = {
-  project: { siteName: "", faviconAssetId: "", code: "" },
-};
-export const user: { email: string | null } | undefined = {
-  email: "hello@webstudio.is",
-};
-export const projectId = "d845c167-ea07-4875-b08d-83e97c09dcce";
 
 const Page = ({}: { system: any }) => {
   return (
