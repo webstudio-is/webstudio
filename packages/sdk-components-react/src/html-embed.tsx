cf772f7f8ecd80d20bfcc21c94411735edb00e37
Oleg Isonen <oleg008@gmail.com>
2024-03-23 10:43:39 +0100

    keep the execution order of scripts when executing embed on the client

--- packages/sdk-components-react/src/html-embed.tsx
+++ packages/sdk-components-react/src/html-embed.tsx
@@ -8,63 +8,97 @@ import {
 import { mergeRefs } from "@react-aria/utils";
 import { ReactSdkContext } from "@webstudio-is/react-sdk";
 
-type Props = {
-  code: string;
-  executeScriptOnCanvas?: boolean;
-  clientOnly?: boolean;
+const insertScript = (
+  sourceScript: HTMLScriptElement
+): Promise<HTMLScriptElement> => {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement("script");
+
+    // Copy all attributes from the source script to the new script, because we are going to replace the source script with the new one
+    // and the user might rely on some attributes.
+    for (const { name, value } of sourceScript.attributes) {
+      script.setAttribute(name, value);
+    }
+
+    if (sourceScript.src) {
+      script.onload = () => {
+        resolve(script);
+      };
+      script.onerror = reject;
+      script.src = sourceScript.src;
+    } else {
+      script.textContent = sourceScript.innerText;
+    }
+
+    document.head.appendChild(script);
+    sourceScript.replaceWith(script);
+
+    // Run the callback immediately for inline scripts.
+    if (sourceScript.hasAttribute("src") === false) {
+      resolve(script);
+    }
+  });
+};
+
+type ScriptTask = () => Promise<HTMLScriptElement>;
+
+// Inspiration https://ghinda.net/article/script-tags
+const execute = async (container: HTMLElement) => {
+  const scripts = container.querySelectorAll("script");
+  const syncTasks: Array<ScriptTask> = [];
+  const asyncTasks: Array<ScriptTask> = [];
+
+  scripts.forEach((script) => {
+    const type = script.getAttribute("type");
+    if (type == null || type == "" || type === "text/javascript") {
+      const tasks = script.hasAttribute("async") ? asyncTasks : syncTasks;
+      tasks.push(() => {
+        return insertScript(script);
+      });
+    }
+  });
+
+  // Insert the script tags in parallel.
+  Promise.all(
+    asyncTasks.map(async (task) => {
+      return await task();
+    })
+  );
+  // Insert the script tags sequentially to preserve execution order.
+  for (const task of syncTasks) {
+    await task();
+  }
 };
 
 type ChildProps = {
   innerRef: ForwardedRef<HTMLDivElement>;
   // code can be actually undefined when prop is not provided
   code?: string;
+  shouldExecute?: boolean;
 };
 
 /**
- * Scripts are executed when rendered client side.
- * Necessary on canvas which does not have server rendering.
+ * Scripts are executed when rendered client side and `shouldExecute` is true, otherwise regular innerHTML won't execute scripts when clientOnly setting is used.
+ * Server-side rendered scripts are executed as like any server-side rendered HTML.
  */
 const ExecutableHtml = (props: ChildProps) => {
-  const { code, innerRef, ...rest } = props;
+  const { code, shouldExecute, innerRef, ...rest } = props;
   const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const container = containerRef.current;
-    if (container === null || code === undefined) {
+    if (container == null || shouldExecute !== true) {
       return;
     }
-    // the trick to execute inserted scripts
-    // https://ghinda.net/article/script-tags
-    const range = document.createRange();
-    range.setStart(container, 0);
-    const fragment = range.createContextualFragment(code);
-    while (container.firstChild) {
-      container.removeChild(container.firstChild);
-    }
-    container.append(fragment);
-  }, [code]);
+    execute(container);
+  }, [shouldExecute]);
 
   return (
     <div
       {...rest}
       ref={mergeRefs(innerRef, containerRef)}
       style={{ display: "contents" }}
-    />
-  );
-};
-
-/**
- * Scripts are executed when rendered server side
- */
-const InnerHtml = (props: ChildProps) => {
-  const { code, innerRef, ...rest } = props;
-
-  return (
-    <div
-      {...rest}
-      ref={innerRef}
-      style={{ display: "contents" }}
-      dangerouslySetInnerHTML={{ __html: props.code ?? "" }}
+      dangerouslySetInnerHTML={{ __html: code ?? "" }}
     />
   );
 };
@@ -78,27 +112,39 @@ const Placeholder = (props: ChildProps) => {
   );
 };
 
-export const HtmlEmbed = forwardRef<HTMLDivElement, Props>((props, ref) => {
-  const { renderer } = useContext(ReactSdkContext);
-  const { code, executeScriptOnCanvas, clientOnly, ...rest } = props;
+type HtmlEmbedProps = {
+  code: string;
+  executeScriptOnCanvas?: boolean;
+  clientOnly?: boolean;
+};
 
-  // code can be actually undefined when prop is not provided
-  //
-  // cast code to string in case non-string value is computed
-  // from expression
-  if (code === undefined || String(code).trim().length === 0) {
-    return <Placeholder innerRef={ref} {...rest} />;
+export const HtmlEmbed = forwardRef<HTMLDivElement, HtmlEmbedProps>(
+  (props, ref) => {
+    const { renderer } = useContext(ReactSdkContext);
+    const { code, executeScriptOnCanvas, clientOnly, ...rest } = props;
+
+    // code can be actually undefined when prop is not provided
+    //
+    // cast code to string in case non-string value is computed
+    // from expression
+    if (code === undefined || String(code).trim().length === 0) {
+      return <Placeholder innerRef={ref} {...rest} />;
+    }
+
+    const shouldExecute =
+      (renderer === "canvas" && executeScriptOnCanvas === true) ||
+      renderer === "preview" ||
+      clientOnly;
+
+    return (
+      <ExecutableHtml
+        innerRef={ref}
+        code={code}
+        shouldExecute={shouldExecute}
+        {...rest}
+      />
+    );
   }
-
-  if (
-    (renderer === "canvas" && executeScriptOnCanvas === true) ||
-    renderer === "preview" ||
-    clientOnly
-  ) {
-    return <ExecutableHtml innerRef={ref} code={code} {...rest} />;
-  }
-
-  return <InnerHtml innerRef={ref} code={code} {...rest} />;
-});
+);
 
 HtmlEmbed.displayName = "HtmlEmbed";
