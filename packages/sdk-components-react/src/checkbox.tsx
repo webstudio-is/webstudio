ef31d6241f5b29d3aff3512b85d5586c4b7e11e0
Oleg Isonen <oleg008@gmail.com>
2024-03-24 11:55:06 +0100

    add defaultChecked and value to checkbox

--- packages/sdk-components-react/src/checkbox.tsx
+++ packages/sdk-components-react/src/checkbox.tsx
@@ -4,7 +4,7 @@ export const defaultTag = "input";
 
 export const Checkbox = forwardRef<
   ElementRef<typeof defaultTag>,
-  Omit<ComponentProps<typeof defaultTag>, "type">
+  Omit<ComponentProps<typeof defaultTag>, "type" | "value"> & { value?: string }
   // Make sure children are not passed down to an input, because this will result in error.
 >(({ children: _children, ...props }, ref) => {
   return <input {...props} type="checkbox" ref={ref} />;
