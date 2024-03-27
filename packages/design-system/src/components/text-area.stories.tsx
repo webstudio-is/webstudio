368853557390d2352e35532566e788b3ff9ad233
d-ivashchuk <dimaiv@me.com>
2024-03-27 16:52:37 +0100

    test lost-pixel changes

--- packages/design-system/src/components/text-area.stories.tsx
+++ packages/design-system/src/components/text-area.stories.tsx
@@ -7,7 +7,7 @@ export default {
 };
 
 const exampleValue =
-  "This is an example value of a text area. It's long enough to show how it wraps.";
+  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ";
 
 export const Demo = () => {
   const [value, setValue] = useState(exampleValue);
