682fbea0d921e74cdad79af5eef0352dd9cbf7f3
Oleg Isonen <oleg008@gmail.com>
2024-03-28 16:56:15 +0100

    adding a property

--- apps/builder/app/builder/features/style-panel/sections/custom-properties/custom-properties.tsx
+++ apps/builder/app/builder/features/style-panel/sections/custom-properties/custom-properties.tsx
@@ -1,46 +1,59 @@
 import { Fragment, useState } from "react";
 import { theme, Grid, Combobox } from "@webstudio-is/design-system";
-import { generatedProperties } from "@webstudio-is/css-data";
+import { properties } from "@webstudio-is/css-data";
 import { CollapsibleSectionWithAddButton } from "~/builder/shared/collapsible-section";
 import type { SectionProps } from "../../style-sections";
 import { CssValueInputContainer } from "../../controls/position/css-value-input-container";
 import { styleConfigByName } from "../../shared/configs";
 import { PropertyName } from "../../shared/property-name";
 import { getStyleSource } from "../../shared/style-info";
-import type { SetProperty } from "../../shared/use-style-data";
+import { atom } from "nanostores";
+import { useStore } from "@nanostores/react";
+import type { StyleProperty } from "@webstudio-is/css-engine";
 
-type GeneratedStyleProperty = keyof typeof generatedProperties;
-const generatedPropertyNames = Object.keys(
-  generatedProperties
-) as Array<GeneratedStyleProperty>;
+const propertyNames = Object.keys(properties) as Array<StyleProperty>;
 
-const initialProperties: Array<GeneratedStyleProperty> = [
+const $listedProperties = atom<Array<StyleProperty>>([
   "opacity",
   "mixBlendMode",
   "cursor",
   "pointerEvents",
   "userSelect",
   "backdropFilter",
-];
+]);
 
 // @todo when adding properties - maybe sort them by popularity from generatedProperties?
-export const CustomProperties = ({
+export const CustomPropertiesSection = ({
   currentStyle,
   setProperty,
-  addProperty,
   deleteProperty,
-}: SectionProps & { addProperty: SetProperty }) => {
-  //const [addingProp, setAddingProp] = useState<GeneratedStyleProperty>();
-  //  <AddProperty onPropSelected={setAddingProp} />
+}: SectionProps) => {
+  const listedProperties = useStore($listedProperties);
+  const [addingProp, setAddingProp] = useState<StyleProperty | "">();
 
   return (
     <CollapsibleSectionWithAddButton
       label="Custom Properties"
-      onAdd={() => {}}
+      onAdd={() => {
+        setAddingProp("");
+      }}
       hasItems={true}
     >
+      {addingProp !== undefined && (
+        <AddProperty
+          propertyNames={propertyNames}
+          onSelect={(value) => {
+            if (value in properties || value.substr(0, 2) === "--") {
+              const property = value as StyleProperty;
+              setAddingProp(undefined);
+              $listedProperties.set([property, ...listedProperties]);
+              setProperty(property)({ type: "guaranteedInvalid" });
+            }
+          }}
+        />
+      )}
       <Grid gap={2} css={{ gridTemplateColumns: `1fr ${theme.spacing[22]}` }}>
-        {initialProperties.map((property) => {
+        {listedProperties.map((property, index) => {
           const { items } = styleConfigByName(property);
           const keywords = items.map((item) => ({
             type: "keyword" as const,
@@ -55,6 +68,7 @@ export const CustomProperties = ({
                 onReset={() => deleteProperty(property)}
               />
               <CssValueInputContainer
+                autoFocus={addingProp !== undefined && index === 0}
                 label={styleConfigByName(property).label}
                 property={property}
                 styleSource={getStyleSource(currentStyle[property])}
@@ -71,18 +85,26 @@ export const CustomProperties = ({
   );
 };
 
-const AddProperty = ({ onPropSelected }: any) => {
-  const [value, setValue] = useState("");
+const AddProperty = ({
+  onSelect,
+  propertyNames,
+}: {
+  onSelect: (value: string) => void;
+  propertyNames: Array<StyleProperty>;
+}) => {
+  const [item, setItem] = useState({ value: "" });
   return (
     <Combobox
       autoFocus
+      // @todo add create logic in matcher like in props
       placeholder="Find or create a property"
-      items={generatedPropertyNames}
-      itemToString={(property) => property ?? ""}
-      onItemSelect={(item) => onPropSelected(item)}
-      value={value}
+      // @todo filter out already added properties
+      items={propertyNames.map((value) => ({ value }))}
+      itemToString={(item) => item?.value ?? ""}
+      onItemSelect={(item) => onSelect(item.value)}
+      value={item}
       onInputChange={(value) => {
-        setValue(value ?? "");
+        setItem({ value: value ?? "" });
       }}
     />
   );
