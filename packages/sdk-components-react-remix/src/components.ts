97f055d94e4063f837858e3a030ad5d426c4ab98
Ivan Starkov <istarkov@gmail.com>
2024-04-03 13:34:05 +0400

    feat: Vanilla Remix Form component (#3079)
    ## Description
    
    Prevoius form renamed to Server Form
    
    
    <img width="251" alt="image"
    src="https://github.com/webstudio-is/webstudio/assets/5077042/0c849bee-1018-468a-a26b-b92bf9472d25">
    
    ## Steps for reproduction
    
    1. click button
    2. expect xyz
    
    ## Code Review
    
    - [ ] hi @kof, I need you to do
      - conceptual review (architecture, feature-correctness)
      - detailed review (read every line)
      - test it on preview
    
    ## Before requesting a review
    
    - [ ] made a self-review
    - [ ] added inline comments where things may be not obvious (the "why",
    not "what")
    
    ## Before merging
    
    - [ ] tested locally and on preview environment (preview dev login:
    5de6)
    - [ ] updated [test
    cases](https://github.com/webstudio-is/webstudio/blob/main/apps/builder/docs/test-cases.md)
    document
    - [ ] added tests
    - [ ] if any new env variables are added, added them to `.env.example`
    and the `builder/env-check.js` if mandatory

--- packages/sdk-components-react-remix/src/components.ts
+++ packages/sdk-components-react-remix/src/components.ts
@@ -1,4 +1,6 @@
 export { Body } from "./body";
 export { Link } from "./link";
 export { RichTextLink } from "./rich-text-link";
-export { Form } from "./form";
+// To not break compatibility with existing forms
+export { ServerForm as Form } from "./server-form";
+export { RemixForm } from "./remix-form";
--- packages/sdk-components-react-remix/src/metas.ts
+++ packages/sdk-components-react-remix/src/metas.ts
@@ -1,4 +1,5 @@
 export { meta as Body } from "./body.ws";
 export { meta as Link } from "./link.ws";
 export { meta as RichTextLink } from "./rich-text-link.ws";
-export { meta as Form } from "./form.ws";
+export { meta as Form } from "./server-form.ws";
+export { meta as RemixForm } from "./remix-form.ws";
