49038fe2de342a03dbd8750e84c52f1e3dfbddd1
Ivan Starkov <istarkov@gmail.com>
2024-04-04 19:05:26 +0400

    build: Add social image to fixture (#3094)
    ## Description
    
    1. What is this PR about (link the issue and add a short description)
    
    ## Steps for reproduction
    
    1. click button
    2. expect xyz
    
    ## Code Review
    
    - [ ] hi @kof, I need you to do
      - conceptual review (architecture, feature-correctness)
      - detailed review (read every line)
      - test it on preview
    
    ## Before requesting a review
    
    - [ ] made a self-review
    - [ ] added inline comments where things may be not obvious (the "why",
    not "what")
    
    ## Before merging
    
    - [ ] tested locally and on preview environment (preview dev login:
    5de6)
    - [ ] updated [test
    cases](https://github.com/webstudio-is/webstudio/blob/main/apps/builder/docs/test-cases.md)
    document
    - [ ] added tests
    - [ ] if any new env variables are added, added them to `.env.example`
    and the `builder/env-check.js` if mandatory

--- fixtures/webstudio-custom-template/app/__generated__/[sitemap.xml].ts
+++ fixtures/webstudio-custom-template/app/__generated__/[sitemap.xml].ts
@@ -2,15 +2,15 @@ export const sitemap = {
   pages: [
     {
       path: "",
-      lastModified: "2024-03-26T18:34:56.154Z",
+      lastModified: "2024-04-04T14:33:58.620Z",
     },
     {
       path: "/script-test",
-      lastModified: "2024-03-26T18:34:56.154Z",
+      lastModified: "2024-04-04T14:33:58.620Z",
     },
     {
       path: "/world",
-      lastModified: "2024-03-26T18:34:56.154Z",
+      lastModified: "2024-04-04T14:33:58.620Z",
     },
   ],
 };
