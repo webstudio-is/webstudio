b9c30a695c9c159d5f4f2bdb04d493156e1b16df
Oleg Isonen <oleg008@gmail.com>
2024-04-05 15:45:10 +0100

    refix typography layout

--- apps/builder/app/builder/features/style-panel/sections/typography/typography.tsx
+++ apps/builder/app/builder/features/style-panel/sections/typography/typography.tsx
@@ -83,53 +83,43 @@ export const TypographySectionFont = (props: SectionProps) => {
   const { currentStyle, setProperty, deleteProperty } = props;
 
   return (
-    <Grid
-      css={{
-        gap: theme.spacing[5],
-      }}
-    >
-      <Grid css={{ gridTemplateColumns: "4fr 6fr" }} gap={2}>
-        <PropertyName
-          style={currentStyle}
-          label="Font"
-          properties={["fontFamily"]}
-          onReset={() => deleteProperty("fontFamily")}
-        />
-        <FontFamilyControl
-          property="fontFamily"
-          currentStyle={currentStyle}
-          setProperty={setProperty}
-          deleteProperty={deleteProperty}
-        />
-      </Grid>
-      <Grid css={{ gridTemplateColumns: "4fr 6fr" }} gap={2}>
-        <PropertyName
-          style={currentStyle}
-          label="Weight"
-          properties={["fontWeight"]}
-          onReset={() => deleteProperty("fontWeight")}
-        />
-        <FontWeightControl
-          property="fontWeight"
-          currentStyle={currentStyle}
-          setProperty={setProperty}
-          deleteProperty={deleteProperty}
-        />
-      </Grid>
-      <Grid css={{ gridTemplateColumns: "4fr 6fr" }} gap={2}>
-        <PropertyName
-          style={currentStyle}
-          label="Color"
-          properties={["color"]}
-          onReset={() => deleteProperty("color")}
-        />
-        <ColorControl
-          property="color"
-          currentStyle={currentStyle}
-          setProperty={setProperty}
-          deleteProperty={deleteProperty}
-        />
-      </Grid>
+    <Grid css={{ gridTemplateColumns: "4fr 6fr" }} gap={2}>
+      <PropertyName
+        style={currentStyle}
+        label="Family"
+        properties={["fontFamily"]}
+        onReset={() => deleteProperty("fontFamily")}
+      />
+      <FontFamilyControl
+        property="fontFamily"
+        currentStyle={currentStyle}
+        setProperty={setProperty}
+        deleteProperty={deleteProperty}
+      />
+      <PropertyName
+        style={currentStyle}
+        label="Weight"
+        properties={["fontWeight"]}
+        onReset={() => deleteProperty("fontWeight")}
+      />
+      <FontWeightControl
+        property="fontWeight"
+        currentStyle={currentStyle}
+        setProperty={setProperty}
+        deleteProperty={deleteProperty}
+      />
+      <PropertyName
+        style={currentStyle}
+        label="Color"
+        properties={["color"]}
+        onReset={() => deleteProperty("color")}
+      />
+      <ColorControl
+        property="color"
+        currentStyle={currentStyle}
+        setProperty={setProperty}
+        deleteProperty={deleteProperty}
+      />
     </Grid>
   );
 };
@@ -150,13 +140,7 @@ const ToggleGroupOrSelectControl = ({
   const onReset = () => deleteProperty(property);
   if (canUseToggleGroup === false) {
     return (
-      <Grid
-        css={{
-          gridTemplateColumns: "auto",
-          gap: theme.spacing[3],
-          width: `calc(${rawTheme.spacing[21]} + ${rawTheme.spacing[3]})`,
-        }}
-      >
+      <Grid gap="1" css={{ gridTemplateColumns: "auto" }}>
         <PropertyName
           style={currentStyle}
           properties={[property]}
@@ -191,13 +175,8 @@ export const TypographySectionSizing = (props: SectionProps) => {
   const { currentStyle, setProperty, deleteProperty } = props;
 
   return (
-    <Grid
-      css={{
-        gridTemplateColumns: "1fr 1fr 1fr",
-        gap: theme.spacing[5],
-      }}
-    >
-      <Grid css={{ gridTemplateColumns: "auto", gap: theme.spacing[3] }}>
+    <Grid gap="2" css={{ gridTemplateColumns: "1fr 1fr 1fr" }}>
+      <Grid gap="1">
         <PropertyName
           style={currentStyle}
           properties={["fontSize"]}
@@ -211,7 +190,7 @@ export const TypographySectionSizing = (props: SectionProps) => {
           deleteProperty={deleteProperty}
         />
       </Grid>
-      <Grid css={{ gridTemplateColumns: "auto", gap: theme.spacing[3] }}>
+      <Grid gap="1">
         <PropertyName
           style={currentStyle}
           properties={["lineHeight"]}
@@ -225,7 +204,7 @@ export const TypographySectionSizing = (props: SectionProps) => {
           deleteProperty={deleteProperty}
         />
       </Grid>
-      <Grid css={{ gridTemplateColumns: "auto", gap: theme.spacing[3] }}>
+      <Grid gap="1">
         <PropertyName
           style={currentStyle}
           properties={["letterSpacing"]}
@@ -245,140 +224,135 @@ export const TypographySectionSizing = (props: SectionProps) => {
 
 export const TypographySectionAdvanced = (props: SectionProps) => {
   return (
-    <Grid gap="2">
-      <Grid align="end" gap="3" columns="2">
+    <Grid gap="2" columns="2" align="end">
+      <ToggleGroupOrSelectControl
+        {...props}
+        property="textAlign"
+        mapValue={(value: string) =>
+          value === "left" ? "start" : value === "right" ? "end" : value
+        }
+        items={[
+          {
+            child: <TextAlignLeftIcon />,
+            title: "Text Align",
+            description: "Aligns the text based on the writing direction.",
+            value: "start",
+            propertyValues: "text-align: start;",
+          },
+          {
+            child: <TextAlignCenterIcon />,
+            title: "Text Align",
+            description: "Centers the text horizontally within its container.",
+            value: "center",
+            propertyValues: "text-align: center;",
+          },
+          {
+            child: <TextAlignRightIcon />,
+            title: "Text Align",
+            description: "Aligns the text based on the writing direction.",
+            value: "end",
+            propertyValues: "text-align: end;",
+          },
+          {
+            child: <TextAlignJustifyIcon />,
+            title: "Text Align",
+            description:
+              "Adjusts word spacing to align text to both the left and right edges of the container",
+            value: "justify",
+            propertyValues: "text-align: justify;",
+          },
+        ]}
+      />
+      <ToggleGroupOrSelectControl
+        {...props}
+        property="textDecorationLine"
+        items={[
+          {
+            child: <CrossSmallIcon />,
+            title: "Text Decoration Line",
+            description: "No decoration is applied to the text.",
+            value: "none",
+            propertyValues: "text-decoration-line: none;",
+          },
+          {
+            title: "Text Decoration Line",
+            child: <TextUnderlineIcon />,
+            description: " Adds a horizontal line underneath the text.",
+            value: "underline",
+            propertyValues: "text-decoration-line: underline;",
+          },
+          {
+            title: "Text Decoration Line",
+            child: <TextStrikethroughIcon />,
+            description:
+              "Draws a horizontal line through the middle of the text.",
+            value: "line-through",
+            propertyValues: "text-decoration-line: line-through;",
+          },
+        ]}
+      />
+      <ToggleGroupOrSelectControl
+        {...props}
+        property="textTransform"
+        items={[
+          {
+            child: <CrossSmallIcon />,
+            title: "Text Transform",
+            description:
+              "No transformation is applied to the text. The text appears as it is.",
+            value: "none",
+            propertyValues: "text-transform: none;",
+          },
+          {
+            child: <TextUppercaseIcon />,
+            title: "Text Transform",
+            description:
+              "Transforms the text to appear in all uppercase letters.",
+            value: "uppercase",
+            propertyValues: "text-transform: uppercase;",
+          },
+          {
+            child: <TextCapitalizeIcon />,
+            title: "Text Transform",
+            description:
+              "Transforms the first character of each word to uppercase, while the remaining characters are in lowercase.",
+            value: "capitalize",
+            propertyValues: "text-transform: capitalize;",
+          },
+          {
+            child: <TextLowercaseIcon />,
+            title: "Text Transform",
+            description:
+              " Transforms the text to appear in all lowercase letters.",
+            value: "lowercase",
+            propertyValues: "text-transform: lowercase;",
+          },
+        ]}
+      />
+      <Grid align="end" gap="1" css={{ gridTemplateColumns: "3fr 1fr" }}>
         <ToggleGroupOrSelectControl
           {...props}
-          property="textAlign"
-          mapValue={(value: string) =>
-            value === "left" ? "start" : value === "right" ? "end" : value
-          }
-          items={[
-            {
-              child: <TextAlignLeftIcon />,
-              title: "Text Align",
-              description: "Aligns the text based on the writing direction.",
-              value: "start",
-              propertyValues: "text-align: start;",
-            },
-            {
-              child: <TextAlignCenterIcon />,
-              title: "Text Align",
-              description:
-                "Centers the text horizontally within its container.",
-              value: "center",
-              propertyValues: "text-align: center;",
-            },
-            {
-              child: <TextAlignRightIcon />,
-              title: "Text Align",
-              description: "Aligns the text based on the writing direction.",
-              value: "end",
-              propertyValues: "text-align: end;",
-            },
-            {
-              child: <TextAlignJustifyIcon />,
-              title: "Text Align",
-              description:
-                "Adjusts word spacing to align text to both the left and right edges of the container",
-              value: "justify",
-              propertyValues: "text-align: justify;",
-            },
-          ]}
-        />
-        <ToggleGroupOrSelectControl
-          {...props}
-          property="textDecorationLine"
+          property="fontStyle"
           items={[
             {
               child: <CrossSmallIcon />,
-              title: "Text Decoration Line",
-              description: "No decoration is applied to the text.",
-              value: "none",
-              propertyValues: "text-decoration-line: none;",
-            },
-            {
-              title: "Text Decoration Line",
-              child: <TextUnderlineIcon />,
-              description: " Adds a horizontal line underneath the text.",
-              value: "underline",
-              propertyValues: "text-decoration-line: underline;",
-            },
-            {
-              title: "Text Decoration Line",
-              child: <TextStrikethroughIcon />,
+              title: "Font Style",
               description:
-                "Draws a horizontal line through the middle of the text.",
-              value: "line-through",
-              propertyValues: "text-decoration-line: line-through;",
+                "The default value. The text appears in a normal, upright style.",
+              value: "normal",
+              propertyValues: "font-style: normal;",
+            },
+            {
+              child: <TextItalicIcon />,
+              title: "Font Style",
+              description:
+                "The text appears in italic style, where it is slanted to the right.",
+              value: "italic",
+              propertyValues: "font-style: italic;",
             },
           ]}
         />
-      </Grid>
-      <Grid align="end" gap="3" columns="2">
-        <ToggleGroupOrSelectControl
-          {...props}
-          property="textTransform"
-          items={[
-            {
-              child: <CrossSmallIcon />,
-              title: "Text Transform",
-              description:
-                "No transformation is applied to the text. The text appears as it is.",
-              value: "none",
-              propertyValues: "text-transform: none;",
-            },
-            {
-              child: <TextUppercaseIcon />,
-              title: "Text Transform",
-              description:
-                "Transforms the text to appear in all uppercase letters.",
-              value: "uppercase",
-              propertyValues: "text-transform: uppercase;",
-            },
-            {
-              child: <TextCapitalizeIcon />,
-              title: "Text Transform",
-              description:
-                "Transforms the first character of each word to uppercase, while the remaining characters are in lowercase.",
-              value: "capitalize",
-              propertyValues: "text-transform: capitalize;",
-            },
-            {
-              child: <TextLowercaseIcon />,
-              title: "Text Transform",
-              description:
-                " Transforms the text to appear in all lowercase letters.",
-              value: "lowercase",
-              propertyValues: "text-transform: lowercase;",
-            },
-          ]}
-        />
-        <Grid align="end" gap="1" css={{ gridTemplateColumns: "3fr 1fr" }}>
-          <ToggleGroupOrSelectControl
-            {...props}
-            property="fontStyle"
-            items={[
-              {
-                child: <CrossSmallIcon />,
-                title: "Font Style",
-                description:
-                  "The default value. The text appears in a normal, upright style.",
-                value: "normal",
-                propertyValues: "font-style: normal;",
-              },
-              {
-                child: <TextItalicIcon />,
-                title: "Font Style",
-                description:
-                  "The text appears in italic style, where it is slanted to the right.",
-                value: "italic",
-                propertyValues: "font-style: italic;",
-              },
-            ]}
-          />
-          <TypographySectionAdvancedPopover {...props} />
-        </Grid>
+        <TypographySectionAdvancedPopover {...props} />
       </Grid>
     </Grid>
   );
