ff76226e04d774d3ce05150d256779d02fa26872
Bogdan Chadkin <trysound@yandex.ru>
2024-04-07 01:41:36 +0700

    refactor: drop theme switcher
    Theme switcher is very complicated and we may not introduce
    dark theme for a while. Easier to drop it for now and
    reintroduce with brand new solution.

--- apps/builder/app/shared/theme/index.ts
+++
@@ -1,6 +0,0 @@
-// This article explains the foundation of the technique we are using
-// https://rossmoody.com/writing/remix-stitches
-
-export * from "./theme";
-export * from "./theme.server";
-export type { ThemeSetting } from "./types";
--- packages/feature-flags/src/flags.ts
+++ packages/feature-flags/src/flags.ts
@@ -1,6 +1,5 @@
 // Only for development, is not supposed to be enabled at all.
 export const displayContents = false;
-export const dark = false;
 export const unsupportedBrowsers = false;
 export const aiRadixComponents = false;
 export const cms = false;
