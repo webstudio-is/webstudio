ce6520cfdf34589ea06a4a52b2e63e538a272195
Bogdan Chadkin <trysound@yandex.ru>
2024-04-07 22:30:31 +0700

    refactor: drop react-use
    Here replaced react-use utilities with own solutions
    because could not bundle it with vite. It also had a lot of transitive
    deps which bloated our bundle and node_modules. Win-win.

--- apps/builder/app/builder/shared/image-manager/uploading-animation.tsx
+++ apps/builder/app/builder/shared/image-manager/uploading-animation.tsx
@@ -1,17 +1,20 @@
+import { useEffect, useState } from "react";
 import { ProgressRadial, styled } from "@webstudio-is/design-system";
-
-import { useState } from "react";
-import { useInterval } from "react-use";
 
 const useFakeProgress = () => {
   const [progressBarPercentage, setProgressBarPercentage] = useState(0);
 
   // @todo rewrite this fake indication to show real progress
-  useInterval(() => {
-    setProgressBarPercentage((percentage) =>
-      percentage < 60 ? percentage + 1 : 0
-    );
-  }, 100);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setProgressBarPercentage((percentage) =>
+        percentage < 60 ? percentage + 1 : 0
+      );
+    }, 100);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
   return progressBarPercentage;
 };
