66ed20b333443d5df827c8e55227a56dc25279f4
Bogdan Chadkin <trysound@yandex.ru>
2024-04-08 18:17:23 +0700

    Add hex, remove copy buttons, make pointer smaller

--- apps/builder/app/builder/features/style-panel/shared/color-picker.tsx
+++ apps/builder/app/builder/features/style-panel/shared/color-picker.tsx
@@ -1,9 +1,8 @@
 import { useState } from "react";
 import { colord, extend, type RgbaColor } from "colord";
-import lchPlugin from "colord/plugins/lch";
 import namesPlugin from "colord/plugins/names";
 import { useDebouncedCallback } from "use-debounce";
-import { RgbaColorPicker } from "react-colorful";
+import { HexColorInput, RgbaColorPicker } from "react-colorful";
 import type {
   InvalidValue,
   KeywordValue,
@@ -16,9 +15,7 @@ import {
   PopoverTrigger,
   PopoverContent,
   useDisableCanvasPointerEvents,
-  Tooltip,
-  Box,
-  Button,
+  css,
 } from "@webstudio-is/design-system";
 import { toValue } from "@webstudio-is/css-engine";
 import { theme } from "@webstudio-is/design-system";
@@ -28,7 +25,14 @@ import type { IntermediateStyleValue } from "./css-value-input/css-value-input";
 import { ColorThumb } from "./color-thumb";
 
 // To support color names
-extend([namesPlugin, lchPlugin]);
+extend([namesPlugin]);
+
+const colorfulStyles = css({
+  ".react-colorful__pointer": {
+    width: 20,
+    height: 20,
+  },
+});
 
 const colorResultToRgbValue = (rgb: RgbaColor): RgbValue => {
   return {
@@ -78,6 +82,25 @@ export type CssColorPickerValueInput =
   | KeywordValue
   | IntermediateStyleValue
   | InvalidValue;
+
+const ControlledHexColorInput = ({
+  defaultColor,
+  onChange,
+}: {
+  defaultColor: string;
+  onChange: (newColor: string) => void;
+}) => {
+  const [color, setColor] = useState(defaultColor);
+  return (
+    <HexColorInput
+      color={color}
+      onChange={(newColor) => {
+        setColor(newColor);
+        onChange(newColor);
+      }}
+    />
+  );
+};
 
 type ColorPickerProps = {
   onChange: (value: CssColorPickerValueInput | undefined) => void;
@@ -166,12 +189,7 @@ export const ColorPicker = ({
   };
 
   const parsedColor = colord(rgbValue);
-  const formats = [
-    parsedColor.toHex(),
-    parsedColor.toRgbString(),
-    parsedColor.toHslString(),
-    parsedColor.toLchString(),
-  ];
+  const currentHex = parsedColor.toHex();
 
   const onComplete = useDebouncedCallback(
     (value: RgbValue) => onChangeComplete({ value }),
@@ -195,6 +213,7 @@ export const ColorPicker = ({
         }}
       >
         <RgbaColorPicker
+          className={colorfulStyles.toString()}
           color={rgbValue}
           onChange={(newValue) => {
             const color = fixColor(newValue);
@@ -203,23 +222,13 @@ export const ColorPicker = ({
             onComplete(color);
           }}
         />
-        <Box css={{ display: "grid" }}>
-          {formats.map((format) => (
-            <Tooltip
-              key={format}
-              content="Copy to clipboard"
-              disableHoverableContent={true}
-            >
-              <Button
-                color="ghost"
-                css={{ justifyContent: "start" }}
-                onClick={() => navigator.clipboard.writeText(format)}
-              >
-                {format}
-              </Button>
-            </Tooltip>
-          ))}
-        </Box>
+        <ControlledHexColorInput
+          key={currentHex}
+          defaultColor={currentHex}
+          onChange={(newHex) => {
+            onComplete(colorResultToRgbValue(colord(newHex).toRgb()));
+          }}
+        />
       </PopoverContent>
     </Popover>
   );
