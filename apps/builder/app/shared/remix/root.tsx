9ae2c0d91402f12b87ab5575ff7b7d9f4c2b9d44
Bogdan Chadkin <trysound@yandex.ru>
2024-04-10 12:56:43 +0700

    refactor: upgrade remix-island
    Here bumped package with minor change to support layouts we don't use.
    Added comment with reference to the PR and repro.

--- apps/builder/app/shared/remix/root.tsx
+++ apps/builder/app/shared/remix/root.tsx
@@ -1,6 +1,10 @@
 import { Links, Meta, Outlet as RemixOutlet, Scripts } from "@remix-run/react";
 import { createHead } from "remix-island";
 
+/**
+ * use remix-island to fix hydration errors when extensions
+ * change layout, see https://github.com/webstudio-is/webstudio/pull/1621
+ */
 export const Head = createHead(() => (
   <>
     <Meta />
