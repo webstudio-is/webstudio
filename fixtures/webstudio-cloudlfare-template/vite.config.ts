f48cfda2edf4edcc64933d3ac79e2b092c921d8c
istarkov <istarkov@gmail.com>
2024-04-14 13:36:50 +0300

    Fix

--- fixtures/webstudio-cloudlfare-template/vite.config.ts
+++ fixtures/webstudio-cloudlfare-template/vite.config.ts
@@ -7,6 +7,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(({ mode }) => ({
   plugins: [
+    // without this, remixCloudflareDevProxy trying to load workerd even for production (it's not needed for production)
     mode === "production" ? undefined : remixCloudflareDevProxy(),
     remix(),
     tsconfigPaths(),
