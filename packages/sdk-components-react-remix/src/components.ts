6bb8684e465b24fa0311419e658f1fe07199e461
Oleg Isonen <oleg008@gmail.com>
2024-04-19 19:04:20 +0100

    move webhook form to data

--- packages/sdk-components-react-remix/src/components.ts
+++ packages/sdk-components-react-remix/src/components.ts
@@ -1,6 +1,6 @@
 export { Body } from "./body";
 export { Link } from "./link";
 export { RichTextLink } from "./rich-text-link";
-// To not break compatibility with existing forms
+// @todo needs migration, to not break compatibility with existing forms we are mapping temporarily
 export { WebhookForm as Form } from "./webhook-form";
 export { RemixForm } from "./remix-form";
