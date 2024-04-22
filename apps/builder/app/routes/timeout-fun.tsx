2daac00790f01f8442d990c7c29102cac86ebf49
istarkov <istarkov@gmail.com>
2024-04-22 21:31:06 +0300

    Fix

--- apps/builder/app/routes/timeout-fun.tsx
+++ apps/builder/app/routes/timeout-fun.tsx
@@ -1,4 +1,4 @@
-import type { ActionFunctionArgs } from "@remix-run/server-runtime";
+import { json, type ActionFunctionArgs } from "@remix-run/server-runtime";
 
 export default function Logout() {
   return <h1>HELLO</h1>;
@@ -6,6 +6,7 @@ export default function Logout() {
 // https://vercel.com/docs/functions/runtimes#max-duration
 export const loader = async ({ request }: ActionFunctionArgs) => {
   await new Promise((resolve) => setTimeout(resolve, 20000));
+  return json({});
 };
 
 export const config = {
