325939d2024245e58ebafa9669b27e571daea6d0
istarkov <istarkov@gmail.com>
2024-04-22 20:36:42 +0300

    Try to export config

--- apps/builder/app/routes/timeout-fun.tsx
+++ apps/builder/app/routes/timeout-fun.tsx
@@ -3,7 +3,11 @@ import type { ActionFunctionArgs } from "@remix-run/server-runtime";
 export default function Logout() {
   return <h1>HELLO</h1>;
 }
-
+// https://vercel.com/docs/functions/runtimes#max-duration
 export const loader = async ({ request }: ActionFunctionArgs) => {
   await new Promise((resolve) => setTimeout(resolve, 20000));
+};
+
+export const config = {
+  maxDuration: 30,
 };
