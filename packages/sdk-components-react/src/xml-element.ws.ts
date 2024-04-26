18b8a235aff331053ad6d890ca1d5e6d657d4885
istarkov <istarkov@gmail.com>
2024-04-26 19:32:33 +0300

    XmlElement

--- packages/sdk-components-react/src/xml-element.ws.ts
+++ packages/sdk-components-react/src/xml-element.ws.ts
@@ -1,48 +1,11 @@
 import { XIcon } from "@webstudio-is/icons/svg";
 
 import {
-  type PresetStyle,
   type WsComponentMeta,
   type WsComponentPropsMeta,
 } from "@webstudio-is/react-sdk";
-import { div, ul } from "@webstudio-is/react-sdk/css-normalize";
-
-// import * as tc from "./theme/tailwind-classes";
 
 import { props } from "./__generated__/xml-element.props";
-
-const presetStyle = {
-  div: [
-    ...div,
-    {
-      property: "color",
-      value: { type: "rgb", alpha: 1, r: 36, g: 12, b: 233 },
-    },
-    {
-      property: "lineHeight",
-      value: { type: "unit", unit: "number", value: 1 },
-    },
-  ],
-  ul: [
-    ...ul,
-    {
-      property: "marginTop",
-      value: { type: "unit", unit: "px", value: 0 },
-    },
-    {
-      property: "marginBottom",
-      value: { type: "unit", unit: "px", value: 2 },
-    },
-    {
-      property: "paddingLeft",
-      value: { type: "unit", unit: "rem", value: 1.5 },
-    },
-    {
-      property: "lineHeight",
-      value: { type: "unit", unit: "number", value: 1 },
-    },
-  ],
-} satisfies PresetStyle<"div" | "ul">;
 
 export const meta: WsComponentMeta = {
   category: "xml",
@@ -51,10 +14,9 @@ export const meta: WsComponentMeta = {
   icon: XIcon,
   stylable: false,
   description: "XmlElement",
-  presetStyle,
 };
 
 export const propsMeta: WsComponentPropsMeta = {
   props,
-  initialProps: ["name"],
+  initialProps: ["tag"],
 };
