18ddb1b91fd6a9d1ed6a5010a47baa8630a7f761
Bogdan Chadkin <trysound@yandex.ru>
2024-04-27 19:49:57 +0400

    Remove describe from test

--- packages/css-engine/src/core/atomic.test.ts
+++ packages/css-engine/src/core/atomic.test.ts
@@ -1,29 +1,28 @@
-import { describe, test, expect } from "@jest/globals";
+import { test, expect } from "@jest/globals";
 import { createRegularStyleSheet } from "./create-style-sheet";
 import { generateAtomic } from "./atomic";
 
 const mediaRuleOptions0 = { minWidth: 0 } as const;
 const mediaId0 = "0";
 
-describe("Style Sheet Atomic", () => {
-  test("use matching media rule", () => {
-    const sheet = createRegularStyleSheet();
-    sheet.addMediaRule("x");
-    const rule = sheet.addNestingRule("");
-    rule.setDeclaration({
-      breakpoint: "x",
-      selector: "",
-      property: "display",
-      value: { type: "keyword", value: "block" },
-    });
-    rule.setDeclaration({
-      breakpoint: "x",
-      selector: "",
-      property: "color",
-      value: { type: "keyword", value: "red" },
-    });
-    expect(generateAtomic(sheet, { getKey: () => "" }).cssText)
-      .toMatchInlineSnapshot(`
+test("use matching media rule", () => {
+  const sheet = createRegularStyleSheet();
+  sheet.addMediaRule("x");
+  const rule = sheet.addNestingRule("");
+  rule.setDeclaration({
+    breakpoint: "x",
+    selector: "",
+    property: "display",
+    value: { type: "keyword", value: "block" },
+  });
+  rule.setDeclaration({
+    breakpoint: "x",
+    selector: "",
+    property: "color",
+    value: { type: "keyword", value: "red" },
+  });
+  expect(generateAtomic(sheet, { getKey: () => "" }).cssText)
+    .toMatchInlineSnapshot(`
 "@media all {
   .ccqp4le {
     display: block
@@ -33,63 +32,63 @@ describe("Style Sheet Atomic", () => {
   }
 }"
 `);
-  });
+});
 
-  test("use nested selector", () => {
-    const sheet = createRegularStyleSheet();
-    sheet.addMediaRule("x");
-    const rule = sheet.addNestingRule("");
-    rule.setDeclaration({
-      breakpoint: "x",
-      selector: ":hover",
-      property: "display",
-      value: { type: "keyword", value: "block" },
-    });
-    expect(generateAtomic(sheet, { getKey: () => "" }).cssText)
-      .toMatchInlineSnapshot(`
+test("use nested selector", () => {
+  const sheet = createRegularStyleSheet();
+  sheet.addMediaRule("x");
+  const rule = sheet.addNestingRule("");
+  rule.setDeclaration({
+    breakpoint: "x",
+    selector: ":hover",
+    property: "display",
+    value: { type: "keyword", value: "block" },
+  });
+  expect(generateAtomic(sheet, { getKey: () => "" }).cssText)
+    .toMatchInlineSnapshot(`
 "@media all {
   .c143pt9k:hover {
     display: block
   }
 }"
 `);
-  });
+});
 
-  test("added classes", () => {
-    const sheet = createRegularStyleSheet();
-    sheet.addMediaRule("x");
-    const rule = sheet.addNestingRule(".instance");
-    rule.setDeclaration({
-      breakpoint: "x",
-      selector: "",
-      property: "display",
-      value: { type: "keyword", value: "block" },
-    });
-    const instances = new Map([[rule, "instanceId"]]);
-    const { classesMap } = generateAtomic(sheet, {
-      getKey: (rule) => instances.get(rule) ?? "",
-    });
-    expect(classesMap.get("instanceId")).toEqual(["ccqp4le"]);
+test("added classes", () => {
+  const sheet = createRegularStyleSheet();
+  sheet.addMediaRule("x");
+  const rule = sheet.addNestingRule(".instance");
+  rule.setDeclaration({
+    breakpoint: "x",
+    selector: "",
+    property: "display",
+    value: { type: "keyword", value: "block" },
   });
+  const instances = new Map([[rule, "instanceId"]]);
+  const { classesMap } = generateAtomic(sheet, {
+    getKey: (rule) => instances.get(rule) ?? "",
+  });
+  expect(classesMap.get("instanceId")).toEqual(["ccqp4le"]);
+});
 
-  test("rule with multiple properties", () => {
-    const sheet = createRegularStyleSheet();
-    sheet.addMediaRule(mediaId0, mediaRuleOptions0);
-    const rule = sheet.addNestingRule(".instance");
-    rule.setDeclaration({
-      breakpoint: mediaId0,
-      selector: "",
-      property: "display",
-      value: { type: "keyword", value: "block" },
-    });
-    rule.setDeclaration({
-      breakpoint: mediaId0,
-      selector: "",
-      property: "color",
-      value: { type: "keyword", value: "red" },
-    });
-    expect(generateAtomic(sheet, { getKey: () => "" }).cssText)
-      .toMatchInlineSnapshot(`
+test("rule with multiple properties", () => {
+  const sheet = createRegularStyleSheet();
+  sheet.addMediaRule(mediaId0, mediaRuleOptions0);
+  const rule = sheet.addNestingRule(".instance");
+  rule.setDeclaration({
+    breakpoint: mediaId0,
+    selector: "",
+    property: "display",
+    value: { type: "keyword", value: "block" },
+  });
+  rule.setDeclaration({
+    breakpoint: mediaId0,
+    selector: "",
+    property: "color",
+    value: { type: "keyword", value: "red" },
+  });
+  expect(generateAtomic(sheet, { getKey: () => "" }).cssText)
+    .toMatchInlineSnapshot(`
 "@media all and (min-width: 0px) {
   .c1qg54vh {
     display: block
@@ -99,39 +98,39 @@ describe("Style Sheet Atomic", () => {
   }
 }"
 `);
-  });
+});
 
-  test("share atomic rules", () => {
-    const sheet = createRegularStyleSheet();
-    sheet.addMediaRule("x");
-    const rule1 = sheet.addNestingRule("1");
-    rule1.setDeclaration({
-      breakpoint: "x",
-      selector: "",
-      property: "display",
-      value: { type: "keyword", value: "block" },
-    });
-    rule1.setDeclaration({
-      breakpoint: "x",
-      selector: "",
-      property: "color",
-      value: { type: "keyword", value: "red" },
-    });
-    const rule2 = sheet.addNestingRule("2");
-    rule2.setDeclaration({
-      breakpoint: "x",
-      selector: "",
-      property: "display",
-      value: { type: "keyword", value: "block" },
-    });
-    const instances = new Map([
-      [rule1, "1"],
-      [rule2, "2"],
-    ]);
-    const { cssText, classesMap } = generateAtomic(sheet, {
-      getKey: (rule) => instances.get(rule) ?? "",
-    });
-    expect(cssText).toMatchInlineSnapshot(`
+test("share atomic rules", () => {
+  const sheet = createRegularStyleSheet();
+  sheet.addMediaRule("x");
+  const rule1 = sheet.addNestingRule("1");
+  rule1.setDeclaration({
+    breakpoint: "x",
+    selector: "",
+    property: "display",
+    value: { type: "keyword", value: "block" },
+  });
+  rule1.setDeclaration({
+    breakpoint: "x",
+    selector: "",
+    property: "color",
+    value: { type: "keyword", value: "red" },
+  });
+  const rule2 = sheet.addNestingRule("2");
+  rule2.setDeclaration({
+    breakpoint: "x",
+    selector: "",
+    property: "display",
+    value: { type: "keyword", value: "block" },
+  });
+  const instances = new Map([
+    [rule1, "1"],
+    [rule2, "2"],
+  ]);
+  const { cssText, classesMap } = generateAtomic(sheet, {
+    getKey: (rule) => instances.get(rule) ?? "",
+  });
+  expect(cssText).toMatchInlineSnapshot(`
 "@media all {
   .ccqp4le {
     display: block
@@ -141,29 +140,29 @@ describe("Style Sheet Atomic", () => {
   }
 }"
 `);
-    expect(classesMap.get("1")).toEqual(["ccqp4le", "cen0ymu"]);
-    expect(classesMap.get("2")).toEqual(["ccqp4le"]);
-  });
+  expect(classesMap.get("1")).toEqual(["ccqp4le", "cen0ymu"]);
+  expect(classesMap.get("2")).toEqual(["ccqp4le"]);
+});
 
-  test("distinct similar declarations from different breakpoints", () => {
-    const sheet = createRegularStyleSheet();
-    sheet.addMediaRule("a");
-    sheet.addMediaRule("b");
-    const rule1 = sheet.addNestingRule("1");
-    rule1.setDeclaration({
-      breakpoint: "a",
-      selector: "",
-      property: "display",
-      value: { type: "keyword", value: "block" },
-    });
-    rule1.setDeclaration({
-      breakpoint: "b",
-      selector: "",
-      property: "display",
-      value: { type: "keyword", value: "block" },
-    });
-    expect(generateAtomic(sheet, { getKey: () => "" }).cssText)
-      .toMatchInlineSnapshot(`
+test("distinct similar declarations from different breakpoints", () => {
+  const sheet = createRegularStyleSheet();
+  sheet.addMediaRule("a");
+  sheet.addMediaRule("b");
+  const rule1 = sheet.addNestingRule("1");
+  rule1.setDeclaration({
+    breakpoint: "a",
+    selector: "",
+    property: "display",
+    value: { type: "keyword", value: "block" },
+  });
+  rule1.setDeclaration({
+    breakpoint: "b",
+    selector: "",
+    property: "display",
+    value: { type: "keyword", value: "block" },
+  });
+  expect(generateAtomic(sheet, { getKey: () => "" }).cssText)
+    .toMatchInlineSnapshot(`
 "@media all {
   .cumb2su {
     display: block
@@ -175,5 +174,4 @@ describe("Style Sheet Atomic", () => {
   }
 }"
 `);
-  });
 });
