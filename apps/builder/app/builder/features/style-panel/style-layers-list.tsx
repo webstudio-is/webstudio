58b9daa6b03e83045df7c827694b696cfb20e48d
Jaya Krishna Namburu <namburu1995@gmail.com>
2024-05-10 18:08:28 +0200

    Update apps/builder/app/builder/features/style-panel/style-layers-list.tsx
    Co-authored-by: Oleg Isonen <oleg008@gmail.com>

--- apps/builder/app/builder/features/style-panel/style-layers-list.tsx
+++ apps/builder/app/builder/features/style-panel/style-layers-list.tsx
@@ -178,7 +178,7 @@ export const LayersList = ({
                           )
                         }
                       />
-                    ) : null}
+                    ) : undefined}
                     <SmallIconButton
                       variant="destructive"
                       tabIndex={-1}
