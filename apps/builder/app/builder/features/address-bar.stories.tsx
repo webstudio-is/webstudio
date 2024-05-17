d1fd9b0b0f2fc1dd8d2144fecbec1b091f796466
Bogdan Chadkin <trysound@yandex.ru>
2024-05-17 15:28:48 +0300

    experimental: save address bar on enter or close
    To keep in history only consistent paths and avoid overfetching
    resources I changed address bar saving logic. Before each type
    in address bar params triggered save. Now only "enter" while param
    is focused or address bar popover is closed triggers saving.
    
    Can be tested with new story.

---
+++ apps/builder/app/builder/features/address-bar.stories.tsx
@@ -0,0 +1,98 @@
+import { computed } from "nanostores";
+import type { Meta, StoryFn } from "@storybook/react";
+import { Box, Text, theme } from "@webstudio-is/design-system";
+import { AddressBarPopover } from "./address-bar";
+import {
+  $dataSourceVariables,
+  $dataSources,
+  $pages,
+  $selectedPage,
+  $selectedPageId,
+} from "~/shared/nano-states";
+import { useStore } from "@nanostores/react";
+
+$dataSources.set(
+  new Map([
+    [
+      "systemId",
+      {
+        id: "systemId",
+        scopeInstanceId: "rootInstanceId",
+        name: "system",
+        type: "parameter",
+      },
+    ],
+  ])
+);
+
+$pages.set({
+  folders: [
+    {
+      id: "rootId",
+      name: "",
+      slug: "",
+      children: ["homeId", "dynamicId"],
+    },
+  ],
+  homePage: {
+    id: "homeId",
+    path: "",
+    name: "",
+    title: "",
+    meta: {},
+    rootInstanceId: "",
+    systemDataSourceId: "",
+  },
+  pages: [
+    {
+      id: "dynamicId",
+      path: "/blog/:date/post/:slug",
+      name: "",
+      title: "",
+      meta: {},
+      rootInstanceId: "rootInstanceId",
+      systemDataSourceId: "systemId",
+    },
+  ],
+});
+
+$selectedPageId.set("dynamicId");
+
+const $selectedPageSystem = computed(
+  [$selectedPage, $dataSourceVariables],
+  (selectedPage, dataSourceVariables) => {
+    if (selectedPage === undefined) {
+      return {};
+    }
+    return dataSourceVariables.get(selectedPage.systemDataSourceId);
+  }
+);
+
+const SystemInspect = () => {
+  const system = useStore($selectedPageSystem);
+  return (
+    <Text variant="mono" css={{ whiteSpace: "pre" }}>
+      {JSON.stringify(system, null, 2)}
+    </Text>
+  );
+};
+
+export default {
+  title: "Builder/Address Bar",
+  component: AddressBarPopover,
+} satisfies Meta;
+
+export const AddressBar: StoryFn = () => (
+  <>
+    <Box
+      css={{
+        height: theme.spacing[15],
+        background: theme.colors.backgroundTopbar,
+        color: theme.colors.foregroundContrastMain,
+      }}
+    >
+      <AddressBarPopover />
+    </Box>
+    <SystemInspect />
+  </>
+);
