8dd79328014faeefd0090d29550a37ddf463182d
Bogdan Chadkin <trysound@yandex.ru>
2024-05-25 16:15:13 +0300

    Tweak test names

--- apps/builder/app/shared/shim.test.ts
+++ apps/builder/app/shared/shim.test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from "@jest/globals";
 import { mapGroupBy, setDifference } from "./shim";
 
-test("set difference", () => {
+test("Set.prototype.difference", () => {
   // this set is bigger than other
   expect(setDifference(new Set([1, 2, 3, 4]), new Set([3, 4, 5]))).toEqual(
     new Set([1, 2])
@@ -12,7 +12,7 @@ test("set difference", () => {
   );
 });
 
-test("groupBy", () => {
+test("Map.groupBy", () => {
   expect(mapGroupBy([1, 2, 3, 4, 5], (item) => item % 2)).toEqual(
     new Map([
       [0, [2, 4]],
