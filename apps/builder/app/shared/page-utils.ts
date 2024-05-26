1b1fa9a0e7f30ab44f856a8a288d3ff1fe0c1ad1
Bogdan Chadkin <trysound@yandex.ru>
2024-05-26 17:25:56 +0300

    refactor: generalize page copying logic
    Ref https://github.com/webstudio-is/webstudio/issues/3397
    
    Here split insertPageCopyMutable from duplicatePage utility.
    It basically can extract page from any data and insert into any data.
    I will reuse it in marketplace later.
    
    Also fixed copying status, redirect and language fields. Their variables
    were not updated with new ids.

---
+++ apps/builder/app/shared/page-utils.ts
@@ -0,0 +1,137 @@
+import { nanoid } from "nanoid";
+import {
+  decodeDataSourceVariable,
+  encodeDataSourceVariable,
+  findPageByIdOrPath,
+  getPagePath,
+  transpileExpression,
+  type Folder,
+  type DataSource,
+  type Page,
+  type Pages,
+  type WebstudioData,
+} from "@webstudio-is/sdk";
+import {
+  extractWebstudioFragment,
+  insertWebstudioFragmentCopy,
+} from "./instance-utils";
+
+const deduplicatePath = (
+  pages: Pages,
+  folderId: Folder["id"],
+  path: Page["path"]
+) => {
+  const folderPath = getPagePath(folderId, pages);
+  if (path === "/") {
+    path = "";
+  }
+  let matchedPage = findPageByIdOrPath(`${folderPath}${path}`, pages);
+  if (matchedPage === undefined) {
+    return path;
+  }
+  let counter = 0;
+  while (matchedPage) {
+    counter += 1;
+    matchedPage = findPageByIdOrPath(
+      `${folderPath}/copy-${counter}${path}`,
+      pages
+    );
+  }
+  return `/copy-${counter}${path}`;
+};
+
+const replaceDataSources = (
+  expression: string,
+  replacements: Map<DataSource["id"], DataSource["id"]>
+) => {
+  return transpileExpression({
+    expression,
+    replaceVariable: (identifier) => {
+      const dataSourceId = decodeDataSourceVariable(identifier);
+      if (dataSourceId === undefined) {
+        return;
+      }
+      return encodeDataSourceVariable(
+        replacements.get(dataSourceId) ?? dataSourceId
+      );
+    },
+  });
+};
+
+export const insertPageCopyMutable = ({
+  source,
+  target,
+}: {
+  source: { data: WebstudioData; pageId: Page["id"] };
+  target: { data: WebstudioData; folderId: Folder["id"] };
+}) => {
+  const page = findPageByIdOrPath(source.pageId, source.data.pages);
+  if (page === undefined) {
+    return;
+  }
+  const fragment = extractWebstudioFragment(source.data, page.rootInstanceId);
+  const { newInstanceIds, newDataSourceIds } = insertWebstudioFragmentCopy({
+    data: target.data,
+    fragment,
+    availableDataSources: new Set(),
+  });
+  const newPageId = nanoid();
+  const { name = page.name, copyNumber } =
+    // extract a number from "name (copyNumber)"
+    page.name.match(/^(?<name>.+) \((?<copyNumber>\d+)\)$/)?.groups ?? {};
+  const newName = `${name} (${Number(copyNumber ?? "0") + 1})`;
+  const newRootInstanceId =
+    newInstanceIds.get(page.rootInstanceId) ?? page.rootInstanceId;
+  const newSystemDataSourceId =
+    newDataSourceIds.get(page.systemDataSourceId) ?? page.systemDataSourceId;
+  const newPage: Page = {
+    ...page,
+    id: newPageId,
+    rootInstanceId: newRootInstanceId,
+    systemDataSourceId: newSystemDataSourceId,
+    name: newName,
+    path: deduplicatePath(target.data.pages, target.folderId, page.path),
+    title: replaceDataSources(page.title, newDataSourceIds),
+    meta: {
+      ...page.meta,
+      description:
+        page.meta.description === undefined
+          ? undefined
+          : replaceDataSources(page.meta.description, newDataSourceIds),
+      excludePageFromSearch:
+        page.meta.excludePageFromSearch === undefined
+          ? undefined
+          : replaceDataSources(
+              page.meta.excludePageFromSearch,
+              newDataSourceIds
+            ),
+      socialImageUrl:
+        page.meta.socialImageUrl === undefined
+          ? undefined
+          : replaceDataSources(page.meta.socialImageUrl, newDataSourceIds),
+      language:
+        page.meta.language === undefined
+          ? undefined
+          : replaceDataSources(page.meta.language, newDataSourceIds),
+      status:
+        page.meta.status === undefined
+          ? undefined
+          : replaceDataSources(page.meta.status, newDataSourceIds),
+      redirect:
+        page.meta.redirect === undefined
+          ? undefined
+          : replaceDataSources(page.meta.redirect, newDataSourceIds),
+      custom: page.meta.custom?.map(({ property, content }) => ({
+        property,
+        content: replaceDataSources(content, newDataSourceIds),
+      })),
+    },
+  };
+  target.data.pages.pages.push(newPage);
+  for (const folder of target.data.pages.folders) {
+    if (folder.id === target.folderId) {
+      folder.children.push(newPageId);
+    }
+  }
+  return newPageId;
+};
