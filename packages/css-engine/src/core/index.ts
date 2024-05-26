d610884258b61682e2404e8f2a0577bfa1e5c013
Bogdan Chadkin <trysound@yandex.ru>
2024-05-26 12:24:16 +0300

    refactor: host hyphenate-style-name
    hyphenate-style-name utility is basically oneliner but requires
    to install two packages. Considering we will switch to hyphenate
    property naming by default, no reason to rely on this package.

--- packages/css-engine/src/core/index.ts
+++ packages/css-engine/src/core/index.ts
@@ -8,7 +8,7 @@ export type {
 export type { StyleSheetRegular } from "./style-sheet-regular";
 export * from "./create-style-sheet";
 export * from "./to-value";
-export * from "./to-property";
+export { hyphenateProperty } from "./to-property";
 export * from "./match-media";
 export * from "./equal-media";
 export * from "./compare-media";
