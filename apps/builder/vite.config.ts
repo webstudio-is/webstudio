ca921ae80ce4ad2cd08882bc8ba4b425b5428893
Bogdan Chadkin <trysound@yandex.ru>
2024-05-28 17:28:39 +0300

    refactor: merge all storybooks into one
    Monorepo setup for storybook is very complicated, it has two major
    problems
    
    - it is very annoying to look for any link in commit status
    - hard to upgrade comparing with single project setup
    
    Here merged all storybooks into one in the root.

--- apps/builder/vite.config.ts
+++ apps/builder/vite.config.ts
@@ -3,14 +3,11 @@ import { defineConfig } from "vite";
 import { vitePlugin as remix } from "@remix-run/dev";
 import { vercelPreset } from "@vercel/remix/vite";
 
-const isStorybook = process.argv[1]?.includes("storybook") ?? false;
-
 export default defineConfig(({ mode }) => ({
   plugins: [
-    isStorybook === false &&
-      remix({
-        presets: [vercelPreset()],
-      }),
+    remix({
+      presets: [vercelPreset()],
+    }),
   ],
   resolve: {
     conditions: ["webstudio", "import", "module", "browser", "default"],
