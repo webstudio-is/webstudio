bea4efc707d756ff8fd0b28e1e8ca143949a9667
Bogdan Chadkin <trysound@yandex.ru>
2024-05-28 17:45:33 +0300

    Fix lost pixel

--- lostpixel.config.js
+++ lostpixel.config.js
@@ -2,8 +2,8 @@ import * as process from "process";
 
 export const config = {
   storybookShots: {
-    storybookUrl: "packages/design-system/storybook-static",
+    storybookUrl: "storybook-static",
   },
-  lostPixelProjectId: "cleii5n4g0efjim0e69w6rme0",
+  lostPixelProjectId: "cleiive6c0gchi40em3uzx1xv",
   apiKey: process.env.LOST_PIXEL_API_KEY,
 };
