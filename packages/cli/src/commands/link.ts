42a145d729262997a6d255d6c870866b86397d2a
Bogdan Chadkin <opensource@trysound.io>
2024-05-30 09:29:41 +0300

    feat: improve CLI link command (#3449)
    Switched from readline to @clack/prompts package
    with more rich ui docorations and better validation.
    
    It should improve UX and @clack/prompts actually can replace both
    prompts and ora packages, in fact it is smaller than each of these and
    has builtin types.
    
    
    
    https://github.com/webstudio-is/webstudio/assets/5635476/b03174f5-33ef-47a5-90b8-90ad0697c442

--- packages/cli/src/commands/link.ts
+++ packages/cli/src/commands/link.ts
@@ -1,7 +1,7 @@
-import { stdin, stdout, cwd } from "node:process";
+import { cwd, exit } from "node:process";
 import { join } from "node:path";
-import * as readline from "node:readline/promises";
 import { readFile, writeFile } from "node:fs/promises";
+import { cancel, isCancel, log, text } from "@clack/prompts";
 import {
   GLOBAL_CONFIG_FILE,
   LOCAL_CONFIG_FILE,
@@ -15,6 +15,40 @@ import type {
   StrictYargsOptionsToInterface,
 } from "./yargs-types";
 
+const parseShareLink = (value: string) => {
+  const url = new URL(value);
+  const origin = url.origin;
+  const token = url.searchParams.get("authToken");
+  // @todo parse with URLPattern once landed in node
+  const segments = url.pathname.split("/").slice(1);
+  if (segments.length !== 2 || segments[0] !== "builder") {
+    throw Error("Segments not matching");
+  }
+  const [_builder, projectId] = segments;
+  if (token == null) {
+    throw Error("Token is missing");
+  }
+  return {
+    origin,
+    projectId,
+    token,
+  };
+};
+
+export const validateShareLink = (value: string) => {
+  if (value.length === 0) {
+    return "Share link is required";
+  }
+  if (URL.canParse(value) === false) {
+    return "Share link is invalid";
+  }
+  try {
+    parseShareLink(value);
+  } catch {
+    return "Share link is invalid";
+  }
+};
+
 export const linkOptions = (yargs: CommonYargsArgv) =>
   yargs.option("link", {
     alias: "l",
@@ -25,62 +59,44 @@ export const linkOptions = (yargs: CommonYargsArgv) =>
 export const link = async (
   options: StrictYargsOptionsToInterface<typeof linkOptions> | { link?: string }
 ) => {
-  let shareLink: string;
+  let shareLink: string | symbol;
   if (options.link) {
     shareLink = options.link;
   } else {
-    const rl = readline.createInterface({ input: stdin, output: stdout });
-    shareLink = await rl.question(
-      `Please paste a link from the Share Dialog in the builder: `
-    );
-    rl.close();
-  }
-
-  const shareLinkUrl = new URL(shareLink);
-  const origin = shareLinkUrl.origin;
-  const token = shareLinkUrl.searchParams.get("authToken");
-  const paths = shareLinkUrl.pathname.split("/").slice(1);
-
-  if (paths[0] !== "builder" || paths.length !== 2) {
-    throw new Error("Invalid share link.");
-  }
-
-  const projectId = paths[1];
-
-  if (token == null) {
-    throw new Error("Invalid share link.");
-  }
-
-  try {
-    const currentConfig = await readFile(GLOBAL_CONFIG_FILE, "utf-8");
-    const currentConfigJson = jsonToGlobalConfig(JSON.parse(currentConfig));
-
-    const newConfig: GlobalConfig = {
-      ...currentConfigJson,
-      [projectId]: {
-        origin,
-        token,
-      },
-    };
-
-    await writeFile(GLOBAL_CONFIG_FILE, JSON.stringify(newConfig, null, 2));
-    console.info(`Saved credentials for project ${projectId}.
-  You can find your config at ${GLOBAL_CONFIG_FILE}
-        `);
-
-    const localConfig: LocalConfig = {
-      projectId,
-    };
-
-    await createFileIfNotExists(
-      join(cwd(), LOCAL_CONFIG_FILE),
-      JSON.stringify(localConfig, null, 2)
-    );
-  } catch (error: unknown) {
-    if (error instanceof Error && "code" in error && error.code === "ENONET") {
-      throw new Error(`Global config file is not found`);
+    shareLink = await text({
+      message: "Please paste a link from the Share Dialog in the builder",
+      validate: validateShareLink,
+    });
+    if (isCancel(shareLink)) {
+      cancel("Project linking is cancelled");
+      exit(1);
     }
-
-    throw error;
   }
+
+  const { origin, projectId, token } = parseShareLink(shareLink);
+
+  const currentConfig = await readFile(GLOBAL_CONFIG_FILE, "utf-8");
+  const currentConfigJson = jsonToGlobalConfig(JSON.parse(currentConfig));
+
+  const newConfig: GlobalConfig = {
+    ...currentConfigJson,
+    [projectId]: {
+      origin,
+      token,
+    },
+  };
+
+  await writeFile(GLOBAL_CONFIG_FILE, JSON.stringify(newConfig, null, 2));
+  log.info(`Saved credentials for project ${projectId}.
+You can find your config at ${GLOBAL_CONFIG_FILE}`);
+
+  const localConfig: LocalConfig = {
+    projectId,
+  };
+
+  await createFileIfNotExists(
+    join(cwd(), LOCAL_CONFIG_FILE),
+    JSON.stringify(localConfig, null, 2)
+  );
+  log.success("The project is linked successfully.");
 };
