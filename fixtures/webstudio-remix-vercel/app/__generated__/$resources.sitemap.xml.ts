b5e15eafee7ee14d724294639e6315ff798ed670
Bogdan Chadkin <opensource@trysound.io>
2024-05-31 00:17:53 +0300

    feat: improve CLI init commands (#3457)
    Switched init command in CLI to @clack/promps
    and dropped both prompts and ora packages.
    
    
    
    https://github.com/webstudio-is/webstudio/assets/5635476/dca53f0d-69ec-46c8-9537-680c24bffa66

--- fixtures/webstudio-remix-vercel/app/__generated__/$resources.sitemap.xml.ts
+++ fixtures/webstudio-remix-vercel/app/__generated__/$resources.sitemap.xml.ts
@@ -1,26 +1,26 @@
 export const sitemap = [
   {
     path: "/",
-    lastModified: "2024-02-01",
+    lastModified: "2024-05-30",
   },
   {
     path: "/_route_with_symbols_",
-    lastModified: "2024-02-01",
+    lastModified: "2024-05-30",
   },
   {
     path: "/form",
-    lastModified: "2024-02-01",
+    lastModified: "2024-05-30",
   },
   {
     path: "/heading-with-id",
-    lastModified: "2024-02-01",
+    lastModified: "2024-05-30",
   },
   {
     path: "/resources",
-    lastModified: "2024-02-01",
+    lastModified: "2024-05-30",
   },
   {
     path: "/nested/nested-page",
-    lastModified: "2024-02-01",
+    lastModified: "2024-05-30",
   },
 ];
