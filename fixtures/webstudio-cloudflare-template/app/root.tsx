6259fe240fce47439e9ece9e530d530c052c5dc1
istarkov <istarkov@gmail.com>
2024-06-03 14:01:01 +0000

    Move language into root

--- fixtures/webstudio-cloudflare-template/app/root.tsx
+++ fixtures/webstudio-cloudflare-template/app/root.tsx
@@ -1,8 +1,21 @@
-import { Links, Meta, Outlet } from "@remix-run/react";
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+
+import { Links, Meta, Outlet, useMatches } from "@remix-run/react";
 
 const Root = () => {
+  // Get language from matches
+  const matches = useMatches();
+
+  const lastMatchWithLanguage = [...matches]
+    .reverse()
+    // @ts-ignore
+    .find((match) => match?.data?.pageMeta?.language != null);
+
+  // @ts-ignore
+  const lang = lastMatchWithLanguage?.data?.pageMeta?.language ?? "en";
+
   return (
-    <html lang="en">
+    <html lang={lang}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
