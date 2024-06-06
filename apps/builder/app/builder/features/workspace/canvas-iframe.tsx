b4c7c21f5785a55aa529d30e19b5fa84b30670d6
istarkov <istarkov@gmail.com>
2024-06-06 11:07:29 +0000

    Simplify

--- apps/builder/app/builder/features/workspace/canvas-iframe.tsx
+++ apps/builder/app/builder/features/workspace/canvas-iframe.tsx
@@ -4,7 +4,7 @@ import {
   css,
   canvasPointerEventsPropertyName,
 } from "@webstudio-is/design-system";
-import { useMount } from "~/shared/hook-utils/use-mount";
+import { useUnmount } from "~/shared/hook-utils/use-mount";
 import { $canvasIframeState } from "~/shared/nano-states";
 
 const iframeStyle = css({
@@ -18,11 +18,9 @@ type CanvasIframeProps = {
 
 export const CanvasIframe = forwardRef<HTMLIFrameElement, CanvasIframeProps>(
   ({ css, ...rest }, ref) => {
-    useMount(() => {
-      return () => {
-        // We can't do this inside canvas as not event is triggered there
-        $canvasIframeState.set("idle");
-      };
+    useUnmount(() => {
+      // Unmount does't work inside iframe.
+      $canvasIframeState.set("idle");
     });
 
     return <iframe {...rest} ref={ref} className={iframeStyle({ css })} />;
