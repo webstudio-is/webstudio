d251e998c82d26592100069077494d1fc8e5c2a0
Bogdan Chadkin <opensource@trysound.io>
2024-06-16 12:58:10 +0300

    refactor: use jsx in component generator tests (#3571)
    Here enahnced our jsx with props support and rewrote component generator
    tests with it as an example.
    
    Props are solved in straightforward way, type of value described prop
    type. Though in case of expression, parameter, asset, page, action used
    classes as opaque types.

--- packages/sdk/src/jsx.test.tsx
+++ packages/sdk/src/jsx.test.tsx
@@ -1,5 +1,12 @@
 import { expect, test } from "@jest/globals";
-import { $, renderJsx } from "./jsx";
+import {
+  $,
+  AssetValue,
+  ExpressionValue,
+  PageValue,
+  ParameterValue,
+  renderJsx,
+} from "./jsx";
 
 test("render jsx into instances with generated id", () => {
   const { instances } = renderJsx(
@@ -79,6 +86,144 @@ test("override generated ids with ws:id prop", () => {
           id: "custom3",
           component: "Span",
           children: [],
+        },
+      ],
+    ])
+  );
+});
+
+test("render text children", () => {
+  const { instances } = renderJsx(<$.Body>children</$.Body>);
+  expect(instances).toEqual(
+    new Map([
+      [
+        "0",
+        {
+          type: "instance",
+          id: "0",
+          component: "Body",
+          children: [{ type: "text", value: "children" }],
+        },
+      ],
+    ])
+  );
+});
+
+test("render literal props", () => {
+  const { props } = renderJsx(
+    <$.Body data-string="string" data-number={0}>
+      <$.Box data-bool={true} data-json={{ param: "value" }}></$.Box>
+    </$.Body>
+  );
+  expect(props).toEqual(
+    new Map([
+      [
+        "0:data-number",
+        {
+          id: "0:data-number",
+          instanceId: "0",
+          name: "data-number",
+          type: "number",
+          value: 0,
+        },
+      ],
+      [
+        "0:data-string",
+        {
+          id: "0:data-string",
+          instanceId: "0",
+          name: "data-string",
+          type: "string",
+          value: "string",
+        },
+      ],
+      [
+        "1:data-bool",
+        {
+          id: "1:data-bool",
+          instanceId: "1",
+          name: "data-bool",
+          type: "boolean",
+          value: true,
+        },
+      ],
+      [
+        "1:data-json",
+        {
+          id: "1:data-json",
+          instanceId: "1",
+          name: "data-json",
+          type: "json",
+          value: { param: "value" },
+        },
+      ],
+    ])
+  );
+});
+
+test("render defined props", () => {
+  const { props } = renderJsx(
+    <$.Body
+      data-expression={new ExpressionValue("1 + 1")}
+      data-parameter={new ParameterValue("parameterId")}
+    >
+      <$.Box
+        data-asset={new AssetValue("assetId")}
+        data-page={new PageValue("pageId")}
+        data-instance={new PageValue("pageId", "instanceId")}
+      ></$.Box>
+    </$.Body>
+  );
+  expect(props).toEqual(
+    new Map([
+      [
+        "0:data-expression",
+        {
+          id: "0:data-expression",
+          instanceId: "0",
+          name: "data-expression",
+          type: "expression",
+          value: "1 + 1",
+        },
+      ],
+      [
+        "0:data-parameter",
+        {
+          id: "0:data-parameter",
+          instanceId: "0",
+          name: "data-parameter",
+          type: "parameter",
+          value: "parameterId",
+        },
+      ],
+      [
+        "1:data-asset",
+        {
+          id: "1:data-asset",
+          instanceId: "1",
+          name: "data-asset",
+          type: "asset",
+          value: "assetId",
+        },
+      ],
+      [
+        "1:data-page",
+        {
+          id: "1:data-page",
+          instanceId: "1",
+          name: "data-page",
+          type: "page",
+          value: "pageId",
+        },
+      ],
+      [
+        "1:data-instance",
+        {
+          id: "1:data-instance",
+          instanceId: "1",
+          name: "data-instance",
+          type: "page",
+          value: { pageId: "pageId", instanceId: "instanceId" },
         },
       ],
     ])
