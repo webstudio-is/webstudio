d8b6aeaf24b7bffcf65baab2733895f2e5aa23cf
Oleg Isonen <oleg008@gmail.com>
2024-07-05 22:11:32 +0100

    fix: Set initial canvas width once selected breakpoint is known (#3684)
    Close https://github.com/webstudio-is/webstudio/issues/3682
    
    ## Description
    
    1. on initial load resize handes were not visible because initial width
    was not set
    
    ## Steps for reproduction
    
    1. click button
    2. expect xyz
    
    ## Code Review
    
    - [ ] hi @kof, I need you to do
      - conceptual review (architecture, feature-correctness)
      - detailed review (read every line)
      - test it on preview
    
    ## Before requesting a review
    
    - [ ] made a self-review
    - [ ] added inline comments where things may be not obvious (the "why",
    not "what")
    
    ## Before merging
    
    - [ ] tested locally and on preview environment (preview dev login:
    5de6)
    - [ ] updated [test
    cases](https://github.com/webstudio-is/webstudio/blob/main/apps/builder/docs/test-cases.md)
    document
    - [ ] added tests
    - [ ] if any new env variables are added, added them to `.env` file

--- apps/builder/app/builder/features/breakpoints/use-set-initial-canvas-width.ts
+++ apps/builder/app/builder/features/breakpoints/use-set-initial-canvas-width.ts
@@ -8,12 +8,11 @@ import {
 } from "~/shared/nano-states";
 import { calcCanvasWidth } from "./calc-canvas-width";
 
-// Fixes initial canvas width jump on wide screens.
-// Calculate canvas width during SSR based on known initial width for wide screens.
-export const setInitialCanvasWidth = (breakpointId: Breakpoint["id"]) => {
+export const setCanvasWidth = (breakpointId: Breakpoint["id"]) => {
   const workspaceRect = $workspaceRect.get();
   const breakpoints = $breakpoints.get();
   const selectedBreakpoint = breakpoints.get(breakpointId);
+
   if (workspaceRect === undefined || selectedBreakpoint === undefined) {
     return false;
   }
@@ -34,45 +33,38 @@ export const setInitialCanvasWidth = (breakpointId: Breakpoint["id"]) => {
 export const useSetCanvasWidth = () => {
   useEffect(() => {
     const update = () => {
-      const breakpoints = $breakpoints.get();
-      const workspaceRect = $workspaceRect.get();
-      if (workspaceRect === undefined || breakpoints.size === 0) {
-        return;
-      }
       const selectedBreakpoint = $selectedBreakpoint.get();
-
-      // When there is selected breakpoint, we want to find the smallest possible size
-      // that is bigger than any max-width breakpoints and smaller than any min-width breakpoints.
-      // When on base breakpoint it will be the biggest possible but smaller than the workspace.
       if (selectedBreakpoint) {
-        const canvasWidth = $canvasWidth.get();
-        const nextWidth = calcCanvasWidth({
-          breakpoints: Array.from(breakpoints.values()),
-          selectedBreakpoint,
-          workspaceWidth: workspaceRect.width,
-          canvasWidth,
-        });
-        $canvasWidth.set(nextWidth);
+        // When there is selected breakpoint, we want to find the smallest possible size
+        // that is bigger than any max-width breakpoints and smaller than any min-width breakpoints.
+        // When on base breakpoint it will be the biggest possible but smaller than the workspace.
+        setCanvasWidth(selectedBreakpoint.id);
       }
     };
 
-    const unsubscribeBreakpointStore = $breakpoints.subscribe(update);
-    const unsubscribeRectStore = $workspaceRect.listen((workspaceRect) => {
-      if (workspaceRect === undefined) {
-        return;
-      }
-      update();
-    });
+    const unsubscribeBreakpoints = $breakpoints.subscribe(update);
+    const unsubscribeRect = $workspaceRect.listen(update);
     const unsubscribeIsPreviewMode = $isPreviewMode.listen((isPreviewMode) => {
       if (isPreviewMode) {
         update();
       }
     });
+    const unsubscribeSelectedBreakpoint = $selectedBreakpoint.subscribe(
+      (selectedBreakpoint) => {
+        // This will set initial width of the canvas once the initial selected breakpoint is known.
+        if (selectedBreakpoint) {
+          update();
+          // We can unsubscribe right away as we only need this once.
+          unsubscribeSelectedBreakpoint();
+        }
+      }
+    );
 
     return () => {
-      unsubscribeBreakpointStore();
-      unsubscribeRectStore();
+      unsubscribeBreakpoints();
+      unsubscribeRect();
       unsubscribeIsPreviewMode();
+      unsubscribeSelectedBreakpoint();
     };
   }, []);
 };
