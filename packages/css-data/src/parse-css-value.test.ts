0373c52a533e7d855dd0fc19e1475296e10a6863
Bogdan Chadkin <opensource@trysound.io>
2024-07-15 11:42:17 +0200

    refactor: move shadow parser into value parser (#3723)
    We don't need separate parsing logic for box-shadow and text-shadow
    properties. Generic css value parser is able to produce the same ast.

--- packages/css-data/src/parse-css-value.test.ts
+++ packages/css-data/src/parse-css-value.test.ts
@@ -619,3 +619,170 @@ test("support custom properties var reference in custom property", () => {
     fallbacks: [{ type: "unparsed", value: "red" }],
   });
 });
+
+describe("parse shadows", () => {
+  test("parses value and returns invalid when used a invalid boxShadow is passed", () => {
+    expect(parseCssValue("boxShadow", `10px 10px 5px foo`)).toEqual({
+      type: "invalid",
+      value: "10px 10px 5px foo",
+    });
+  });
+
+  test("parses value and returns invalid when a invalid textShadow is passed", () => {
+    expect(parseCssValue("textShadow", `10px 10px 5px foo`)).toEqual({
+      type: "invalid",
+      value: "10px 10px 5px foo",
+    });
+  });
+
+  test("throws error when passed a value without a unit", () => {
+    expect(parseCssValue("boxShadow", `10 10px 5px red`)).toEqual({
+      type: "invalid",
+      value: "10 10px 5px red",
+    });
+  });
+
+  test("parses values and returns a layer when a valid textShadow is passes", () => {
+    expect(parseCssValue("textShadow", "1px 1px 2px black")).toEqual({
+      type: "layers",
+      value: [
+        {
+          type: "tuple",
+          value: [
+            { type: "unit", unit: "px", value: 1 },
+            { type: "unit", unit: "px", value: 1 },
+            { type: "unit", unit: "px", value: 2 },
+            { type: "keyword", value: "black" },
+          ],
+        },
+      ],
+    });
+  });
+
+  test("inset and color values can be interchanged", () => {
+    expect(parseCssValue("boxShadow", `inset 10px 10px 5px black`)).toEqual({
+      type: "layers",
+      value: [
+        {
+          type: "tuple",
+          value: [
+            { type: "keyword", value: "inset" },
+            { type: "unit", unit: "px", value: 10 },
+            { type: "unit", unit: "px", value: 10 },
+            { type: "unit", unit: "px", value: 5 },
+            { type: "keyword", value: "black" },
+          ],
+        },
+      ],
+    });
+  });
+
+  test("parses value when inset is used but missing blur-radius", () => {
+    expect(parseCssValue("boxShadow", `inset 5em 1em gold`)).toEqual({
+      type: "layers",
+      value: [
+        {
+          type: "tuple",
+          value: [
+            { type: "keyword", value: "inset" },
+            { type: "unit", unit: "em", value: 5 },
+            { type: "unit", unit: "em", value: 1 },
+            { type: "keyword", value: "gold" },
+          ],
+        },
+      ],
+    });
+  });
+
+  test("parses value when offsetX and offsetY are used", () => {
+    expect(parseCssValue("boxShadow", `60px -16px teal`)).toEqual({
+      type: "layers",
+      value: [
+        {
+          type: "tuple",
+          value: [
+            { type: "unit", unit: "px", value: 60 },
+            { type: "unit", unit: "px", value: -16 },
+            { type: "keyword", value: "teal" },
+          ],
+        },
+      ],
+    });
+  });
+
+  test("parses value from figma", () => {
+    expect(
+      parseCssValue(
+        "boxShadow",
+        "0 60px 80px rgba(0,0,0,0.60), 0 45px 26px rgba(0,0,0,0.14)"
+      )
+    ).toEqual({
+      type: "layers",
+      value: [
+        {
+          type: "tuple",
+          value: [
+            { type: "unit", unit: "number", value: 0 },
+            { type: "unit", unit: "px", value: 60 },
+            { type: "unit", unit: "px", value: 80 },
+            { alpha: 0.6, b: 0, g: 0, r: 0, type: "rgb" },
+          ],
+        },
+        {
+          type: "tuple",
+          value: [
+            { type: "unit", unit: "number", value: 0 },
+            { type: "unit", unit: "px", value: 45 },
+            { type: "unit", unit: "px", value: 26 },
+            { alpha: 0.14, b: 0, g: 0, r: 0, type: "rgb" },
+          ],
+        },
+      ],
+    });
+  });
+
+  test(`parses multiple layers of box-shadow property`, () => {
+    expect(
+      parseCssValue(
+        "boxShadow",
+        `
+        0 0 5px rgba(0, 0, 0, 0.2),
+        inset 0 0 10px rgba(0, 0, 0, 0.3),
+        0 0 15px rgba(0, 0, 0, 0.4)
+        `
+      )
+    ).toEqual({
+      type: "layers",
+      value: [
+        {
+          type: "tuple",
+          value: [
+            { type: "unit", unit: "number", value: 0 },
+            { type: "unit", unit: "number", value: 0 },
+            { type: "unit", unit: "px", value: 5 },
+            { alpha: 0.2, b: 0, g: 0, r: 0, type: "rgb" },
+          ],
+        },
+        {
+          type: "tuple",
+          value: [
+            { type: "keyword", value: "inset" },
+            { type: "unit", unit: "number", value: 0 },
+            { type: "unit", unit: "number", value: 0 },
+            { type: "unit", unit: "px", value: 10 },
+            { alpha: 0.3, b: 0, g: 0, r: 0, type: "rgb" },
+          ],
+        },
+        {
+          type: "tuple",
+          value: [
+            { type: "unit", unit: "number", value: 0 },
+            { type: "unit", unit: "number", value: 0 },
+            { type: "unit", unit: "px", value: 15 },
+            { alpha: 0.4, b: 0, g: 0, r: 0, type: "rgb" },
+          ],
+        },
+      ],
+    });
+  });
+});
