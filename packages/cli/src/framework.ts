aede354cbdc9c7eea71ca122524200a64138e74c
Bogdan Chadkin <opensource@trysound.io>
2024-07-18 13:09:08 +0200

    refactor: isolate remix specifics in cli (#3753)
    We wanna support other frameworks than remix.
    At least to support SSG we need to additionally deploy vike.dev.
    
    Here split all remix specifics into separate module. It provides the
    list of components (including overrides) and templates getter.
    
    Templates allow to provide multiple module per route which is essential
    for many frameworks like svelte and vike which do not rely on
    treeshaking for client/server splitting.

---
+++ packages/cli/src/framework.ts
@@ -0,0 +1,19 @@
+import type { WsComponentMeta } from "@webstudio-is/react-sdk";
+
+type FrameworkComponentEntry = {
+  source: string;
+  metas: Record<string, WsComponentMeta>;
+};
+
+type FrameworkTemplateEntry = {
+  file: string;
+  template: string;
+};
+
+export type Framework = {
+  components: FrameworkComponentEntry[];
+  html: (params: { pagePath: string }) => FrameworkTemplateEntry[];
+  xml: (params: { pagePath: string }) => FrameworkTemplateEntry[];
+  redirect: (params: { pagePath: string }) => FrameworkTemplateEntry[];
+  defaultSitemap: () => FrameworkTemplateEntry[];
+};
