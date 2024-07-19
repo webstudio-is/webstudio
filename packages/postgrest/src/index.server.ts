a85716b3b78e8eaba8afd6017dec63ced8b17c9f
istarkov <istarkov@gmail.com>
2024-07-19 22:13:37 +0000

    Fix

--- packages/postgrest/src/index.server.ts
+++ packages/postgrest/src/index.server.ts
@@ -10,24 +10,6 @@ export const createClient = (url: string, apiKey: string): Client => {
       apikey: apiKey,
       Authorization: `Bearer ${apiKey}`,
     },
-    fetch: async (input, init) => {
-      // delete init.headers["X-Client-Info"];
-      try {
-        // For unknown reason this allows brotli encoding to work
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-expect-error
-        delete init.headers["Accept"];
-      } catch {
-        // do nothing
-      }
-      const res = await fetch(input, init);
-
-      if (!res.ok) {
-        throw new Error(await res.text());
-      }
-
-      return res;
-    },
   });
 
   return client;
