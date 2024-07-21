c60e55feea47ac4e5cc0fccdf053ea1d3b94563a
Bogdan Chadkin <opensource@trysound.io>
2024-07-21 15:23:26 +0200

    refactor: add page link to basic fixture (#3769)
    We need at least to test page links in basic fixtures.

---
+++ fixtures/webstudio-remix-netlify-functions/app/__generated__/[another-page]._index.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable */
+/* This is a auto generated file for building the project */
+
+import { Fragment, useState } from "react";
+import type { FontAsset, ImageAsset } from "@webstudio-is/sdk";
+import { useResource } from "@webstudio-is/react-sdk";
+import { Body as Body } from "@webstudio-is/sdk-components-react-remix";
+import { Heading as Heading } from "@webstudio-is/sdk-components-react";
+
+export const siteName = "";
+
+export const favIconAsset: ImageAsset | undefined = undefined;
+
+// Font assets on current page (can be preloaded)
+export const pageFontAssets: FontAsset[] = [];
+
+export const pageBackgroundImageAssets: ImageAsset[] = [];
+
+const Page = ({}: { system: any }) => {
+  return (
+    <Body data-ws-id="n_VBMr7klpx25buS0NV7R" data-ws-component="Body">
+      <Heading data-ws-id="wthNByqb3RPmheb-56VYI" data-ws-component="Heading">
+        {"Another page"}
+      </Heading>
+    </Body>
+  );
+};
+
+export { Page };
