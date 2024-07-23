159b448c4327b9a7d81e36a1491a4be3a1e5d7b2
Oleg Isonen <oleg008@gmail.com>
2024-07-23 14:57:46 +0100

    adapt humanizeString for attributes

--- apps/builder/app/shared/string-utils.ts
+++ apps/builder/app/shared/string-utils.ts
@@ -5,10 +5,18 @@ import { titleCase } from "title-case";
 const cache: Map<string, string> = new Map([
   ["id", "ID"],
   ["url", "URL"],
+  ["className", "Class"],
 ]);
 
 export const humanizeString = (string: string): string => {
-  const result = cache.get(string) ?? titleCase(noCase(string));
+  let result = cache.get(string);
+  // We don't want to change casing for strings like data- or aria-
+  if (string.includes("-")) {
+    result = string;
+  }
+  if (result === undefined) {
+    result = titleCase(noCase(string));
+  }
   cache.set(string, result);
   return result;
 };
