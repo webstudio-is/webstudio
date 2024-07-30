1f435cc7f98987c987fd382477f2f43a45449342
Oleg Isonen <oleg008@gmail.com>
2024-07-30 03:01:36 +0100

    chore: Disable marketplace resource (#3799)
    Fixes https://github.com/webstudio-is/webstudio/issues/3680
    And fixes " Marketplace lets people access gated features (resource
    settings need to be disabled on free)" in
    https://github.com/webstudio-is/webstudio/issues/3700
    
    ## Description
    
    - Allow user see resources from marketplace but no changes if not on pro
    - When not on pro, you can fully see hints and even select Resources,
    but you can't change them, changes won't be saved and user will see a
    banner
    
    
    ## Steps for reproduction
    
    1. click button
    2. expect xyz
    
    ## Code Review
    
    - [ ] hi @kof, I need you to do
      - conceptual review (architecture, feature-correctness)
      - detailed review (read every line)
      - test it on preview
    
    ## Before requesting a review
    
    - [ ] made a self-review
    - [ ] added inline comments where things may be not obvious (the "why",
    not "what")
    
    ## Before merging
    
    - [ ] tested locally and on preview environment (preview dev login:
    5de6)
    - [ ] updated [test
    cases](https://github.com/webstudio-is/webstudio/blob/main/apps/builder/docs/test-cases.md)
    document
    - [ ] added tests
    - [ ] if any new env variables are added, added them to `.env` file

--- apps/builder/app/builder/features/settings-panel/settings-panel.tsx
+++ apps/builder/app/builder/features/settings-panel/settings-panel.tsx
@@ -2,17 +2,54 @@ import type { Instance } from "@webstudio-is/sdk";
 import { SettingsSection } from "./settings-section";
 import { PropsSectionContainer } from "./props-section/props-section";
 import { VariablesSection } from "./variables-section";
+import {
+  Flex,
+  Link,
+  PanelBanner,
+  Text,
+  rawTheme,
+} from "@webstudio-is/design-system";
+import { UploadIcon } from "@webstudio-is/icons";
+import { useStore } from "@nanostores/react";
+import { $userPlanFeatures } from "~/builder/shared/nano-states";
+import cmsUpgradeBanner from "./cms-upgrade-banner.svg?url";
 
 export const SettingsPanelContainer = ({
   selectedInstance,
 }: {
   selectedInstance: Instance;
 }) => {
+  const { allowDynamicData } = useStore($userPlanFeatures);
   return (
     <>
       <SettingsSection />
       <PropsSectionContainer selectedInstance={selectedInstance} />
       <VariablesSection />
+      {allowDynamicData === false && (
+        <PanelBanner>
+          <img
+            src={cmsUpgradeBanner}
+            alt="Upgrade for CMS"
+            width={rawTheme.spacing[28]}
+            style={{ aspectRatio: "4.1" }}
+          />
+          <Text variant="regularBold">Upgrade for CMS</Text>
+          <Text>
+            Integrate content from other tools to create blogs, directories, and
+            any other structured content.
+          </Text>
+          <Flex align="center" gap={1}>
+            <UploadIcon />
+            <Link
+              color="inherit"
+              target="_blank"
+              href="https://webstudio.is/pricing"
+            >
+              Upgrade to Pro
+            </Link>
+          </Flex>
+        </PanelBanner>
+      )}
     </>
   );
 };
