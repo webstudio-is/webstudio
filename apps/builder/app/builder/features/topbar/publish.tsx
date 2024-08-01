c71de50730561e543733ee178e94c802b17ecfe5
Ivan Starkov <istarkov@gmail.com>
2024-08-01 19:13:32 +0300

    experimental: Set vanilla export as default and add static site help link (#3853)
    ## Description
    
    Set vanilla export as default and add static site help link
    
    <img width="356" alt="image"
    src="https://github.com/user-attachments/assets/87af7283-da72-46b2-ab51-c04483dd2c73">
    
    
    - [ ] - Doc must be created here https://wstd.us/ssg cc @kof @johnsicili
    
    ## Steps for reproduction
    
    1. click button
    2. expect xyz
    
    ## Code Review
    
    - [ ] hi @kof, I need you to do
      - conceptual review (architecture, feature-correctness)
      - detailed review (read every line)
      - test it on preview
    
    ## Before requesting a review
    
    - [ ] made a self-review
    - [ ] added inline comments where things may be not obvious (the "why",
    not "what")
    
    ## Before merging
    
    - [ ] tested locally and on preview environment (preview dev login:
    5de6)
    - [ ] updated [test
    cases](https://github.com/webstudio-is/webstudio/blob/main/apps/builder/docs/test-cases.md)
    document
    - [ ] added tests
    - [ ] if any new env variables are added, added them to `.env` file

--- apps/builder/app/builder/features/topbar/publish.tsx
+++ apps/builder/app/builder/features/topbar/publish.tsx
@@ -751,7 +751,7 @@ const isDeployTargets = (value: string): value is DeployTargets =>
 
 const ExportContent = (props: { projectId: Project["id"] }) => {
   const npxCommand = "npx webstudio@latest";
-  const [deployTarget, setDeployTarget] = useState<DeployTargets>("vercel");
+  const [deployTarget, setDeployTarget] = useState<DeployTargets>("vanilla");
 
   return (
     <Grid
@@ -790,6 +790,20 @@ const ExportContent = (props: { projectId: Project["id"] }) => {
               projectId={props.projectId}
               templates={deployTargets[deployTarget].ssgTemplates}
             />
+            <div />
+            <Text color="subtle">
+              Learn about deploying static sites{" "}
+              <Link
+                variant="inherit"
+                color="inherit"
+                href="https://wstd.us/ssg"
+                target="_blank"
+                rel="noreferrer"
+              >
+                here
+              </Link>
+            </Text>
+
             <div />
             <div />
             <Grid
