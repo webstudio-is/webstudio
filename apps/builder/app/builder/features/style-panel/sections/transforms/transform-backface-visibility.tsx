9a1d87e727d319819a966396f7de90ac110f93dc
Jaya Krishna <namburu1995@gmail.com>
2024-08-06 11:54:19 +0530

    use default controls to show backface visibility proeprty

--- apps/builder/app/builder/features/style-panel/sections/transforms/transform-backface-visibility.tsx
+++ apps/builder/app/builder/features/style-panel/sections/transforms/transform-backface-visibility.tsx
@@ -3,21 +3,30 @@ import {
   CssValueListItem,
   Grid,
   Label,
-  Select,
   SmallIconButton,
+  SmallToggleButton,
   theme,
 } from "@webstudio-is/design-system";
 import { humanizeString } from "~/shared/string-utils";
 import type { SectionProps } from "../shared/section";
 import { FloatingPanel } from "~/builder/shared/floating-panel";
 import { transformPanels } from "./transforms";
-import { SubtractIcon } from "@webstudio-is/icons";
+import {
+  EyeconClosedIcon,
+  EyeconOpenIcon,
+  SubtractIcon,
+} from "@webstudio-is/icons";
+import { TextControl } from "../../controls";
+import { PropertyName } from "../../shared/property-name";
+import { styleConfigByName } from "../../shared/configs";
 
 const property: StyleProperty = "backfaceVisibility";
 
 export const BackfaceVisibility = (props: SectionProps) => {
   const { currentStyle, setProperty, deleteProperty } = props;
   const value = currentStyle[property]?.local;
+  const { label } = styleConfigByName(property);
+
   if (value?.type !== "keyword") {
     return;
   }
@@ -31,14 +40,17 @@ export const BackfaceVisibility = (props: SectionProps) => {
           align="center"
           css={{ p: theme.spacing[9], gridTemplateColumns: "1.5fr 1fr" }}
         >
-          <Label>Backface Visibility</Label>
-          <Select
-            options={["visible", "hidden"]}
-            value={toValue(value)}
-            getLabel={humanizeString}
-            onChange={(value) => {
-              setProperty(property)({ type: "keyword", value });
-            }}
+          <PropertyName
+            label={label}
+            properties={[property]}
+            style={currentStyle}
+            onReset={() => deleteProperty(property)}
+          />
+          <TextControl
+            property={property}
+            currentStyle={currentStyle}
+            setProperty={setProperty}
+            deleteProperty={deleteProperty}
           />
         </Grid>
       }
@@ -46,18 +58,33 @@ export const BackfaceVisibility = (props: SectionProps) => {
       <CssValueListItem
         id={property}
         index={transformPanels.length + 1}
+        hidden={value.hidden}
         label={
           <Label truncate>
-            {humanizeString(property)}: {toValue(value)}
+            {humanizeString(property)}: {toValue({ ...value, hidden: false })}
           </Label>
         }
         buttons={
-          <SmallIconButton
-            variant="destructive"
-            tabIndex={-1}
-            icon={<SubtractIcon />}
-            onClick={() => deleteProperty(property)}
-          />
+          <>
+            <SmallToggleButton
+              variant="normal"
+              pressed={value.hidden}
+              tabIndex={-1}
+              onPressedChange={() =>
+                setProperty(property)({
+                  ...value,
+                  hidden: value.hidden ? false : true,
+                })
+              }
+              icon={value.hidden ? <EyeconClosedIcon /> : <EyeconOpenIcon />}
+            />
+            <SmallIconButton
+              variant="destructive"
+              tabIndex={-1}
+              icon={<SubtractIcon />}
+              onClick={() => deleteProperty(property)}
+            />
+          </>
         }
       ></CssValueListItem>
     </FloatingPanel>
