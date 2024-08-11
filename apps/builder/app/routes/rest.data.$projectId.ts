474c43394e3520892e2998f0d683c4b0b3c0516d
Bogdan Chadkin <trysound@yandex.ru>
2024-08-11 13:52:38 +0200

    refactor: load compact data into builder
    Load only arrays instead of maps from server
    and convert data to map on client.
    
    This reduces download size and server cpu usage.
    
    340 kB -> 269 kB
    218 kB -> 170 kB
    22%

--- apps/builder/app/routes/rest.data.$projectId.ts
+++ apps/builder/app/routes/rest.data.$projectId.ts
@@ -1,6 +1,6 @@
 import type { LoaderFunctionArgs } from "@remix-run/server-runtime";
 import { db } from "@webstudio-is/project/index.server";
-import { loadBuildByProjectId } from "@webstudio-is/project-build/index.server";
+import { loadCompactBuildByProjectId } from "@webstudio-is/project-build/index.server";
 import { loadAssetsByProject } from "@webstudio-is/asset-uploader/index.server";
 import { createContext } from "~/shared/context.server";
 
@@ -16,10 +16,10 @@ export const loader = async ({ params, request }: LoaderFunctionArgs) => {
   if (project.userId === null) {
     throw new Error("Project must have project userId defined");
   }
-  const devBuild = await loadBuildByProjectId(context, project.id);
+  const build = await loadCompactBuildByProjectId(context, project.id);
   const assets = await loadAssetsByProject(project.id, context);
   return {
-    build: devBuild,
+    ...build,
     assets,
   };
 };
