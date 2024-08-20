5d06c3665977eeb361dcd35a10e09d33e438db14
istarkov <istarkov@gmail.com>
2024-08-20 06:32:36 +0000

    Remove logs

--- packages/postgrest/src/index.server.ts
+++ packages/postgrest/src/index.server.ts
@@ -6,27 +6,9 @@ export type Client = PostgrestClient<Database>;
 
 export const createClient = (url: string, apiKey: string): Client => {
   const client = new PostgrestClient<Database>(url, {
-    async fetch(input: string | URL | globalThis.Request, init?: RequestInit) {
-      const res = await fetch(input, init);
-
-      if (typeof input === "string" || input instanceof URL) {
-        const url = new URL(input);
-
-        if (url.pathname.endsWith("/Build")) {
-          console.info(
-            "======ENCODING IS=====",
-            res.headers.get("content-encoding")
-          );
-        }
-      }
-
-      return res;
-    },
-
     headers: {
       apikey: apiKey,
       Authorization: `Bearer ${apiKey}`,
-      // "Accept-Encoding": "br, gzip, deflate, identity",
     },
   });
 
