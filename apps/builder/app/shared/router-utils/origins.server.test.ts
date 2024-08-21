009e762dbfcb2783a79e5902390ebbdd8f387787
Ivan Starkov <istarkov@gmail.com>
2024-08-21 22:16:30 +0300

    chore: New Auth for the Projects (#3978)
    ## Description
    
    Part of:
    
    https://github.com/webstudio-is/webstudio-saas/issues/317#issuecomment-2289241819
    
    This configuration is necessary for the new authentication process.
    
    - NODE_TLS_REJECT_UNAUTHORIZED: Enables service-to-service
    communications by allowing self-signed certificates.
    - cors: Ensures CORS settings match those in the production environment.
    - cors logout: Allows logout requests on subdomains using fetch with
    credentials.
    
    
    
    ## Steps for reproduction
    
    1. click button
    2. expect xyz
    
    ## Code Review
    
    - [ ] hi @kof, I need you to do
      - conceptual review (architecture, feature-correctness)
      - detailed review (read every line)
      - test it on preview
    
    ## Before requesting a review
    
    - [ ] made a self-review
    - [ ] added inline comments where things may be not obvious (the "why",
    not "what")
    
    ## Before merging
    
    - [ ] tested locally and on preview environment (preview dev login:
    5de6)
    - [ ] updated [test
    cases](https://github.com/webstudio-is/webstudio/blob/main/apps/builder/docs/test-cases.md)
    document
    - [ ] added tests
    - [ ] if any new env variables are added, added them to `.env` file

---
+++ apps/builder/app/shared/router-utils/origins.server.test.ts
@@ -0,0 +1,81 @@
+import { expect, test } from "@jest/globals";
+import { parseBuilderUrl } from "./origins.server";
+
+test("parseBuilderUrl wstd.dev", async () => {
+  expect(
+    parseBuilderUrl("https://p-090e6e14-ae50-4b2e-bd22-71733cec05bb.wstd.dev")
+  ).toMatchInlineSnapshot(`
+    {
+      "projectId": "090e6e14-ae50-4b2e-bd22-71733cec05bb",
+      "sourceOrigin": "https://wstd.dev",
+    }
+  `);
+});
+
+test("parseBuilderUrl localhost", async () => {
+  expect(
+    parseBuilderUrl("https://p-090e6e14-ae50-4b2e-bd22-71733cec05bb.localhost")
+  ).toMatchInlineSnapshot(`
+    {
+      "projectId": "090e6e14-ae50-4b2e-bd22-71733cec05bb",
+      "sourceOrigin": "https://localhost",
+    }
+  `);
+});
+
+test("parseBuilderUrl localhost", async () => {
+  expect(parseBuilderUrl("https://p-eee.localhost")).toMatchInlineSnapshot(`
+    {
+      "projectId": undefined,
+      "sourceOrigin": "https://p-eee.localhost",
+    }
+  `);
+});
+
+test("parseBuilderUrl development.webstudio.is", async () => {
+  expect(
+    parseBuilderUrl(
+      "https://p-090e6e14-ae50-4b2e-bd22-71733cec05bb.development.webstudio.is"
+    )
+  ).toMatchInlineSnapshot(`
+    {
+      "projectId": "090e6e14-ae50-4b2e-bd22-71733cec05bb",
+      "sourceOrigin": "https://development.webstudio.is",
+    }
+  `);
+});
+
+test("parseBuilderUrl development.webstudio.is", async () => {
+  expect(
+    parseBuilderUrl(
+      "https://p-090e6e14-ae50-4b2e-bd22-71733cec05bb-dot-main.development.webstudio.is"
+    )
+  ).toMatchInlineSnapshot(`
+    {
+      "projectId": "090e6e14-ae50-4b2e-bd22-71733cec05bb",
+      "sourceOrigin": "https://development.webstudio.is",
+    }
+  `);
+});
+
+test("parseBuilderUrl apps.webstudio.is", async () => {
+  expect(
+    parseBuilderUrl(
+      "https://p-090e6e14-ae50-4b2e-bd22-71733cec05bb.apps.webstudio.is"
+    )
+  ).toMatchInlineSnapshot(`
+    {
+      "projectId": "090e6e14-ae50-4b2e-bd22-71733cec05bb",
+      "sourceOrigin": "https://apps.webstudio.is",
+    }
+  `);
+});
+
+test("parseBuilderUrl apps.webstudio.is", async () => {
+  expect(parseBuilderUrl("https://apps.webstudio.is")).toMatchInlineSnapshot(`
+    {
+      "projectId": undefined,
+      "sourceOrigin": "https://apps.webstudio.is",
+    }
+  `);
+});
--- apps/builder/vite.config.ts
+++ apps/builder/vite.config.ts
@@ -1,43 +1,85 @@
 import { resolve } from "node:path";
-import { defineConfig, type Plugin } from "vite";
+import { defineConfig, type CorsOptions } from "vite";
 import { vitePlugin as remix } from "@remix-run/dev";
 import { vercelPreset } from "@vercel/remix/vite";
-import basicSsl from "@vitejs/plugin-basic-ssl";
+import type { IncomingMessage } from "node:http";
+import {
+  getAuthorizationServerOrigin,
+  isBuilderUrl,
+} from "./app/shared/router-utils/origins.server";
+import { readFileSync } from "node:fs";
 
-export default defineConfig(({ mode }) => ({
-  plugins: [
-    basicSsl({
-      name: "dev",
-      domains: ["wstd.dev", "*.wstd.dev"],
-      certDir: ".dev/cert",
-    }) as Plugin<unknown>,
-    remix({
-      presets: [vercelPreset()],
-    }),
-  ],
-  resolve: {
-    conditions: ["webstudio", "import", "module", "browser", "default"],
-    alias: [
-      {
-        find: "~",
-        replacement: resolve("app"),
-      },
+export default defineConfig(({ mode }) => {
+  if (mode === "development") {
+    // Enable self-signed certificates for development service 2 service fetch calls.
+    // This is particularly important for secure communication with the oauth.ws.token endpoint.
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+  }
+
+  return {
+    plugins: [
+      remix({
+        presets: [vercelPreset()],
+      }),
     ],
-  },
-  define: {
-    "process.env.NODE_ENV": JSON.stringify(mode),
-  },
-  ssr: {
-    external: ["@webstudio-is/prisma-client"],
-  },
-  server: {
-    host: true,
-    // Needed for SSL
-    proxy: {},
-    https: {
-      cert: ".dev/cert.pem",
-      key: ".dev/key.pem",
+    resolve: {
+      conditions: ["webstudio", "import", "module", "browser", "default"],
+      alias: [
+        {
+          find: "~",
+          replacement: resolve("app"),
+        },
+      ],
     },
-  },
-  envPrefix: "GITHUB_",
-}));
+    define: {
+      "process.env.NODE_ENV": JSON.stringify(mode),
+    },
+    ssr: {
+      external: ["@webstudio-is/prisma-client"],
+    },
+    server: {
+      // Service-to-service OAuth token call requires a specified host for the wstd.dev domain
+      host: "wstd.dev",
+      // Needed for SSL
+      proxy: {},
+
+      https: {
+        key: readFileSync("../../https/privkey.pem"),
+        cert: readFileSync("../../https/fullchain.pem"),
+      },
+
+      cors: ((
+        req: IncomingMessage,
+        callback: (error: Error | null, options: CorsOptions | null) => void
+      ) => {
+        // Handle CORS preflight requests in development to mimic Remix production behavior
+        if (req.method === "OPTIONS") {
+          if (req.headers.origin != null && req.url != null) {
+            const url = new URL(req.url, `https://${req.headers.host}`);
+
+            // Allow CORS for /logout path when requested from the authorization server
+            if (url.pathname === "/logout" && isBuilderUrl(url.href)) {
+              return callback(null, {
+                origin: getAuthorizationServerOrigin(url.href),
+                preflightContinue: false,
+                credentials: true,
+              });
+            }
+          }
+
+          // Respond with method not allowed for other preflight requests
+          return callback(null, {
+            preflightContinue: false,
+            optionsSuccessStatus: 405,
+          });
+        }
+
+        // Disable CORS for all other requests
+        return callback(null, {
+          origin: false,
+        });
+      }) as never,
+    },
+    envPrefix: "GITHUB_",
+  };
+});
