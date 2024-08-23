5fd4a5fc51880eb3214f5bd87b15a49609383e98
istarkov <istarkov@gmail.com>
2024-08-23 17:21:12 +0000

    Remove most hacks we used

--- apps/builder/app/entry.server.tsx
+++ apps/builder/app/entry.server.tsx
@@ -1,26 +1,14 @@
-import { renderToString } from "react-dom/server";
+/**
+ * By default, Remix will handle generating the HTTP Response for you.
+ * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
+ * For more information, see https://remix.run/file-conventions/entry.server
+ */
+
 import type { EntryContext } from "@remix-run/node";
+import { flushCss } from "@webstudio-is/design-system";
 import { RemixServer } from "@remix-run/react";
-import { handleRequest as handleRequestBuilder } from "./shared/remix/handle-request.server";
-import { isCanvas } from "./shared/router-utils";
 
-const handleRequestCanvas = (
-  request: Request,
-  responseStatusCode: number,
-  responseHeaders: Headers,
-  remixContext: EntryContext
-) => {
-  const markup = renderToString(
-    <RemixServer context={remixContext} url={request.url} />
-  );
-
-  responseHeaders.set("Content-Type", "text/html");
-
-  return new Response(`<!DOCTYPE html>${markup}`, {
-    status: responseStatusCode,
-    headers: responseHeaders,
-  });
-};
+import ReactDOMServer from "react-dom/server";
 
 export default function handleRequest(
   request: Request,
@@ -28,18 +16,83 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-  let handle = handleRequestBuilder;
+  const markup = ReactDOMServer.renderToString(
+    <RemixServer context={remixContext} url={request.url} />
+  ).replace(/<\/head>/, `<style>${flushCss()}</style></head>`);
 
-  // @todo canvas can be deployed as separate app
-  // and separate request handler will not be necessary anymore
-  if (
-    remixContext.staticHandlerContext.matches.some(
-      (match) => match.route.id === "routes/_index"
-    ) &&
-    isCanvas(request)
-  ) {
-    handle = handleRequestCanvas;
-  }
+  responseHeaders.set("Content-Type", "text/html");
 
-  return handle(request, responseStatusCode, responseHeaders, remixContext);
+  return new Response("<!DOCTYPE html>" + markup, {
+    status: responseStatusCode,
+    headers: responseHeaders,
+  });
 }
+/*
+export default function handleRequest(
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  remixContext: EntryContext,
+  // This is ignored so we can keep it in the template for visibility.  Feel
+  // free to delete this parameter in your app if you're not using it!
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  _loadContext: AppLoadContext
+) {
+  return handleBrowserRequest(
+    request,
+    responseStatusCode,
+    responseHeaders,
+    remixContext
+  );
+}
+
+function handleBrowserRequest(
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  remixContext: EntryContext
+) {
+  return new Promise((resolve, reject) => {
+    let shellRendered = false;
+    const { pipe, abort } = renderToPipeableStream(
+      <RemixServer
+        context={remixContext}
+        url={request.url}
+        abortDelay={ABORT_DELAY}
+      />,
+      {
+        onShellReady() {
+          shellRendered = true;
+          const body = new PassThrough();
+          const stream = createReadableStreamFromReadable(body);
+
+          responseHeaders.set("Content-Type", "text/html");
+
+          resolve(
+            new Response(stream, {
+              headers: responseHeaders,
+              status: responseStatusCode,
+            })
+          );
+
+          pipe(body);
+        },
+        onShellError(error: unknown) {
+          reject(error);
+        },
+        onError(error: unknown) {
+          responseStatusCode = 500;
+          // Log streaming rendering errors from inside the shell.  Don't log
+          // errors encountered during initial shell rendering since they'll
+          // reject and get logged in handleDocumentRequest.
+          if (shellRendered) {
+            console.error(error);
+          }
+        },
+      }
+    );
+
+    setTimeout(abort, ABORT_DELAY);
+  });
+}
+*/
