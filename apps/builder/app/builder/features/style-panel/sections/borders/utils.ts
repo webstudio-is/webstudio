b75626cc5ca0c81d3678e0587b8915b78683ba53
Bogdan Chadkin <opensource@trysound.io>
2024-09-07 00:23:52 +0200

    experimental: migrate borders section to style object model (#4076)
    Ref https://github.com/webstudio-is/webstudio/issues/1536
    https://github.com/webstudio-is/webstudio/issues/3399
    
    Fixes
    https://discord.com/channels/955905230107738152/1281501486580502568/1281501486580502568
    
    Refactored borders section with new style engine. And fixed the issue
    with default border radius in shorthand input.
    
    <img width="257" alt="Screenshot 2024-09-06 at 17 29 12"
    src="https://github.com/user-attachments/assets/bf0df9ff-d8ed-413c-a260-3aa1d6ce1678">

--- apps/builder/app/builder/features/style-panel/sections/borders/utils.ts
+++ apps/builder/app/builder/features/style-panel/sections/borders/utils.ts
@@ -1,36 +1,4 @@
 import { theme, type CSS } from "@webstudio-is/design-system";
-import type { StyleProperty } from "@webstudio-is/css-engine";
-import type {
-  CreateBatchUpdate,
-  DeleteProperty,
-  SetProperty,
-} from "../../shared/use-style-data";
-
-export const deleteAllProperties: (
-  styleProperties: readonly StyleProperty[],
-  createBatchUpdate: CreateBatchUpdate
-) => DeleteProperty =
-  (styleProperties, createBatchUpdate) => (_propertyName, options) => {
-    const batch = createBatchUpdate();
-    for (const property of styleProperties) {
-      batch.deleteProperty(property);
-    }
-    batch.publish(options);
-  };
-
-export const setAllProperties: (
-  styleProperties: readonly StyleProperty[],
-  createBatchUpdate: CreateBatchUpdate
-) => SetProperty =
-  (styleProperties, createBatchUpdate) =>
-  (_propertyName) =>
-  (value, options) => {
-    const batch = createBatchUpdate();
-    for (const property of styleProperties) {
-      batch.setProperty(property)(value);
-    }
-    batch.publish(options);
-  };
 
 export const rowCss: CSS = {
   // Our aim is to maintain consistent styling throughout the property and align
