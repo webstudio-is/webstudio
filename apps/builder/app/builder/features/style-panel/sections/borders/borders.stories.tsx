51781b5f8eb9a0c6547ead7f8d43555feec3e45c
Bogdan Chadkin <trysound@yandex.ru>
2024-09-06 17:42:21 +0200

    Fix stories

--- apps/builder/app/builder/features/style-panel/sections/borders/borders.stories.tsx
+++ apps/builder/app/builder/features/style-panel/sections/borders/borders.stories.tsx
@@ -1,19 +1,5 @@
 import { styled, theme } from "@webstudio-is/design-system";
-import { useRef, useState } from "react";
-import type { StyleInfo } from "../../shared/style-info";
-import type {
-  CreateBatchUpdate,
-  DeleteProperty,
-  SetProperty,
-} from "../../shared/use-style-data";
 import { Section } from "./borders";
-
-const styleInfoInitial: StyleInfo = {
-  borderTopColor: {
-    local: { type: "rgb", r: 0, g: 0, b: 0, alpha: 1 },
-    value: { type: "rgb", r: 0, g: 0, b: 0, alpha: 1 },
-  },
-};
 
 const Panel = styled("div", {
   width: theme.spacing[30],
@@ -21,69 +7,9 @@ const Panel = styled("div", {
 });
 
 export const Borders = () => {
-  const [styleInfo, setStyleInfo] = useState(() => styleInfoInitial);
-
-  const setProperty: SetProperty = (name) => (value, options) => {
-    if (options?.isEphemeral) {
-      return;
-    }
-
-    setStyleInfo((styleInfo) => ({
-      ...styleInfo,
-      [name]: {
-        ...styleInfo[name],
-        value: value,
-        local: value,
-      },
-    }));
-  };
-
-  const deleteProperty: DeleteProperty = (name, options) => {
-    if (options?.isEphemeral) {
-      return;
-    }
-
-    setStyleInfo((styleInfo) => {
-      const { [name]: _, ...rest } = styleInfo;
-      return rest;
-    });
-  };
-
-  const execCommands = useRef<(() => void)[]>([]);
-
-  const createBatchUpdate: CreateBatchUpdate = () => ({
-    deleteProperty: (property) => {
-      execCommands.current.push(() => {
-        deleteProperty(property);
-      });
-    },
-    setProperty: (property) => (style) => {
-      execCommands.current.push(() => {
-        setProperty(property)(style);
-      });
-    },
-    publish: (options) => {
-      if (options?.isEphemeral) {
-        execCommands.current = [];
-        return;
-      }
-
-      for (const command of execCommands.current) {
-        command();
-      }
-
-      execCommands.current = [];
-    },
-  });
-
   return (
     <Panel>
-      <Section
-        currentStyle={styleInfo}
-        setProperty={setProperty}
-        deleteProperty={deleteProperty}
-        createBatchUpdate={createBatchUpdate}
-      />
+      <Section />
     </Panel>
   );
 };
