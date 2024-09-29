3adc6775daa3018251715dbaedd2c5a91610c672
Bogdan Chadkin <trysound@yandex.ru>
2024-09-29 17:19:47 +0200

    refactor: remove deprecated tree
    We refactored both pages and navigator with new tree.

--- packages/design-system/src/components/__DEPRECATED__/tree/index.ts
+++
@@ -1,9 +0,0 @@
-export type { ItemSelector, ItemDropTarget } from "./item-utils";
-export * from "./tree";
-export {
-  TreeItemBody,
-  TreeItemLabel,
-  getNodeVars,
-  getPlacementIndicatorAlignment,
-  type TreeItemRenderProps,
-} from "./tree-node";
