6f57d498a4639262084cacd00e7c0e48ce3563d6
Bogdan Chadkin <trysound@yandex.ru>
2024-10-26 16:53:08 +0300

    build: drop prisma build

--- packages/prisma-client/migrations-cli/args.ts
+++ packages/prisma-client/migrations-cli/args.ts
@@ -4,6 +4,10 @@ export const { values, positionals } = parseArgs({
   args: process.argv.slice(2),
   allowPositionals: true,
   options: {
+    cwd: {
+      type: "string",
+      default: "./",
+    },
     dev: {
       type: "boolean",
     },
