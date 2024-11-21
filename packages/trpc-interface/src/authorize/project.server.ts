a560d1f3eaf95f2d74f77cc45f8e53c64497ad3b
johnsicili <93934362+johnsicili@users.noreply.github.com>
2024-11-21 05:55:02 -0700

    chore: Add pet grooming template to trpc (#4452)
    Pet Grooming (Local Services)
    909a139b-1f2d-415a-ac90-382fa19fa7d8
    
    ## Description
    
    1. What is this PR about (link the issue and add a short description)
    
    ## Steps for reproduction
    
    1. click button
    2. expect xyz
    
    ## Code Review
    
    - [ ] hi @kof, I need you to do
      - conceptual review (architecture, feature-correctness)
      - detailed review (read every line)
      - test it on preview
    
    ## Before requesting a review
    
    - [ ] made a self-review
    - [ ] added inline comments where things may be not obvious (the "why",
    not "what")
    
    ## Before merging
    
    - [ ] tested locally and on preview environment (preview dev login:
    0000)
    - [ ] updated [test
    cases](https://github.com/webstudio-is/webstudio/blob/main/apps/builder/docs/test-cases.md)
    document
    - [ ] added tests
    - [ ] if any new env variables are added, added them to `.env` file

--- packages/trpc-interface/src/authorize/project.server.ts
+++ packages/trpc-interface/src/authorize/project.server.ts
@@ -116,6 +116,7 @@ export const checkProjectPermit = async (
     "2e802ad7-ef32-48e6-8706-3a162785ef95",
     "01f6f1d8-06f5-4a6c-a3b1-89a0448046c7",
     "5b33acf4-53cf-4f03-8973-d5679772edee",
+    "909a139b-1f2d-415a-ac90-382fa19fa7d8",
     // Staging IDs
     "c236999d-be6b-43fb-9edc-78a2ba59e56d",
     "a1371dce-752c-4ccf-8ea4-88bab577fe50",
