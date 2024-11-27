922a85a2e2ec1e0950264ffee5110d6bdbe37018
istarkov <istarkov@gmail.com>
2024-11-27 09:55:23 +0000

    Fix

--- apps/builder/app/canvas/features/build-mode/editable-block-template.tsx
+++ apps/builder/app/canvas/features/build-mode/editable-block-template.tsx
@@ -5,46 +5,34 @@ import {
   type WebstudioComponentSystemProps,
 } from "@webstudio-is/react-sdk";
 import * as React from "react";
-
 import { $isDesignMode, $selectedInstanceSelector } from "~/shared/nano-states";
-import type { InstanceSelector } from "~/shared/tree-utils";
 
 export const EditableBlockTemplate = React.forwardRef<
   HTMLDivElement,
   WebstudioComponentSystemProps & { children: React.ReactNode }
->(({ children, ...props }, ref) => {
+>(({ ...props }, ref) => {
   const isDesignMode = useStore($isDesignMode);
   const selectedInstanceSelector = useStore($selectedInstanceSelector);
   const templateInstanceStringSelector = props[selectorIdAttribute];
 
   if (!isDesignMode) {
-    return <></>;
+    return;
   }
 
   if (selectedInstanceSelector === undefined) {
-    return <></>;
+    return;
+  }
+
+  if (selectedInstanceSelector.length === 0) {
+    return;
   }
 
   const selectedSelector = selectedInstanceSelector.join(",");
 
-  // Exclude all selected ancestors and self
-  if (templateInstanceStringSelector.endsWith(selectedSelector)) {
-    return <div style={{ display: "contents" }} ref={ref} {...props} />;
+  // Exclude all selected ancestors
+  if (!selectedSelector.endsWith(templateInstanceStringSelector)) {
+    return;
   }
 
-  const visibleChildren = React.Children.toArray(children)
-    .filter((child) => React.isValidElement(child))
-    .filter((child) => {
-      const { instanceSelector } = child.props as {
-        instanceSelector: InstanceSelector;
-      };
-
-      return selectedSelector.endsWith(instanceSelector.join(","));
-    });
-
-  return (
-    <div style={{ display: "contents" }} ref={ref} {...props}>
-      {visibleChildren}
-    </div>
-  );
+  return <div style={{ display: "contents" }} ref={ref} {...props} />;
 }) as AnyComponent;
--- apps/builder/app/canvas/features/build-mode/editable-block.tsx
+++ apps/builder/app/canvas/features/build-mode/editable-block.tsx
@@ -8,71 +8,58 @@ import {
 } from "@webstudio-is/react-sdk";
 
 import * as React from "react";
-import { $instances, $selectedInstanceSelector } from "~/shared/nano-states";
+import {
+  $instances,
+  $isDesignMode,
+  $selectedInstanceSelector,
+} from "~/shared/nano-states";
 
 export const EditableBlock = React.forwardRef<
   HTMLDivElement,
   { children: React.ReactNode } & WebstudioComponentSystemProps
 >(({ children, ...props }, ref) => {
   const instances = useStore($instances);
+  const isDesignMode = useStore($isDesignMode);
   const instanceId = props[idAttribute];
   const instance = instances.get(instanceId);
   const selectedInstanceSelector = useStore($selectedInstanceSelector);
-
-  if (instance === undefined) {
-    return <div>Editable Block instance is undefined</div>;
-  }
-
-  const templateInstanceId = instance.children.find(
-    (child) =>
-      child.type === "id" &&
-      instances.get(child.value)?.component === editableBlockTemplateComponent
-  )?.value;
-
-  if (templateInstanceId === undefined) {
-    return <div>Editable Block template instance not found</div>;
-  }
-
-  const templateInstance = instances.get(templateInstanceId);
-  if (templateInstance === undefined) {
-    return <div>Editable Block template instance is undefined</div>;
-  }
-
-  const templateChildrenIds = templateInstance.children
-    .filter((child) => child.type === "id")
-    .map((child) => child.value);
-
-  if (templateChildrenIds === undefined) {
-    return <div>Editable block template children not found</div>;
-  }
 
   const childArray = React.Children.toArray(children).filter((child) =>
     React.isValidElement(child)
   );
 
-  if (selectedInstanceSelector !== undefined) {
-    const selectedSelector = selectedInstanceSelector.join(",");
-    // If any template child is selected then render only template
-    const stringSelector = props[selectorIdAttribute];
+  if (isDesignMode) {
+    if (instance === undefined) {
+      return <div>Editable Block instance is undefined</div>;
+    }
 
-    const isTemplateChildSelected = templateChildrenIds.some((childId) => {
-      const childSelector = `${childId},${templateInstanceId},${stringSelector}`;
+    const templateInstanceId = instance.children.find(
+      (child) =>
+        child.type === "id" &&
+        instances.get(child.value)?.component === editableBlockTemplateComponent
+    )?.value;
 
-      if (selectedSelector.endsWith(childSelector)) {
-        return true;
+    if (templateInstanceId === undefined) {
+      return <div>Editable Block template instance not found</div>;
+    }
+
+    if (selectedInstanceSelector !== undefined) {
+      const selectedSelector = selectedInstanceSelector.join(",");
+      // If any template child is selected then render only template
+      const stringSelector = props[selectorIdAttribute];
+      const templateSelector = `${templateInstanceId},${stringSelector}`;
+
+      if (selectedSelector.endsWith(templateSelector)) {
+        return (
+          <div style={{ display: "contents" }} ref={ref} {...props}>
+            {childArray.filter((child) => {
+              const { instanceSelector } = child.props;
+
+              return instanceSelector[0] === templateInstanceId;
+            })}
+          </div>
+        );
       }
-    });
-
-    if (isTemplateChildSelected) {
-      return (
-        <div style={{ display: "contents" }} ref={ref} {...props}>
-          {childArray.filter((child) => {
-            const { instanceSelector } = child.props;
-
-            return instanceSelector[0] === templateInstanceId;
-          })}
-        </div>
-      );
     }
   }
 
