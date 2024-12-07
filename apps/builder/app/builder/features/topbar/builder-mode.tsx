2bd06308699e0904c3b378e44095c797b703c679
istarkov <istarkov@gmail.com>
2024-12-07 11:38:07 +0000

    Add new button

--- apps/builder/app/builder/features/topbar/builder-mode.tsx
+++ apps/builder/app/builder/features/topbar/builder-mode.tsx
@@ -1,5 +1,6 @@
 import { useStore } from "@nanostores/react";
 import {
+  ChevronDownIcon,
   NotebookAndPenIcon,
   PaintBrushIcon,
   PlayIcon,
@@ -12,9 +13,10 @@ import {
   DropdownMenuSeparator,
   Flex,
   Kbd,
+  MenuCheckedIcon,
   menuItemCss,
-  styled,
   theme,
+  ToolbarToggleGroup,
   ToolbarToggleItem,
 } from "@webstudio-is/design-system";
 import {
@@ -29,16 +31,10 @@ import {
   $isDesignModeAllowed,
   isBuilderMode,
   setBuilderMode,
-  type BuilderMode,
+  toggleBuilderMode,
 } from "~/shared/nano-states";
 import { useState } from "react";
 import { isFeatureEnabled } from "@webstudio-is/feature-flags";
-
-const StyledMenuItem = styled(DropdownMenuRadioItem, {
-  "&:where([data-state='checked'])": {
-    backgroundColor: `oklch(from ${theme.colors.backgroundItemMenuItemHover} l c h / 0.3)`,
-  },
-});
 
 export const BuilderModeDropDown = () => {
   const builderMode = useStore($builderMode);
@@ -60,18 +56,23 @@ export const BuilderModeDropDown = () => {
       shortcut: ["cmd", "shift", "c"],
       enabled: isContentModeAllowed && isFeatureEnabled("contentEditableMode"),
     },
-    preview: {
+  } as const;
+
+  /*
+      preview: {
       icon: <PlayIcon />,
       description: "View the page as it will appear to users",
       title: "Preview",
       shortcut: ["cmd", "shift", "p"],
       enabled: true,
     },
-  } as const;
+  */
 
-  const [activeMode, setActiveMode] = useState<BuilderMode | undefined>();
+  const [activeMode, setActiveMode] = useState<
+    keyof typeof menuItems | undefined
+  >();
 
-  const handleFocus = (mode: BuilderMode) => () => {
+  const handleFocus = (mode: keyof typeof menuItems) => () => {
     setActiveMode(mode);
   };
 
@@ -80,62 +81,102 @@ export const BuilderModeDropDown = () => {
   };
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <ToolbarToggleItem
-          value="preview"
-          aria-label="Toggle Preview"
-          variant="preview"
-          tabIndex={0}
-        >
-          {menuItems[builderMode].icon}
-        </ToolbarToggleItem>
-      </DropdownMenuTrigger>
+    <Flex
+      css={{
+        gap: theme.spacing[2],
 
-      <DropdownMenuPortal>
-        <DropdownMenuContent
-          sideOffset={4}
-          collisionPadding={16}
-          side="bottom"
-          loop
+        [`&:hover ${ToolbarToggleItem}`]: {
+          background: theme.colors.backgroundTopbarHover,
+        },
+      }}
+    >
+      <ToolbarToggleGroup
+        type="single"
+        value={builderMode}
+        onValueChange={() => {
+          toggleBuilderMode("preview");
+        }}
+      >
+        <ToolbarToggleItem
+          variant="preview"
+          value="preview"
+          tabIndex={0}
+          css={{
+            paddingRight: 0,
+          }}
         >
-          <DropdownMenuRadioGroup
-            value={builderMode}
-            onValueChange={(value) => {
-              if (isBuilderMode(value)) {
-                setBuilderMode(value);
-              }
+          <PlayIcon />
+        </ToolbarToggleItem>
+      </ToolbarToggleGroup>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <ToolbarToggleItem
+            value="preview"
+            aria-label="Toggle Preview"
+            variant="preview"
+            tabIndex={0}
+            css={{
+              minWidth: "unset",
+              padding: 0,
+              "&:focus-visible": "unset",
+              "&:focus-visible::after": {
+                content: "unset",
+                outlineWidth: 0,
+              },
             }}
           >
-            {Object.entries(menuItems)
-              .filter(([_, { enabled }]) => enabled)
-              .map(([mode, { icon, title, shortcut }]) => (
-                <StyledMenuItem
-                  key={mode}
-                  value={mode}
-                  onFocus={handleFocus(mode as BuilderMode)}
-                  onBlur={handleBlur}
-                >
-                  <Flex css={{ px: theme.spacing[3] }} gap={2}>
-                    {icon}
-                    <Box>{title}</Box>
-                  </Flex>
-                  <DropdownMenuItemRightSlot>
-                    <Kbd value={shortcut} />
-                  </DropdownMenuItemRightSlot>
-                  &nbsp;
-                </StyledMenuItem>
-              ))}
-          </DropdownMenuRadioGroup>
-          <DropdownMenuSeparator />
+            <ChevronDownIcon />
+          </ToolbarToggleItem>
+        </DropdownMenuTrigger>
 
-          <div className={menuItemCss({ hint: true })}>
-            <Box css={{ width: theme.spacing[25] }}>
-              {menuItems[activeMode ?? builderMode].description}
-            </Box>
-          </div>
-        </DropdownMenuContent>
-      </DropdownMenuPortal>
-    </DropdownMenu>
+        <DropdownMenuPortal>
+          <DropdownMenuContent
+            sideOffset={4}
+            collisionPadding={16}
+            side="bottom"
+            loop
+          >
+            <DropdownMenuRadioGroup
+              value={builderMode}
+              onValueChange={(value) => {
+                if (isBuilderMode(value)) {
+                  setBuilderMode(value);
+                }
+              }}
+            >
+              {Object.entries(menuItems)
+                .filter(([_, { enabled }]) => enabled)
+                .map(([mode, { icon, title, shortcut }]) => (
+                  <DropdownMenuRadioItem
+                    key={mode}
+                    value={mode}
+                    onFocus={handleFocus(mode as keyof typeof menuItems)}
+                    onBlur={handleBlur}
+                    icon={<MenuCheckedIcon />}
+                  >
+                    <Flex css={{ px: theme.spacing[3] }} gap={2}>
+                      {icon}
+                      <Box>{title}</Box>
+                    </Flex>
+                    <DropdownMenuItemRightSlot>
+                      <Kbd value={shortcut} />
+                    </DropdownMenuItemRightSlot>
+                    &nbsp;
+                  </DropdownMenuRadioItem>
+                ))}
+            </DropdownMenuRadioGroup>
+            <DropdownMenuSeparator />
+
+            <div className={menuItemCss({ hint: true })}>
+              <Box css={{ width: theme.spacing[25] }}>
+                {activeMode
+                  ? menuItems[activeMode].description
+                  : "Select Design or Content mode"}
+              </Box>
+            </div>
+          </DropdownMenuContent>
+        </DropdownMenuPortal>
+      </DropdownMenu>
+    </Flex>
   );
 };
