18082cb69f804f7704c43fecf778e21b2b5fbd8d
Bogdan Chadkin <opensource@trysound.io>
2024-12-11 18:41:32 +0700

    fix: relax list item constraint (#4554)
    Latest constraints improvements let us support direct child constraints.
    Which can be useful for very coupled components. Though in case of list
    and list item should be avoided because list item can be used inside of
    slot and collection.
    
    Perhaps eventually can be fixed with tag matchers.

--- packages/sdk-components-react/src/list-item.ws.ts
+++ packages/sdk-components-react/src/list-item.ws.ts
@@ -17,7 +17,10 @@ export const meta: WsComponentMeta = {
   category: "general",
   type: "container",
   constraints: {
-    relation: "parent",
+    // cannot use parent relation here
+    // because list item can be put inside of collection or slot
+    // perhaps can be eventually fixed with tag matchers
+    relation: "ancestor",
     component: { $eq: "List" },
   },
   description: "Adds a new item to an existing list.",
