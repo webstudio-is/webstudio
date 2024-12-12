d12657af68814d1ede835496faefe316915fd79d
istarkov <istarkov@gmail.com>
2024-12-12 08:29:35 +0000

    Add storybook examples

--- apps/builder/app/builder/features/workspace/canvas-tools/outline/outline.stories.tsx
+++ apps/builder/app/builder/features/workspace/canvas-tools/outline/outline.stories.tsx
@@ -13,7 +13,10 @@ export const Basic = () => (
       <Box css={{ width: "min-content", textAlign: "center" }}>
         Selected outline
       </Box>
-      <Outline rect={new DOMRect(0, 0, 150, 150)} />
+      <Outline
+        rect={new DOMRect(0, 0, 150, 150)}
+        clampingRect={new DOMRect(0, 0, 150, 150)}
+      />
     </Grid>
 
     <Flex
@@ -24,7 +27,11 @@ export const Basic = () => (
       <Box css={{ width: "min-content", textAlign: "center" }}>
         Collaboration outline
       </Box>
-      <Outline rect={new DOMRect(0, 0, 150, 150)} variant="collaboration" />
+      <Outline
+        rect={new DOMRect(0, 0, 150, 150)}
+        clampingRect={new DOMRect(0, 0, 150, 150)}
+        variant="collaboration"
+      />
     </Flex>
 
     <Flex
@@ -35,8 +42,15 @@ export const Basic = () => (
       <Box css={{ width: "min-content", textAlign: "center" }}>
         Collaboration outline over Selected
       </Box>
-      <Outline rect={new DOMRect(0, 0, 150, 150)} />
-      <Outline rect={new DOMRect(0, 0, 150, 150)} variant="collaboration" />
+      <Outline
+        rect={new DOMRect(0, 0, 150, 150)}
+        clampingRect={new DOMRect(0, 0, 150, 150)}
+      />
+      <Outline
+        rect={new DOMRect(0, 0, 150, 150)}
+        clampingRect={new DOMRect(0, 0, 150, 150)}
+        variant="collaboration"
+      />
     </Flex>
 
     <Flex
@@ -47,8 +61,57 @@ export const Basic = () => (
       <Box css={{ width: "min-content", textAlign: "center" }}>
         Selected outline over Collaboration
       </Box>
-      <Outline rect={new DOMRect(0, 0, 150, 150)} variant="collaboration" />
-      <Outline rect={new DOMRect(0, 0, 150, 150)} />
+      <Outline
+        rect={new DOMRect(0, 0, 150, 150)}
+        clampingRect={new DOMRect(0, 0, 150, 150)}
+        variant="collaboration"
+      />
+      <Outline
+        rect={new DOMRect(0, 0, 150, 150)}
+        clampingRect={new DOMRect(0, 0, 150, 150)}
+      />
+    </Flex>
+
+    <Flex
+      align="center"
+      justify="center"
+      css={{ position: "relative", height: 150, width: 150 }}
+    >
+      <Box css={{ width: "min-content", textAlign: "center" }}>
+        Clamped left
+      </Box>
+      <Outline
+        rect={new DOMRect(-10, 0, 150, 150)}
+        clampingRect={new DOMRect(0, 0, 150, 150)}
+      />
+    </Flex>
+
+    <Flex
+      align="center"
+      justify="center"
+      css={{ position: "relative", height: 150, width: 150 }}
+    >
+      <Box css={{ width: "min-content", textAlign: "center" }}>
+        Clamped right
+      </Box>
+      <Outline
+        rect={new DOMRect(0, 0, 160, 150)}
+        clampingRect={new DOMRect(0, 0, 150, 150)}
+      />
+    </Flex>
+
+    <Flex
+      align="center"
+      justify="center"
+      css={{ position: "relative", height: 150, width: 150 }}
+    >
+      <Box css={{ width: "min-content", textAlign: "center" }}>
+        Clamped left-right
+      </Box>
+      <Outline
+        rect={new DOMRect(-10, 0, 170, 150)}
+        clampingRect={new DOMRect(0, 0, 150, 150)}
+      />
     </Flex>
   </Grid>
 );
