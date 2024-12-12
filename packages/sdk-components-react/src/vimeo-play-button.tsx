5710ef39a08e679273bf94a5a64ac6a7207e735e
istarkov <istarkov@gmail.com>
2024-12-12 13:23:22 +0000

    Undo

--- packages/sdk-components-react/src/vimeo-play-button.tsx
+++ packages/sdk-components-react/src/vimeo-play-button.tsx
@@ -19,7 +19,6 @@ export const VimeoPlayButton = forwardRef<ElementRef<typeof defaultTag>, Props>(
 
     const handleClick = useCallback(async () => {
       await interactionResponse();
-      await new Promise((resolve) => setTimeout(resolve, 1000));
       vimeoContext.onInitPlayer();
     }, [vimeoContext]);
 
