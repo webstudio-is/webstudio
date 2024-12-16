aa70b242cdb5df20dbc9bddf44d3146e988c83a1
Bogdan Chadkin <opensource@trysound.io>
2024-12-16 23:29:21 +0700

    Fix overrides detection in CLI

---
+++ packages/sdk-components-react-remix/src/metas.ts
@@ -0,0 +1,9 @@
+// this lets CLI detect which components are overriden
+// without importing components runtime
+export {
+  Body,
+  Link,
+  RichTextLink,
+  Form,
+  RemixForm,
+} from "@webstudio-is/sdk-components-react/metas";
