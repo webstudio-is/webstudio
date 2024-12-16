c779a8ec9e3ab6d69dfa3ef2eb39c6c0a628d1bd
Oleg Isonen <oleg008@gmail.com>
2024-12-16 14:29:17 +0000

    experimental: Commands UI design tweaks (#4594)
    ## Description
    
    - Bigger border radius for the dialog and floating panels as well for
    consistency
    - Bigger search container height, same as titles in dialogs
    
    ## Steps for reproduction
    
    1. click button
    2. expect xyz
    
    ## Code Review
    
    - [ ] hi @kof, I need you to do
      - conceptual review (architecture, feature-correctness)
      - detailed review (read every line)
      - test it on preview
    
    ## Before requesting a review
    
    - [ ] made a self-review
    - [ ] added inline comments where things may be not obvious (the "why",
    not "what")
    
    ## Before merging
    
    - [ ] tested locally and on preview environment (preview dev login:
    0000)
    - [ ] updated [test
    cases](https://github.com/webstudio-is/webstudio/blob/main/apps/builder/docs/test-cases.md)
    document
    - [ ] added tests
    - [ ] if any new env variables are added, added them to `.env` file

--- packages/design-system/src/components/command.tsx
+++ packages/design-system/src/components/command.tsx
@@ -37,10 +37,10 @@ const inputBorderBottomSize = "--command-input-border-bottom-width";
 const StyledCommand = styled(CommandPrimitive, {
   boxSizing: "border-box",
   width: panelWidth,
-  borderRadius: theme.borderRadius[4],
   boxShadow: theme.shadows.menuDropShadow,
   backgroundColor: theme.colors.backgroundControls,
   border: `1px solid ${theme.colors.borderMain}`,
+  borderRadius: theme.borderRadius[7],
   // clip selected item background
   overflow: "clip",
   // remove input border bottom when no command matches
@@ -101,7 +101,7 @@ export const Command = (props: CommandProps) => {
 
 const CommandDialogContent = styled(DialogContent, {
   position: "absolute",
-  top: 140,
+  top: "20%",
   left: `calc(50% - ${panelWidth} / 2)`,
   width: panelWidth,
 });
@@ -129,7 +129,7 @@ export const CommandDialog = ({
 const CommandInputContainer = styled("div", {
   display: "grid",
   gridTemplateColumns: `${itemHeight} 1fr max-content`,
-  height: itemHeight,
+  height: theme.spacing[15],
   borderBottom: `var(${inputBorderBottomSize}) solid ${theme.colors.borderMain}`,
 });
 
@@ -181,7 +181,7 @@ export const CommandInput = (
       <Text
         variant="labelsTitleCase"
         color="moreSubtle"
-        css={{ alignSelf: "center", paddingInline: 8 }}
+        css={{ alignSelf: "center", paddingInline: theme.spacing[5] }}
       >
         {action} <Kbd value={["enter"]} color="moreSubtle" />
       </Text>
@@ -254,7 +254,7 @@ export const CommandFooter = () => {
     <Flex
       justify="end"
       align="center"
-      css={{ height: itemHeight, paddingInline: 4 }}
+      css={{ height: itemHeight, paddingInline: theme.spacing[3] }}
       ref={actionsRef}
     >
       <Popover open={isActionOpen} onOpenChange={setIsActionOpen}>
@@ -337,9 +337,9 @@ export const CommandGroupHeading = styled("div", {
   ...textVariants.titles,
   color: theme.colors.foregroundMoreSubtle,
   display: "flex",
-  gap: 8,
+  gap: theme.spacing[5],
   alignItems: "center",
-  paddingInline: 8,
+  paddingInline: theme.spacing[5],
   height: itemHeight,
 });
 
@@ -348,15 +348,15 @@ export const CommandItem = styled(CommandPrimitive.Item, {
   gridTemplateColumns: `1fr max-content`,
   alignItems: "center",
   minHeight: itemHeight,
-  paddingInline: 16,
+  paddingInline: theme.spacing[9],
   "&[aria-selected=true]": {
     backgroundColor: theme.colors.backgroundHover,
   },
 });
 
 export const CommandIcon = styled("div", {
-  width: 16,
-  height: 16,
+  width: theme.spacing[9],
+  height: theme.spacing[9],
   placeSelf: "center",
   color: theme.colors.foregroundSubtle,
 });
