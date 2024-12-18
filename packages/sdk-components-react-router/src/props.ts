0b18855de26ab4ac8f0c0b70b2050d6ce9ff9332
Bogdan Chadkin <opensource@trysound.io>
2024-12-18 17:10:55 +0700

    Remove props and meta

--- packages/sdk-components-react-router/src/props.ts
+++
@@ -1,5 +0,0 @@
-export { propsMeta as Body } from "./body.ws";
-export { propsMeta as Link } from "./link.ws";
-export { propsMeta as RichTextLink } from "./rich-text-link.ws";
-export { propsMeta as Form } from "./webhook-form.ws";
-export { propsMeta as RemixForm } from "./remix-form.ws";
