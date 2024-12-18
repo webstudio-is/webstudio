4449b5230c2ddda5c0596cb8ed7b97c8c202cc54
Bogdan Chadkin <opensource@trysound.io>
2024-12-18 17:10:55 +0700

    Remove props and meta

--- packages/sdk-components-react-router/src/body.ws.ts
+++
@@ -1,2 +0,0 @@
-export { Body as meta } from "@webstudio-is/sdk-components-react/metas";
-export { Body as propsMeta } from "@webstudio-is/sdk-components-react/props";
--- packages/sdk-components-react-router/src/rich-text-link.ws.ts
+++
@@ -1,6 +0,0 @@
-import type { WsComponentPropsMeta } from "@webstudio-is/react-sdk";
-import { propsMeta as linkPropsMeta } from "./link.ws";
-
-export { RichTextLink as meta } from "@webstudio-is/sdk-components-react/metas";
-
-export const propsMeta: WsComponentPropsMeta = linkPropsMeta;
