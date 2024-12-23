4fe0d44518e1ba2fb2698a2c063a4d413c974424
Oleg Isonen <oleg008@gmail.com>
2024-12-23 20:18:37 +0000

    3rd batch

--- packages/icons/src/index.ts
+++ packages/icons/src/index.ts
@@ -9,6 +9,5 @@ export * from "./cross-circled-filled";
 export * from "./cross";
 export * from "./cross-small";
 export * from "./text-hyphen";
-export * from "./text-truncate";
 export * from "./text-direction-ltr";
 export * from "./text-direction-rtl";
