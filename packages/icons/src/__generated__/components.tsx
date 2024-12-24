fd158236fd7cf486bdb520a3a30be584bfca1d37
Oleg Isonen <oleg008@gmail.com>
2024-12-24 15:53:52 +0000

    fix box stroke

--- packages/icons/src/__generated__/components.tsx
+++ packages/icons/src/__generated__/components.tsx
@@ -1447,7 +1447,12 @@ export const BoxIcon: IconComponent = forwardRef(
         {...props}
         ref={forwardedRef}
       >
-        <path d="M12.667 2H3.333C2.597 2 2 2.597 2 3.333v9.334C2 13.403 2.597 14 3.333 14h9.334c.736 0 1.333-.597 1.333-1.333V3.333C14 2.597 13.403 2 12.667 2Z" />
+        <path
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M12.667 2H3.333C2.597 2 2 2.597 2 3.333v9.334C2 13.403 2.597 14 3.333 14h9.334c.736 0 1.333-.597 1.333-1.333V3.333C14 2.597 13.403 2 12.667 2Z"
+        />
       </svg>
     );
   }
