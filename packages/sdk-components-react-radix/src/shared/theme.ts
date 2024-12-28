8774fe4244de0d7bfc3c6373855d171b84ef56e4
Bogdan Chadkin <opensource@trysound.io>
2024-12-28 15:40:52 +0700

    refactor: rewrite radix label template with jsx
    - added radix templates to components panel
    - support interpolations in css tagged template
    - added theme with static values

---
+++ packages/sdk-components-react-radix/src/shared/theme.ts
@@ -0,0 +1,60 @@
+export const fontSize = {
+  xs: "0.75rem",
+  sm: "0.875rem",
+  base: "1rem",
+  lg: "1.125rem",
+  xl: "1.25rem",
+  "2xl": "1.5rem",
+  "3xl": "1.875rem",
+  "4xl": "2.25rem",
+  "5xl": "3rem",
+  "6xl": "3.75rem",
+  "7xl": "4.5rem",
+  "8xl": "6rem",
+  "9xl": "8rem",
+} as const;
+
+export const fontSizeLineHeight = {
+  xs: "1rem",
+  sm: "1.25rem",
+  base: "1.5rem",
+  lg: "1.75rem",
+  xl: "1.75rem",
+  "2xl": "2rem",
+  "3xl": "2.25rem",
+  "4xl": "2.5rem",
+  "5xl": "1",
+  "6xl": "1",
+  "7xl": "1",
+  "8xl": "1",
+  "9xl": "1",
+} as const;
+
+export const lineHeight = {
+  "3": "0.75rem",
+  "4": "1rem",
+  "5": "1.25rem",
+  "6": "1.5rem",
+  "7": "1.75rem",
+  "8": "2rem",
+  "9": "2.25rem",
+  "10": "2.5rem",
+  none: "1",
+  tight: "1.25",
+  snug: "1.375",
+  normal: "1.5",
+  relaxed: "1.625",
+  loose: "2",
+} as const;
+
+export const weights = {
+  thin: "100",
+  extralight: "200",
+  light: "300",
+  normal: "400",
+  medium: "500",
+  semibold: "600",
+  bold: "700",
+  extrabold: "800",
+  black: "900",
+} as const;
