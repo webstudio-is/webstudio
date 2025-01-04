d453b65c977fbd5303ef2538c5395ea2ff83cc77
istarkov <istarkov@gmail.com>
2025-01-04 14:29:53 +0000

    fix: Sitemap Index - Unexpected Server Error

--- packages/react-sdk/src/runtime.ts
+++ packages/react-sdk/src/runtime.ts
@@ -2,6 +2,13 @@ export * from "./context";
 export * from "./hook";
 export * from "./variable-state";
 
+/**
+ * React has issues rendering certain elements, such as errors when a <link> element has children.
+ * To render XML, we wrap it with an <svg> tag and add a suffix to avoid React's default behavior on these elements.
+ */
+export const xmlNodeTagSuffix =
+  "-ws-xml-node-fb77f896-8e96-40b9-b8f8-90a4e70d724a";
+
 export const getIndexWithinAncestorFromComponentProps = (
   props: Record<string, unknown>
 ) => {
