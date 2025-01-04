62e67e6c6f9ff48f2fbe6574398430354a2381e9
istarkov <istarkov@gmail.com>
2025-01-04 15:13:01 +0000

    fix: Jump links don't work in preview after first click

--- apps/builder/app/shared/nano-states/pages.ts
+++ apps/builder/app/shared/nano-states/pages.ts
@@ -3,6 +3,6 @@ import type { Page, Pages } from "@webstudio-is/sdk";
 
 export const $pages = atom<undefined | Pages>(undefined);
 
-export const $selectedPageHash = atom<string>("");
+export const $selectedPageHash = atom<{ hash: string }>({ hash: "" });
 
 export const $editingPageId = atom<undefined | Page["id"]>();
