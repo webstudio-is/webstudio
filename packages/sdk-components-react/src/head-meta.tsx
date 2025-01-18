d05ede555c02ff894a29658db95e77b54d986e5b
istarkov <istarkov@gmail.com>
2025-01-18 12:27:20 +0000

    Add order

--- packages/sdk-components-react/src/head-meta.tsx
+++ packages/sdk-components-react/src/head-meta.tsx
@@ -9,21 +9,34 @@ import { XmlNode } from "./xml-node";
 
 export const defaultTag = "meta";
 
+const PROPS_ORDER = ["property", "name", "content"] as const;
+
 export const HeadMeta = forwardRef<
   ElementRef<"div">,
   ComponentProps<typeof defaultTag>
 >(({ ...props }, ref) => {
   const { renderer } = useContext(ReactSdkContext);
 
-  const cleanProps = Object.fromEntries(
-    Object.entries(props).filter(([_, value]) => value)
-  );
+  const propsSet = new Set([...PROPS_ORDER, ...Object.keys(props)]) as Set<
+    keyof typeof props
+  >;
 
-  if (renderer === undefined) {
-    return <meta {...cleanProps} />;
+  const cleanOrderedProps: Record<string, unknown> = {};
+
+  for (const prop of propsSet) {
+    // Boolean check is not a mistake; it excludes empty values.
+    // Empty properties must be excluded because there is no UI to reset them to undefined.
+    // Additionally, <meta property="" name="someName" content="someContent" /> is invalid.
+    if (prop in props && Boolean(props[prop])) {
+      cleanOrderedProps[prop] = props[prop];
+    }
   }
 
-  return <XmlNode tag={defaultTag} {...cleanProps} ref={ref} />;
+  if (renderer === undefined) {
+    return <meta {...cleanOrderedProps} />;
+  }
+
+  return <XmlNode tag={defaultTag} {...cleanOrderedProps} ref={ref} />;
 });
 
 HeadMeta.displayName = "HeadMeta";
