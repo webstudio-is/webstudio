56ac613a20f5147103038d6a22a6d1130404a4e7
istarkov <istarkov@gmail.com>
2025-01-18 16:47:16 +0000

    Improve visual

--- packages/sdk-components-react/src/head-link.ws.ts
+++ packages/sdk-components-react/src/head-link.ws.ts
@@ -19,5 +19,5 @@ export const meta: WsComponentMeta = {
 
 export const propsMeta: WsComponentPropsMeta = {
   props,
-  initialProps: ["rel", "href", "type", "hrefLang", "as"],
+  initialProps: ["rel", "hrefLang", "href", "type", "as"],
 };
