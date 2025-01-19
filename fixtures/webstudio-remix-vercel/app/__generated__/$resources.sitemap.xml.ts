b7c5eb9c00e1e1f8ea202636855e7417b5e1f716
istarkov <istarkov@gmail.com>
2025-01-19 14:03:36 +0000

    Update fixture

--- fixtures/webstudio-remix-vercel/app/__generated__/$resources.sitemap.xml.ts
+++ fixtures/webstudio-remix-vercel/app/__generated__/$resources.sitemap.xml.ts
@@ -1,26 +1,26 @@
 export const sitemap = [
   {
     path: "/",
-    lastModified: "2025-01-18",
+    lastModified: "2025-01-19",
   },
   {
     path: "/_route_with_symbols_",
-    lastModified: "2025-01-18",
+    lastModified: "2025-01-19",
   },
   {
     path: "/form",
-    lastModified: "2025-01-18",
+    lastModified: "2025-01-19",
   },
   {
     path: "/heading-with-id",
-    lastModified: "2025-01-18",
+    lastModified: "2025-01-19",
   },
   {
     path: "/resources",
-    lastModified: "2025-01-18",
+    lastModified: "2025-01-19",
   },
   {
     path: "/nested/nested-page",
-    lastModified: "2025-01-18",
+    lastModified: "2025-01-19",
   },
 ];
