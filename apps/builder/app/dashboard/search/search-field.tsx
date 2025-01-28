67a4d8f6389c6c9b75f0747975dfb00154ebe2e8
Oleg Isonen <oleg008@gmail.com>
2025-01-28 11:43:00 +0000

    experimentally delete viewTransition

--- apps/builder/app/dashboard/search/search-field.tsx
+++ apps/builder/app/dashboard/search/search-field.tsx
@@ -33,7 +33,6 @@ export const Search = () => {
             // Remember the last path to return to on abort
             {
               state: { previousPathname: location.pathname },
-              viewTransition: true,
             }
           );
           return;
