e8de913a28dc86ad9e29904cb1e396e64ba03e85
Bogdan Chadkin <opensource@trysound.io>
2025-02-04 11:03:36 +0400

    refactor: generalize features fixture (#4820)
    We have a fixture which is used for testing generated features.
    Historically we use it as vercel template fixture.
    
    Here migrated it to react-router, removed vercel stuff from it and
    renamed it to webstudio-features.

---
+++ fixtures/webstudio-features/app/constants.mjs
@@ -0,0 +1,12 @@
+/**
+ * We use mjs extension as constants in this file is shared with the build script
+ * and we use `node --eval` to extract the constants.
+ */
+export const assetBaseUrl = "/assets/";
+
+/**
+ * @type {import("@webstudio-is/image").ImageLoader}
+ */
+export const imageLoader = ({ src }) => {
+  return src;
+};
