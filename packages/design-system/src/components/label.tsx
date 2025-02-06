de74f0f67e4ca35fe27caea6c7d80060194302ca
Oleg Isonen <oleg008@gmail.com>
2025-02-06 18:22:02 +0000

    faster transtion

--- packages/design-system/src/components/label.tsx
+++ packages/design-system/src/components/label.tsx
@@ -34,7 +34,7 @@ const StyledLabel = styled(RadixLabel, {
   px: theme.spacing[2],
   border: "1px solid transparent",
   borderRadius: theme.borderRadius[3],
-  transition: "200ms color, 200ms background-color",
+  transition: "150ms color, 150ms background-color",
   color: theme.colors.foregroundMain,
 
   // https://github.com/webstudio-is/webstudio/issues/1271#issuecomment-1478436340
