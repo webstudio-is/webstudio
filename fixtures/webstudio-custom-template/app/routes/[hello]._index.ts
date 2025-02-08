0eaf5c3c7a5305d5f48f5138655dd8877f6d69bc
Bogdan Chadkin <opensource@trysound.io>
2025-02-08 07:50:40 +0400

    build: drop webstudio-custom-template fixture (#4834)
    This fixture is only useful to check whether custom CLI templates are
    working on not though checked anyway with new react-router templates
    when apply monorepo tweaks.

--- fixtures/webstudio-custom-template/app/routes/[hello]._index.ts
+++
@@ -1,6 +0,0 @@
-import { redirect } from "@remix-run/server-runtime";
-import { url, status } from "../__generated__/[hello]._index";
-
-export const loader = () => {
-  return redirect(url, status);
-};
