5fabe66c7fd30a530bcc32d82fd1957d629e6a33
Bogdan Chadkin <opensource@trysound.io>
2025-02-13 17:02:48 +0800

    feat: add new vercel template based on react-router (#4825)
    Had to add huge @vercel/react-router but it works now.

---
+++ packages/cli/templates/react-router-vercel/app/constants.mjs
@@ -0,0 +1,25 @@
+/**
+ * We use mjs extension as constants in this file is shared with the build script
+ * and we use `node --eval` to extract the constants.
+ */
+export const assetBaseUrl = "/assets/";
+
+/**
+ * @type {import("@webstudio-is/image").ImageLoader}
+ */
+export const imageLoader = (props) => {
+  if (import.meta.env.DEV) {
+    return props.src;
+  }
+
+  if (props.format === "raw") {
+    return props.src;
+  }
+
+  // https://vercel.com/blog/build-your-own-web-framework#automatic-image-optimization
+  const searchParams = new URLSearchParams();
+  searchParams.set("url", props.src);
+  searchParams.set("w", props.width.toString());
+  searchParams.set("q", props.quality.toString());
+  return `/_vercel/image?${searchParams}`;
+};
--- packages/sdk/src/schema/deployment.ts
+++ packages/sdk/src/schema/deployment.ts
@@ -4,6 +4,7 @@ export const Templates = z.enum([
   "vanilla",
   "docker",
   "vercel",
+  "vercel-legacy",
   "netlify",
   "ssg",
   "ssg-netlify",
