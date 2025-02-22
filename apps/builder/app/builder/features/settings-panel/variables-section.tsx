8706da7f4cf0c1ade4f18fdabc1daadfe9578730
Bogdan Chadkin <opensource@trysound.io>
2025-02-23 01:41:15 +0700

    Remove confusing delete label

--- apps/builder/app/builder/features/settings-panel/variables-section.tsx
+++ apps/builder/app/builder/features/settings-panel/variables-section.tsx
@@ -281,7 +281,7 @@ const VariablesItem = ({
                         });
                       }}
                     >
-                      Delete legacy system variable
+                      Delete
                     </DropdownMenuItem>
                   )}
               </DropdownMenuContent>
