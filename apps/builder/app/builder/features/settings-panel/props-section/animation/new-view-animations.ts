0048d9b73ccf3a306e41a54c37680cc4772d5ba8
istarkov <istarkov@gmail.com>
2025-02-26 11:04:48 +0000

    experimental: Add  predefined animations

--- apps/builder/app/builder/features/settings-panel/props-section/animation/new-view-animations.ts
+++ apps/builder/app/builder/features/settings-panel/props-section/animation/new-view-animations.ts
@@ -1,7 +1,7 @@
 import { parseCssValue } from "@webstudio-is/css-data";
 import type { ViewAnimation } from "@webstudio-is/sdk";
 
-export const newViewAnimation: ViewAnimation = {
+const newViewAnimation: ViewAnimation = {
   name: "New Animation",
   description: "Create a new animation.",
 
@@ -20,7 +20,7 @@ export const newViewAnimation: ViewAnimation = {
 };
 
 // @todo: visit https://github.com/argyleink/open-props/blob/main/src/props.animations.css
-export const newFadeInViewAnimation: ViewAnimation = {
+const newFadeInViewAnimation: ViewAnimation = {
   name: "Fade In",
   description: "Fade in the element as it scrolls into view.",
 
@@ -40,7 +40,7 @@ export const newFadeInViewAnimation: ViewAnimation = {
   ],
 };
 
-export const newFadeOutViewAnimation: ViewAnimation = {
+const newFadeOutViewAnimation: ViewAnimation = {
   name: "Fade Out",
   description: "Fade out the element as it scrolls out of view.",
 
@@ -59,3 +59,106 @@ export const newFadeOutViewAnimation: ViewAnimation = {
     },
   ],
 };
+
+const newFlyInViewAnimation: ViewAnimation = {
+  name: "Fly In",
+  description: "Fly in the element as it scrolls into view.",
+
+  timing: {
+    rangeStart: ["entry", { type: "unit", value: 0, unit: "%" }],
+    rangeEnd: ["entry", { type: "unit", value: 100, unit: "%" }],
+    fill: "backwards",
+    easing: "linear",
+  },
+  keyframes: [
+    {
+      offset: 0,
+      styles: {
+        translate: parseCssValue("translate", "0 100px"),
+      },
+    },
+  ],
+};
+
+const newFlyOutViewAnimation: ViewAnimation = {
+  name: "Fly Out",
+  description: "Fly out the element as it scrolls out of view.",
+
+  timing: {
+    rangeStart: ["exit", { type: "unit", value: 0, unit: "%" }],
+    rangeEnd: ["exit", { type: "unit", value: 100, unit: "%" }],
+    fill: "forwards",
+    easing: "linear",
+  },
+  keyframes: [
+    {
+      offset: 1,
+      styles: {
+        translate: parseCssValue("translate", "0 -100px"),
+      },
+    },
+  ],
+};
+
+const newWipeInViewAnimation: ViewAnimation = {
+  name: "Wipe In",
+  description: "Wipe in the element as it scrolls into view.",
+
+  timing: {
+    rangeStart: ["contain", { type: "unit", value: 0, unit: "%" }],
+    rangeEnd: ["contain", { type: "unit", value: 50, unit: "%" }],
+    fill: "backwards",
+    easing: "linear",
+  },
+  keyframes: [
+    {
+      offset: 0,
+      styles: {
+        "clip-path": parseCssValue("translate", "inset(0 100% 0 0)"),
+      },
+    },
+    {
+      offset: 1,
+      styles: {
+        "clip-path": parseCssValue("translate", "inset(0 0 0 0)"),
+      },
+    },
+  ],
+};
+
+const newWipeOutViewAnimation: ViewAnimation = {
+  name: "Wipe Out",
+  description: "Wipe out the element as it scrolls out of view.",
+
+  timing: {
+    rangeStart: ["contain", { type: "unit", value: 50, unit: "%" }],
+    rangeEnd: ["contain", { type: "unit", value: 100, unit: "%" }],
+    fill: "forwards",
+    easing: "linear",
+  },
+  keyframes: [
+    {
+      offset: 0,
+      styles: {
+        "clip-path": parseCssValue("translate", "inset(0 0 0 0)"),
+      },
+    },
+
+    {
+      offset: 1,
+      styles: {
+        "clip-path": parseCssValue("translate", "inset(0 0 0 100%)"),
+      },
+    },
+  ],
+};
+
+export const newViewAnimations = [
+  newViewAnimation,
+  newFadeInViewAnimation,
+  newFlyInViewAnimation,
+  newWipeInViewAnimation,
+  newFadeOutViewAnimation,
+  newFlyOutViewAnimation,
+  newWipeOutViewAnimation,
+];
