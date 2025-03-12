fcb4e25a9eeac104703c26b61a5c7687ceb06cb3
Bogdan Chadkin <opensource@trysound.io>
2025-03-13 00:20:46 +0700

    feat: show unresolved inherited values in style panel (#4990)
    Per user request we will not show computed value when inherited from
    ancestor.
    Chrome devtools seem to show unresolved values in "inherited" section.
    
    <img width="252" alt="image"
    src="https://github.com/user-attachments/assets/b7f22ec5-e2ea-47aa-9bad-80622d0aa5f9"
    />

--- apps/builder/app/shared/style-object-model.ts
+++ apps/builder/app/shared/style-object-model.ts
@@ -342,6 +342,7 @@ export const getComputedStyleDecl = ({
       state,
       property,
     });
+    const inheritedCascadedValue = cascadedValue;
     cascadedValue = cascaded?.value;
     source = cascaded?.source ?? { name: "default" };
 
@@ -372,6 +373,7 @@ export const getComputedStyleDecl = ({
     // defaulting https://drafts.csswg.org/css-cascade-5/#defaulting
     else if (inherited) {
       specifiedValue = inheritedValue;
+      cascadedValue = inheritedCascadedValue;
       source = inheritedSource;
     } else {
       specifiedValue = initialValue;
@@ -436,8 +438,8 @@ export const getComputedStyleDecl = ({
     usedValue = currentColor.usedValue;
   }
 
-  // fallback to inherited value
-  cascadedValue ??= computedValue;
+  // fallback to initial value
+  cascadedValue ??= initialValue;
 
   return { property, source, cascadedValue, computedValue, usedValue };
 };
