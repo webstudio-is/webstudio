6198dea70f9c54c9f6dd10fb1026cf91ea267666
Oleg Isonen <oleg008@gmail.com>
2025-03-13 00:58:10 +0000

    simplify story

--- apps/builder/app/builder/shared/css-editor/css-editor.stories.tsx
+++ apps/builder/app/builder/shared/css-editor/css-editor.stories.tsx
@@ -1,51 +1,15 @@
-import { getStyleDeclKey, type StyleDecl } from "@webstudio-is/sdk";
-import { registerContainers } from "~/shared/sync";
 import { CssEditor as CssEditorComponent } from "./css-editor";
-import {
-  $breakpoints,
-  $instances,
-  $selectedBreakpointId,
-  $styles,
-  $styleSourceSelections,
-} from "~/shared/nano-states";
-import { setProperty } from "../../features/style-panel/shared/use-style-data";
-import { $awareness } from "~/shared/awareness";
 import type { ComputedStyleDecl } from "~/shared/style-object-model";
-
-const backgroundImage: StyleDecl = {
-  breakpointId: "base",
-  styleSourceId: "local",
-  property: "backgroundImage",
-  value: {
-    type: "layers",
-    value: [{ type: "keyword", value: "none" }],
-  },
-};
-
-registerContainers();
-$breakpoints.set(new Map([["base", { id: "base", label: "" }]]));
-$selectedBreakpointId.set("base");
-$styles.set(new Map([[getStyleDeclKey(backgroundImage), backgroundImage]]));
-$styleSourceSelections.set(
-  new Map([["box", { instanceId: "box", values: ["local"] }]])
-);
-$instances.set(
-  new Map([
-    ["box", { type: "instance", id: "box", component: "Box", children: [] }],
-  ])
-);
-$awareness.set({
-  pageId: "",
-  instanceSelector: ["box"],
-});
-
-setProperty("accentColor")({ type: "keyword", value: "red" });
-setProperty("alignContent")({ type: "keyword", value: "normal" });
-setProperty("opacity")({ type: "unit", unit: "number", value: 11.2 });
 
 export const CssEditor = () => {
   const declarations = [
-    ["background-image", backgroundImage.value],
+    [
+      "background-image",
+      {
+        type: "layers",
+        value: [{ type: "keyword", value: "none" }],
+      },
+    ],
     ["accent-color", { type: "keyword", value: "red" }],
     ["align-content", { type: "keyword", value: "normal" }],
     ["opacity", { type: "unit", unit: "number", value: 11.2 }],
