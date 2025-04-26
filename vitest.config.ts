6f6dc942889800615bc2b760fab908f96e14b7a9
Bogdan Chadkin <opensource@trysound.io>
2025-04-26 14:04:42 +0700

    Support client code in tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,14 @@
-import { defaultServerConditions, defineConfig } from "vite";
+import {
+  defaultClientConditions,
+  defaultServerConditions,
+  defineConfig,
+} from "vite";
 
 export default defineConfig({
   // resolve webstudio condition in tests
+  resolve: {
+    conditions: ["webstudio", ...defaultClientConditions],
+  },
   ssr: {
     resolve: {
       conditions: ["webstudio", ...defaultServerConditions],
