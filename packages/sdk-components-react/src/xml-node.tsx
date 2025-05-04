0f4a1cb0a8c57f26fd07ee3998357d10ce0f9905
Jaya Krishna <namburu1995@gmail.com>
2025-05-04 11:50:07 +0530

    fix: set xml node background to white

--- packages/sdk-components-react/src/xml-node.tsx
+++ packages/sdk-components-react/src/xml-node.tsx
@@ -71,7 +71,7 @@ export const XmlNode = forwardRef<ElementRef<"div">, Props>(
     };
 
     return (
-      <div {...props}>
+      <div {...props} style={{ backgroundColor: "rgba(255,255,255,1)" }}>
         <span>
           <span style={{ color: "#800000" }}>&lt;{elementName}</span>
           {attributeEntries.length > 0 && renderAttributes(attributeEntries)}
