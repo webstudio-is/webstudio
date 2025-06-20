da04adba85c406221171a1e06783da8274e92c3a
Bogdan Chadkin <opensource@trysound.io>
2025-06-20 21:00:59 +0300

    Shorten fixture image to fix cloning on windows
    Fixes https://github.com/webstudio-is/webstudio/issues/5267

--- fixtures/webstudio-features/app/__generated__/[radix]._index.server.tsx
+++ fixtures/webstudio-features/app/__generated__/[radix]._index.server.tsx
@@ -22,8 +22,7 @@ export const getPageMeta = ({
       "Delve deep into the radix roots of feline behaviors. At KittyNoTouchy, we dissect the core essence, or 'radix', of what makes cats the enigmatic creatures they are. Join us as we explore the radix of their instincts, habits, and quirks.",
     excludePageFromSearch: true,
     language: undefined,
-    socialImageAssetName:
-      "DALL_E_2023-10-30_12.39.46_-_Photo_logo_with_a_bold_cat_silhouette_centered_on_a_contrasting_background_designed_for_clarity_at_small_32x32_favicon_resolution_00h6cEA8u2pJRvVJv7hRe.png",
+    socialImageAssetName: undefined,
     socialImageUrl: undefined,
     status: undefined,
     redirect: undefined,
