9a1486b306f752e5791e1b5f523c281e5f3cd088
Bogdan Chadkin <opensource@trysound.io>
2025-07-06 14:03:10 +0300

    fix: avoid error when domain does not exist in iana (#5315)
    We use iana registry to find if cloudflare nameserver is used for domain
    and we can avoid using www.

--- packages/domain/src/rdap.ts
+++ packages/domain/src/rdap.ts
@@ -88,18 +88,17 @@ export const isDomainUsingCloudflareNameservers = async (domain: string) => {
 
   const rdapServer = await findRdapServer(topLevelDomain);
   if (!rdapServer) {
-    throw new Error(
+    console.error(
       "RDAP Server for the given top level domain could not be found."
     );
+    return undefined;
   }
 
-  if (rdapServer) {
-    const data = await fetchRdap(rdapServer, domain);
-    if (data) {
-      // detect by nameservers rather than registrar url
-      // sometimes stored as *.NS.CLOUDFLARE.COM
-      return data.toLowerCase().includes(".ns.cloudflare.com");
-    }
+  const data = await fetchRdap(rdapServer, domain);
+  if (data) {
+    // detect by nameservers rather than registrar url
+    // sometimes stored as *.NS.CLOUDFLARE.COM
+    return data.toLowerCase().includes(".ns.cloudflare.com");
   }
   return false;
 };
