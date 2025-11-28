d23beb4fec495d79e5771b7092e63fa6df965209
Bogdan Chadkin <opensource@trysound.io>
2025-11-28 21:03:54 +0300

    Fix thumbnail when autocomplete colors

--- apps/builder/app/builder/features/style-panel/shared/css-value-input/css-value-input.tsx
+++ apps/builder/app/builder/features/style-panel/shared/css-value-input/css-value-input.tsx
@@ -932,8 +932,8 @@ export const CssValueInput = ({
                             {toValue(item.fallback)}
                           </Text>
                         )}
-                        {/* @todo */}
-                        {item.fallback?.type === "rgb" && (
+                        {(item.fallback?.type === "rgb" ||
+                          item.fallback?.type === "color") && (
                           <ColorThumb color={toValue(item.fallback)} />
                         )}
                       </Flex>
