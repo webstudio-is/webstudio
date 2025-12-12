d18e5f7fab28a1ca66921c94041b3c447072e7f7
Oleg Isonen <oleg008@gmail.com>
2025-12-12 10:06:00 +0000

    Update vitest.config.ts

--- packages/design-system/vitest.config.ts
+++ packages/design-system/vitest.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  resolve: {
+    conditions: ["webstudio", "browser", "development|production"],
+  },
+  // resolve webstudio condition in tests
+  ssr: {
+    resolve: {
+      conditions: ["webstudio", "node", "development|production"],
+    },
+  },
   test: {
     environment: "jsdom",
   },
