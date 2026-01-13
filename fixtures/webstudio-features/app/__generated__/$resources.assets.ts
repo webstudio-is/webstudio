de24e40426445879679a629664636a84bd4e4161
Oleg Isonen <oleg008@gmail.com>
2026-01-13 12:11:49 +0000

    fixtures

---
+++ fixtures/webstudio-features/app/__generated__/$resources.assets.ts
@@ -0,0 +1,17 @@
+export const assets = {
+  "7cf5892080fa66b5e6175ffd2d27c304ee6b09ce1f21847a95000225ad1afa59": {
+    url: "https://placeholder.local/cgi/image/cat_silhouette_BDpTbUFSpVbfUWQZNxbBG.png?format=raw",
+    width: 790,
+    height: 786,
+  },
+  "9a8bc926-7804-4d3f-af81-69196b1d2ed8": {
+    url: "https://placeholder.local/cgi/image/small-avif-kitty_FnabJsioMWpBtXZSGf4DR.webp?format=raw",
+    width: 100,
+    height: 100,
+  },
+  "cd939c56-bcdd-4e64-bd9c-567a9bccd3da": {
+    url: "https://placeholder.local/cgi/image/_937084ed-a798-49fe-8664-df93a2af605e_uiBk3o6UWdqolyakMvQJ9.jpeg?format=raw",
+    width: 1024,
+    height: 1024,
+  },
+};
