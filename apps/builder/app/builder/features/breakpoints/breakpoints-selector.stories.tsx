11c9ca59d7a369565ccf57d34b409d128e86485f
Oleg Isonen <oleg008@gmail.com>
2026-03-15 20:48:19 +0000

    fix: render stories without requiring clicks, use sidebar width

--- apps/builder/app/builder/features/breakpoints/breakpoints-selector.stories.tsx
+++ apps/builder/app/builder/features/breakpoints/breakpoints-selector.stories.tsx
@@ -1,15 +1,28 @@
 import { StorySection } from "@webstudio-is/design-system";
 import { BreakpointsSelector as BreakpointsSelectorComponent } from "./breakpoints-selector";
+import { $breakpoints } from "~/shared/nano-states";
+import { $selectedBreakpointId } from "~/shared/nano-states/breakpoints";
+
+const breakpointsMap = new Map([
+  ["1", { id: "1", label: "Mobile", maxWidth: 479 }],
+  ["2", { id: "2", label: "Tablet", maxWidth: 991 }],
+  ["3", { id: "3", label: "" }],
+  ["4", { id: "4", label: "Desktop", minWidth: 1280 }],
+  ["5", { id: "5", label: "Wide", minWidth: 1440 }],
+]);
+
+$breakpoints.set(breakpointsMap);
+$selectedBreakpointId.set("3");
 
 export const BreakpointsSelector = () => {
   return (
-    <StorySection title="Breakpoints Selector">
+    <StorySection title="Breakpoints selector">
       <BreakpointsSelectorComponent />
     </StorySection>
   );
 };
 
 export default {
-  title: "Breakpoints Selector",
+  title: "Breakpoints selector",
   component: BreakpointsSelector,
 };
