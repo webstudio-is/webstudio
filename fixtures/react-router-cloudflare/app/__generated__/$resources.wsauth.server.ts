12acdeaa0a9330284486491954b4a8ed70cadb24
Oleg Isonen <oleg008@gmail.com>
2026-05-27 16:36:57 +0000

    Update react router cloudflare auth fixture

--- fixtures/react-router-cloudflare/app/__generated__/$resources.wsauth.server.ts
+++ fixtures/react-router-cloudflare/app/__generated__/$resources.wsauth.server.ts
@@ -1,13 +1,3 @@
 import type { WsAuthRoute } from "@webstudio-is/wsauth";
 
-export const authRoutes: WsAuthRoute[] = [
-  {
-    route: "/",
-    auth: {
-      method: "basic",
-      login: "admin",
-      password: "secret",
-      credentials: "admin:secret",
-    },
-  },
-];
+export const authRoutes: WsAuthRoute[] = [];
