cd382a7920c84cd9cb367e67ad1b8b346f0afde8
Oleg Isonen <oleg008@gmail.com>
2026-06-03 22:19:01 +0000

    Initialize page templates on pages migration

--- packages/project-migrations/src/pages.ts
+++ packages/project-migrations/src/pages.ts
@@ -116,7 +116,7 @@ export const migratePages = (pages: unknown): Pages => {
   ) {
     const currentPages = pages as Pages;
     const result = PagesSchema.safeParse(currentPages);
-    if (result.success) {
+    if (result.success && currentPages.pageTemplates !== undefined) {
       return currentPages;
     }
     return {
@@ -141,7 +141,7 @@ export const migratePages = (pages: unknown): Pages => {
       pages: nextPages,
       pageTemplates:
         pages.pageTemplates === undefined
-          ? undefined
+          ? new Map()
           : toMap<PageTemplate>(pages.pageTemplates),
       folders: removeOrphanFolderChildren(nextPages, nextFolders),
     };
