4d3cca74e2a6548a8666efa55830c5d8d53b4fad
Oleg Isonen <oleg008@gmail.com>
2026-06-09 21:00:20 +0100

    fix: Enable greedy redirects (#5801)
    Closes #5625
    
    ## Description
    
    Fixes greedy redirects like:
    
    ```txt
    /privat/* -> /*

--- packages/cli/templates/defaults/app/route-templates/redirect.tsx
+++ packages/cli/templates/defaults/app/route-templates/redirect.tsx
@@ -1,6 +1,7 @@
-import { redirect } from "@remix-run/server-runtime";
+import { type LoaderFunctionArgs, redirect } from "@remix-run/server-runtime";
+import { generateRedirectUrl } from "../redirect-url";
 import { url, status } from "__REDIRECT__";
 
-export const loader = () => {
-  return redirect(url, status);
+export const loader = (arg: LoaderFunctionArgs) => {
+  return redirect(generateRedirectUrl(url, arg.params), status);
 };
