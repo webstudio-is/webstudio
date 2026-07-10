da13e301c92cbd1a07099ebe0164e8f85b5c6b2f
Oleg Isonen <oleg008@gmail.com>
2026-07-10 11:55:20 +0100

    chore: Post merge fixes (#5860)

--- packages/cli/vite.config.ts
+++ packages/cli/vite.config.ts
@@ -3,13 +3,21 @@ import {
   defaultServerConditions,
   defineConfig,
 } from "vite";
+import { createRequire } from "node:module";
 import pkg from "./package.json";
+
+const require = createRequire(import.meta.url);
+const nodeDecodeNamedCharacterReference = require.resolve(
+  "decode-named-character-reference"
+);
 
 const externalDependencies = new Set(
   Object.keys(pkg.dependencies).filter((name) => {
     return name.startsWith("@webstudio-is/") === false;
   })
 );
+
+const bundledDependencies = new Set(["acorn"]);
 
 const getPackageName = (id: string) => {
   if (id.startsWith("@")) {
@@ -18,11 +26,20 @@ const getPackageName = (id: string) => {
   return id.split("/")[0];
 };
 
-const isExternal = (id: string) =>
-  id.startsWith("node:") || externalDependencies.has(getPackageName(id));
+const isExternal = (id: string) => {
+  const packageName = getPackageName(id);
+  return (
+    id.startsWith("node:") ||
+    (externalDependencies.has(packageName) &&
+      bundledDependencies.has(packageName) === false)
+  );
+};
 
 export default defineConfig({
   resolve: {
+    alias: {
+      "decode-named-character-reference": nodeDecodeNamedCharacterReference,
+    },
     conditions: ["webstudio", ...defaultClientConditions],
   },
   ssr: {
