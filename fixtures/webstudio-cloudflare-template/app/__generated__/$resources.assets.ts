702f871a0e3ce5ec7557bf77fa99028b69291b9a
Oleg Isonen <oleg008@gmail.com>
2026-07-23 16:50:15 +0100

    fix: refresh generated fixture asset URLs

--- fixtures/webstudio-cloudflare-template/app/__generated__/$resources.assets.ts
+++ fixtures/webstudio-cloudflare-template/app/__generated__/$resources.assets.ts
@@ -1,11 +1,11 @@
 export const assets = {
   "1d8bf4398f643f5333d415091507d778aaed62f28883642636cbed0be156a0ee": {
-    url: "/cgi/image/iconly_svg_converted-converted_zMaMiAAutUl8XrITgz7d1.svg?format=raw",
+    url: "/assets/iconly_svg_converted-converted_zMaMiAAutUl8XrITgz7d1.svg",
     width: 14,
     height: 16,
   },
   d0974db9300c1a3b0fb8b291dd9fabd45ad136478908394280af2f7087e3aecd: {
-    url: "/cgi/image/147-1478573_cat-icon-png-black-cat-png-icon.png_ZJ6-qJjk1RlFzuYwyCXdp.jpeg?format=raw",
+    url: "/assets/147-1478573_cat-icon-png-black-cat-png-icon.png_ZJ6-qJjk1RlFzuYwyCXdp.jpeg",
     width: 820,
     height: 985,
   },
--- fixtures/webstudio-features/app/__generated__/$resources.assets.ts
+++ fixtures/webstudio-features/app/__generated__/$resources.assets.ts
@@ -1,25 +1,25 @@
 export const assets = {
   "2b151fc7b4b0324e6ab78c40f72c7f59273f81fb1be3d08b3f9976428601b95e": {
-    url: "/cgi/asset/e-mail-39993_vrxyjxQv3j67Krs62Vz7Y.mp3?format=raw",
+    url: "/assets/e-mail-39993_vrxyjxQv3j67Krs62Vz7Y.mp3",
   },
   "4afe692f78ec0530e355a551a3860302c4478037db7b25a3bdae82c32c78634d": {
-    url: "/cgi/asset/webm-example_2r_6VmRBjhAy3ldaqz0gk.webm?format=raw",
+    url: "/assets/webm-example_2r_6VmRBjhAy3ldaqz0gk.webm",
   },
   "7cf5892080fa66b5e6175ffd2d27c304ee6b09ce1f21847a95000225ad1afa59": {
-    url: "/cgi/image/cat_silhouette_BDpTbUFSpVbfUWQZNxbBG.png?format=raw",
+    url: "/assets/cat_silhouette_BDpTbUFSpVbfUWQZNxbBG.png",
     width: 790,
     height: 786,
   },
   "9a8bc926-7804-4d3f-af81-69196b1d2ed8": {
-    url: "/cgi/image/small-avif-kitty_FnabJsioMWpBtXZSGf4DR.webp?format=raw",
+    url: "/assets/small-avif-kitty_FnabJsioMWpBtXZSGf4DR.webp",
     width: 100,
     height: 100,
   },
   cbf6b1b052e52b256cef54a032a546bf43bf3f5441be4d1c5eeaabce26903d78: {
-    url: "/cgi/asset/video_QamtUWsD-ShifhzZLoNIv_ald_1xtyEb3uHhFb7nCQa.mp4?format=raw",
+    url: "/assets/video_QamtUWsD-ShifhzZLoNIv_ald_1xtyEb3uHhFb7nCQa.mp4",
   },
   "cd939c56-bcdd-4e64-bd9c-567a9bccd3da": {
-    url: "/cgi/image/_937084ed-a798-49fe-8664-df93a2af605e_uiBk3o6UWdqolyakMvQJ9.jpeg?format=raw",
+    url: "/assets/_937084ed-a798-49fe-8664-df93a2af605e_uiBk3o6UWdqolyakMvQJ9.jpeg",
     width: 1024,
     height: 1024,
   },
