ee9d90c92e60983ec0cfc8cb8bbd75bd1ec4f245
Oleg Isonen <oleg008@gmail.com>
2026-07-24 12:05:23 +0100

    fix: regenerate query-free asset manifests

--- fixtures/ssg-netlify-by-project-id/app/__generated__/$resources.asset-query-manifest.ts
+++ fixtures/ssg-netlify-by-project-id/app/__generated__/$resources.asset-query-manifest.ts
@@ -1,2 +1,2 @@
 export const assetQueryDeploymentId = "8f0fdff2-e76b-4f86-9203-ea7df5f1143c";
-export const assetQueryManifest = [];
+export const assetQueryManifest = undefined;
--- fixtures/webstudio-features/app/__generated__/$resources.asset-query-manifest.ts
+++ fixtures/webstudio-features/app/__generated__/$resources.asset-query-manifest.ts
@@ -1,2 +1,2 @@
 export const assetQueryDeploymentId = "1b66ee06-8ea5-4420-9a0e-e0d3a67aca32";
-export const assetQueryManifest = [];
+export const assetQueryManifest = undefined;
