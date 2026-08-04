edecff993ef12bd41be552b7ba74d8e279ac3d78
Oleg Isonen <oleg008@gmail.com>
2026-08-04 17:03:29 +0100

    refactor: keep canvas asset loading local

--- packages/sdk-components-react/src/code-text/canvas.tsx
+++ packages/sdk-components-react/src/code-text/canvas.tsx
@@ -2,16 +2,14 @@ import { bundledLanguages } from "shiki/langs";
 import { bundledThemes } from "shiki/themes";
 import { createCodeText } from "./index";
 
+const load = async <Registration,>(
+  loader: (() => Promise<{ default: Registration }>) | undefined
+) => (await loader?.())?.default;
+
 export const CanvasCodeText = createCodeText({
   loaders: {
-    language: async (language) => {
-      const loader =
-        bundledLanguages[language as keyof typeof bundledLanguages];
-      return (await loader?.())?.default;
-    },
-    theme: async (theme) => {
-      const loader = bundledThemes[theme as keyof typeof bundledThemes];
-      return (await loader?.())?.default;
-    },
+    language: (language) =>
+      load(bundledLanguages[language as keyof typeof bundledLanguages]),
+    theme: (theme) => load(bundledThemes[theme as keyof typeof bundledThemes]),
   },
 });
