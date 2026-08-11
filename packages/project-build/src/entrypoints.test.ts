04d6a9ac2947ff283651a8398b4d02c3c8e3f205
Oleg Isonen <oleg008@gmail.com>
2026-08-11 16:14:10 +0100

    test: cover project preview entrypoint

--- packages/project-build/src/entrypoints.test.ts
+++ packages/project-build/src/entrypoints.test.ts
@@ -20,7 +20,7 @@ describe("package entrypoints", () => {
   test("does not expose Node-only surfaces through generic conditions", () => {
     for (const entrypoint of [
       "./mcp",
-      "./vision",
+      "./preview",
       "./transfer/server",
       "./server",
     ]) {
