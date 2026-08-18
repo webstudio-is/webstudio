70e706783067d6df902ff2d98e504e100cef29a2
Oleg Isonen <oleg008@gmail.com>
2026-08-18 23:59:24 +0100

    fix: preserve Webhook Form method in exports

--- packages/sdk-components-react/src/webhook-form.tsx
+++ packages/sdk-components-react/src/webhook-form.tsx
@@ -11,8 +11,8 @@ type Props = ComponentProps<"form"> & {
 };
 
 export const WebhookForm = forwardRef<ElementRef<"form">, Props>(
-  ({ children, state = "initial", ...props }, ref) => (
-    <form {...props} ref={ref}>
+  ({ children, method = "post", state = "initial", ...props }, ref) => (
+    <form {...props} method={method} ref={ref}>
       {children}
     </form>
   )
